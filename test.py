00464f02b7cdc94c752a2cbb8b68fbedfc67af10
Benhao Qu <s3773865@student.rmit.edu.au>
2020-10-26 17:32:16 +1100

    using unittest

---
+++ test.py
@@ -0,0 +1,18 @@
+import sys
+import unittest
+from importlib.machinery import SourceFileLoader
+
+# Question ID that wants to test, modify here as passing arguments
+QUESTION = 1480
+SOLUTION = SourceFileLoader("module.name", f"./{QUESTION}/solution.py").load_module().Solution()
+TESTCASE = SourceFileLoader("module.name", f"./{QUESTION}/testcase.py").load_module().Testcase()
+
+class Test(unittest.TestCase):
+    def test(self):
+        for testcase in TESTCASE.get_testcases():
+            i, o = testcase
+            self.assertEqual(o, SOLUTION.solve(test_input=i),msg=f"input = {i}")
+
+
+if __name__ == '__main__':
+    unittest.main()
