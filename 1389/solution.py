4e4638c3a81163e4f7efed5fb033d75b93b7a285
Benhao Qu <s3773865@student.rmit.edu.au>
2020-11-14 13:50:08 +1100

    1389 another solution that can save more space but might cause more time spending

--- 1389/solution.py
+++ 1389/solution.py
@@ -7,7 +7,11 @@ class Solution(solution.Solution):
         return self.createTargetArray(nums,index)
 
     def createTargetArray(self, nums, index):
-        result_list = []
+        # result_list = []
         for i in range(len(nums)):
-            result_list.insert(index[i],nums[i])
-        return result_list
+            if index[i] != i:
+                value = nums.pop(i)
+                nums.insert(index[i],value)
+            # result_list.insert(index[i],nums[i])
+        # return result_list
+        return nums
