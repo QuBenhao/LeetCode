d9aaffbdfda7c4a7ea2f693b32e419484f42c343
Benhao Qu <s3773865@student.rmit.edu.au>
2020-11-14 13:41:49 +1100

    1389

--- 1389/solution.py
+++ 1389/solution.py
@@ -9,6 +9,5 @@ class Solution(solution.Solution):
     def createTargetArray(self, nums, index):
         result_list = []
         for i in range(len(nums)):
-            nums
             result_list.insert(index[i],nums[i])
         return result_list
