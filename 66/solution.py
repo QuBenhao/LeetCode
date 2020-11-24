0f17689cb837e8ac966f32d9466d7443d05fa07c
Benhao Qu <s3773865@student.rmit.edu.au>
2020-11-24 18:46:02 +1100

    66 improved

--- 66/solution.py
+++ 66/solution.py
@@ -10,12 +10,10 @@ class Solution(solution.Solution):
         :type digits: List[int]
         :rtype: List[int]
         """
-        for i in range(len(digits)-1,-1,-1):
+        for i in range(len(digits) - 1, -1, -1):
             digits[i] += 1
-            if digits[i] > 9:
-                digits[i] = 0
-                if i == 0:
-                    list.insert(digits,0,1)
-                    return digits
-            else:
+            if digits[i] <= 9:
                 return digits
+            digits[i] = 0
+        list.insert(digits, 0, 1)
+        return digits
