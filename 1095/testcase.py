de9c84ba2b287c443093df57c66a38d2c145c0f8
Benhao Qu <s3773865@student.rmit.edu.au>
2020-12-08 19:09:55 +1100

    reformat 1095

--- 1095/testcase.py
+++ 1095/testcase.py
@@ -7,11 +7,10 @@ case = namedtuple("Testcase", ["Input", "Output"])
 class Testcase(testcase.Testcase):
     def __init__(self):
         self.testcases = []
-        self.testcases.append(case(Input=([1,2,3,4,5,3,1],3), Output=2))
-        self.testcases.append(case(Input=([0,1,2,4,2,1],3), Output=-1))
-        self.testcases.append(case(Input=([1,5,2],2), Output=2))
-        self.testcases.append(case(Input=([1,2,3,4,5,3,1],2), Output=1))
-
+        self.testcases.append(case(Input=([1, 2, 3, 4, 5, 3, 1], 3), Output=2))
+        self.testcases.append(case(Input=([0, 1, 2, 4, 2, 1], 3), Output=-1))
+        self.testcases.append(case(Input=([1, 5, 2], 2), Output=2))
+        self.testcases.append(case(Input=([1, 2, 3, 4, 5, 3, 1], 2), Output=1))
 
     def get_testcases(self):
         return self.testcases
