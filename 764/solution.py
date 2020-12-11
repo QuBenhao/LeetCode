b94b9cf730bd7fabe8c2c2a9f13c869c9ecc4caa
Benhao Qu <s3773865@student.rmit.edu.au>
2020-12-11 21:15:28 +1100

    764 update my own solution

--- 764/solution.py
+++ 764/solution.py
@@ -45,3 +45,34 @@ class Solution(solution.Solution):
                     res = grid[i][j]
 
         return res
+
+        # # Exceed time limit first solution
+        # if len(mines) == N * N:
+        #     return 0
+        #
+        # left = [[0 for i in range(N)] for i in range(N)]
+        # up = [[0 for i in range(N)] for i in range(N)]
+        # right = [[0 for i in range(N)] for i in range(N)]
+        # down = [[0 for i in range(N)] for i in range(N)]
+        # for i in range(N):
+        #     for j in range(N):
+        #         if j > 0:
+        #             if [i, j - 1] not in mines:
+        #                 left[i][j] = left[i][j - 1] + 1
+        #             if [i, N - j] not in mines:
+        #                 right[i][N - 1 - j] = right[i][N - j] + 1
+        #         if i > 0:
+        #             if [i - 1, j] not in mines:
+        #                 up[i][j] = up[i - 1][j] + 1
+        #             if [N - i, j] not in mines:
+        #                 down[N - 1 - i][j] = down[N - i][j] + 1
+        # largest = 0
+        # for i in range(N):
+        #     for j in range(N):
+        #         if [i, j] in mines:
+        #             continue
+        #         curr = min(left[i][j], right[i][j], up[i][j], down[i][j])
+        #         if curr > largest:
+        #             largest = curr
+        #
+        # return largest + 1
