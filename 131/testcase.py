74f8106474209be8bc76f22a516fc1639126049d
Benhao Qu <s3773865@student.rmit.edu.au>
2020-12-14 19:50:30 +1100

    131 reformat

--- 131/testcase.py
+++ 131/testcase.py
@@ -7,8 +7,8 @@ case = namedtuple("Testcase", ["Input", "Output"])
 class Testcase(testcase.Testcase):
     def __init__(self):
         self.testcases = []
-        self.testcases.append(case(Input="aab",Output=[["a","a","b"],["aa","b"]]))
-        self.testcases.append(case(Input="a",Output=[["a"]]))
+        self.testcases.append(case(Input="aab", Output=[["a", "a", "b"], ["aa", "b"]]))
+        self.testcases.append(case(Input="a", Output=[["a"]]))
 
     def get_testcases(self):
         return self.testcases
