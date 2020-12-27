467ce5fd45b0cf2297d2e9250a4bfc2c6d9f0962
Benhao Qu <s3773865@student.rmit.edu.au>
2020-12-28 10:43:57 +1100

    1705

--- 1705/solution.py
+++ 1705/solution.py
@@ -4,7 +4,7 @@ import solution
 class Solution(solution.Solution):
     def solve(self, test_input=None):
         apples, days = test_input
-        return self.eatenApples(list(apples),list(days))
+        return self.eatenApples(list(apples), list(days))
 
     def eatenApples(self, apples, days):
         """
@@ -12,3 +12,18 @@ class Solution(solution.Solution):
         :type days: List[int]
         :rtype: int
         """
+        import heapq
+        queue = []
+        n = len(apples)
+        i = 0
+        count = 0
+        while i < n or queue:
+            if i < n:
+                heapq.heappush(queue, [i + days[i] - 1, apples[i]])
+            while queue and (queue[0][0] < i or queue[0][1] == 0):
+                heapq.heappop(queue)
+            if queue:
+                queue[0][1] -= 1
+                count += 1
+            i += 1
+        return count
