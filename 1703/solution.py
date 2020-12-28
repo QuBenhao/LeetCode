3a0f1b2f750a1102046957fb0427a89c07f685a1
Benhao Qu <s3773865@student.rmit.edu.au>
2020-12-28 22:26:28 +1100

    1703

--- 1703/solution.py
+++ 1703/solution.py
@@ -4,7 +4,7 @@ import solution
 class Solution(solution.Solution):
     def solve(self, test_input=None):
         nums, k = test_input
-        return self.minMoves(nums.copy(),k)
+        return self.minMoves(nums.copy(), k)
 
     def minMoves(self, nums, k):
         """
@@ -12,3 +12,35 @@ class Solution(solution.Solution):
         :type k: int
         :rtype: int
         """
+        if k == 1:
+            return 0
+        if k % 2 == 1:
+            radius = (k - 1) // 2
+            save = radius * (radius + 1)
+        else:
+            radius = k // 2 - 1
+            save = (radius + 1) ** 2
+
+        n = len(nums)
+        prefix = [i for i in range(n) if nums[i]]
+        window = []
+
+        ans = float("inf")
+        left = right = 0
+
+        while prefix:
+            while len(window) < k:
+                window.append(prefix.pop(0))
+                if len(window) <= radius:
+                    left += window[-1]
+                elif len(window) > radius + 1:
+                    right += window[-1]
+            if k % 2 == 1:
+                ans = min(ans, right - left)
+            else:
+                ans = min(ans, right - left - window[radius])
+            left += window[radius]
+            right -= window[radius+1]
+            left -= window.pop(0)
+
+        return ans - save
--- 1703/testcase.py
+++ 1703/testcase.py
@@ -10,6 +10,8 @@ class Testcase(testcase.Testcase):
         self.testcases.append(case(Input=([1, 0, 0, 1, 0, 1], 2), Output=1))
         self.testcases.append(case(Input=([1, 0, 0, 0, 0, 0, 1, 1], 3), Output=5))
         self.testcases.append(case(Input=([1, 1, 0, 1], 2), Output=0))
+        self.testcases.append(case(Input=([1, 1], 1), Output=0))
+        self.testcases.append(case(Input=([0, 1, 1, 0, 0, 1, 0, 0, 0], 3), Output=2))
 
     def get_testcases(self):
         return self.testcases
