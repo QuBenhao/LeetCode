241d709993bebe19307f5bd4b47ec9d5b6048d52
Benhao Qu <s3773865@student.rmit.edu.au>
2020-12-29 21:49:43 +1100

    123

--- 123/solution.py
+++ 123/solution.py
@@ -10,12 +10,25 @@ class Solution(solution.Solution):
         :type prices: List[int]
         :rtype: int
         """
+        times = 2
         n = len(prices)
+        dp = [[0 for i in range(n)] for i in range(times)]
 
-        profit = 0
+        curr_min = buy = prices[0]
+        last = -1
+        for i in range(1, n):
+            if prices[i] <= buy:
+                buy = prices[i]
+                dp[0][i] = dp[0][i - 1]
+                dp[1][i] = dp[1][i - 1]
+            else:
+                dp[1][i] = max(prices[i] - curr_min, dp[1][i - 1])
+                if last > 0:
+                    dp[0][i] = max(dp[1][i-1] + prices[i] - buy,dp[0][last]+prices[i]-prices[last])
+                buy = prices[i]
+                last = i
+            curr_min = min(curr_min, buy)
 
-        for i in range(1,n):
-            if prices[i] > prices[i-1]:
-                profit += prices[i] - prices[i-1]
+        # print(dp)
 
-        return profit
+        return max(max(dp[0]), dp[1][-1])
--- 123/testcase.py
+++ 123/testcase.py
@@ -11,6 +11,11 @@ class Testcase(testcase.Testcase):
         self.testcases.append(case(Input=[1, 2, 3, 4, 5], Output=4))
         self.testcases.append(case(Input=[7, 6, 4, 3, 1], Output=0))
         self.testcases.append(case(Input=[1], Output=0))
+        self.testcases.append(case(Input=[2, 1, 2, 0, 1], Output=2))
+        self.testcases.append(case(Input=[6, 1, 3, 2, 4, 7], Output=7))
+        self.testcases.append(case(Input=[8, 3, 6, 2, 8, 8, 8, 4, 2, 0, 7, 2, 9, 4, 9], Output=15))
+        self.testcases.append(case(Input=[8, 6, 4, 3, 3, 2, 3, 5, 8, 3, 8, 2, 6], Output=11))
+        self.testcases.append(case(Input=[2,4,1],Output=2))
 
     def get_testcases(self):
         return self.testcases
