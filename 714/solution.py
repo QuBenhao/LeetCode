4eb01b2c2b250bc935726dd1bf54cded004b5365
Benhao Qu <s3773865@student.rmit.edu.au>
2020-12-29 18:37:31 +1100

    714

--- 714/solution.py
+++ 714/solution.py
@@ -13,3 +13,20 @@ class Solution(solution.Solution):
         :rtype: int
         """
         n = len(prices)
+        dp = [0] * n
+
+        buy = prices[0]
+
+        for i in range(1,n):
+            if prices[i] < buy:
+                buy = prices[i]
+                dp[i] = dp[i-1]
+            elif prices[i] - buy > fee:
+                dp[i] = dp[i-1] + prices[i] - buy - fee
+                buy = prices[i] - fee
+            else:
+                dp[i] = dp[i-1]
+
+        # print(dp)
+
+        return dp[-1]
--- 714/testcase.py
+++ 714/testcase.py
@@ -8,6 +8,7 @@ class Testcase(testcase.Testcase):
     def __init__(self):
         self.testcases = []
         self.testcases.append(case(Input=([1, 3, 2, 8, 4, 9], 2), Output=8))
+        self.testcases.append(case(Input=([1, 3, 7, 5, 10, 3], 3), Output=6))
 
     def get_testcases(self):
         return self.testcases
