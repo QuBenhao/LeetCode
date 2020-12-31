fa0afba8809386dbe21343adba1a629f13a07a0f
Benhao Qu <s3773865@student.rmit.edu.au>
2020-12-31 14:46:10 +1100

    289 infinite solution

--- 289/solution.py
+++ 289/solution.py
@@ -24,3 +24,23 @@ class Solution(solution.Solution):
                     board[i][j] = 1
 
         return board
+
+    # infinite solution
+
+    # def gameOfLifeInfinite(self, live):
+    #     import collections
+    #     ctr = collections.Counter((I, J)
+    #                               for i, j in live
+    #                               for I in range(i-1, i+2)
+    #                               for J in range(j-1, j+2)
+    #                               if I != i or J != j)
+    #     return {ij
+    #             for ij in ctr
+    #             if ctr[ij] == 3 or ctr[ij] == 2 and ij in live}
+    #
+    # def gameOfLife(self, board):
+    #     live = {(i, j) for i, row in enumerate(board) for j, live in enumerate(row) if live}
+    #     live = self.gameOfLifeInfinite(live)
+    #     for i, row in enumerate(board):
+    #         for j in range(len(row)):
+    #             row[j] = int((i, j) in live)
