aa3eb647518bb3d98799d5230f40ac0187cbc503
Benhao Qu <s3773865@student.rmit.edu.au>
2021-01-05 19:07:18 +1100

    reformat 83

--- 83/solution.py
+++ 83/solution.py
@@ -4,26 +4,28 @@ import solution
 class Solution(solution.Solution):
     def solve(self, test_input=None):
         last = None
-        for i in range(len(test_input)-1,-1,-1):
-            last = ListNode(test_input[i], next = last)
-        return self.deleteDuplicates(head=last)
+        for i in range(len(test_input) - 1, -1, -1):
+            last = ListNode(test_input[i], next=last)
+        head = self.deleteDuplicates(head=last)
+        nums = []
+        while head:
+            nums.append(head.val)
+            head = head.next
+        return nums
 
     def deleteDuplicates(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        nums = []
         curr = head
         while curr:
             node = curr.next
             while node and node.val == curr.val:
                 curr.next = node.next
                 node = node.next
-            nums.append(curr.val)
             curr = curr.next
-        return nums
-        # return head
+        return head
 
 
 class ListNode(object):
