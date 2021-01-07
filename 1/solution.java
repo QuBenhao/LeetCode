673488635872f48ec6c96eb11cf117057ae0288d
Benhao Qu <s3773865@student.rmit.edu.au>
2021-01-07 17:44:01 +1100

    1 Java

--- 1/solution.java
+++ 1/solution.java
@@ -1,17 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] indexes = new int[2];
         HashMap<Integer,Integer> temp = new HashMap<>();
         for(int i = 0; i < nums.length;i++){
-            if(temp.containsKey(target-nums[i])){
-                indexes[0] = temp.get(target-nums[i]);
-                indexes[1] = i;
-                return indexes;
-            }
-            else{
+            if(temp.containsKey(target-nums[i]))
+                return new int[]{temp.get(target-nums[i]),i};
+            else
                 temp.put(nums[i],i);
-            }
         }
-        return indexes;
+        return null;
     }
 }
