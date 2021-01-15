1295be8c6ce9a7d65a5e0d14f2c59dba0ccedf94
Benhao Qu <s3773865@student.rmit.edu.au>
2021-01-15 20:39:47 +1100

    1723 binary search + backtracking

--- 1723/solution.py
+++ 1723/solution.py
@@ -12,3 +12,37 @@ class Solution(solution.Solution):
         :type k: int
         :rtype: int
         """
+
+        # answer at least max (k=n) and at most sum (k=1)
+        n, left, right = len(jobs), max(jobs), sum(jobs)
+        # assign jobs with most time first
+        jobs.sort(reverse=True)
+
+        def isPossible(index):
+            # assign successfully
+            if index == n:
+                return True
+            for j in range(k):
+                if assign[j] >= jobs[index]:
+                    # assign jobs[index] to jth person
+                    assign[j] -= jobs[index]
+                    if isPossible(index+1):
+                        return True
+                    # backtracking
+                    assign[j] += jobs[index]
+                # we cannot assign anything to jth person
+                if assign[j] == mid:
+                    break
+            return False
+
+        # binary search
+        while left < right:
+            mid = (left + right) // 2
+            assign = [mid] * k
+            # answer is at most mid
+            if isPossible(0):
+                right = mid
+            # cannot assign, answer is at least mid + 1
+            else:
+                left = mid + 1
+        return left
