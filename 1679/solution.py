1bb04da6ede7691440af27ece1ef857926f2c389
Benhao Qu <s3773865@student.rmit.edu.au>
2021-01-18 19:14:28 +1100

    1679 dictionary solution

--- 1679/solution.py
+++ 1679/solution.py
@@ -12,17 +12,33 @@ class Solution(solution.Solution):
         :type k: int
         :rtype: int
         """
-        nums.sort()
-        i, j = 0, len(nums) - 1
-        res = 0
-        while i < j:
-            summ = nums[i] + nums[j]
-            if summ == k:
-                i += 1
-                j -= 1
-                res += 1
-            elif summ < k:
-                i += 1
+
+        counter = dict()
+        ans = 0
+        for num in nums:
+            if k - num in counter:
+                if counter[k-num] == 1:
+                    del counter[k-num]
+                else:
+                    counter[k-num] -= 1
+                ans += 1
+            elif num in counter:
+                counter[num] += 1
             else:
-                j -= 1
-        return res
+                counter[num] = 1
+        return ans
+
+        # nums.sort()
+        # i, j = 0, len(nums) - 1
+        # res = 0
+        # while i < j:
+        #     summ = nums[i] + nums[j]
+        #     if summ == k:
+        #         i += 1
+        #         j -= 1
+        #         res += 1
+        #     elif summ < k:
+        #         i += 1
+        #     else:
+        #         j -= 1
+        # return res
