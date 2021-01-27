8b40006e41ee19fbb581f9ff9d1a55481c932941
Benhao Qu <s3773865@student.rmit.edu.au>
2021-01-27 19:30:34 +1100

    1680 faster solution

--- 1680/solution.py
+++ 1680/solution.py
@@ -10,7 +10,23 @@ class Solution(solution.Solution):
         :type n: int
         :rtype: int
         """
-        num = ""
-        for i in range(n + 1):
-            num += "{0:b}".format(i)
-        return int(num, 2) % 1000000007
+
+        def multiply(X, Y):
+            return [[sum(a * b for a, b in zip(X_row, Y_col)) % 1000000007 for Y_col in zip(*Y)] for X_row in X]
+
+        def x_level_k_product(x, k):
+            res = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+            mat = [[2 ** k, 1, 0], [0, 1, 1], [0, 0, 1]]
+            while x > 0:
+                if x & 1: res = multiply(res, mat)
+                mat, x = multiply(mat, mat), x >> 1
+            return res
+
+        ans, acc, level = [[1], [2], [1]], 1, 1
+        while acc < n:
+            next_level = 2 ** (level + 1)
+            take = min(n, next_level - 1) - acc
+            ans = multiply(x_level_k_product(take, level + 1), ans)
+            acc, level = acc + take, level + 1
+
+        return ans[0][0]
