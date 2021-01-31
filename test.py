2dded18673d4839564d0803d56e4b8404b899057
Benhao Qu <s3773865@student.rmit.edu.au>
2021-01-31 15:51:31 +1100

    reimplement test, allow multiple answers and almost equal

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import unittest
 from importlib.machinery import SourceFileLoader
 
 # Question ID that wants to test, modify here as passing arguments
-QUESTION = 1742
+QUESTION = 1743
 SOLUTION = SourceFileLoader("module.name", f"./{QUESTION}/solution.py").load_module().Solution()
 TESTCASE = SourceFileLoader("module.name", f"./{QUESTION}/testcase.py").load_module().Testcase()
 
@@ -11,12 +11,14 @@ class Test(unittest.TestCase):
     def test(self):
         for testcase in TESTCASE.get_testcases():
             i, o = testcase
-            self.assertEqual(o, SOLUTION.solve(test_input=i), msg=f"input = {i}")
-
-    # def test_close(self):
-    #     for testcase in TESTCASE.get_testcases():
-    #         i, o = testcase
-    #         self.assertAlmostEqual(o, SOLUTION.solve(test_input=i), msg=f"input = {i}", delta=0.00001)
+            result = SOLUTION.solve(test_input=i)
+            try:
+                self.assertEqual(result, o, msg=f"input = {i}")
+            except AssertionError:
+                try:
+                    self.assertIn(result, o, msg=f"input = {i}")
+                except AssertionError:
+                    self.assertAlmostEqual(result, o, msg=f"input = {i}", delta=0.00001)
 
 
 if __name__ == '__main__':
