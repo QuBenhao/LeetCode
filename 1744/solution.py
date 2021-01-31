b7cd90a887cc65a5a5fbec268aa18938dc22aad1
Benhao Qu <s3773865@student.rmit.edu.au>
2021-01-31 18:27:06 +1100

    1744 reformat

--- 1744/solution.py
+++ 1744/solution.py
@@ -13,12 +13,12 @@ class Solution(solution.Solution):
         :rtype: List[bool]
         """
         n = len(candiesCount)
-        presum = [0] * (n+1)
+        presum = [0] * (n + 1)
         for i in range(n):
-            presum[i+1] = presum[i] + candiesCount[i]
+            presum[i + 1] = presum[i] + candiesCount[i]
         ans = []
-        for ctype, day, daily in queries:
-            if presum[ctype]//daily<= day < presum[ctype+1]:
+        for c_type, day, daily in queries:
+            if presum[c_type] // daily <= day < presum[c_type + 1]:
                 ans.append(True)
             else:
                 ans.append(False)
