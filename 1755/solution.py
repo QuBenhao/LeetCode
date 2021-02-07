5ebcb1c0083e7c0b214baa6519acabc97fd84b53
Benhao Qu <s3773865@student.rmit.edu.au>
2021-02-07 19:00:57 +1100

    1755 44ms solution

--- 1755/solution.py
+++ 1755/solution.py
@@ -12,44 +12,35 @@ class Solution(solution.Solution):
         :type goal: int
         :rtype: int
         """
-        from bisect import bisect_left
+        n = len(nums)
+        nums.sort(key=lambda x: -abs(x))
+        neg = [0 for _ in range(n + 1)]
+        pos = [0 for _ in range(n + 1)]
+        for i in range(n - 1, -1, -1):
+            if nums[i] < 0:
+                neg[i] = neg[i + 1] + nums[i]
+                pos[i] = pos[i + 1]
+            else:
+                pos[i] = pos[i + 1] + nums[i]
+                neg[i] = neg[i + 1]
+        ans = abs(goal)
+        s = set([0])
 
-        def g(a):
-            s=set([0])
-            for i in a:
-                s.update(set([i+j for j in s]))
-            return s
-        n=len(nums)
-        f=g(nums[:n//2])
-        s=[-(1<<32)]+sorted(g(nums[n//2:]))+[1<<32]
-        z=1<<32
-        for i in f:
-            z=min(z,abs(i+s[bisect_left(s,goal-i)-1]-goal),abs(i+s[bisect_left(s,goal-i)]-goal))
-        return z
+        def check(a, b):
+            if b < goal - ans or goal + ans < a:
+                return False
+            return True
 
-        # # function that generates all possible sums of sebsequences
-        # def dfs(i, cur, arr, sums):
-        #     if i == len(arr):
-        #         sums.add(cur)
-        #         return
-        #     dfs(i + 1, cur, arr, sums)
-        #     dfs(i + 1, cur + arr[i], arr, sums)
-        #
-        # sums1, sums2 = set(), set()
-        # # generate all possible sums of the 1st and 2nd half
-        # dfs(0, 0, nums[:len(nums) // 2], sums1)
-        # dfs(0, 0, nums[len(nums) // 2:], sums2)
-        #
-        # # sort the possible sums of the 2nd half
-        # s2 = sorted(sums2)
-        # ans = 10 ** 10
-        # # for each possible sum of the 1st half, find the sum in the 2nd half
-        # # that gives a value closest to the goal using binary search
-        # for s in sums1:
-        #     remain = goal - s
-        #     i1 = bisect_left(s2, remain)
-        #     if i1 < len(s2):
-        #         ans = min(ans, abs(remain - s2[i1]))
-        #     if i1 > 0:
-        #         ans = min(ans, abs(remain - s2[i1 - 1]))
-        # return ans
+        for i in range(n):
+            sl = [x for x in s if check(x + neg[i], x + pos[i])]
+            if len(sl) == 0:
+                break
+            s = set(sl)
+            for x in sl:
+                y = x + nums[i]
+                if abs(y - goal) < ans:
+                    ans = abs(y - goal)
+                if ans == 0:
+                    return 0
+                s.add(y)
+        return ans
