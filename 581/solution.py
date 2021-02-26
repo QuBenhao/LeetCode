fddb8e55c6d24813216ea2a00c847ce341ac9a34
Benhao Qu <s3773865@student.rmit.edu.au>
2021-02-26 08:37:35 +0800

    581

--- 581/solution.py
+++ 581/solution.py
@@ -10,22 +10,18 @@ class Solution(solution.Solution):
         :type nums: List[int]
         :rtype: int
         """
-        m, m_index, start, end = float("-inf"), -1, len(nums), -1
+        m, end = float("-inf"), -1
         for i, n in enumerate(nums):
             if n > m:
                 m = n
-                m_index = i
             elif n < m:
-                start = min(m_index, start)
-                end = max(i, end)
-        m, m_index = float("inf"), -1
+                end = i
+        m,start = float("inf"), len(nums)
         for i in range(len(nums)-1,-1,-1):
             if nums[i] < m:
                 m = nums[i]
-                m_index = i
             elif nums[i] > m:
-                start = min(i, start)
-                end = max(m_index, end)
+                start = i
         if start == len(nums) or end == -1:
             return 0
         return end - start + 1
