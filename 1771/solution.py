888554f3d4bb1710820e4e0d559951691cd7fd37
Benhao Qu <s3773865@student.rmit.edu.au>
2021-02-28 12:15:05 +0800

    1771 dp

--- 1771/solution.py
+++ 1771/solution.py
@@ -12,3 +12,19 @@ class Solution(solution.Solution):
         :type word2: str
         :rtype: int
         """
+        # dp[i][j] = dp[i+1][j-1] + 2 if s[i] == s[j], else = max(dp[i+1][j], dp[i][j-1])
+        s = word1 + word2
+        dp = [[0] * len(s) for _ in range(len(s))]
+
+        res = 0
+        for i in range(len(s) - 1, -1, -1):
+            dp[i][i] = 1
+            for j in range(i + 1, len(s)):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1] + 2
+                    if i < len(word1) <= j:
+                        res = max(res, dp[i][j])
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+
+        return res
