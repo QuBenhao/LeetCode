5f6cedcff53a65600a91ae6640d5164e15c2fcaf
Benhao Qu <s3773865@student.rmit.edu.au>
2021-03-02 09:12:03 +0800

    304 reformat

--- 304/solution.py
+++ 304/solution.py
@@ -9,7 +9,7 @@ class Solution(solution.Solution):
         ans = [None]
         for i in range(1, len(test_input)):
             row1, col1, row2, col2 = test_input[i]
-            param_1 = obj.sumRegion(row1,col1,row2,col2)
+            param_1 = obj.sumRegion(row1, col1, row2, col2)
             ans.append(param_1)
         return ans
 
@@ -23,10 +23,10 @@ class NumMatrix(object):
         if not matrix:
             return
         m, n = len(matrix), len(matrix[0])
-        self.dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
-        for i in range(1,m+1):
-            for j in range(1,n+1):
-                self.dp[i][j] = self.dp[i-1][j] + self.dp[i][j-1] + matrix[i-1][j-1] - self.dp[i-1][j-1]
+        self.dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                self.dp[i][j] = self.dp[i - 1][j] + self.dp[i][j - 1] + matrix[i - 1][j - 1] - self.dp[i - 1][j - 1]
 
     def sumRegion(self, row1, col1, row2, col2):
         """
@@ -36,4 +36,4 @@ class NumMatrix(object):
         :type col2: int
         :rtype: int
         """
-        return self.dp[row2+1][col2+1] - self.dp[row2+1][col1] - self.dp[row1][col2+1] + self.dp[row1][col1]
+        return self.dp[row2 + 1][col2 + 1] - self.dp[row2 + 1][col1] - self.dp[row1][col2 + 1] + self.dp[row1][col1]
