4ecf161ca75d4755960a21645e19c1fac1ffc310
Benhao Qu <s3773865@student.rmit.edu.au>
2021-03-06 08:37:24 +0800

    503

--- 503/solution.py
+++ 503/solution.py
@@ -10,28 +10,25 @@ class Solution(solution.Solution):
         :type nums: List[int]
         :rtype: List[int]
         """
-        n = len(nums)
-        ans = [-1] * n
-        stack = []
-        for i in range(n):
-            while stack and nums[stack[-1]] < nums[i]:
-                ans[stack.pop()] = nums[i]
-            stack.append(i)
-        for i in range(n):
-            while stack and nums[stack[-1]] < nums[i]:
-                ans[stack.pop()] = nums[i]
-        return ans
-
         # n = len(nums)
-        # stack, res = [], [-1] * n
-        # for i in range(n*2):
-        #     temp = i
-        #     if i >= n:
-        #         i -= n
-        #     while stack and (nums[stack[-1]] < nums[i]):
-        #         res[stack.pop()] = nums[i]
-        #     if temp < n:
-        #         stack.append(i)
-        #     elif not stack:
-        #         break
-        # return res
+        # ans = [-1] * n
+        # stack = []
+        # for i in range(n):
+        #     while stack and nums[stack[-1]] < nums[i]:
+        #         ans[stack.pop()] = nums[i]
+        #     stack.append(i)
+        # for i in range(n):
+        #     while stack and nums[stack[-1]] < nums[i]:
+        #         ans[stack.pop()] = nums[i]
+        # return ans
+
+        n = len(nums)
+        stack, ans = [], [-1] * n
+        for i in range(2 * n):
+            while stack and nums[stack[-1]] < nums[i%n]:
+                ans[stack.pop()] = nums[i%n]
+            if i < n:
+                stack.append(i)
+            elif not stack:
+                break
+        return ans
