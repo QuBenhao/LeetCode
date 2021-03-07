9c2913552533cace73f9d049c36816becc440972
Benhao Qu <s3773865@student.rmit.edu.au>
2021-03-07 09:53:07 +0800

    1781

--- 1781/solution.py
+++ 1781/solution.py
@@ -13,15 +13,11 @@ class Solution(solution.Solution):
         ans = 0
         li = []
         for c in s:
-            new = [[0] * 26]
+            new = [0] * 26
             i = ord(c) - ord('a')
-            new[0][i] = 1
+            new[i] = 1
             for counter in li:
                 counter[i] += 1
-                m = max(counter)
-                mi = min([k for k in counter if k])
-                if m > mi:
-                    ans += m - mi
-                new.append(counter)
-            li = new
+                ans += max(counter) - min(k for k in counter if k)
+            li.append(new)
         return ans
