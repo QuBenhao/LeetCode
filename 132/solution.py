0ca687321863f5eca539cd3657599244ba44d469
Benhao Qu <s3773865@student.rmit.edu.au>
2021-03-08 09:51:38 +0800

    132

--- 132/solution.py
+++ 132/solution.py
@@ -11,35 +11,20 @@ class Solution(solution.Solution):
         :rtype: int
         """
 
-        def is_palindrome(string):
-            if string in palindrome:
-                return True
-            if string == string[::-1]:
-                palindrome.add(string)
-                return True
-            return False
-
-        if s == s[::-1]:
-            return 0
-
         n = len(s)
-        palindrome = set()
-        dp = [i for i in range(n)]
 
+        is_palindrome = [[True for _ in range(n)] for _ in range(n)]
         for i in range(n):
-            for j in range(i+1, n):
-                if is_palindrome(s[i:j]):
-                    if i == 0:
-                        if j > 0:
-                            dp[j - 1] = 0
-                        dp[j] = min(dp[j], 1)
-                    else:
-                        if j > 0:
-                            dp[j-1] = min(dp[j-1], dp[i - 1] + 1)
-                        dp[j] = min(dp[j], dp[i - 1] + 2)
-            if is_palindrome(s[i:]):
-                dp[-1] = min(dp[-1], dp[i - 1] + 1)
-                if dp[-1] == 1:
-                    return 1
+            for j in range(i):
+                is_palindrome[j][i] = s[j] == s[i] and is_palindrome[j + 1][i - 1]
+
+        dp = [i for i in range(n)]
+        for i in range(n):
+            if is_palindrome[0][i]:
+                dp[i] = 0
+            else:
+                for j in range(1, i + 1):
+                    if is_palindrome[j][i]:
+                        dp[i] = min(dp[i], dp[j - 1] + 1)
 
         return dp[-1]
