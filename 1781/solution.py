16f45efe36c6cfcec3c5a375fca763948adac6d1
Benhao Qu <s3773865@student.rmit.edu.au>
2021-03-09 09:28:25 +0800

    1781 comments

--- 1781/solution.py
+++ 1781/solution.py
@@ -11,13 +11,19 @@ class Solution(solution.Solution):
         :rtype: int
         """
         ans = 0
+        # store all freq tables for each iterate
         li = []
         for c in s:
+            # we will have a new freq table start from this char
             new = [0] * 26
             i = ord(c) - ord('a')
             new[i] = 1
+            # for every freq tables from last iterate
             for counter in li:
+                # add curr char to the table first
                 counter[i] += 1
+                # calculate diff between max and min
                 ans += max(counter) - min(k for k in counter if k)
+            # add the new freq table for next iterate
             li.append(new)
         return ans
