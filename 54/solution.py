88a508df11f01da3ae6f25d86ac48fc1d9eaccd1
Benhao Qu <s3773865@student.rmit.edu.au>
2021-03-15 11:53:59 +0800

    54

--- 54/solution.py
+++ 54/solution.py
@@ -10,11 +10,35 @@ class Solution(solution.Solution):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        from sys import version_info
-        if version_info.major < 3:
-            return matrix and list(matrix.pop(0)) + self.spiralOrder(zip(*matrix)[::-1])
-        else:
-            return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
+        # from sys import version_info
+        # if version_info.major < 3:
+        #     return matrix and list(matrix.pop(0)) + self.spiralOrder(zip(*matrix)[::-1])
+        # else:
+        #     return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
+
+        ans = []
+        m, n = len(matrix), len(matrix[0])
+        left, top, right, down = 0, 0, n - 1, m - 1
+        while left <= right and top <= down:
+            for i in range(left, right):
+                ans.append(matrix[top][i])
+            if top == down:
+                ans.append(matrix[top][right])
+                break
+            for i in range(top, down):
+                ans.append(matrix[i][right])
+            if left == right:
+                ans.append(matrix[down][right])
+                break
+            for i in range(right, left, -1):
+                ans.append(matrix[down][i])
+            for i in range(down, top, -1):
+                ans.append(matrix[i][left])
+            left += 1
+            top += 1
+            right -= 1
+            down -= 1
+        return ans
 
         # ans = []
         # m, n = len(matrix), len(matrix[0])
