24c295b1b9f94ebcee1a036979d07f8c8060541e
Benhao Qu <s3773865@student.rmit.edu.au>
2021-04-14 11:12:08 +0800

    python 使用字典嵌套字典

--- 208/solution.py
+++ 208/solution.py
@@ -17,6 +17,54 @@ class Solution(solution.Solution):
         return ans
 
 
+# class Trie(object):
+#
+#     def __init__(self):
+#         """
+#         Initialize your data structure here.
+#         """
+#         self.root = {}
+#
+#     def insert(self, word):
+#         """
+#         Inserts a word into the trie.
+#         :type word: str
+#         :rtype: None
+#         """
+#         node = self.root
+#         for c in word:
+#             if c not in node:
+#                 node[c] = {}
+#             node = node[c]
+#         node['end'] = True
+#
+#     def search(self, word):
+#         """
+#         Returns if the word is in the trie.
+#         :type word: str
+#         :rtype: bool
+#         """
+#         node = self.root
+#         for c in word:
+#             if c not in node:
+#                 return False
+#             node = node[c]
+#         return 'end' in node
+#
+#     def startsWith(self, prefix):
+#         """
+#         Returns if there is any word in the trie that starts with the given prefix.
+#         :type prefix: str
+#         :rtype: bool
+#         """
+#         node = self.root
+#         for c in prefix:
+#             if c not in node:
+#                 return False
+#             node = node[c]
+#         return True
+
+
 class Trie(object):
 
     def __init__(self):
@@ -68,7 +116,6 @@ class TrieNode(object):
     def __init__(self):
         self.children = {}
         self.is_word = False
-
 
 # class Trie(object):
 #     def __init__(self):
