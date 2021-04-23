a14137a46a7ebb5ffcec36f3b69bf1ad420f88bd
Benhao Qu <s3773865@student.rmit.edu.au>
2021-04-23 12:17:58 +0800

    368 更新有向图求最长路径解法

--- 368/solution.py
+++ 368/solution.py
@@ -35,3 +35,35 @@ class Solution(solution.Solution):
         #         if nums[i] % nums[j] == 0 and len(dp[j]) + 1 > len(dp[i]):
         #             dp[i] = dp[j] + [nums[i]]
         # return max(dp,key=len)
+
+        # n = len(nums)
+        # # 获得有序数组
+        # nums.sort()
+        # # matrix[i][0]  代表 到该节点的最长步数
+        # # matrix[i][1] 代表 最长步数时的上一个节点
+        # matrix = [(0,0)] * n
+        #
+        # maxDis = maxPos = -1
+        # # 从前往后遍历
+        # for i in range(n):
+        #     for j in range(i+1,n):
+        #         if nums[j]%nums[i]==0:
+        #             # 满足题目条件的情况下
+        #             # 若从当前节点（i）往目标节点（j）的步数较之前的元素更大，则更新最长步数与上一个节点
+        #             if matrix[i][0]+1 > matrix[j][0]:
+        #                 matrix[j] = (matrix[i][0] + 1, i)
+        #
+        #     # 每一轮遍历结束后检查是否为最大值（当前节点i的最大步数只由i之前的元素决定）
+        #     # 记录下最大值所在的位置
+        #     if matrix[i][0]>maxDis:
+        #         maxDis = matrix[i][0];
+        #         maxPos = i
+        #
+        # # 根据最大步数所在的位置，逆向推出所选子集
+        # re = []
+        # re.append(nums[maxPos])
+        # while matrix[maxPos][0]!=0:
+        #     re.append(nums[matrix[maxPos][1]])
+        #     maxPos = matrix[maxPos][1]
+        # re.reverse()
+        # return re
