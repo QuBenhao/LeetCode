19ebe548dd36b3159f149590ced083af5d35a109
Benhao Qu <s3773865@student.rmit.edu.au>
2021-04-25 08:49:54 +0800

    897 两种递归解法

--- 897/solution.py
+++ 897/solution.py
@@ -38,24 +38,48 @@ class Solution(solution.Solution):
         :type root: TreeNode
         :rtype: TreeNode
         """
-        parent = node = root
-        while node:
-            if node.left:
-                temp = curr = node.left
-                while curr.right:
-                    curr = curr.right
-                curr.right = node
-                node.left = None
-                if parent == node:
-                    root = temp
-                    parent = node = root
-                else:
-                    parent.right = temp
-                    node = parent
-            else:
-                parent = node
-                node = node.right
-        return root
+        # if not root:
+        #     return
+        # if root.left:
+        #     temp = root
+        #     root = self.increasingBST(root.left)
+        #     temp.left = None
+        #     curr = root
+        #     while curr.right:
+        #         curr = curr.right
+        #     curr.right = temp
+        #     temp.right = self.increasingBST(temp.right)
+        # elif root.right:
+        #     root.right = self.increasingBST(root.right)
+        # return root
+
+        def reorder_BST(root, tail=None):
+            if not root:
+                return tail
+            res = reorder_BST(root.left, root)
+            root.left = None
+            root.right = reorder_BST(root.right, tail)
+            return res
+        return reorder_BST(root)
+
+        # parent = node = root
+        # while node:
+        #     if node.left:
+        #         temp = curr = node.left
+        #         while curr.right:
+        #             curr = curr.right
+        #         curr.right = node
+        #         node.left = None
+        #         if parent == node:
+        #             root = temp
+        #             parent = node = root
+        #         else:
+        #             parent.right = temp
+        #             node = parent
+        #     else:
+        #         parent = node
+        #         node = node.right
+        # return root
 
 
 class TreeNode(object):
