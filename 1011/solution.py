1d19b1a08c73e98601e08a249f2591a209984f7f
Benhao Qu <s3773865@student.rmit.edu.au>
2021-04-26 09:04:55 +0800

    1011 加点儿注释

--- 1011/solution.py
+++ 1011/solution.py
@@ -12,7 +12,7 @@ class Solution(solution.Solution):
         :type D: int
         :rtype: int
         """
-
+        # max_weight能否在D天内运载全部货物
         def helper(max_weight):
             count = count_w = 0
             for w in weights:
@@ -20,13 +20,11 @@ class Solution(solution.Solution):
                 if count_w > max_weight:
                     count += 1
                     count_w = w
-                elif count_w == max_weight:
-                    count += 1
-                    count_w = 0
             if count_w:
                 count += 1
             return count > D
 
+        # 左边界至少要比最大的货物重，也要比每天平均货物大；右边界至多是全部的和，一天运完
         left, right = max(sum(weights) // D, max(weights)), sum(weights)
         while left < right:
             mid = (left + right) // 2
