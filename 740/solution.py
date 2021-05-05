cae570afdb18d316ca5530fd4a05a4d173059fc8
Benhao Qu <s3773865@student.rmit.edu.au>
2021-05-05 08:47:15 +0800

    740 不需要dp数组

--- 740/solution.py
+++ 740/solution.py
@@ -13,16 +13,12 @@ class Solution(solution.Solution):
         """
         cnt = Counter(nums)
         ns = sorted(cnt.keys())
-        # 对当前最大值和上次最大值（延时更新）的动态规划
-        dp = [0] * len(cnt)
-        max_so_far = dp[0] = cnt[ns[0]] * ns[0]
+        # 当前最大值和上次最大值的动态规划
+        max_so_far = cnt[ns[0]] * ns[0]
         max_except_last = 0
         for i in range(1,len(cnt)):
             if ns[i-1] == ns[i] - 1:
-                dp[i] = max_except_last
+                max_except_last, max_so_far = max_so_far, max(max_so_far, max_except_last + cnt[ns[i]] * ns[i])
             else:
-                dp[i] = max_so_far
-            dp[i] += cnt[ns[i]] * ns[i]
-            max_except_last = max_so_far
-            max_so_far = max(max_so_far, dp[i])
-        return max(dp)
+                max_except_last,max_so_far = max_so_far, max_so_far + cnt[ns[i]] * ns[i]
+        return max_so_far
