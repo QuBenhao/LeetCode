fd57997fa490d2478e2be3466784ac1655fa1d43
Benhao Qu <s3773865@student.rmit.edu.au>
2021-05-07 11:28:49 +0800

    1486 O(1)异或推理

--- 1486/solution.py
+++ 1486/solution.py
@@ -14,7 +14,7 @@ class Solution(solution.Solution):
 
         if n % 4 == 0, there will be an even number of 2, and the xor of them is 0.
 
-        if n % 4 == 1, you can exclude the last numebr which is start + 2 * (n - 1).
+        if n % 4 == 1, you can exclude the last number which is start + 2 * (n - 1).
         The xor of left numbers is 0, so the result is last.
 
         if n % 4 == 2, you can exclude the last two numbers. The xor of left numbers is 0,
@@ -34,3 +34,25 @@ class Solution(solution.Solution):
             n -= 1
         if n % 2 == 0: return start ^ (n & 2)
         return start ^ last ^ (n & 2)
+
+        # def cal(x):
+        #     """
+        #         4a ^ (4a + 1) = 1
+        #         4a ^ (4a + 1) ^ (4a + 2) = 1 ^ (4a + 2) = 4a + 3
+        #         4a ^ (4a + 1) ^ (4a + 2) ^ (4a + 3) = (4a + 3) ^ (4a + 3) = 0
+        #     """
+        #     if x % 4 == 0:
+        #         return x
+        #     elif x % 4 == 1:
+        #         return 1
+        #     elif x % 4 == 2:
+        #         return x + 1
+        #     return 0
+        #
+        # # 整体除以2, 利用 %4 结论计算 ans 中除「最低一位」的结果
+        # s = start >> 1
+        # # 计算 1 到 s - 1的异或结果，再计算 1 到 s + n - 1的异或结果，两者异或得到ans中除最后一位的结果
+        # prefix = cal(s - 1) ^ cal(s + n - 1)
+        # # 利用「奇偶性」计算 ans 中的「最低一位」结果
+        # last = n & start & 1
+        # return prefix << 1 | last
