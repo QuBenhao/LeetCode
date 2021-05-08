fee4fdda36b69dcf93de991948ec4f19c20c78ed
Benhao Qu <s3773865@student.rmit.edu.au>
2021-05-08 12:13:24 +0800

    1723 二分查找 或 优化dfs， 可以研究状态压缩dp

--- 1723/solution.py
+++ 1723/solution.py
@@ -13,36 +13,52 @@ class Solution(solution.Solution):
         :rtype: int
         """
 
-        # answer at least max (k=n) and at most sum (k=1)
-        n, left, right = len(jobs), max(jobs), sum(jobs)
-        # assign jobs with most time first
         jobs.sort(reverse=True)
+        n, left, right = len(jobs), jobs[0], sum(jobs)
 
-        def isPossible(index):
-            # assign successfully
+        def is_possible(index):
             if index == n:
                 return True
             for j in range(k):
+                # 尝试将第index个任务分配给第j个人
                 if assign[j] >= jobs[index]:
-                    # assign jobs[index] to jth person
                     assign[j] -= jobs[index]
-                    if isPossible(index+1):
+                    # 递归
+                    if is_possible(index + 1):
                         return True
-                    # backtracking
+                    # 分配给第j个人无法完成其他人的分配
                     assign[j] += jobs[index]
-                # we cannot assign anything to jth person
+                # 无法分配任何任务给第j个人
                 if assign[j] == mid:
                     break
             return False
 
-        # binary search
         while left < right:
             mid = (left + right) // 2
             assign = [mid] * k
-            # answer is at most mid
-            if isPossible(0):
+            # 分配为最大mid是可行的
+            if is_possible(0):
                 right = mid
-            # cannot assign, answer is at least mid + 1
             else:
                 left = mid + 1
-        return left
+        return right
+
+        # assign, n = [0] * k, len(jobs)
+        #
+        # def dfs(index, nxt, curr_max, ans):
+        #     if curr_max >= ans:
+        #         return float("inf")
+        #     if index == n:
+        #         return curr_max
+        #     # 优先分配给没分配过的人
+        #     if nxt < k:
+        #         assign[nxt] = jobs[index]
+        #         ans = min(ans, dfs(index + 1, nxt + 1, max(curr_max, assign[nxt]), ans))
+        #         assign[nxt] = 0
+        #     for i in range(nxt):
+        #         assign[i] += jobs[index]
+        #         ans = min(ans, dfs(index+1, nxt, max(curr_max, assign[i]), ans))
+        #         assign[i] -= jobs[index]
+        #     return ans
+        #
+        # return dfs(0, 0, 0, float("inf"))
