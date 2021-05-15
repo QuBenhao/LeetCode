ba45c02686cbfb779bc757c9dc4391bfb111b0d8
Benhao Qu <s3773865@student.rmit.edu.au>
2021-05-15 19:28:13 +0800

    1735 质因数分解后的重复组合数

--- 1735/solution.py
+++ 1735/solution.py
@@ -1,4 +1,7 @@
 import solution
+from functools import lru_cache
+from collections import Counter
+import math
 
 
 class Solution(solution.Solution):
@@ -10,3 +13,28 @@ class Solution(solution.Solution):
         :type queries: List[List[int]]
         :rtype: List[int]
         """
+        # target最大为10000，那么质因数最大为97
+        primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+        # 分解质因数
+        @lru_cache(None)
+        def div(num):
+            c = Counter()
+            for i in primes:
+                while num % i == 0:
+                    c[i] += 1
+                    num //= i
+            if num > 1:
+                c[num] += 1
+            return c
+
+        ans = []
+        for l,t in queries:
+            count = div(t)
+            cur = 1
+            # 对于每个质因子，分配方式为将v个质因数填入l个箱子中有多少种填法
+            # 球同，盒不同，盒可以为空，组合数结果为C(N+M-1, M-1)
+            for v in count.values():
+                cur *= math.comb(l+v-1,v)
+            ans.append(cur % (10 ** 9 + 7))
+        return ans
