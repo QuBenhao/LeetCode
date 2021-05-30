fec57d39104e97a99fa855e1ad6425fe7b5778fc
Benhao Qu <s3773865@student.rmit.edu.au>
2021-05-30 09:00:05 +0800

    1878 使用set存直接去重，使用前缀和加速

--- problems/1878/solution.py
+++ problems/1878/solution.py
@@ -11,34 +11,34 @@ class Solution(solution.Solution):
         :rtype: List[int]
         """
         m, n = len(grid), len(grid[0])
+        sum1 = [[0] * (n + 2) for _ in range(m + 1)]
+        sum2 = [[0] * (n + 2) for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                sum1[i][j] = sum1[i - 1][j - 1] + grid[i - 1][j - 1]
+                sum2[i][j] = sum2[i - 1][j + 1] + grid[i - 1][j - 1]
+
         val = set()
-
-        psum1 = [[0] * (m + 1) for _ in range(m + n)]
-        psum2 = [[0] * (m + 1) for _ in range(m + n)]
-
-        for k in range(m + n):
-            for i in range(m):
-                j = k - i
-                if 0 <= j < n:
-                    psum1[k][i + 1] = psum1[k][i] + grid[i][j]
-                j = k + i - (m - 1)
-                if 0 <= j < n:
-                    psum2[k][i + 1] = psum2[k][i] + grid[i][j]
-
         for i in range(m):
             for j in range(n):
+                # 单独的一个格子也是 rhombus
                 val.add(grid[i][j])
-                for k in range(1, m):
-                    if 0 <= i + 2 * k < m and 0 <= j - k < n and 0 <= j + k < n:
-                        tmp = 0
-                        idx1 = i + j
-                        idx2 = i + j + 2 * k
-                        idx3 = j - i + (m - 1)
-                        idx4 = j - i + (m - 1) - 2 * k
-                        tmp += psum1[idx1][i + k + 1] - psum1[idx1][i + 1] + psum1[idx2][i + 2 * k] - psum1[idx2][i + k]
-                        tmp += psum2[idx3][i + k] - psum2[idx3][i] + psum2[idx4][i + 2 * k + 1] - psum2[idx4][i + k + 1]
-                        val.add(tmp)
-                    else:
+                for k in range(i + 2, m, 2):
+                    ux, uy = i, j
+                    dx, dy = k, j
+                    lx, ly = (i + k) // 2, j - (k - i) // 2
+                    rx, ry = (i + k) // 2, j + (k - i) // 2
+
+                    if ly < 0 or ry >= n:
                         break
-        val = sorted(val)[::-1]
-        return val[:3]
+
+                    val.add(
+                        (sum2[lx + 1][ly + 1] - sum2[ux][uy + 2]) +
+                        (sum1[rx + 1][ry + 1] - sum1[ux][uy]) +
+                        (sum1[dx + 1][dy + 1] - sum1[lx][ly]) +
+                        (sum2[dx + 1][dy + 1] - sum2[rx][ry + 2]) -
+                        (grid[ux][uy] + grid[dx][dy] + grid[lx][ly] + grid[rx][ry])
+                    )
+
+        return sorted(val, reverse=True)[:3]
