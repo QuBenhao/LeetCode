df1d614f7a65f6e236c1b5f9563eb2a7e6ac31ef
Benhao Qu <s3773865@student.rmit.edu.au>
2021-06-01 09:56:04 +0800

    1744 前缀和问题

--- problems/1744/solution.py
+++ problems/1744/solution.py
@@ -1,4 +1,5 @@
 import solution
+from itertools import accumulate
 
 
 class Solution(solution.Solution):
@@ -12,14 +13,8 @@ class Solution(solution.Solution):
         :type queries: List[List[int]]
         :rtype: List[bool]
         """
-        n = len(candiesCount)
-        presum = [0] * (n + 1)
-        for i in range(n):
-            presum[i + 1] = presum[i] + candiesCount[i]
-        ans = []
-        for c_type, day, daily in queries:
-            if presum[c_type] // daily <= day < presum[c_type + 1]:
-                ans.append(True)
-            else:
-                ans.append(False)
-        return ans
+        # day + 1 是因为我们考虑的是需要的天数而不是第几天，第二天实际上已经吃第三天了
+        # 按最快的速度 cap 吃，至少需要的天数（把前面的presum[ty]吃光）
+        # presum[ty+1] 按最慢的速度吃，至多的天数
+        presum = list(accumulate([0] + candiesCount))
+        return [presum[t]//c <= d < presum[t+1] for t,d,c in queries]
