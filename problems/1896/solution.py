e3529e40af213d2ae8396267fd5579b41926ee57
Benhao Qu <s3773865@student.rmit.edu.au>
2021-06-15 00:49:39 +0800

    1896 栈+动态规划

--- problems/1896/solution.py
+++ problems/1896/solution.py
@@ -11,3 +11,51 @@ class Solution(solution.Solution):
         :type expression: str
         :rtype: int
         """
+        """
+        p1  op  p2  val, change
+        0   |   0   0, min(c1,c2)
+        0   |   1   1, 1
+        1   |   0   1, 1
+        1   |   1   1, min(c1,c2)+1
+        0   &   0   0, min(c1,c2)+1
+        0   &   1   0, 1
+        1   &   0   0, 1
+        1   &   1   1, min(c1,c2)
+        """
+        # l中没有括号，计算l的结果:val,change
+        def cal(l):
+            val, change = l[0]
+            idx = 1
+            while idx < len(l):
+                op = l[idx]
+                v, c = l[idx+1]
+                if op == '|':
+                    if val + v == 1:
+                        val, change = 1, 1
+                    elif not v:
+                        val, change = 0, min(change, c)
+                    else:
+                        val, change = 1, min(change, c) + 1
+                else:
+                    if val + v == 1:
+                        val, change = 0, 1
+                    elif not v:
+                        val, change = 0, min(change, c) + 1
+                    else:
+                        val, change = 1, min(change, c)
+                idx += 2
+            return val, change
+
+        stack = [[]]
+        for c in expression:
+            if c == '(':
+                stack.append([])
+            elif c == ')':
+                value, changes = cal(stack.pop())
+                stack[-1].append((value,changes))
+            elif c in '&|':
+                stack[-1].append(c)
+            else:
+                # val, change for 0 or 1
+                stack[-1].append((int(c),1))
+        return cal(stack[0])[1]
