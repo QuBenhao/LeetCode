06b77525734248c845c194213d65e286b81be83d
Benhao Qu <s3773865@student.rmit.edu.au>
2021-06-17 21:32:51 +0800

    1690 dp

--- problems/1690/solution.py
+++ problems/1690/solution.py
@@ -1,4 +1,5 @@
 import solution
+from itertools import accumulate
 
 
 class Solution(solution.Solution):
@@ -11,20 +12,10 @@ class Solution(solution.Solution):
         :rtype: int
         """
         n = len(stones)
-        dp = [[0 for i in range(n)] for i in range(n)]
-
-        for i in range(1, n):
-            stones[i] += stones[i - 1]
-        stones.insert(0, 0)
-
-        # stones : 5 3 1 4 2
-        # sum of stones : 0 5 8 9 13 15
-        # pop out 15 will get 13 - 0 points (pop out j = stones[j] - stones[i])
-        # pop out 5 will get 15 - 5 points (pop out i = stones[j+1] - stones[i+1])
-        for i in range(n - 2, -1, -1):
-            for j in range(i + 1, n):
-                # max between pop out j and pop out i
-                # minus because the other player always choose its best strategy
-                dp[i][j] = max(stones[j] - stones[i] - dp[i][j - 1], stones[j + 1] - stones[i + 1] - dp[i + 1][j])
-
+        dp = [[0] * n for _ in range(n)]
+        presum = [0] + list(accumulate(stones))
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                dp[i][j] = max(presum[j + 1] - presum[i + 1] - dp[i + 1][j], presum[j] - presum[i] - dp[i][j - 1])
         return dp[0][n - 1]
