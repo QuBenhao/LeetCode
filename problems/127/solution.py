0ad133dbf23d8dc5e2cf3ef7f873c5a6a4db0af8
Benhao Qu <s3773865@student.rmit.edu.au>
2021-06-17 14:07:50 +0800

    127 另一种双端优先队列 双向bfs （加入启发式函数展开更小的那个，但是时间上并没有什么优化）

--- problems/127/solution.py
+++ problems/127/solution.py
@@ -1,6 +1,7 @@
 import solution
 import string
 from collections import deque
+import heapq
 
 
 class Solution(solution.Solution):
@@ -16,41 +17,83 @@ class Solution(solution.Solution):
         :rtype: int
         """
 
-        class Queue:
+        # class Queue:
+        #     def __init__(self):
+        #         self.queue = deque([])
+        #         self.explored = dict()
+        #
+        #     def __contains__(self, item):
+        #         return item in self.explored
+        #
+        #     def __add__(self, other):
+        #         self.queue.append(other[1])
+        #         self.explored[other[1]] = other[0]
+        #
+        #     def __len__(self):
+        #         return len(self.queue)
+        #
+        #     def pop(self):
+        #         return self.queue.popleft()
+        #
+        # wordList = set(wordList)
+        # if endWord not in wordList:
+        #     return 0
+        # front, back = Queue(), Queue()
+        # front.__add__((1, beginWord))
+        # back.__add__((1, endWord))
+        # while front and back:
+        #     if len(front) > len(back):
+        #         ts, ot = back, front
+        #     else:
+        #         ts, ot = front, back
+        #     s = ts.pop()
+        #     for i in range(len(s)):
+        #         for c in string.ascii_lowercase:
+        #             t = s[:i] + c + s[i + 1:]
+        #             if t in wordList and t not in ts:
+        #                 if t in ot:
+        #                     return ts.explored[s] + ot.explored[t]
+        #                 ts.__add__((ts.explored[s] + 1, t))
+        # return 0
+
+        class ProirtyQueue:
             def __init__(self):
-                self.queue = deque([])
+                self.queue = []
                 self.explored = dict()
 
             def __contains__(self, item):
                 return item in self.explored
 
             def __add__(self, other):
-                self.queue.append(other)
-                self.explored[other[1]] = other[0]
+                heapq.heappush(self.queue, other)
+                self.explored[other[2]] = other[0]
 
             def __len__(self):
                 return len(self.queue)
 
             def pop(self):
-                return self.queue.popleft()
+                return heapq.heappop(self.queue)
+
+        def heuristic(curr, target):
+            return sum(1 for a, b in zip(curr, target) if a != b)
 
         wordList = set(wordList)
         if endWord not in wordList:
             return 0
-        front, back = Queue(), Queue()
-        front.__add__((1, beginWord))
-        back.__add__((1, endWord))
+        front, back = ProirtyQueue(), ProirtyQueue()
+        front.__add__((1, 0, beginWord))
+        back.__add__((1, 0, endWord))
         while front and back:
             if len(front) > len(back):
-                ts, ot = back, front
+                ts, ot, target = back, front, beginWord
             else:
-                ts, ot = front, back
-            length, s = ts.pop()
+                ts, ot, target = front, back, endWord
+            l, _, s = ts.pop()
             for i in range(len(s)):
                 for c in string.ascii_lowercase:
                     t = s[:i] + c + s[i + 1:]
                     if t in wordList and t not in ts:
                         if t in ot:
-                            return length + ot.explored[t]
-                        ts.__add__((length + 1, t))
+                            return l + ot.explored[t]
+                        ts.__add__((l + 1, heuristic(t, target), t))
         return 0
