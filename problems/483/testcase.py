82191dd979899a85327acfc21fd5a78583c9c9cd
Benhao Qu <s3773865@student.rmit.edu.au>
2021-06-18 23:48:53 +0800

    483 添加一个测试,判断num==右边的除法不能使用浮点数除，因为结果会被保存为浮点数(大数字丢失精度)

--- problems/483/testcase.py
+++ problems/483/testcase.py
@@ -10,6 +10,7 @@ class Testcase(testcase.Testcase):
         self.testcases.append(case(Input="13", Output="3"))
         self.testcases.append(case(Input="4681", Output="8"))
         self.testcases.append(case(Input="1000000000000000000", Output="999999999999999999"))
+        self.testcases.append(case(Input="14919921443713777", Output="496"))
 
     def get_testcases(self):
         return self.testcases
