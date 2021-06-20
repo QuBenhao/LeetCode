5707751e8851d72f440dfa490abb9186a56e36d0
Benhao Qu <s3773865@student.rmit.edu.au>
2021-06-20 16:10:40 +0800

    1906 差分数组

--- problems/1906/solution.py
+++ problems/1906/solution.py
@@ -13,20 +13,21 @@ class Solution(solution.Solution):
         :type queries: List[List[int]]
         :rtype: List[int]
         """
+        m = max(nums)
         # 差分数组
-        diff = [[0] * 101]
+        diff = [[0] * (m + 1)]
         for num in nums:
             diff.append(list(diff[-1]))
             diff[-1][num] += 1
 
         ans = []
         for l, r in queries:
-            res = 100  # 最大不会超过100
-            last = -100
+            res = m  # 最大不会超过最大值
+            last = -m  # 保证第一个数做差不影响结果
             # 我们通过差分数组求得l到r之间有哪些数
-            for i in range(1, 101):
+            for i in range(1, m + 1):
                 if diff[r + 1][i] - diff[l][i] > 0:
                     res = min(res, i - last)
                     last = i
-            ans.append(res if res < 100 else -1)
+            ans.append(res if res < m else -1)
         return ans
