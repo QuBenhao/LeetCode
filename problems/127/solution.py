0e44ff78976d33a68235940b7c729e8f811801d5
Benhao Qu <s3773865@student.rmit.edu.au>
2021-06-27 14:36:28 +0800

    127 更正A*要统计如果有更小的方式到达一个地方要重新入队

--- problems/127/solution.py
+++ problems/127/solution.py
@@ -56,39 +56,22 @@ class Solution(solution.Solution):
                         ts.__add__((ts.explored[s] + 1, t))
         return 0
 
-        # class PriorityQueue:
-        #     def __init__(self):
-        #         self.queue = []
-        #         self.explored = dict()
-        #
-        #     def __contains__(self, item):
-        #         return item in self.explored
-        # 
-        #     def __add__(self, other):
-        #         heapq.heappush(self.queue, other)
-        #         self.explored[other[2]] = other[0]
-        #
-        #     def __len__(self):
-        #         return len(self.queue)
-        #
-        #     def pop(self):
-        #         return heapq.heappop(self.queue)
-        #
         # def heuristic(curr, target):
         #     return sum(1 for a, b in zip(curr, target) if a != b)
         #
         # wordList = set(wordList)
         # if endWord not in wordList:
         #     return 0
-        # front = PriorityQueue()
-        # front.__add__((1, 0, beginWord))
-        # while front:
-        #     l, _, s = front.pop()
+        # pq = [(0, 0, beginWord)]
+        # costs = {beginWord:0}
+        # while pq:
+        #     _, g, s = heapq.heappop(pq)
         #     if s == endWord:
-        #         return l
+        #         return g + 1
         #     for i in range(len(s)):
         #         for c in string.ascii_lowercase:
         #             t = s[:i] + c + s[i + 1:]
-        #             if t in wordList and t not in front:
-        #                 front.__add__((l + 1, heuristic(t, endWord), t))
+        #             if t in wordList and (t not in costs or costs[t] > g + 1):
+        #                 costs[t] = g + 1
+        #                 heapq.heappush(pq, (heuristic(t, endWord) + g + 1, g + 1, t))
         # return 0
