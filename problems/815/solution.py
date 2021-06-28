05af1b28d3f055d31de9c367f5a22899f5f3c3d7
Benhao Qu <s3773865@student.rmit.edu.au>
2021-06-28 10:33:55 +0800

    815 BFS

--- problems/815/solution.py
+++ problems/815/solution.py
@@ -14,22 +14,28 @@ class Solution(solution.Solution):
         :type target: int
         :rtype: int
         """
+        # 每个车站可以乘坐的公交车
         stations = defaultdict(set)
         for i, stops in enumerate(routes):
             for stop in stops:
                 stations[stop].add(i)
+        # 每个公交车线路可以到达的车站
         routes = [set(x) for x in routes]
 
         q = deque([(source, 0)])
+        # 已经乘坐了的公交车
         buses = set()
+        # 已经到达了的车站
         stops = {source}
         while q:
             pos, cost = q.popleft()
             if pos == target:
                 return cost
+            # 当前车站中尚未乘坐的公交车
             for bus in stations[pos] - buses:
-                for stop in routes[bus] - stops:
+                # 该公交车尚未到达过的车站
+                for s in routes[bus] - stops:
                     buses.add(bus)
-                    stops.add(stop)
-                    q.append((stop, cost + 1))
+                    stops.add(s)
+                    q.append((s, cost + 1))
         return -1
