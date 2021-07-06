d5ab9ec3efbecca012ccfcc94bf9b11bcb7db62a
Benhao Qu <s3773865@student.rmit.edu.au>
2021-07-06 13:45:03 +0800

    1418

--- problems/1418/solution.py
+++ problems/1418/solution.py
@@ -20,22 +20,29 @@ class Solution(solution.Solution):
         # return [["Table"] + [food for food in foods]] + [[table] + [str(tables[table][food]) for food in foods] for
         #                                                  table in sorted(tables.keys(), key=int)]
 
-        # 转换orders为，所有订单里的全部桌和全部食物
-        obj = list(zip(*orders))
-        # 排序好的tables，每个table对应结果数组中的第几行
-        tables = SortedDict({v:i for i,v in enumerate(sorted(map(int,set(obj[1]))), 1)})
-        # 排序好的foods，每个food对应结果数组中的第几列
-        foods = SortedDict({v:i for i,v in enumerate(sorted(set(obj[2])), 1)})
-        res = [[0] * (len(foods) + 1) for _ in range(len(tables) + 1)]
-        res[0][0] = "Table"
-        # 结果数组的第一行
-        for i, food in enumerate(foods.keys(),1):
-            res[0][i] = food
-        # 统计每桌每个食物的数量
+        # # 转换orders为，所有订单里的全部桌和全部食物
+        # obj = list(zip(*orders))
+        # # 排序好的tables，每个table对应结果数组中的第几行
+        # tables = SortedDict({v:i for i,v in enumerate(sorted(map(int,set(obj[1]))), 1)})
+        # # 排序好的foods，每个food对应结果数组中的第几列
+        # foods = SortedDict({v:i for i,v in enumerate(sorted(set(obj[2])), 1)})
+        # # 结果数组的第一行
+        # res = [["Table"] + [food for food in foods]] + [[str(key)] + ["0"] * len(foods) for key in tables]
+        # # 统计每桌每个食物的数量
+        # for _, table, food in orders:
+        #     res[tables[int(table)]][foods[food]] = str(int(res[tables[int(table)]][foods[food]]) + 1)
+        # return res
+
+        tables, foods = set(), set()
         for _, table, food in orders:
-            res[tables[int(table)]][foods[food]] += 1
-        # 每行加入桌号，变成str即可
-        for i in range(1, len(res)):
-            res[i][0] = tables.keys()[i-1]
-            res[i] = list(map(str, res[i]))
+            tables.add(table)
+            foods.add(food)
+        foods = sorted(foods)
+        tables = sorted(tables, key=int)
+        res = [["Table"] + foods] + [[tables[i]] + ["0"] * len(foods) for i in range(len(tables))]
+        tables = {v:i for i,v in enumerate(tables, 1)}
+        foods = {v:i for i,v in enumerate(foods, 1)}
+
+        for _, table, food in orders:
+            res[tables[table]][foods[food]] = str(int(res[tables[table]][foods[food]]) + 1)
         return res
