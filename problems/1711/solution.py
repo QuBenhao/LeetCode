d31be5a6e1b12ab183c2885ff172b920eef1de26
Benhao Qu <s3773865@student.rmit.edu.au>
2021-07-07 08:33:12 +0800

    1711 遍历22个两数之和

--- problems/1711/solution.py
+++ problems/1711/solution.py
@@ -1,7 +1,11 @@
 import solution
+from collections import Counter
 
 
 class Solution(solution.Solution):
+    powersOfTwo = [2 ** i for i in range(22)]
+    mod = 10 ** 9 + 7
+
     def solve(self, test_input=None):
         return self.countPairs(list(test_input))
 
@@ -10,13 +14,7 @@ class Solution(solution.Solution):
         :type deliciousness: List[int]
         :rtype: int
         """
-        import collections
-        mod = 10 ** 9 + 7
-        ans = 0
-        freq = collections.defaultdict(int)
-        for x in deliciousness:
-            # the max sum is 2^20 + 2^20 = 2^21
-            for k in range(22):
-                ans += freq[2 ** k - x]
-            freq[x] += 1
-        return ans % mod
+        cnts = Counter(deliciousness)
+        return (sum(
+            cnts[key] * (cnts[key] - 1) if key == target - key else cnts[key] * cnts[target - key]
+            for key in cnts for target in self.powersOfTwo)) // 2 % self.mod
