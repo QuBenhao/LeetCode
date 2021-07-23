4078d9a264d94fa4b9d3bd3c2950ef329c8d31e8
Benhao Qu <qubenhao2@gmail.com>
2021-07-23 09:52:33 +0800

    1893 差分数组

--- problems/1893/solution.py
+++ problems/1893/solution.py
@@ -13,4 +13,15 @@ class Solution(solution.Solution):
         :type right: int
         :rtype: bool
         """
-        return all(any(l <= i <= r for l, r in ranges) for i in range(left, right + 1))
+        # return all(any(l <= i <= r for l, r in ranges) for i in range(left, right + 1))
+        
+        diff = [0] * 52
+        for l, r in ranges:
+            diff[l] += 1
+            diff[r+1] -= 1
+        curr = 0
+        for i in range(1, right + 1):
+            curr += diff[i]
+            if curr <= 0 and left <= i:
+                return False
+        return True
