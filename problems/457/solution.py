3e23946f8ce7c39ed0b91b09714cf3405531cf04
Benhao Qu <qubenhao2@gmail.com>
2021-08-07 19:40:38 +0800

    457 原数组里标记

--- problems/457/solution.py
+++ problems/457/solution.py
@@ -14,23 +14,34 @@ class Solution(solution.Solution):
         """
         n = len(nums)
         for i in range(n):
+            # 已经被检查过了
             if nums[i] >= self.mark:
                 continue
+            # i为起点, 标记值，以及上一个的值
             cur, tag, last = i, self.mark + i, -1
+            # 循环要全正还是全负
             flag = nums[cur] > 0
             while True:
+                # 下一个点
                 nxt = (cur + nums[cur]) % n
+                # 当前的值
                 last = nums[cur]
+                # 修改数组里为标记过
                 nums[cur] = tag
+                # 移动到下一个点
                 cur = nxt
+                # 回到出发点了，可以停止
                 if cur == i:
                     break
+                # 回到标记过的点了，可以停止
                 if nums[cur] >= self.mark:
                     break
+                # 以下两种情况为正负相反出现了，不符合题目要求，可以停止
                 if flag and nums[cur] < 0:
                     break
                 if not flag and nums[cur] > 0:
                     break
+            # 如果最后出现的点不是指向他自己的，并且当前的值为当前标记值，我们才认定构成一个要的循环
             if last % n != 0 and nums[cur] == tag:
                 return True
         return False
