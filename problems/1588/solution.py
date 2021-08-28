7765faf71abbe510269526f00fbdab635b949f32
qubh <qubenhao2@gmail.com>
2021-08-29 07:15:28 +0800

    1588 组合 容斥原理

--- problems/1588/solution.py
+++ problems/1588/solution.py
@@ -10,18 +10,21 @@ class Solution(solution.Solution):
         :type arr: List[int]
         :rtype: int
         """
-
-        """
-        The frequency of current num is based on the frequency of last num 
-        except the subarray that contains the last element but not current element
-        and add the subarray that contains the current element but not the last element.
-        
-        The frequency of the first element is how many odds number from 1 to the length of array.
-        """
-        res = 0
-        freq = 0
+        # 长度为n的数组，第一位出现在多少个奇数长度的子数组？
+        # 1, 3, 5, ..., length-1/length
+        # (length + 1)//2
         n = len(arr)
-        for i in range(n):
-            freq = freq - (i + 1) // 2 + (n - i + 1) // 2
-            res += freq * arr[i]
-        return res
+        l, r, ans, times = 0, n - 1, 0, (n + 1) // 2
+        while l <= r:
+            # 对称性，前后对称位置出现的次数一样
+            if l < r:
+                ans += times * (arr[l] + arr[r])
+            else:
+                ans += times * arr[l]
+            l += 1
+            r -= 1
+            # 下一个数比前一个数多了后一个数构成的不带前一个数的奇数子数组的个数
+            times += (n - l + 1) // 2
+            # 下一个数比前一个数少了前一个数构成的不带后一个数的奇数子数组的个数
+            times -= (l + 1) // 2
+        return ans
