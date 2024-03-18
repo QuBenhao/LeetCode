b7b1c1775862cc8b7eb4bb12584088e6e1c52d19
benhao <qubenhao2@gmail.com>
2024-03-18 21:12:15 +0800

    fix <frozen importlib._bootstrap>:530: DeprecationWarning: the load_module() method is deprecated and slated for removal in Python 3.12; use exec_module() instead

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os.path
 import sys
 import unittest
-from importlib.machinery import SourceFileLoader
+from importlib.util import spec_from_file_location, module_from_spec
 
 # Question ID that wants to test, modify here as passing arguments
 QUESTION = "303"
@@ -14,13 +14,21 @@ class Test(unittest.TestCase):
     def test(self):
         self.assertTrue(os.path.exists(f"problems/{QUESTION}"), msg="Please set up the problem env first!")
 
-        solution_obj = SourceFileLoader("module.name", f"./problems/{QUESTION}/solution.py").load_module().Solution()
-        testcase_obj = SourceFileLoader("module.name", f"./problems/{QUESTION}/testcase.py").load_module().Testcase()
-        for testcase in testcase_obj.get_testcases():
-            i, o = testcase
+        solution_spec = spec_from_file_location("module.name", f"./problems/{QUESTION}/solution.py")
+        solution = module_from_spec(solution_spec)
+        solution_spec.loader.exec_module(solution)
+        solution_obj = solution.Solution()
+
+        testcase_spec = spec_from_file_location("module.name", f"./problems/{QUESTION}/testcase.py")
+        testcase = module_from_spec(testcase_spec)
+        testcase_spec.loader.exec_module(testcase)
+        testcase_obj = testcase.Testcase()
+
+        for test in testcase_obj.get_testcases():
+            i, o = test
             result = solution_obj.solve(test_input=i)
             try:
-                if o and type(o) == list and (None not in o and (type(o[0]) == list and not any(None in x for x in o))):
+                if o and isinstance(o, list) and (None not in o and (isinstance(o[0], list) and not any(None in x for x in o))):
                     self.assertListEqual(sorted(o), sorted(result), msg=f"input = {i}")
                 else:
                     self.assertEqual(o, result, msg=f"input = {i}")
