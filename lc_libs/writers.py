0e5879b9688441d8139b7719ef54a9b9220a7b30
Benhao <qubenhao2@gmail.com>
2024-03-19 17:18:51 +0800

    Improve solution writer to handle class definitions in code
    The modified write_solution function now correctly processes the code that includes "class Solution" or commented-out class definitions. This update ensures that all lines of code are correctly formatted and appropriately included in the final code block, improving the accuracy of the code parsing and retrieval process.

--- lc_libs/writers.py
+++ lc_libs/writers.py
@@ -18,9 +18,44 @@ def write_testcase(testcases, outputs) -> str:
 
 
 def write_solution(code: str) -> str:
+    if "class Solution" or "# class" in code:
+        start = False
+        strip_start = False
+        strip_code = []
+        define_class = []
+        for line in code.split("\n"):
+            if line.startswith("# class"):
+                start = True
+            if line.startswith("#"):
+                if start:
+                    define_class.append(line[2:])
+                else:
+                    if not strip_start:
+                        define_class.append(line)
+                    else:
+                        strip_code.append(line)
+                strip_start = False
+            else:
+                if strip_start:
+                    strip_code.append(line)
+                if line.startswith("class Solution"):
+                    strip_start = True
+                start = False
+        return ("import solution\n"
+                "from typing import *\n\n\n"
+                "{}"
+                "class Solution(solution.Solution):\n"
+                "    def solve(self, test_input=None):\n"
+                "        pass\n\n\n"
+                "{}").format("\n".join(define_class) + "\n\n\n" if define_class else "", "\n".join(strip_code))
     return ("import solution\n"
             "from typing import *\n\n\n"
             "class Solution(solution.Solution):\n"
             "    def solve(self, test_input=None):\n"
             "        pass\n\n\n"
             "{}\n").format(code)
+
+
+if __name__ == '__main__':
+    print(write_solution(
+        "# Definition for a binary tree node.\n# class TreeNode:\n#     def __init__(self, val=0, left=None, right=None):\n#         self.val = val\n#         self.left = left\n#         self.right = right\nclass Solution:\n    def isSymmetric(self, root: Optional[TreeNode]) -> bool:"))
