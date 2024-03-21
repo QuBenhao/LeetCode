27f416aa2f39a6596d56019276ba737cc147ae07
Benhao <qubenhao2@gmail.com>
2024-03-21 09:50:51 +0800

    fix 2671 and 133 solution template

--- problems/133/solution.py
+++ problems/133/solution.py
@@ -3,6 +3,10 @@ from typing import *
 
 
 # Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
 
 
 class Solution(solution.Solution):
--- problems/2671/solution.py
+++ problems/2671/solution.py
@@ -2,14 +2,28 @@ import solution
 from typing import *
 
 
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        pass
+
+
+class FrequencyTracker:
+
+    def __init__(self):
+
+
+    def add(self, number: int) -> None:
+
+
+    def deleteOne(self, number: int) -> None:
+
+
+    def hasFrequency(self, frequency: int) -> bool:
+
+
+
 # Your FrequencyTracker object will be instantiated and called as such:
 # obj = FrequencyTracker()
 # obj.add(number)
 # obj.deleteOne(number)
 # param_3 = obj.hasFrequency(frequency)
-
-
-class Solution(solution.Solution):
-    def solve(self, test_input=None):
-        pass
-
