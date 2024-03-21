db8f767fc35d45236fb9e9e5cdd4e38dec69e7a0
Benhao <qubenhao2@gmail.com>
2024-03-21 12:32:16 +0800

    [20240321] solve study plan 201

--- problems/201/solution.py
+++ problems/201/solution.py
@@ -4,6 +4,15 @@ from typing import *
 
 class Solution(solution.Solution):
     def solve(self, test_input=None):
-        pass
+        return self.rangeBitwiseAnd(*test_input)
 
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        ans = 0
+        for i in range(31, -1, -1):
+            cur = 1 << i
+            l, r = left & cur, right & cur
+            if r > 0 and l == 0:
+                return ans
+            if r > 0 and l > 0:
+                ans |= cur
+        return ans
