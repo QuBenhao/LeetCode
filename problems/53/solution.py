5f23fba049db42cc0671864da92421cfa2c01b63
benhao <qubenhao2@gmail.com>
2024-03-21 11:19:20 +0800

    div_and_con solution for 53

--- problems/53/solution.py
+++ problems/53/solution.py
@@ -10,15 +10,34 @@ class Solution(solution.Solution):
         :type nums: List[int]
         :rtype: int
         """
+        from math import inf
+
+        def div_and_con(left, right):
+            if left == right:
+                return nums[left]
+            mid = (left + right) // 2
+            left_div = div_and_con(left, mid)
+            right_div = div_and_con(mid + 1, right)
+            l_max, ls = -inf, 0
+            for i in range(mid, left - 1, -1):
+                ls += nums[i]
+                l_max = max(l_max, ls)
+            r_max, rs = -inf, 0
+            for i in range(mid + 1, right + 1):
+                rs += nums[i]
+                r_max = max(r_max, rs)
+            return max(left_div, right_div, l_max + r_max)
+
+        return div_and_con(0, len(nums) - 1)
 
         # # dynamic programming
-        n = len(nums)
-
-        dp = [nums[i] for i in range(n)]
-
-        for i in range(1, n):
-            dp[i] = max(dp[i - 1] + nums[i], dp[i])
-        return max(dp)
+        # n = len(nums)
+        #
+        # dp = [nums[i] for i in range(n)]
+        #
+        # for i in range(1, n):
+        #     dp[i] = max(dp[i - 1] + nums[i], dp[i])
+        # return max(dp)
 
         # # divide and conquer
         # def div_con_sum(left, right):
