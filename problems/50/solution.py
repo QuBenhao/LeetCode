abe373566171f1deca6bab974a52a03db3be5094
Benhao <qubenhao2@gmail.com>
2024-03-22 15:51:49 +0800

    [20240322] solve study plan 50 矩阵快速幂

--- problems/50/solution.py
+++ problems/50/solution.py
@@ -4,6 +4,18 @@ from typing import *
 
 class Solution(solution.Solution):
     def solve(self, test_input=None):
-        pass
+        return self.myPow(*test_input)
 
     def myPow(self, x: float, n: int) -> float:
+        # 矩阵快速幂
+        if x == 0.0:
+            return 0.0
+        if n < 0:
+            x, n = 1 / x, -n
+        ans = 1.0
+        while n:
+            if n & 1:
+                ans *= x
+            x *= x
+            n >>= 1
+        return ans
