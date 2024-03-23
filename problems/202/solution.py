9a3ef5681c9354b427661081ef1e3e89518a68ec
benhao <qubenhao2@gmail.com>
2024-03-23 11:54:52 +0800

    [20240323] solve study plan 202

--- problems/202/solution.py
+++ problems/202/solution.py
@@ -1,9 +1,37 @@
 import solution
 from typing import *
+from functools import lru_cache
 
 
 class Solution(solution.Solution):
     def solve(self, test_input=None):
-        pass
+        return self.isHappy(test_input)
 
     def isHappy(self, n: int) -> bool:
+        @lru_cache(None)
+        def helper(x: int) -> int:
+            res = 0
+            while x:
+                x, y = divmod(x, 10)
+                res += y * y
+            return res
+
+        fast = slow = n
+        while fast > 1:
+            fast = helper(helper(fast))
+            slow = helper(slow)
+            if fast > 1 and fast == slow:
+                return False
+        return True
+
+        # explored = {n}
+        # while n > 1:
+        #     nxt = 0
+        #     while n:
+        #         nxt += (n % 10) ** 2
+        #         n //= 10
+        #     if nxt in explored:
+        #         return False
+        #     explored.add(nxt)
+        #     n = nxt
+        # return True
--- problems/202/testcase.py
+++ problems/202/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=19, Output=True))
 		self.testcases.append(case(Input=2, Output=False))
+		self.testcases.append(case(Input=10, Output=True))
 
 	def get_testcases(self):
 		return self.testcases
