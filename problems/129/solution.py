235264b3242cf5eab00bfc1193300320377674a0
benhao <qubenhao2@gmail.com>
2024-03-23 12:14:35 +0800

    [20240323] solve study plan 129

--- problems/129/solution.py
+++ problems/129/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+from object_libs import list_to_tree
 
 
 # Definition for a binary tree node.
@@ -12,6 +13,22 @@ class TreeNode:
 
 class Solution(solution.Solution):
     def solve(self, test_input=None):
-        pass
+        root = list_to_tree(test_input)
+        return self.sumNumbers(root)
 
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        def dfs(node, v):
+            nv = 10 * v + node.val
+            if not node.left and not node.right:
+                return nv
+            ans = 0
+            if node.left:
+                ans += dfs(node.left, nv)
+            if node.right:
+                ans += dfs(node.right, nv)
+            return ans
+
+        return dfs(root, 0)
