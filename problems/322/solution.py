c3599a850698bfd607e3b040d4ba2d4330449379
benhao <qubenhao2@gmail.com>
2024-03-24 14:02:42 +0800

    [20240324] solve 322, 二进制BFS or 记忆化搜索

--- problems/322/solution.py
+++ problems/322/solution.py
@@ -1,9 +1,37 @@
 import solution
 from typing import *
+from functools import lru_cache
+from math import inf
 
 
 class Solution(solution.Solution):
     def solve(self, test_input=None):
-        pass
+        return self.coinChange(*test_input)
 
     def coinChange(self, coins: List[int], amount: int) -> int:
+        # @lru_cache(None)
+        # def dfs(remain):
+        #     return min(dfs(remain - c) for c in coins) + 1 if remain > 0 else (0 if not remain else inf)
+        #
+        # return -1 if (ans := dfs(amount)) == inf else ans
+
+        """
+        二进制转换，$a+b+c=x$求解可以转化为$2^a*2^b*2^c=2^x$，也就是$1 << x = 1 << a << b << c$
+        题目可以转化为：
+            我们的取硬币操作可以理解为右移操作，
+            取的总数变为$1<<amount$，
+            我们最少需要右移多少次直到最小位出现1，
+            也就是我们找到了取$1<<0$的路径了。这个时候其他位是不是1不影响，只要有一个路径出现就代表找到了。
+        """
+        if not amount:
+            return 0
+        step, dp = 0, 1 << amount
+        while dp:
+            nxt = 0
+            step += 1
+            for c in coins:
+                nxt |= dp >> c
+            if nxt & 1:
+                return step
+            dp = nxt
+        return -1
--- problems/322/testcase.py
+++ problems/322/testcase.py
@@ -5,11 +5,12 @@ case = namedtuple("Testcase", ["Input", "Output"])
 
 
 class Testcase(testcase.Testcase):
-	def __init__(self):
-		self.testcases = []
-		self.testcases.append(case(Input=[[1, 2, 5], 11], Output=3))
-		self.testcases.append(case(Input=[[2], 3], Output=-1))
-		self.testcases.append(case(Input=[[1], 0], Output=0))
+    def __init__(self):
+        self.testcases = []
+        self.testcases.append(case(Input=[[1, 2, 5], 11], Output=3))
+        self.testcases.append(case(Input=[[2], 3], Output=-1))
+        self.testcases.append(case(Input=[[1], 0], Output=0))
+        self.testcases.append(case(Input=[[186, 419, 83, 408], 6249], Output=20))
 
-	def get_testcases(self):
-		return self.testcases
+    def get_testcases(self):
+        return self.testcases
