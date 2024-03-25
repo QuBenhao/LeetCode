2ec00126a0c7bec4fcbddf3870441b52af360eec
benhao <qubenhao2@gmail.com>
2024-03-25 12:17:11 +0800

    [2024.03.25] solve study plan 222

--- problems/222/solution.py
+++ problems/222/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+from object_libs import list_to_tree
 
 
 # Definition for a binary tree node.
@@ -12,6 +13,25 @@ class TreeNode:
 
 class Solution(solution.Solution):
     def solve(self, test_input=None):
-        pass
+        return self.countNodes(list_to_tree(test_input))
 
     def countNodes(self, root: Optional[TreeNode]) -> int:
+        """
+        满二叉树的深度由它一直往左递归构成。
+        如果左右子树深度不一致，说明左子树是不满的，右子树是满的，我们递归统计左子树，加上当前右子树的满二叉树数目 + 根节点 (1 << right)
+        如果左右子树深度一致，说明左子树是满的，右子树是不满的，我们递归统计右子树，加上当前左子树的满二叉树数目 + 根节点 (1 << left)
+        """
+        def depth(node):
+            h = 0
+            while node:
+                node = node.left
+                h += 1
+            return h
+
+        if not root:
+            return 0
+        left, right = depth(root.left), depth(root.right)
+        if left == right:
+            return self.countNodes(root.right) + (1 << left)
+        else:
+            return self.countNodes(root.left) + (1 << right)
--- problems/222/testcase.py
+++ problems/222/testcase.py
@@ -5,11 +5,12 @@ case = namedtuple("Testcase", ["Input", "Output"])
 
 
 class Testcase(testcase.Testcase):
-	def __init__(self):
-		self.testcases = []
-		self.testcases.append(case(Input=[1, 2, 3, 4, 5, 6], Output=6))
-		self.testcases.append(case(Input=[], Output=0))
-		self.testcases.append(case(Input=[1], Output=1))
+    def __init__(self):
+        self.testcases = []
+        self.testcases.append(case(Input=[1, 2, 3, 4, 5, 6], Output=6))
+        self.testcases.append(case(Input=[], Output=0))
+        self.testcases.append(case(Input=[1], Output=1))
+        self.testcases.append(case(Input=[1, 2, 3, 4], Output=4))
 
-	def get_testcases(self):
-		return self.testcases
+    def get_testcases(self):
+        return self.testcases
