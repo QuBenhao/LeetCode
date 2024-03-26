cd3389a629ecaf1aa002d51852d8cd91ff4748a5
benhao <qubenhao2@gmail.com>
2024-03-26 15:26:54 +0800

    [20240326] solve 2642

--- problems/2642/solution.py
+++ problems/2642/solution.py
@@ -1,6 +1,9 @@
 import solution
 from typing import *
 from object_libs import call_method
+import heapq
+from collections import defaultdict
+from math import inf
 
 
 class Solution(solution.Solution):
@@ -9,19 +12,29 @@ class Solution(solution.Solution):
         obj = Graph(*inputs[0])
         return [None] + [call_method(obj, op, *ipt) for op, ipt in zip(ops[1:], inputs[1:])]
 
+
 class Graph:
-
     def __init__(self, n: int, edges: List[List[int]]):
-            pass
-
+        self.graph = defaultdict(list)
+        for a, b, cost in edges:
+            self.graph[a].append((b, cost))
 
     def addEdge(self, edge: List[int]) -> None:
-            pass
-
+        self.graph[edge[0]].append((edge[1], edge[2]))
 
     def shortestPath(self, node1: int, node2: int) -> int:
-            pass
-
+        dist = defaultdict(lambda: defaultdict(lambda: inf))
+        pq = [(0, node1)]
+        while pq:
+            print(pq)
+            cur, node = heapq.heappop(pq)
+            if node == node2:
+                return cur
+            for neighbor, cost in self.graph[node]:
+                if (nxt := cur + cost) < dist[node][neighbor]:
+                    heapq.heappush(pq, (nxt, neighbor))
+                    dist[node][neighbor] = nxt
+        return -1
 
 
 # Your Graph object will be instantiated and called as such:
