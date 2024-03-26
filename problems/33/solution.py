96343663345c3b2b571baced470c6924ef6d6660
benhao <qubenhao2@gmail.com>
2024-03-26 17:33:12 +0800

    [20240326] solve study plan 33

--- problems/33/solution.py
+++ problems/33/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+from bisect import bisect_left
 
 
 class Solution(solution.Solution):
@@ -7,4 +8,36 @@ class Solution(solution.Solution):
         return self.search(*test_input)
 
     def search(self, nums: List[int], target: int) -> int:
-            pass
+        n = len(nums)
+        idx = bisect_left(nums, True, key=lambda x: x < nums[0])
+        left, right = idx, n + idx - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid % n] < target:
+                left = mid + 1
+            else:
+                right = mid
+        return left % n if nums[left % n] == target else -1
+
+        # if target == nums[0]:
+        #     return 0
+        # left, right = 0, len(nums)
+        # while left < right:
+        #     mid = (left + right) // 2
+        #     if nums[mid] < nums[0]:
+        #         right = mid
+        #     else:
+        #         left = mid + 1
+        #
+        # idx = left
+        # if target > nums[0]:
+        #     left, right = 0, idx
+        # else:
+        #     left, right = idx, len(nums)
+        # while left < right:
+        #     mid = (left + right) // 2
+        #     if nums[mid] < target:
+        #         left = mid + 1
+        #     else:
+        #         right = mid
+        # return left if 0 <= left < len(nums) and nums[left] == target else -1
--- problems/33/testcase.py
+++ problems/33/testcase.py
@@ -5,11 +5,16 @@ case = namedtuple("Testcase", ["Input", "Output"])
 
 
 class Testcase(testcase.Testcase):
-	def __init__(self):
-		self.testcases = []
-		self.testcases.append(case(Input=[[4, 5, 6, 7, 0, 1, 2], 0], Output=4))
-		self.testcases.append(case(Input=[[4, 5, 6, 7, 0, 1, 2], 3], Output=-1))
-		self.testcases.append(case(Input=[[1], 0], Output=-1))
+    def __init__(self):
+        self.testcases = []
+        self.testcases.append(case(Input=[[4, 5, 6, 7, 0, 1, 2], 0], Output=4))
+        self.testcases.append(case(Input=[[4, 5, 6, 7, 0, 1, 2], 3], Output=-1))
+        self.testcases.append(case(Input=[[1], 0], Output=-1))
+        self.testcases.append(case(Input=[[1, 3], 3], Output=1))
+        self.testcases.append(case(Input=[[1, 3], 0], Output=-1))
+        self.testcases.append(case(Input=[[3, 1], 1], Output=1))
+        self.testcases.append(case(Input=[[3, 1], 3], Output=0))
+        self.testcases.append(case(Input=[[5, 1, 3], 3], Output=2))
 
-	def get_testcases(self):
-		return self.testcases
+    def get_testcases(self):
+        return self.testcases
