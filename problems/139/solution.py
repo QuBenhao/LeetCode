b9c9d18ab1965a4ff695f98511a9d28e98873271
benhao <qubenhao2@gmail.com>
2024-03-26 15:48:14 +0800

    [20240326] solve study plan 139

--- problems/139/solution.py
+++ problems/139/solution.py
@@ -7,4 +7,15 @@ class Solution(solution.Solution):
         return self.wordBreak(*test_input)
 
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-            pass
+        n, words, ml = len(s), set(wordDict), max(len(word) for word in wordDict)
+        cur = {0}
+        for i in range(1, n + 1):
+            tmp = set(cur)
+            for idx in tmp:
+                if i - idx > ml:
+                    cur.remove(idx)
+                    continue
+                if idx < i and s[idx:i] in words:
+                    cur.add(i)
+                    break
+        return n in cur
