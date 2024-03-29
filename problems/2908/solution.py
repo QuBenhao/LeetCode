c1225133ad113be4dd6e6db8c0dbe04ef85385a8
benhao <qubenhao2@gmail.com>
2024-03-29 11:08:19 +0800

    [20240329] solve 2908

--- problems/2908/solution.py
+++ problems/2908/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+from math import inf
 
 
 class Solution(solution.Solution):
@@ -7,4 +8,13 @@ class Solution(solution.Solution):
         return self.minimumSum(test_input)
 
     def minimumSum(self, nums: List[int]) -> int:
-            pass
+        n = len(nums)
+        left_min, right_min = [inf] * n, [inf] * n
+        for i in range(n):
+            left_min[i] = min(left_min[i - 1], nums[i])
+            right_min[n - 1 - i] = min(right_min[min(n - i, n - 1)], nums[n - 1 - i])
+        ans = inf
+        for i in range(1, n - 1):
+            if nums[i] > left_min[i - 1] and nums[i] > right_min[i + 1]:
+                ans = min(ans, left_min[i - 1] + right_min[i + 1] + nums[i])
+        return ans if ans != inf else -1
