ce4e23654406a7f919deceed5353d66f5c35ac48
benhao <qubenhao2@gmail.com>
2024-03-30 19:08:20 +0800

    [20240330] solve study plan 373

--- problems/373/solution.py
+++ problems/373/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+import heapq
 
 
 class Solution(solution.Solution):
@@ -7,4 +8,13 @@ class Solution(solution.Solution):
         return self.kSmallestPairs(*test_input)
 
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-                pass
+        pq, ans = [(nums1[0] + nums2[0], 0, 0)], []
+        for _ in range(k):
+            _, idx1, idx2 = heapq.heappop(pq)
+            # 避免出现重复入堆
+            if idx2 == 0 and idx1 < len(nums1) - 1:
+                heapq.heappush(pq, (nums1[idx1 + 1] + nums2[idx2], idx1 + 1, idx2))
+            if idx2 < len(nums2) - 1:
+                heapq.heappush(pq, (nums1[idx1] + nums2[idx2 + 1], idx1, idx2 + 1))
+            ans.append([nums1[idx1], nums2[idx2]])
+        return ans
--- problems/373/testcase.py
+++ problems/373/testcase.py
@@ -5,10 +5,15 @@ case = namedtuple("Testcase", ["Input", "Output"])
 
 
 class Testcase(testcase.Testcase):
-	def __init__(self):
-		self.testcases = []
-		self.testcases.append(case(Input=[[1, 7, 11], [2, 4, 6], 3], Output=[[1, 2], [1, 4], [1, 6]]))
-		self.testcases.append(case(Input=[[1, 1, 2], [1, 2, 3], 2], Output=[[1, 1], [1, 1]]))
+    def __init__(self):
+        self.testcases = []
+        self.testcases.append(case(Input=[[1, 7, 11], [2, 4, 6], 3], Output=[[1, 2], [1, 4], [1, 6]]))
+        self.testcases.append(case(Input=[[1, 1, 2], [1, 2, 3], 2], Output=[[1, 1], [1, 1]]))
+        self.testcases.append(case(Input=[[1, 2, 4, 5, 6], [3, 5, 7, 9], 3], Output=[[1, 3], [2, 3], [1, 5]]))
+        self.testcases.append(case(Input=[[1, 2, 4, 5, 6], [3, 5, 7, 9], 20],
+                                   Output=[[1, 3], [2, 3], [1, 5], [2, 5], [4, 3], [1, 7], [5, 3], [2, 7], [4, 5],
+                                           [6, 3], [1, 9], [5, 5], [2, 9], [4, 7], [6, 5], [5, 7], [4, 9], [6, 7],
+                                           [5, 9], [6, 9]]))
 
-	def get_testcases(self):
-		return self.testcases
+    def get_testcases(self):
+        return self.testcases
