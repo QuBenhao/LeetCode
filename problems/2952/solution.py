3a87402b61aab7b986c6958f322e70a9bb8c38dd
GitHub Action <action@github.com>
2024-03-30 14:50:50 +0000

    [20240330] Add daily problem solution

--- problems/2952/solution.py
+++ problems/2952/solution.py
@@ -7,4 +7,13 @@ class Solution(solution.Solution):
         return self.minimumAddedCoins(*test_input)
 
     def minimumAddedCoins(self, coins: List[int], target: int) -> int:
-            pass
+        coins.sort()
+        ans, s, i = 0, 1, 0
+        while s <= target:
+            if i < len(coins) and coins[i] <= s:
+                s += coins[i]
+                i += 1
+            else:
+                s *= 2  # 必须添加 s
+                ans += 1
+        return ans
