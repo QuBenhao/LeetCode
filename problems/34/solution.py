3de5eaf5c2d7a0b5f1e67209b52c60a605ff140b
benhao <qubenhao2@gmail.com>
2024-04-01 10:19:40 +0800

    [20240401] solve study plan 34

--- problems/34/solution.py
+++ problems/34/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+import bisect
 
 
 class Solution(solution.Solution):
@@ -7,4 +8,5 @@ class Solution(solution.Solution):
         return self.searchRange(*test_input)
 
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-            pass
+        left, right = bisect.bisect_left(nums, target), bisect.bisect_right(nums, target)
+        return [left, right - 1] if left < len(nums) and nums[left] == target else [-1, -1]
