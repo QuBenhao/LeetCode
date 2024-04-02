e85dc4285b2b8c7af8882e818a2f5bd1a841232f
benhao <qubenhao2@gmail.com>
2024-04-02 20:48:05 +0800

    [20240402] solve study plan 150, using stack and operator handling for div

--- problems/150/solution.py
+++ problems/150/solution.py
@@ -1,50 +1,26 @@
 import solution
+from typing import *
+import operator
+
+
+op_dict = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.truediv
+}
 
 
 class Solution(solution.Solution):
     def solve(self, test_input=None):
-        return self.evalRPN(list(test_input))
+        return self.evalRPN(test_input)
 
-    def evalRPN(self, tokens):
-        """
-        :type tokens: List[str]
-        :rtype: int
-        """
-
+    def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        for c in tokens:
-            if c not in "+-*/":
-                stack.append(int(c))
+        for s in tokens:
+            if s in op_dict:
+                b, a = stack.pop(), stack.pop()
+                stack.append(int(op_dict[s](a, b)))
             else:
-                num2 = stack.pop()
-                num1 = stack.pop()
-                if c == '+':
-                    stack.append(num1 + num2)
-                elif c == '-':
-                    stack.append(num1 - num2)
-                elif c == '*':
-                    stack.append(num1 * num2)
-                else:
-                    stack.append(int(float(num1) / num2))
-        return stack[0]
-
-        # from operator import add, sub, mul
-        # op_to_binary_fn = {
-        #     "+": add,
-        #     "-": sub,
-        #     "*": mul,
-        #     "/": lambda x, y: int(float(x) / y),  # 需要注意 python 中负数除法的表现与题目不一致
-        # }
-        #
-        # stack = list()
-        # for token in tokens:
-        #     try:
-        #         num = int(token)
-        #     except ValueError:
-        #         num2 = stack.pop()
-        #         num1 = stack.pop()
-        #         num = op_to_binary_fn[token](num1, num2)
-        #     finally:
-        #         stack.append(num)
-        #
-        # return stack[0]
+                stack.append(int(s))
+        return stack[-1]
