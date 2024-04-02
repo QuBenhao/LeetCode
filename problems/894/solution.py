5ff7e67c847ce513c7ce1047fbdca9fbcf359639
benhao <qubenhao2@gmail.com>
2024-04-02 16:45:42 +0800

    [20240402] solve 894

--- problems/894/solution.py
+++ problems/894/solution.py
@@ -1,6 +1,9 @@
+import itertools
+
 import solution
 from typing import *
 from object_libs import tree_to_list
+
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -8,12 +11,22 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 # Definition for a binary tree node.
 class Solution(solution.Solution):
     def solve(self, test_input=None):
         n = test_input
         res = self.allPossibleFBT(n)
-        return tree_to_list(res)
+        return [tree_to_list(root) for root in res]
 
     def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
-            pass
+        ans = []
+        if n % 2 != 0:
+            if n == 1:
+                ans.append(TreeNode())
+            else:
+                for i in range(1, n, 2):
+                    a, b = self.allPossibleFBT(i), self.allPossibleFBT(n - 1 - i)
+                    for l, r in itertools.product(a, b):
+                        ans.append(TreeNode(left=l, right=r))
+        return ans
