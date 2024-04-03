1f714edc162e3d6cf24e09a1e840935387c9375e
benhao <qubenhao2@gmail.com>
2024-04-03 08:49:59 +0800

    [20240403] solve study plan 19

--- problems/19/solution.py
+++ problems/19/solution.py
@@ -2,12 +2,14 @@ import solution
 from typing import *
 from object_libs import list_to_linked_list, linked_list_to_list
 
+
+# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-# Definition for singly-linked list.
+
 class Solution(solution.Solution):
     def solve(self, test_input=None):
         nums0, n = test_input
