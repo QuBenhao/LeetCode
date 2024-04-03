92d4dc05f60b0fdf4b8e1426517a29e565f0c4e5
benhao <qubenhao2@gmail.com>
2024-04-03 08:55:18 +0800

    [20240403] solve study plan 106

--- problems/106/solution.py
+++ problems/106/solution.py
@@ -2,13 +2,15 @@ import solution
 from typing import *
 from object_libs import tree_to_list
 
+
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-# Definition for a binary tree node.
+
 class Solution(solution.Solution):
     def solve(self, test_input=None):
         inorder, postorder = test_input
@@ -16,4 +18,10 @@ class Solution(solution.Solution):
         return tree_to_list(res)
 
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-            pass
+        if not postorder:
+            return
+        root_val = postorder[-1]
+        idx = inorder.index(root_val)
+        return TreeNode(root_val,
+                        self.buildTree(inorder[:idx], postorder[:idx]),
+                        self.buildTree(inorder[idx+1:], postorder[idx:len(postorder) - 1]))
