57df43bd820b44b7a8bd31c2fb1d682d6e0b0f69
benhao <qubenhao2@gmail.com>
2024-04-06 09:14:38 +0800

    fix [20240405] 1026

--- problems/1026/solution.py
+++ problems/1026/solution.py
@@ -2,11 +2,13 @@ import solution
 from typing import *
 from object_libs import list_to_tree
 
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
 
 # Definition for a binary tree node.
 class Solution(solution.Solution):
@@ -16,4 +18,12 @@ class Solution(solution.Solution):
         return self.maxAncestorDiff(root0)
 
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
-            pass
+        def helper(node, mx, mn):
+            if not node:
+                return 0
+            cur = max(mx - node.val, node.val - mn)
+            mx, mn = max(node.val, mx), min(node.val, mn)
+            left, right = helper(node.left, mx, mn), helper(node.right, mx, mn)
+            return max(cur, left, right)
+
+        return helper(root, root.val, root.val)
