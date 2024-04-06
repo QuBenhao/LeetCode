e6a8bea0ce9266346c0278a47b09c7903c04904d
benhao <qubenhao2@gmail.com>
2024-04-06 09:27:28 +0800

    [20240406] 1483 倍增思想，二进制转换，处理父亲的父亲跳跃式降低复杂度

--- problems/1483/solution.py
+++ problems/1483/solution.py
@@ -9,16 +9,25 @@ class Solution(solution.Solution):
         obj = TreeAncestor(*inputs[0])
         return [None] + [call_method(obj, op, *ipt) for op, ipt in zip(ops[1:], inputs[1:])]
 
+
 class TreeAncestor:
 
     def __init__(self, n: int, parent: List[int]):
-            pass
-
+        m = n.bit_length() - 1
+        pa = [[p] + [-1] * m for p in parent]
+        for i in range(m):
+            for x in range(n):
+                if (p := pa[x][i]) != -1:
+                    pa[x][i + 1] = pa[p][i]
+        self.pa = pa
 
     def getKthAncestor(self, node: int, k: int) -> int:
-            pass
-
-
+        for i in range(k.bit_length()):
+            if (k >> i) & 1:
+                node = self.pa[node][i]
+                if node == -1:
+                    break
+        return node
 
 # Your TreeAncestor object will be instantiated and called as such:
 # obj = TreeAncestor(n, parent)
