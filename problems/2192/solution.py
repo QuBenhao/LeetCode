7441472e530f41e468d2ce1ba83d36085c2fc93d
benhao <qubenhao2@gmail.com>
2024-04-06 09:04:29 +0800

    fix [20240404] 2192

--- problems/2192/solution.py
+++ problems/2192/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+from collections import defaultdict, deque
 
 
 class Solution(solution.Solution):
@@ -7,4 +8,20 @@ class Solution(solution.Solution):
         return self.getAncestors(*test_input)
 
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
-            pass
+        graph, direct, ans = defaultdict(list), [0] * n, [set() for _ in range(n)]
+        for a, b in edges:
+            graph[a].append(b)
+            direct[b] += 1
+        q = deque([])
+        for i in range(n):
+            if direct[i] == 0:
+                q.append(i)
+        while q:
+            node = q.popleft()
+            for child in graph[node]:
+                ans[child].update(ans[node])
+                ans[child].add(node)
+                direct[child] -= 1
+                if direct[child] == 0:
+                    q.append(child)
+        return [list(sorted(s)) for s in ans]
