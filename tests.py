4279e0eebfa0aa13cbdc95c2a0b8c6c3bfabb8e0
GitHub Action <action@github.com>
2024-04-07 16:16:06 +0000

    [20240408] Add daily LeetCode problem

--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 from utils import get_default_folder
 
 # Question ID that wants to test, modify here as passing arguments
-QUESTIONS = ['172', '117']
+QUESTIONS = ['153', '82']
 
 
 class Test(unittest.TestCase):
