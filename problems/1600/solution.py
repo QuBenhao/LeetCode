03ba79045aabe0f2836eec4575f3adc1b9f8c7e4
benhao <qubenhao2@gmail.com>
2024-04-07 10:33:41 +0800

    rewrite problem 1600

--- problems/1600/solution.py
+++ problems/1600/solution.py
@@ -1,61 +1,31 @@
 import solution
-from collections import defaultdict
+from typing import *
 from object_libs import call_method
 
 
 class Solution(solution.Solution):
     def solve(self, test_input=None):
-        # Your ThroneInheritance object will be instantiated and called as such:
-        # obj = ThroneInheritance(kingName)
-        # obj.birth(parentName,childName)
-        # obj.death(name)
-        # param_3 = obj.getInheritanceOrder()
         ops, inputs = test_input
         obj = ThroneInheritance(*inputs[0])
         return [None] + [call_method(obj, op, *ipt) for op, ipt in zip(ops[1:], inputs[1:])]
 
 
-class ThroneInheritance(object):
+class ThroneInheritance:
 
-    def __init__(self, kingName):
-        """
-        :type kingName: str
-        """
-        self.root = kingName
-        # parent -> children
-        self.tree = defaultdict(list)
-        self.deads = set()
+    def __init__(self, kingName: str):
+        pass
 
-    def birth(self, parentName, childName):
-        """
-        :type parentName: str
-        :type childName: str
-        :rtype: None
-        """
-        self.tree[parentName].append(childName)
+    def birth(self, parentName: str, childName: str) -> None:
+        pass
 
-    def death(self, name):
-        """
-        :type name: str
-        :rtype: None
-        """
-        self.deads.add(name)
+    def death(self, name: str) -> None:
+        pass
 
-    def getInheritanceOrder(self):
-        """
-        :rtype: List[str]
-        """
-        """
-        Successor(x, curOrder):
-        如果 x 没有孩子或者所有 x 的孩子都在 curOrder 中：
-            如果 x 是国王，那么返回 null
-            否则，返回 Successor(x 的父亲, curOrder)
-        否则，返回 x 不在 curOrder 中最年长的孩子
-        """
-        def preOrder(node):
-            if node not in self.deads:
-                yield node
-            if node in self.tree:
-                for child in self.tree[node]:
-                    yield from preOrder(child)
-        return list(preOrder(self.root))
+    def getInheritanceOrder(self) -> List[str]:
+        pass
+
+# Your ThroneInheritance object will be instantiated and called as such:
+# obj = ThroneInheritance(kingName)
+# obj.birth(parentName,childName)
+# obj.death(name)
+# param_3 = obj.getInheritanceOrder()
--- problems/1600/testcase.py
+++ problems/1600/testcase.py
@@ -5,16 +5,9 @@ case = namedtuple("Testcase", ["Input", "Output"])
 
 
 class Testcase(testcase.Testcase):
-    def __init__(self):
-        self.testcases = []
-        self.testcases.append(case(
-            Input=[['ThroneInheritance', 'birth', 'birth', 'birth', 'birth', 'birth', 'birth', 'getInheritanceOrder',
-                    'death', 'getInheritanceOrder'], [['king'], ['king', 'andy'], ['king', 'bob'],
-                                                      ['king', 'catherine'], ['andy', 'matthew'], ['bob', 'alex'],
-                                                      ['bob', 'asha'], [None], ['bob'], [None]]],
-            Output=[None, None, None, None, None, None, None,
-                    ['king', 'andy', 'matthew', 'bob', 'alex', 'asha', 'catherine'], None,
-                    ['king', 'andy', 'matthew', 'alex', 'asha', 'catherine']]))
+	def __init__(self):
+		self.testcases = []
+		self.testcases.append(case(Input=[['ThroneInheritance', 'birth', 'birth', 'birth', 'birth', 'birth', 'birth', 'getInheritanceOrder', 'death', 'getInheritanceOrder'], [['king'], ['king', 'andy'], ['king', 'bob'], ['king', 'catherine'], ['andy', 'matthew'], ['bob', 'alex'], ['bob', 'asha'], [None], ['bob'], [None]]], Output=[None, None, None, None, None, None, None, ['king', 'andy', 'matthew', 'bob', 'alex', 'asha', 'catherine'], None, ['king', 'andy', 'matthew', 'alex', 'asha', 'catherine']]))
 
-    def get_testcases(self):
-        return self.testcases
+	def get_testcases(self):
+		return self.testcases
