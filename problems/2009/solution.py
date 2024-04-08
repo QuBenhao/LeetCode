a9801788178ffd16792b1167aec9704edf4ecd4c
GitHub Action <action@github.com>
2024-04-08 03:05:16 +0000

    [20240408] Add daily problem solution

--- problems/2009/solution.py
+++ problems/2009/solution.py
@@ -7,4 +7,11 @@ class Solution(solution.Solution):
         return self.minOperations(test_input)
 
     def minOperations(self, nums: List[int]) -> int:
-            pass
+        n = len(nums)
+        values = sorted(set(nums))
+        ans = left = 0
+        for i, num in enumerate(values):
+            while values[left] < num - n + 1:
+                left += 1
+            ans = max(ans, i - left + 1)
+        return n - ans
