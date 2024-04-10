79abf0507d0c3b2263257919a332e5595ce86826
benhao <qubenhao2@gmail.com>
2024-04-10 20:55:22 +0800

    [20240410] solve study plan 103

--- problems/103/solution.py
+++ problems/103/solution.py
@@ -1,12 +1,15 @@
 import solution
 from typing import *
 from object_libs import list_to_tree
+from collections import deque
+
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
 
 # Definition for a binary tree node.
 class Solution(solution.Solution):
@@ -16,4 +19,23 @@ class Solution(solution.Solution):
         return self.zigzagLevelOrder(root0)
 
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-            pass
+        ans = []
+        if not root:
+            return ans
+        q = deque([root])
+        level = 0
+        while q:
+            cur = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                cur.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            if level != 0:
+                ans.append(cur[::-1])
+            else:
+                ans.append(cur)
+            level ^= 1
+        return ans
