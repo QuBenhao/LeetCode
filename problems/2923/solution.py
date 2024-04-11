09fcab4545fe452fc15748a5ea050b47440e21c1
GitHub Action <action@github.com>
2024-04-11 16:16:15 +0000

    [20240412] Add daily LeetCode problem

---
+++ problems/2923/solution.py
@@ -0,0 +1,10 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.findChampion(test_input)
+
+    def findChampion(self, grid: List[List[int]]) -> int:
+            pass
--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 from utils import get_default_folder
 
 # Question ID that wants to test, modify here as passing arguments
-QUESTIONS = ['295', '219']
+QUESTIONS = ['188', '124']
 
 
 class Test(unittest.TestCase):
