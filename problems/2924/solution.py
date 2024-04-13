9197273871c340a80d7a540b61f4c465183db3a4
GitHub Action <action@github.com>
2024-04-13 14:07:20 +0000

    [20240413] Add daily problem solution

--- problems/2924/solution.py
+++ problems/2924/solution.py
@@ -7,4 +7,13 @@ class Solution(solution.Solution):
         return self.findChampion(*test_input)
 
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
-            pass
+        count = [0] * n
+        for a, b in edges:
+            count[b] += 1
+        ans = -1
+        for i in range(n):
+            if count[i] == 0:
+                if ans != -1:
+                    return -1
+                ans = i
+        return ans
