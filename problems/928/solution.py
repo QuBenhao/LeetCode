f08ab5dfc29ce597e12e084e4a265a4052395fea
GitHub Action <action@github.com>
2024-04-16 16:18:31 +0000

    [20240417] Add daily LeetCode problem

---
+++ problems/928/solution.py
@@ -0,0 +1,10 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.minMalwareSpread(*test_input)
+
+    def minMalwareSpread(self, graph: List[List[int]], initial: List[int]) -> int:
+            pass
--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import constants
 from utils import get_default_folder
 
 # Question ID that wants to test, modify here as passing arguments
-QUESTION = "924"
+QUESTION = "928"
 # QUESTION = "Interview/10_02"
 # QUESTION = "LCP/07"
 # QUESTION = "剑指Offer/52"
