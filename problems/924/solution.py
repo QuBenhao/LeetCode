48ea7fe94237e8e73286cbb16db6f98224c4dc08
benhao <qubenhao2@gmail.com>
2024-04-16 23:48:55 +0800

    solve 924

--- problems/924/solution.py
+++ problems/924/solution.py
@@ -7,4 +7,28 @@ class Solution(solution.Solution):
         return self.minMalwareSpread(*test_input)
 
     def minMalwareSpread(self, graph: List[List[int]], initial: List[int]) -> int:
-            pass
+        st = set(initial)
+        vis = [False] * len(graph)
+        def dfs(x: int) -> None:
+            vis[x] = True
+            nonlocal node_id, size
+            size += 1
+            # 按照状态机更新 node_id
+            if node_id != -2 and x in st:
+                node_id = x if node_id == -1 else -2
+            for y, conn in enumerate(graph[x]):
+                if conn and not vis[y]:
+                    dfs(y)
+
+        ans = -1
+        max_size = 0
+        for x in initial:
+            if vis[x]:
+                continue
+            node_id = -1
+            size = 0
+            dfs(x)
+            if node_id >= 0 and (size > max_size or size == max_size and node_id < ans):
+                ans = node_id
+                max_size = size
+        return min(initial) if ans < 0 else ans
