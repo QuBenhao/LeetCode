36e6ea8149a1478985e5c3503393328a9e9ada33
benhao <qubenhao2@gmail.com>
2024-04-18 20:49:15 +0800

    solve 2007

--- problems/2007/solution.py
+++ problems/2007/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+from collections import Counter
 
 
 class Solution(solution.Solution):
@@ -7,4 +8,22 @@ class Solution(solution.Solution):
         return self.findOriginalArray(test_input)
 
     def findOriginalArray(self, changed: List[int]) -> List[int]:
-            pass
+        counter = Counter(changed)
+        zeros = counter.pop(0, 0)
+        if zeros % 2 != 0:
+            return []
+        ans = [0] * (zeros // 2)
+        for x in counter:
+            if x % 2 == 0 and x // 2 in counter:
+                continue
+            while x in counter:
+                count_x = counter[x]
+                if count_x > counter[x * 2]:
+                    return []
+                ans.extend([x] * count_x)
+                if count_x < counter[x * 2]:
+                    counter[x * 2] -= count_x
+                    x *= 2
+                else:
+                    x *= 4
+        return ans
