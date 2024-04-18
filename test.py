167f1b45129c4614e278664a9b5ae9cf8aa7c874
GitHub Action <action@github.com>
2024-04-18 16:18:25 +0000

    [20240419] Add daily LeetCode problem

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import constants
 from utils import get_default_folder
 
 # Question ID that wants to test, modify here as passing arguments
-QUESTION = "2007"
+QUESTION = "1883"
 # QUESTION = "Interview/10_02"
 # QUESTION = "LCP/07"
 # QUESTION = "剑指Offer/52"
