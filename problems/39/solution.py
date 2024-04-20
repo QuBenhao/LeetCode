07fca9a8b4c9119197e81f07f6ecf6b05ddb9048
GitHub Action <action@github.com>
2024-04-20 14:06:23 +0000

    [20240420] Add daily problem solution

--- problems/39/solution.py
+++ problems/39/solution.py
@@ -7,4 +7,22 @@ class Solution(solution.Solution):
         return self.combinationSum(*test_input)
 
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-            pass
+        candidates.sort()
+        ans = []
+        path = []
+
+        def dfs(x, s):
+            if s == 0:
+                ans.append(list(path))
+                return
+            if x < 0 or s < 0:
+                return
+            # 不选当前
+            dfs(x - 1, s)
+            # 选当前
+            path.append(candidates[x])
+            dfs(x, s - candidates[x])
+            path.pop()
+        
+        dfs(len(candidates) - 1, target)
+        return ans
