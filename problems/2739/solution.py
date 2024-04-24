13dcee39dd0844607548ec416ec9c3d3baa10a2f
GitHub Action <action@github.com>
2024-04-24 16:17:31 +0000

    [20240425] Add daily LeetCode problem

---
+++ problems/2739/solution.py
@@ -0,0 +1,10 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.distanceTraveled(*test_input)
+
+    def distanceTraveled(self, mainTank: int, additionalTank: int) -> int:
+            pass
