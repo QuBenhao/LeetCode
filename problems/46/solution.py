3386aa839583d7b18e95ffbef1a0ec6feab06bc5
benhao <qubenhao2@gmail.com>
2024-04-27 12:30:14 +0800

    add solution 46

--- problems/46/solution.py
+++ problems/46/solution.py
@@ -7,4 +7,16 @@ class Solution(solution.Solution):
         return self.permute(test_input)
 
     def permute(self, nums: List[int]) -> List[List[int]]:
-            pass
+        ans = []
+
+        def dfs(x):
+            if x == len(nums) - 1:
+                ans.append(list(nums))
+                return
+            for i in range(x, len(nums)):
+                nums[i], nums[x] = nums[x], nums[i]
+                dfs(x + 1)
+                nums[i], nums[x] = nums[x], nums[i]
+
+        dfs(0)
+        return ans
