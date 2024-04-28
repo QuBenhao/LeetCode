565580bd40ced51e707aaf0a82b29e6839c71ef5
GitHub Action <action@github.com>
2024-04-28 14:07:00 +0000

    [20240428] Add daily problem solution

--- problems/1017/solution.py
+++ problems/1017/solution.py
@@ -7,4 +7,11 @@ class Solution(solution.Solution):
         return self.baseNeg2(test_input)
 
     def baseNeg2(self, n: int) -> str:
-            pass
+        val = 0x55555555 ^ (0x55555555 - n)
+        if val == 0:
+            return "0"
+        res = []
+        while val:
+            res.append(str(val & 1))
+            val >>= 1
+        return ''.join(res[::-1])
