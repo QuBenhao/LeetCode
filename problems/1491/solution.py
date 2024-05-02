44b6e3ea6541a7f957d872ac4ecc5243b5c486e6
GitHub Action <action@github.com>
2024-05-02 16:17:49 +0000

    [20240503] Add daily LeetCode problem

---
+++ problems/1491/solution.py
@@ -0,0 +1,10 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.average(test_input)
+
+    def average(self, salary: List[int]) -> float:
+            pass
