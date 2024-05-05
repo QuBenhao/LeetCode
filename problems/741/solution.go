24774a70ecc14d8a283b8c1c7573271172282f39
GitHub Action <action@github.com>
2024-05-05 16:18:28 +0000

    [20240506] Add daily LeetCode problem

---
+++ problems/741/solution.go
@@ -0,0 +1,22 @@
+package problem741
+
+import (
+	"encoding/json"
+	"log"
+	"strings"
+)
+
+func Solve(input string) int {
+	values := strings.Split(input, "\n")
+	var grid [][]int
+
+	if err := json.Unmarshal([]byte(values[0]), &grid); err != nil {
+		log.Fatal(err)
+	}
+
+	return cherryPickup(grid)
+}
+
+func cherryPickup(grid [][]int) int {
+
+}
---
+++ problems/741/solution.py
@@ -0,0 +1,10 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.cherryPickup(test_input)
+
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+            pass
