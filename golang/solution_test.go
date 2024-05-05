85a1fee2453160d89cb21e94e51dac058fc9b1d0
benhao <qubenhao2@gmail.com>
2024-05-05 17:56:47 +0800

    feat(golang): golang auto testcase support
    Init auto run golang
    
    BREAKING CHANGE: Support for golang

---
+++ golang/solution_test.go
@@ -0,0 +1,63 @@
+package golang
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	problem "leetCode/problems/1235"
+	"log"
+	"os"
+	"path"
+	"runtime"
+	"strings"
+	"testing"
+)
+
+const TestcaseFolderFmt = "problems/%s/testcase"
+
+var problemId string = "1235"
+
+type TestCase struct {
+	input string
+	want  interface{}
+}
+
+func processTestcase() (tests []TestCase) {
+	inputs := make([]string, 0)
+	outputs := make([]interface{}, 0)
+	_, b, _, _ := runtime.Caller(0)
+	basePath := path.Dir(path.Dir(b))
+	testcasePath := path.Join(basePath, fmt.Sprintf(TestcaseFolderFmt, problemId))
+	testcaseContent, readErr := os.ReadFile(testcasePath)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+	testcasesStr := string(testcaseContent)
+	testcasesSplit := strings.Split(testcasesStr, "\n")
+	inputErr := json.Unmarshal([]byte(testcasesSplit[0]), &inputs)
+	if inputErr != nil {
+		log.Fatal(inputErr)
+	}
+	outputErr := json.Unmarshal([]byte(testcasesSplit[1]), &outputs)
+	if outputErr != nil {
+		log.Fatal(outputErr)
+	}
+	for i, input := range inputs {
+		tests = append(tests, TestCase{input, outputs[i]})
+	}
+	return
+}
+
+func TestSolution(t *testing.T) {
+	ast := assert.New(t)
+	tests := processTestcase()
+	for i, testcase := range tests {
+		t.Run(fmt.Sprintf("%s/Testcase#%d", problemId, i), func(t *testing.T) {
+			gotResp := problem.Solve(testcase.input)
+			// TODO: Not able to compare type currently
+			if !ast.Equal(fmt.Sprintf("%#v", gotResp), fmt.Sprintf("%#v", testcase.want)) {
+				t.Errorf("[%s] Solution gotResp = %v, want %v", problemId, gotResp, testcase.want)
+			}
+		})
+	}
+}
--- problems/1235/solution.go
+++ problems/1235/solution.go
@@ -1,17 +1,42 @@
-func jobScheduling(startTime, endTime, profit []int) int {
-    n := len(startTime)
-    type job struct{ start, end, profit int }
-    jobs := make([]job, n)
-    for i, start := range startTime {
-        jobs[i] = job{start, endTime[i], profit[i]}
-    }
-    slices.SortFunc(jobs, func(a, b job) int { return a.end - b.end }) // 按照结束时间排序
+package problem1235
 
-    f := make([]int, n+1)
-    for i, job := range jobs {
-        j := sort.Search(i, func(j int) bool { return jobs[j].end > job.start })
-        // 状态转移中，为什么是 j 不是 j+1：上面算的是 > start，-1 后得到 <= start，但由于还要 +1，抵消了
-        f[i+1] = max(f[i], f[j]+job.profit)
-    }
-    return f[n]
+import (
+	"encoding/json"
+	"log"
+	"slices"
+	"sort"
+	"strings"
+)
+
+func Solve(input string) int {
+	arrays := strings.Split(input, "\n")
+	arr1, arr2, arr3 := make([]int, 0), make([]int, 0), make([]int, 0)
+	if err := json.Unmarshal([]byte(arrays[0]), &arr1); err != nil {
+		log.Fatal(err)
+	}
+	if err := json.Unmarshal([]byte(arrays[1]), &arr2); err != nil {
+		log.Fatal(err)
+	}
+	if err := json.Unmarshal([]byte(arrays[2]), &arr3); err != nil {
+		log.Fatal(err)
+	}
+	return jobScheduling(arr1, arr2, arr3)
+}
+
+func jobScheduling(startTime, endTime, profit []int) int {
+	n := len(startTime)
+	type job struct{ start, end, profit int }
+	jobs := make([]job, n)
+	for i, start := range startTime {
+		jobs[i] = job{start, endTime[i], profit[i]}
+	}
+	slices.SortFunc(jobs, func(a, b job) int { return a.end - b.end }) // 按照结束时间排序
+
+	f := make([]int, n+1)
+	for i, job := range jobs {
+		j := sort.Search(i, func(j int) bool { return jobs[j].end > job.start })
+		// 状态转移中，为什么是 j 不是 j+1：上面算的是 > start，-1 后得到 <= start，但由于还要 +1，抵消了
+		f[i+1] = max(f[i], f[j]+job.profit)
+	}
+	return f[n]
 }
