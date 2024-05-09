6bb57f1757364ab78826a07d3998a064c46b3dc3
benhao <qubenhao2@gmail.com>
2024-05-10 01:28:08 +0800

    test: Add object testcase
    Object design problem template

--- python/dev/solution_code_test.py
+++ python/dev/solution_code_test.py
@@ -103,7 +103,13 @@ golang_test_list = [
     "/**\n * Definition for singly-linked list.\n * type ListNode struct {\n *     Val int\n *     Next *ListNode\n"
     " * }\n */\nfunc swapPairs(head *ListNode) *ListNode {\n\n}",
 
-
+    "type OrderedStream struct {\n\n}\n\n\nfunc Constructor(n int) OrderedStream {\n\n}\n\n\n"
+    "func (this *OrderedStream) Insert(idKey int, value string) []string {\n\n}\n\n\n"
+    "/**\n"
+    " * Your OrderedStream object will be instantiated and called as such:\n"
+    " * obj := Constructor(n);\n"
+    " * param_1 := obj.Insert(idKey,value);\n"
+    " */"
 ]
 
 if __name__ == '__main__':
