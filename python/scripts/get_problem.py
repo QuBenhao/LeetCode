d71d0635597718bc33189030f8684da4276d482a
benhao <qubenhao2@gmail.com>
2024-05-09 23:59:32 +0800

    fix: Write null, true, false back
    Fix back the true, false, null as originally

--- python/scripts/get_problem.py
+++ python/scripts/get_problem.py
@@ -55,7 +55,9 @@ def process_single_algorithm_problem(problem_folder: str, problem_id: str, probl
     with open(f"{dir_path}/testcase.py", "w", encoding="utf-8") as f:
         f.write(write_testcase(testcases, outputs))
     with open(f"{dir_path}/testcase", "w", encoding="utf-8") as f:
-        f.writelines([testcase_str, "\n", str(outputs)])
+        f.writelines([testcase_str, "\n",
+                      str(outputs).replace("None", "null")
+                     .replace("True", "true").replace("False", "false")])
     for key, val in code_maps.items():
         match key:
             case "python3":
