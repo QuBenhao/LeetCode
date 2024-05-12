8ec7c2326c0858553f5e884ea1e0db3e464daaf6
benhao <qubenhao2@gmail.com>
2024-05-12 21:01:21 +0800

    fix: 2105 golang format
    new golang test

--- problems/2105/solution.go
+++ problems/2105/solution.go
@@ -6,7 +6,36 @@ import (
 	"strings"
 )
 
-func Solve(input string) int {
+func minimumRefill(plants []int, capacityA, capacityB int) (ans int) {
+	a, b := capacityA, capacityB
+	i, j := 0, len(plants)-1
+	for i < j {
+		// Alice 给植物 i 浇水
+		if a < plants[i] {
+			// 没有足够的水，重新灌满水罐
+			ans++
+			a = capacityA
+		}
+		a -= plants[i]
+		i++
+		// Bob 给植物 j 浇水
+		if b < plants[j] {
+			// 没有足够的水，重新灌满水罐
+			ans++
+			b = capacityB
+		}
+		b -= plants[j]
+		j--
+	}
+	// Alice 和 Bob 到达同一株植物，那么当前水罐中水更多的人会给这株植物浇水
+	if i == j && max(a, b) < plants[i] {
+		// 没有足够的水，重新灌满水罐
+		ans++
+	}
+	return
+}
+
+func Solve(input string) interface{} {
 	values := strings.Split(input, "\n")
 	var plants []int
 	var capacityA int
@@ -23,33 +52,4 @@ func Solve(input string) int {
 	}
 
 	return minimumRefill(plants, capacityA, capacityB)
-}
-
-func minimumRefill(plants []int, capacityA, capacityB int) (ans int) {
-    a, b := capacityA, capacityB
-    i, j := 0, len(plants)-1
-    for i < j {
-        // Alice 给植物 i 浇水
-        if a < plants[i] {
-            // 没有足够的水，重新灌满水罐
-            ans++
-            a = capacityA
-        }
-        a -= plants[i]
-        i++
-        // Bob 给植物 j 浇水
-        if b < plants[j] {
-            // 没有足够的水，重新灌满水罐
-            ans++
-            b = capacityB
-        }
-        b -= plants[j]
-        j--
-    }
-    // Alice 和 Bob 到达同一株植物，那么当前水罐中水更多的人会给这株植物浇水
-    if i == j && max(a, b) < plants[i] {
-        // 没有足够的水，重新灌满水罐
-        ans++
-    }
-    return
 }
