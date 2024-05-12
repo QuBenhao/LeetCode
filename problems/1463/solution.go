fdaff7778ecf443c468b16f2d629baaa5edd5276
benhao <qubenhao2@gmail.com>
2024-05-12 21:05:36 +0800

    fix: 1463 golang format
    new golang test

--- problems/1463/solution.go
+++ problems/1463/solution.go
@@ -6,7 +6,30 @@ import (
 	"strings"
 )
 
-func Solve(input string) int {
+func cherryPickup(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	pre := make([][]int, n+2)
+	cur := make([][]int, n+2)
+	for i := range pre {
+		pre[i] = make([]int, n+2)
+		cur[i] = make([]int, n+2)
+	}
+	for i := m - 1; i >= 0; i-- {
+		for j := 0; j < min(n, i+1); j++ {
+			for k := max(j+1, n-1-i); k < n; k++ {
+				cur[j+1][k+1] = max(
+					pre[j][k], pre[j][k+1], pre[j][k+2],
+					pre[j+1][k], pre[j+1][k+1], pre[j+1][k+2],
+					pre[j+2][k], pre[j+2][k+1], pre[j+2][k+2],
+				) + grid[i][j] + grid[i][k]
+			}
+		}
+		pre, cur = cur, pre // 下一个 i 的 pre 是 cur
+	}
+	return pre[1][n]
+}
+
+func Solve(input string) interface{} {
 	values := strings.Split(input, "\n")
 	var grid [][]int
 
@@ -15,27 +38,4 @@ func Solve(input string) int {
 	}
 
 	return cherryPickup(grid)
-}
-
-func cherryPickup(grid [][]int) int {
-    m, n := len(grid), len(grid[0])
-    pre := make([][]int, n+2)
-    cur := make([][]int, n+2)
-    for i := range pre {
-        pre[i] = make([]int, n+2)
-        cur[i] = make([]int, n+2)
-    }
-    for i := m - 1; i >= 0; i-- {
-        for j := 0; j < min(n, i+1); j++ {
-            for k := max(j+1, n-1-i); k < n; k++ {
-                cur[j+1][k+1] = max(
-                    pre[j][k], pre[j][k+1], pre[j][k+2],
-                    pre[j+1][k], pre[j+1][k+1], pre[j+1][k+2],
-                    pre[j+2][k], pre[j+2][k+1], pre[j+2][k+2],
-                ) + grid[i][j] + grid[i][k]
-            }
-        }
-        pre, cur = cur, pre // 下一个 i 的 pre 是 cur
-    }
-    return pre[1][n]
 }
