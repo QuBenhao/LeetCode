0f2f983be076e42f1ebb362ee5f4330d9ffdae0e
GitHub Action <action@github.com>
2024-05-15 14:09:10 +0000

    [20240515] Add daily problem solution

---
+++ problems/138/solution.go
@@ -0,0 +1,45 @@
+package problem138
+
+import (
+	"encoding/json"
+	. "leetCode/golang/list_random"
+	"log"
+	"strings"
+)
+
+func copyRandomList(head *Node) *Node {
+    if head == nil {
+        return nil
+    }
+    for node := head; node != nil; node = node.Next.Next {
+        node.Next = &Node{Val: node.Val, Next: node.Next}
+    }
+    for node := head; node != nil; node = node.Next.Next {
+        if node.Random != nil {
+            node.Next.Random = node.Random.Next
+        }
+    }
+    headNew := head.Next
+    for node := head; node != nil; node = node.Next {
+        nodeNew := node.Next
+        node.Next = node.Next.Next
+        if nodeNew.Next != nil {
+            nodeNew.Next = nodeNew.Next.Next
+        }
+    }
+    return headNew
+}
+
+
+func Solve(input string) interface{} {
+	values := strings.Split(input, "\n")
+	var head *Node
+
+	var arr0 [][]interface{}
+	if err := json.Unmarshal([]byte(values[0]), &arr0); err != nil {
+		log.Fatal(err)
+	}
+	head = IntRandomArrayToNodeArray(arr0)
+
+	return NodeArrayToIntRandomArray(copyRandomList(head))
+}
--- problems/2589/solution.py
+++ problems/2589/solution.py
@@ -7,4 +7,18 @@ class Solution(solution.Solution):
         return self.findMinimumTime(test_input)
 
     def findMinimumTime(self, tasks: List[List[int]]) -> int:
-            pass
+        tasks.sort(key=lambda t: t[1])
+        # 栈中保存闭区间左右端点，栈底到栈顶的区间长度的和
+        st = [(-2, -2, 0)]  # 哨兵，保证不和任何区间相交
+        for start, end, d in tasks:
+            _, r, s = st[bisect_left(st, (start,)) - 1]
+            d -= st[-1][2] - s  # 去掉运行中的时间点
+            if start <= r:  # start 在区间 st[i] 内
+                d -= r - start + 1  # 去掉运行中的时间点
+            if d <= 0:
+                continue
+            while end - st[-1][1] <= d:  # 剩余的 d 填充区间后缀
+                l, r, _ = st.pop()
+                d += r - l + 1  # 合并区间
+            st.append((end - d + 1, end, st[-1][2] + d))
+        return st[-1][2]
