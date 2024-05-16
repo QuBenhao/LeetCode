2f0feb393a4cba6e9b876d854376465003fba331
benhao <qubenhao2@gmail.com>
2024-05-16 22:26:37 +0800

    fix: 1953
    import slices

--- problems/1953/solution.go
+++ problems/1953/solution.go
@@ -3,21 +3,21 @@ package problem1953
 import (
 	"encoding/json"
 	"log"
+	"slices"
 	"strings"
 )
 
 func numberOfWeeks(milestones []int) int64 {
-    s := 0
-    for _, x := range milestones {
-        s += x
-    }
-    m := slices.Max(milestones)
-    if m > s-m+1 {
-        return int64((s-m)*2 + 1)
-    }
-    return int64(s)
+	s := 0
+	for _, x := range milestones {
+		s += x
+	}
+	m := slices.Max(milestones)
+	if m > s-m+1 {
+		return int64((s-m)*2 + 1)
+	}
+	return int64(s)
 }
-
 
 func Solve(input string) interface{} {
 	values := strings.Split(input, "\n")
