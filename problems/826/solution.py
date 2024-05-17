2ee0ed5800e2270f2e00534942425c9b544ba1d5
GitHub Action <action@github.com>
2024-05-17 14:08:44 +0000

    [20240517] Add daily problem solution

--- problems/826/solution.py
+++ problems/826/solution.py
@@ -7,4 +7,12 @@ class Solution(solution.Solution):
         return self.maxProfitAssignment(*test_input)
 
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
-                pass
+        jobs = sorted(zip(difficulty, profit))
+        worker.sort()
+        ans = j = max_profit = 0
+        for w in worker:
+            while j < len(jobs) and jobs[j][0] <= w:
+                max_profit = max(max_profit, jobs[j][1])
+                j += 1
+            ans += max_profit
+        return ans
