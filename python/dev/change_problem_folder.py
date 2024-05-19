1bd6ed606fc5b558d736b901915dfbcab0af1475
benhao <qubenhao2@gmail.com>
2024-05-19 17:08:23 +0800

    fix: dev script fix java
    solution.java -> Solution.java

--- python/dev/change_problem_folder.py
+++ python/dev/change_problem_folder.py
@@ -29,19 +29,19 @@ def fix_java_files(dir_path):
     for root, dirs, files in os.walk(dir_path):
         if not files:
             continue
-        package_path = ".".join(root.split("/")[-2:])
+        # package_path = ".".join(root.split("/")[-2:])
         for f in files:
             if not f.endswith(".java"):
                 continue
-            with open(os.path.join(root, f), 'r', encoding="utf-8") as file:
-                lines = file.readlines()
-            if "package" not in lines[0]:
-                lines = [f"package {package_path};\n\n"] + lines
-            for i, l in enumerate(lines):
-                if "class Solution" in l and "public" not in l:
-                    lines[i] = "public " + l
-            with open(os.path.join(root, f), 'w', encoding="utf-8") as file:
-                file.writelines(lines)
+            # with open(os.path.join(root, f), 'r', encoding="utf-8") as file:
+            #     lines = file.readlines()
+            # if "package" not in lines[0]:
+            #     lines = [f"package {package_path};\n\n"] + lines
+            # for i, l in enumerate(lines):
+            #     if "class Solution" in l and "public" not in l:
+            #         lines[i] = "public " + l
+            # with open(os.path.join(root, f), 'w', encoding="utf-8") as file:
+            #     file.writelines(lines)
             if f[0].isupper():
                 continue
             os.rename(src=str(os.path.join(root, f)), dst=str(os.path.join(root, str(f[0].upper()) + "".join(f[1:]))))
@@ -51,8 +51,8 @@ def main(problem_folders: set[str]):
     root_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     for folder in problem_folders:
         dir_path = os.path.join(root_path, folder)
-        process_folder(dir_path)
-        # fix_java_files(dir_path)
+        # process_folder(dir_path)
+        fix_java_files(dir_path)
 
 
 if __name__ == '__main__':
