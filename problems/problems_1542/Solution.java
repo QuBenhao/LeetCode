678adec4ecede724f29dfd29365243e828fde4bb
GitHub Action <action@github.com>
2024-05-20 14:55:08 +0000

    [20240520] Add daily problem solution

--- problems/problems_1542/Solution.java
+++ problems/problems_1542/Solution.java
@@ -5,9 +5,28 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
-    public int longestAwesome(String s) {
+    private static final int D = 10; // s 中的字符种类数
 
+    public int longestAwesome(String s) {
+        int n = s.length();
+        int[] pos = new int[1 << D];
+        Arrays.fill(pos, n); // n 表示没有找到异或前缀和
+        pos[0] = -1; // pre[-1] = 0
+        int ans = 0;
+        int pre = 0;
+        for (int i = 0; i < n; i++) {
+            pre ^= 1 << (s.charAt(i) - '0');
+            for (int d = 0; d < D; d++) {
+                ans = Math.max(ans, i - pos[pre ^ (1 << d)]); // 奇数
+            }
+            ans = Math.max(ans, i - pos[pre]); // 偶数
+            if (pos[pre] == n) { // 首次遇到值为 pre 的前缀异或和，记录其下标 i
+                pos[pre] = i;
+            }
+        }
+        return ans;
     }
+
 
     @Override
     public Object solve(String[] values) {
