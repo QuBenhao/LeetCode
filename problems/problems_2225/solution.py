b4c956aa794197b10b42c3ede51cfe940d130d70
benhao <qubenhao2@gmail.com>
2024-05-22 08:20:06 +0800

    test: 2225 python
    solve

--- problems/problems_2225/solution.py
+++ problems/problems_2225/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+from collections import defaultdict
 
 
 class Solution(solution.Solution):
@@ -7,5 +8,16 @@ class Solution(solution.Solution):
         return self.findWinners(test_input)
 
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        pass
-
+        graph = defaultdict(list)
+        div = defaultdict(int)
+        for win, lose in matches:
+            graph[win].append(lose)
+            div[lose] += 1
+        ans = [[], []]
+        ans[0].extend(list(graph.keys() - div.keys()))
+        ans[0].sort()
+        for k, v in div.items():
+            if v == 1:
+                ans[1].append(k)
+        ans[1].sort()
+        return ans
