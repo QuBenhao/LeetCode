db4f7dabe2a9e1373a5d4d6afc5c15c58b18d755
benhao <qubenhao2@gmail.com>
2024-05-23 07:55:33 +0800

    test: 2831 python
    solve

--- problems/problems_2831/solution.py
+++ problems/problems_2831/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+from collections import defaultdict
 
 
 class Solution(solution.Solution):
@@ -7,5 +8,17 @@ class Solution(solution.Solution):
         return self.longestEqualSubarray(*test_input)
 
     def longestEqualSubarray(self, nums: List[int], k: int) -> int:
-        pass
+        pos_lists = defaultdict(list)
+        for i, x in enumerate(nums):
+            pos_lists[x].append(i - len(pos_lists[x]))
 
+        ans = 0
+        for pos in pos_lists.values():
+            if len(pos) <= ans:
+                continue  # 无法让 ans 变得更大
+            left = 0
+            for right, p in enumerate(pos):
+                while p - pos[left] > k:  # 要删除的数太多了
+                    left += 1
+                ans = max(ans, right - left + 1)
+        return ans
