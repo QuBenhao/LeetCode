1a3385a16f6900b1eb6ffc392ca3fe4af3a24cfd
benhao <qubenhao2@gmail.com>
2024-05-23 08:06:17 +0800

    test: solve java 2831
    Add json to list int

--- problems/problems_2831/Solution.java
+++ problems/problems_2831/Solution.java
@@ -7,13 +7,34 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int longestEqualSubarray(List<Integer> nums, int k) {
+        int n = nums.size();
+        List<Integer>[] posLists = new ArrayList[n + 1];
+        Arrays.setAll(posLists, i -> new ArrayList<>());
+        for (int i = 0; i < n; i++) {
+            int x = nums.get(i);
+            posLists[x].add(i - posLists[x].size());
+        }
 
+        int ans = 0;
+        for (List<Integer> pos : posLists) {
+            if (pos.size() <= ans) {
+                continue; // 无法让 ans 变得更大
+            }
+            int left = 0;
+            for (int right = 0; right < pos.size(); right++) {
+                while (pos.get(right) - pos.get(left) > k) { // 要删除的数太多了
+                    left++;
+                }
+                ans = Math.max(ans, right - left + 1);
+            }
+        }
+        return ans;
     }
 
     @Override
     public Object solve(String[] values) {
-        List<Integer> nums = FIXME(values[0])
-		int k = Integer.parseInt(values[1]);
+        List<Integer> nums = jsonArrayToIntList(values[0]);
+        int k = Integer.parseInt(values[1]);
         return JSON.toJSON(longestEqualSubarray(nums, k));
     }
 }
