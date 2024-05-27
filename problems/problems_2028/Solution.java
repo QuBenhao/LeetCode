dfa9cf7761135f64b3a82a80ee472067bfee06b7
GitHub Action <action@github.com>
2024-05-27 14:08:45 +0000

    [20240527] Add daily problem solution

--- problems/problems_2028/Solution.java
+++ problems/problems_2028/Solution.java
@@ -7,7 +7,19 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int[] missingRolls(int[] rolls, int mean, int n) {
-
+        int remain = mean * (rolls.length + n);
+        for (int roll: rolls) {
+            remain -= roll;
+        }
+        if (remain < n || remain > n * 6) {
+            return new int[]{};
+        }
+        int avg = remain / n, extra = remain % n;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = avg + (i < extra ? 1 : 0);
+        }
+        return ans;
     }
 
     @Override
