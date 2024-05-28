6d82b4bf84488359382d9bd0a4718c391bb228d9
GitHub Action <action@github.com>
2024-05-28 16:18:33 +0000

    [20240529] Add daily LeetCode problem

--- WORKSPACE
+++ WORKSPACE
@@ -15,6 +15,6 @@ git_repository(
 
 new_local_repository(
     name = "problems",
-    path = "problems/problems_2951/",
+    path = "problems/problems_2981/",
     build_file = "//cpp:solution.BUILD",
 )
--- python/test.py
+++ python/test.py
@@ -8,7 +8,7 @@ import constants
 from utils import get_default_folder
 
 # Question ID that wants to test, modify here as passing arguments
-QUESTION = "2951"
+QUESTION = "2981"
 # QUESTION = "Interview/10_02"
 # QUESTION = "LCP/07"
 # QUESTION = "剑指Offer/52"
