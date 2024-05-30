adf5a7d25da4a70257749610b69ac8f24338118a
benhao <qubenhao2@gmail.com>
2024-05-30 19:41:32 +0800

    test: solve 2982
    python, cpp, golang, java

--- problems/problems_2982/Solution.java
+++ problems/problems_2982/Solution.java
@@ -6,8 +6,29 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
-    public int maximumLength(String s) {
+    public int maximumLength(String S) {
+        char[] s = S.toCharArray();
+        List<Integer>[] groups = new ArrayList[26];
+        Arrays.setAll(groups, i -> new ArrayList<>());
+        int cnt = 0;
+        for (int i = 0; i < s.length; i++) {
+            cnt++;
+            if (i + 1 == s.length || s[i] != s[i + 1]) {
+                groups[s[i] - 'a'].add(cnt); // 统计连续字符长度
+                cnt = 0;
+            }
+        }
 
+        int ans = 0;
+        for (List<Integer> a : groups) {
+            if (a.isEmpty()) continue;
+            a.sort(Collections.reverseOrder());
+            a.add(0);
+            a.add(0); // 假设还有两个空串
+            ans = Math.max(ans, Math.max(a.get(0) - 2, Math.max(Math.min(a.get(0) - 1, a.get(1)), a.get(2))));
+        }
+
+        return ans > 0 ? ans : -1;
     }
 
     @Override
