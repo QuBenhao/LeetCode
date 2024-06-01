2242675313177d121dd90f32085c4a990beab07e
benhao <qubenhao2@gmail.com>
2024-06-01 10:44:53 +0800

    test: solve 2928 python
    python combination num

--- problems/problems_2928/solution.py
+++ problems/problems_2928/solution.py
@@ -1,5 +1,13 @@
 import solution
 from typing import *
+from functools import lru_cache
+
+
+@lru_cache(None)
+def combination_nums(total):
+    if total <= 1:
+        return 0
+    return total * (total - 1) // 2
 
 
 class Solution(solution.Solution):
@@ -7,5 +15,13 @@ class Solution(solution.Solution):
         return self.distributeCandies(*test_input)
 
     def distributeCandies(self, n: int, limit: int) -> int:
-        pass
+        # 总方案数 C n+2 2
+        # 一个人超limit的个数为 C n+1-limit 2
+        # 两个人超limit的个数为 C n-2*limit 2
+        # 三个人超limit的个数为 C n-1-3*limit 2
 
+        return (combination_nums(n + 2)
+                - 3 * combination_nums(n + 1 - limit)
+                + 3 * combination_nums(n - 2 * limit)
+                - combination_nums(n - 1 - 3 * limit)
+                )
--- problems/problems_2928/testcase.py
+++ problems/problems_2928/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=[5, 2], Output=3))
 		self.testcases.append(case(Input=[3, 3], Output=10))
+		self.testcases.append(case(Input=[4, 1], Output=0))
 
 	def get_testcases(self):
 		return self.testcases
