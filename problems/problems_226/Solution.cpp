d7c4e900b3747087b31de30128a34c864590f576
benhao <qubenhao2@gmail.com>
2024-06-02 22:41:51 +0800

    fix: cpp 226 solution and TreeNode bug
    cpp TreeNode nullptr bug fixed

---
+++ problems/problems_226/Solution.cpp
@@ -0,0 +1,47 @@
+//go:build ignore
+#include "cpp/common/Solution.h"
+#include "cpp/models/TreeNode.h"
+
+using namespace std;
+using json = nlohmann::json;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return root;
+        }
+        TreeNode* tmp_right = invertTree(root->left);
+        root->left = invertTree(root->right);
+        root->right = tmp_right;
+        return root;
+    }
+};
+
+json leetcode::qubh::Solve(string input)
+{
+	vector<string> inputArray;
+	size_t pos = input.find('\n');
+	while (pos != string::npos) {
+		inputArray.push_back(input.substr(0, pos));
+		input = input.substr(pos + 1);
+		pos = input.find('\n');
+	}
+	inputArray.push_back(input);
+
+	Solution solution;
+	json root_array = json::parse(inputArray.at(0));
+	TreeNode *root = JsonArrayToTreeNode(root_array);
+	return TreeNodeToJsonArray(solution.invertTree(root));
+}
