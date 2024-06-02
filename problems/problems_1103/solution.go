ea811039ef9aa7c8184e1027d3da46e2b63eff7a
benhao <qubenhao2@gmail.com>
2024-06-03 07:58:21 +0800

    test: golang 1103 solution
    solve golang 1103

--- problems/problems_1103/solution.go
+++ problems/problems_1103/solution.go
@@ -3,11 +3,29 @@ package problem1103
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"strings"
 )
 
 func distributeCandies(candies int, num_people int) []int {
-
+	// (x + 2)^2 > (x + 2) * (x + 1) > 2 * candies >= x * (x + 1) > x^2
+	f := math.Sqrt(float64(candies * 2))
+	x := int(f + 1)
+	var s int
+	for s = x * (x + 1) / 2; s > candies; x-- {
+		s -= x
+	}
+	remain := candies - s
+	div, mod := x/num_people, x%num_people
+	ans := make([]int, num_people)
+	for i := 0; i < num_people; i++ {
+		ans[i] += (i+1)*div + num_people*div*(div-1)/2
+		if i < mod {
+			ans[i] += num_people*div + i + 1
+		}
+	}
+	ans[mod] += remain
+	return ans
 }
 
 func Solve(input string) interface{} {
