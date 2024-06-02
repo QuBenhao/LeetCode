519e0530924b0a4958004eb4a754e22f3eebef6f
benhao <qubenhao2@gmail.com>
2024-06-02 11:09:42 +0800

    test: solve 3 golang
    golang solution

---
+++ problems/problems_3/solution.go
@@ -0,0 +1,30 @@
+package problem3
+
+import (
+	"encoding/json"
+	"log"
+	"strings"
+)
+
+func lengthOfLongestSubstring(s string) (ans int) {
+	mp, j := map[rune]int{}, -1
+	for i, v := range s {
+		if idx, ok := mp[v]; ok {
+			j = max(j, idx)
+		}
+		ans = max(ans, i-j)
+		mp[v] = i
+	}
+	return
+}
+
+func Solve(input string) interface{} {
+	values := strings.Split(input, "\n")
+	var s string
+
+	if err := json.Unmarshal([]byte(values[0]), &s); err != nil {
+		log.Fatal(err)
+	}
+
+	return lengthOfLongestSubstring(s)
+}
