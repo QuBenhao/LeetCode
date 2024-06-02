c1a29a2f244bc95574bfe0c11b9fd9449b47bd6f
benhao <qubenhao2@gmail.com>
2024-06-03 07:48:25 +0800

    test: python 1103 solution
    solve python 1103

--- problems/problems_1103/solution.py
+++ problems/problems_1103/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+from math import ceil
 
 
 class Solution(solution.Solution):
@@ -7,5 +8,27 @@ class Solution(solution.Solution):
         return self.distributeCandies(*test_input)
 
     def distributeCandies(self, candies: int, num_people: int) -> List[int]:
-        pass
-
+        """
+        x * (x + 1) // 2 >= candies > x * (x - 1) // 2
+        (x + 1) * (x + 1) > x * (x + 1) >= 2 * candies > x * (x - 1) > (x - 1) * (x - 1)
+        x + 1 > 根号下(2 * candies) > x - 1
+        {} + 1 > x > {} - 1
+        """
+        f = (candies * 2) ** 0.5
+        # 从最大往最小逼近
+        x = int(f + 1)
+        if (s := x * (x + 1) // 2) > candies:
+            s -= x
+            if s > candies:
+                x -= 1
+                s -= x
+            x -= 1
+        remain = candies - s
+        d, m = divmod(x, num_people)
+        ans = [0] * num_people
+        for i in range(num_people):
+            ans[i] = (i + 1) * d + num_people * (d - 1) * d // 2
+            if i < m:
+                ans[i] += i + 1 + num_people * d
+        ans[m] += remain
+        return ans
--- problems/problems_1103/testcase.py
+++ problems/problems_1103/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=[7, 4], Output=[1, 2, 3, 1]))
 		self.testcases.append(case(Input=[10, 3], Output=[5, 2, 3]))
+		self.testcases.append(case(Input=[90, 4], Output=[27, 18, 21, 24]))
 
 	def get_testcases(self):
 		return self.testcases
