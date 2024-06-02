f7b6921f9e5c548062ee916a19b6812ab63deb2d
benhao <qubenhao2@gmail.com>
2024-06-02 22:35:41 +0800

    fix: golang 226 solution
    solve 226

---
+++ problems/problems_226/solution.go
@@ -0,0 +1,33 @@
+package problem226
+
+import (
+	. "leetCode/golang/models"
+	"strings"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	tmpRight := invertTree(root.Left)
+	root.Left = invertTree(root.Right)
+	root.Right = tmpRight
+	return root
+}
+
+func Solve(input string) interface{} {
+	values := strings.Split(input, "\n")
+	var root *TreeNode
+
+	root = ArrayToTree(values[0])
+
+	return TreeToArray(invertTree(root))
+}
