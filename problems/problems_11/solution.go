83a07d88dccf82287f10f82729e61bb3778a9d04
benhao <qubenhao2@gmail.com>
2024-06-02 16:26:02 +0800

    test: solve 11 golang
    solution

---
+++ problems/problems_11/solution.go
@@ -0,0 +1,30 @@
+package problem11
+
+import (
+	"encoding/json"
+	"log"
+	"strings"
+)
+
+func maxArea(height []int) (ans int) {
+	for i, j := 0, len(height)-1; i < j; {
+		ans = max(ans, min(height[i], height[j])*(j-i))
+		if height[i] > height[j] {
+			j--
+		} else {
+			i++
+		}
+	}
+	return
+}
+
+func Solve(input string) interface{} {
+	values := strings.Split(input, "\n")
+	var height []int
+
+	if err := json.Unmarshal([]byte(values[0]), &height); err != nil {
+		log.Fatal(err)
+	}
+
+	return maxArea(height)
+}
