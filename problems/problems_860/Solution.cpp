6684eafdde6c040eacb9a7fc14749691cca6dd9c
benhao <qubenhao2@gmail.com>
2024-06-04 00:16:25 +0800

    test: cpp 860
    solution

--- problems/problems_860/Solution.cpp
+++ problems/problems_860/Solution.cpp
@@ -8,7 +8,24 @@ using json = nlohmann::json;
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-
+        int fives = 0, tens = 0;
+        for (auto bill: bills) {
+            if (bill == 5) {
+                fives++;
+            } else if (bill == 10) {
+                tens++;
+                fives--;
+            } else if (tens > 0) {
+                tens--;
+                fives--;
+            } else {
+                fives -= 3;
+            }
+            if (fives < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 };
 
