288c3092a2b49ede1c3a8bbb975a97dffea04154
benhao <qubenhao2@gmail.com>
2024-06-03 08:08:56 +0800

    test: java 1103 solution
    solve java 1103

--- problems/problems_1103/Solution.java
+++ problems/problems_1103/Solution.java
@@ -7,7 +7,21 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int[] distributeCandies(int candies, int num_people) {
-
+        double f = Math.sqrt(candies * 2);
+        int x = (int)f + 1, s;
+        for (s = x * (x + 1) / 2; s > candies; x--) {
+            s -= x;
+        }
+        int remain = candies - s, div = x / num_people, mod = x % num_people;
+        int[] ans = new int[num_people];
+        for (int i = 0; i < num_people; i++) {
+            ans[i] = (i + 1) * div + num_people * div * (div - 1) / 2;
+            if (i < mod) {
+                ans[i] += num_people * div + i + 1;
+            }
+        }
+        ans[mod] += remain;
+        return ans;
     }
 
     @Override
