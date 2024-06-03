ac5b2c6d4b99f33f20aab6ef90499d4ec2fa08c6
benhao <qubenhao2@gmail.com>
2024-06-03 23:51:49 +0800

    test: golang 860 solution
    golang match

--- problems/problems_860/solution.go
+++ problems/problems_860/solution.go
@@ -7,7 +7,24 @@ import (
 )
 
 func lemonadeChange(bills []int) bool {
-
+	fives, tens := 0, 0
+	for _, bill := range bills {
+		if bill == 5 {
+			fives++
+		} else if bill == 10 {
+			tens++
+			fives--
+		} else if tens > 0 {
+			tens--
+			fives--
+		} else {
+			fives -= 3
+		}
+		if fives < 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func Solve(input string) interface{} {
