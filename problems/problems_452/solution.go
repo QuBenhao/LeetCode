f64696a6be79f7f873bf8b1bc293952405acc9d6
benhao <qubenhao2@gmail.com>
2024-06-05 21:06:09 +0800

    test: solve 452 golang
    solution

---
+++ problems/problems_452/solution.go
@@ -0,0 +1,33 @@
+package problem452
+
+import (
+	"encoding/json"
+	"log"
+	"sort"
+	"strings"
+)
+
+func findMinArrowShots(points [][]int) int {
+	sort.Slice(points, func(i, j int) bool {
+		return points[i][1] < points[j][1]
+	})
+	cur, ans := points[0][1], 1
+	for _, point := range points {
+		if point[0] > cur {
+			ans++
+			cur = point[1]
+		}
+	}
+	return ans
+}
+
+func Solve(input string) interface{} {
+	values := strings.Split(input, "\n")
+	var points [][]int
+
+	if err := json.Unmarshal([]byte(values[0]), &points); err != nil {
+		log.Fatal(err)
+	}
+
+	return findMinArrowShots(points)
+}
