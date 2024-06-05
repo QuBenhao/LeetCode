755c8f9a5ff95b992336756ced1d1b2b55ec4ee9
benhao <qubenhao2@gmail.com>
2024-06-05 20:21:35 +0800

    test: solve 976 1572 cpp
    solution

--- problems/problems_1572/Solution.cpp
+++ problems/problems_1572/Solution.cpp
@@ -8,7 +8,15 @@ using json = nlohmann::json;
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-
+        int ans = 0;
+        size_t n = mat.size();
+        for (int i = 0; i < n; i++) {
+            ans += mat[i][i] + mat[i][n - 1 - i];
+        }
+        if (n % 2 == 1) {
+            ans -= mat[n/2][n/2];
+        }
+        return ans;
     }
 };
 
--- problems/problems_976/Solution.cpp
+++ problems/problems_976/Solution.cpp
@@ -8,7 +8,13 @@ using json = nlohmann::json;
 class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
-
+        std::sort(nums.begin(), nums.end(), std::greater<>());
+        for (int i = 0; i < nums.size() - 2; i++) {
+            if (nums[i] < nums[i + 1] + nums[i + 2]) {
+                return nums[i] + nums[i + 1] + nums[i + 2];
+            }
+        }
+        return 0;
     }
 };
 
