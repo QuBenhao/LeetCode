767e7af067dca63da6e324c5f2900457f4cd4b1f
benhao <qubenhao2@gmail.com>
2024-06-05 20:00:11 +0800

    test: solve 976, 1572 python
    solution

--- problems/problems_1572/solution.py
+++ problems/problems_1572/solution.py
@@ -7,5 +7,7 @@ class Solution(solution.Solution):
         return self.diagonalSum(test_input)
 
     def diagonalSum(self, mat: List[List[int]]) -> int:
-        pass
-
+        n, ans = len(mat), 0
+        for i in range(n):
+            ans += mat[i][i] + mat[i][n - 1 - i]
+        return ans - mat[n // 2][n // 2] if n % 2 else ans
