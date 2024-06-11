7bab710957e035d1cf4174e5a4b20e171c916afa
benhao <qubenhao2@gmail.com>
2024-06-11 21:53:11 +0800

    test: fix golang byte array
    419 solution & read bytes from json

--- problems/problems_419/solution.go
+++ problems/problems_419/solution.go
@@ -6,16 +6,31 @@ import (
 	"strings"
 )
 
-func countBattleships(board [][]byte) int {
-
+func countBattleships(board [][]byte) (ans int) {
+	for i, row := range board {
+		for j, r := range row {
+			if r == 'X' && (i == 0 || board[i-1][j] != 'X') && (j == 0 || board[i][j-1] != 'X') {
+				ans++
+			}
+		}
+	}
+	return
 }
 
 func Solve(input string) interface{} {
 	values := strings.Split(input, "\n")
-	var board [][]byte
+	var str [][]string
 
-	if err := json.Unmarshal([]byte(values[0]), &board); err != nil {
+	if err := json.Unmarshal([]byte(values[0]), &str); err != nil {
 		log.Fatal(err)
+	}
+
+	board := make([][]byte, len(str))
+	for i := 0; i < len(board); i++ {
+		board[i] = make([]byte, len(str[i]))
+		for j := 0; j < len(board[i]); j++ {
+			board[i][j] = str[i][j][0]
+		}
 	}
 
 	return countBattleships(board)
