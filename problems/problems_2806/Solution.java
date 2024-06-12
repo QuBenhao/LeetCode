9355b339e3ab75e9ed8d243024b113aeef6dfb79
benhao <qubenhao2@gmail.com>
2024-06-12 21:23:58 +0800

    test: java 2, 54, 2806 solve
    solution

--- problems/problems_2806/Solution.java
+++ problems/problems_2806/Solution.java
@@ -7,7 +7,7 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int accountBalanceAfterPurchase(int purchaseAmount) {
-
+        return 100 - (purchaseAmount + 5) / 10 * 10;
     }
 
     @Override
--- problems/problems_54/Solution.java
+++ problems/problems_54/Solution.java
@@ -1,13 +1,36 @@
 package problems.problems_54;
 
 import com.alibaba.fastjson.JSON;
+
 import java.util.*;
+
 import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
     public List<Integer> spiralOrder(int[][] matrix) {
-
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return new ArrayList<>();
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int left = 0, right = matrix[0].length - 1, top = 0, bottom = matrix.length - 1;
+             left <= right && top <= bottom; left++, right--, top++, bottom--) {
+            for (int col = left; col <= right; col++) {
+                ans.add(matrix[top][col]);
+            }
+            for (int row = top + 1; row <= bottom; row++) {
+                ans.add(matrix[row][right]);
+            }
+            if (left < right && top < bottom) {
+                for (int col = right - 1; col > left; col--) {
+                    ans.add(matrix[bottom][col]);
+                }
+                for (int row = bottom; row > top; row--) {
+                    ans.add(matrix[row][left]);
+                }
+            }
+        }
+        return ans;
     }
 
     @Override
