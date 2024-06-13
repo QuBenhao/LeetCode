8ced4cf8dda3078eb8a72e06b75964429137a9b8
benhao <qubenhao2@gmail.com>
2024-06-13 20:04:50 +0800

    test: python 2813, 1491, 28
    solution

--- problems/problems_2813/solution.py
+++ problems/problems_2813/solution.py
@@ -7,5 +7,20 @@ class Solution(solution.Solution):
         return self.findMaximumElegance(*test_input)
 
     def findMaximumElegance(self, items: List[List[int]], k: int) -> int:
-        pass
-
+        items.sort(key=lambda p: -p[0])  # 把利润从大到小排序
+        ans = total_profit = 0
+        vis = set()
+        duplicate = []  # 重复类别的利润
+        for i, (profit, category) in enumerate(items):
+            if i < k:
+                total_profit += profit  # 累加前 k 个项目的利润
+                if category not in vis:
+                    vis.add(category)
+                else:  # 重复类别
+                    duplicate.append(profit)
+            elif duplicate and category not in vis:  # 之前没有的类别
+                vis.add(category)  # len(vis) 变大
+                total_profit += profit - duplicate.pop()  # 用最小利润替换
+            # else: 比前面的利润小，而且类别还重复了，选它只会让 total_profit 变小，len(vis) 不变，优雅度不会变大
+            ans = max(ans, total_profit + len(vis) * len(vis))
+        return ans
