98f6b752e049481882a7aa8fdcd7091aff030629
GitHub Action <action@github.com>
2024-06-14 14:11:11 +0000

    [20240614] Add daily problem solution

--- problems/problems_2786/solution.py
+++ problems/problems_2786/solution.py
@@ -1,11 +1,11 @@
 import solution
 from typing import *
-from math import inf
 
 
 class Solution(solution.Solution):
     def solve(self, test_input=None):
         return self.maxScore(*test_input)
+
 
     def maxScore(self, nums: List[int], x: int) -> int:
         ans = nums[0]
@@ -17,3 +17,4 @@ class Solution(solution.Solution):
             ans = max(ans, cur)
             dp[idx] = max(dp[idx], cur)
         return ans
+
