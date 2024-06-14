bed941074f78d8d9c376e94067ef9356d767c6e1
benhao <qubenhao2@gmail.com>
2024-06-14 08:03:48 +0800

    test: python 2786, 682, 242
    solution

--- problems/problems_2786/solution.py
+++ problems/problems_2786/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+from math import inf
 
 
 class Solution(solution.Solution):
@@ -7,5 +8,12 @@ class Solution(solution.Solution):
         return self.maxScore(*test_input)
 
     def maxScore(self, nums: List[int], x: int) -> int:
-        pass
-
+        ans = nums[0]
+        dp = [-inf, -inf]
+        dp[nums[0] % 2] = nums[0]
+        for num in nums[1:]:
+            idx = num % 2
+            cur = max(dp[idx] + num, dp[idx ^ 1] + num - x)
+            ans = max(ans, cur)
+            dp[idx] = max(dp[idx], cur)
+        return ans
--- problems/problems_682/solution.py
+++ problems/problems_682/solution.py
@@ -7,5 +7,18 @@ class Solution(solution.Solution):
         return self.calPoints(test_input)
 
     def calPoints(self, operations: List[str]) -> int:
-        pass
-
+        ans, cur = 0, []
+        for op in operations:
+            match op:
+                case "+":
+                    cur.append(cur[-1] + cur[-2])
+                    ans += cur[-1]
+                case "D":
+                    cur.append(cur[-1] * 2)
+                    ans += cur[-1]
+                case "C":
+                    ans -= cur.pop()
+                case _:
+                    cur.append(int(op))
+                    ans += cur[-1]
+        return ans
