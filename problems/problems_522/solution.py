57494779aec14b5c1f6517a0ad00e059ba4283a0
benhao <qubenhao2@gmail.com>
2024-06-17 08:02:09 +0800

    test: python 522, 1822, 50
    solution

--- problems/problems_522/solution.py
+++ problems/problems_522/solution.py
@@ -7,5 +7,19 @@ class Solution(solution.Solution):
         return self.findLUSlength(test_input)
 
     def findLUSlength(self, strs: List[str]) -> int:
-        pass
+        def is_sub_seq(s: str, t: str) -> bool:
+            ptr_s = ptr_t = 0
+            while ptr_s < len(s) and ptr_t < len(t):
+                if s[ptr_s] == t[ptr_t]:
+                    ptr_s += 1
+                ptr_t += 1
+            return ptr_s == len(s)
 
+        ans = -1
+        for i, s in enumerate(strs):
+            for j, t in enumerate(strs):
+                if i != j and is_sub_seq(s, t):
+                    break
+            else:
+                ans = max(ans, len(s))
+        return ans
