b568b41ae4f76fffd6113cb7f4d97206a415b745
benhao <qubenhao2@gmail.com>
2024-06-18 06:56:06 +0800

    test: python 2288, 13
    solution

--- problems/problems_2288/solution.py
+++ problems/problems_2288/solution.py
@@ -7,5 +7,10 @@ class Solution(solution.Solution):
         return self.discountPrices(*test_input)
 
     def discountPrices(self, sentence: str, discount: int) -> str:
-        pass
-
+        ans = []
+        for s in sentence.split(" "):
+            if s.startswith("$") and len(s) > 1 and all(s[i].isdigit() for i in range(1, len(s))):
+                ans.append("$%.2f" % (int(s[1:]) * (100 - discount) / 100))
+            else:
+                ans.append(s)
+        return " ".join(ans)
