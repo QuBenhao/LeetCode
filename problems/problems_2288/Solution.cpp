c7eb0bf622cd9769067d277d2c455604e8cdcfe5
benhao <qubenhao2@gmail.com>
2024-06-18 07:42:08 +0800

    test: c++ 2288, 13
    solution

--- problems/problems_2288/Solution.cpp
+++ problems/problems_2288/Solution.cpp
@@ -1,5 +1,7 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
+#include <iomanip>
+#include <sstream>
 
 
 using namespace std;
@@ -8,7 +10,22 @@ using json = nlohmann::json;
 class Solution {
 public:
     string discountPrices(string sentence, int discount) {
-        
+        double d = 1 - discount / 100.0;
+        stringstream ss(sentence);
+        string ans, w;
+        while (ss >> w) { // 一边分割，一边加到答案中
+            if (!ans.empty()) {
+                ans += ' ';
+            }
+            if (w.length() > 1 && w[0] == '$' && all_of(w.begin() + 1, w.end(), ::isdigit)) {
+                stringstream s;
+                s << fixed << setprecision(2) << '$' << static_cast<double>(stoll(w.substr(1))) * d;
+                ans += s.str();
+            } else {
+                ans += w;
+            }
+        }
+        return ans;
     }
 };
 
