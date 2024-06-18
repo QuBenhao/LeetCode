02fb5b6c8ccc323882662713a6c8f7a7fc7ecb54
GitHub Action <action@github.com>
2024-06-18 14:09:55 +0000

    [20240618] Add daily problem solution

--- problems/problems_2288/solution.py
+++ problems/problems_2288/solution.py
@@ -6,11 +6,19 @@ class Solution(solution.Solution):
     def solve(self, test_input=None):
         return self.discountPrices(*test_input)
 
+
     def discountPrices(self, sentence: str, discount: int) -> str:
         ans = []
         for s in sentence.split(" "):
-            if s.startswith("$") and len(s) > 1 and all(s[i].isdigit() for i in range(1, len(s))):
-                ans.append("$%.2f" % (int(s[1:]) * (100 - discount) / 100))
-            else:
-                ans.append(s)
+            if len(s) > 1 and s.startswith("$"):
+                cur = 0
+                for i in range(1, len(s)):
+                    if not s[i].isdigit():
+                        break
+                    cur = 10 * cur + ord(s[i]) - ord('0')
+                else:
+                    ans.append("$%.2f" % (cur * (100 - discount) / 100))
+                    continue
+            ans.append(s)
         return " ".join(ans)
+
