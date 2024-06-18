c55854c9434801adfee3c41694535f3772ef5eb1
benhao <qubenhao2@gmail.com>
2024-06-18 22:17:26 +0800

    fix: python 2288 testcase
    issue add testcase with space inside

--- problems/problems_2288/solution.py
+++ problems/problems_2288/solution.py
@@ -10,15 +10,8 @@ class Solution(solution.Solution):
     def discountPrices(self, sentence: str, discount: int) -> str:
         ans = []
         for s in sentence.split(" "):
-            if len(s) > 1 and s.startswith("$"):
-                cur = 0
-                for i in range(1, len(s)):
-                    if not s[i].isdigit():
-                        break
-                    cur = 10 * cur + ord(s[i]) - ord('0')
-                else:
-                    ans.append("$%.2f" % (cur * (100 - discount) / 100))
-                    continue
-            ans.append(s)
+            if s.startswith("$") and len(s) > 1 and all(s[i].isdigit() for i in range(1, len(s))):
+                ans.append("$%.2f" % (int(s[1:]) * (100 - discount) / 100))
+            else:
+                ans.append(s)
         return " ".join(ans)
-
