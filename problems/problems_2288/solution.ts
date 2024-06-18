8b1e63da42831cb97c4b7126a490a5e6d1b1b3f5
benhao <qubenhao2@gmail.com>
2024-06-18 08:18:46 +0800

    test: typescript 2288, 13
    solution

--- problems/problems_2288/solution.ts
+++ problems/problems_2288/solution.ts
@@ -1,5 +1,28 @@
 function discountPrices(sentence: string, discount: number): string {
-    
+    const d: number = (100.0 - discount) / 100.0;
+	const ans: string[] = [];
+	sentence.split(" ").forEach(s => {
+		if (s.length > 1 && s.charAt(0) == '$') {
+			let cur: number = 0
+			let isNumber: boolean = true
+			for (let i = 1; i < s.length; i++) {
+				if (s.charAt(i) < '0' || s.charAt(i) > '9') {
+					isNumber = false
+					break
+				}
+				cur = cur * 10 + s.charCodeAt(i) - '0'.charCodeAt(0);
+			}
+			if (isNumber) {
+				const c: number = d * cur
+				ans.push(`$${c.toFixed(2)}`)
+			} else {
+				ans.push(s);
+			}
+		} else {
+			ans.push(s);
+		}
+	})
+	return ans.join(" ")
 };
 
 export function Solve(inputJsonElement: string): any {
