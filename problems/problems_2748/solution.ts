f7621d9c88ec171d5cf7b9be96aefaf7e30515c5
benhao <qubenhao2@gmail.com>
2024-06-20 22:52:30 +0800

    test: typescript 2748, 422
    solution

--- problems/problems_2748/solution.ts
+++ problems/problems_2748/solution.ts
@@ -1,5 +1,23 @@
-function countBeautifulPairs(nums: number[]): number {
+function gcd(a: number, b: number): number {
+	return b == 0 ? a : gcd(b, a % b);
+}
 
+function countBeautifulPairs(nums: number[]): number {
+	let ans: number = 0;
+	const counter: Array<number> = new Array<number>(10).fill(0);
+	for (let num of nums) {
+		const cur: number = num % 10;
+		for (let i: number = 1; i < counter.length; i++) {
+			if (counter[i] > 0 && gcd(cur, i) == 1) {
+				ans += counter[i];
+			}
+		}
+		while (num >= 10) {
+			num = Math.floor(num / 10);
+		}
+		counter[num]++;
+	}
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_422/solution.ts
+++ problems/problems_422/solution.ts
@@ -1,5 +1,17 @@
 function validWordSquare(words: string[]): boolean {
-
+	for (let i: number = 0; i < words.length; i++) {
+		if (words[i].length > words.length) {
+			return false;
+		}
+		for (let j: number = 0; j < i; j++) {
+			if ((words[i].length <= j) != (words[j].length <= i)) {
+				return false;
+			} else if (words[i].length > j && words[i][j] != words[j][i]) {
+				return false;
+			}
+		}
+	}
+	return true;
 };
 
 export function Solve(inputJsonElement: string): any {
