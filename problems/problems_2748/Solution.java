6e1ca65da0a70e0320648ec3287051e5a953a6b8
benhao <qubenhao2@gmail.com>
2024-06-20 22:40:09 +0800

    test: java 2748, 422
    solution

--- problems/problems_2748/Solution.java
+++ problems/problems_2748/Solution.java
@@ -6,8 +6,26 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
-    public int countBeautifulPairs(int[] nums) {
+    private int gcb(int a, int b) {
+        return b == 0 ? a : gcb(b, a % b);
+    }
 
+    public int countBeautifulPairs(int[] nums) {
+        int ans = 0;
+        int[] counter = new int[10];
+        for (int num: nums) {
+            int cur = num % 10;
+            for (int i = 1; i < 10; i++) {
+                if (counter[i] > 0 && gcb(cur, i) == 1) {
+                    ans += counter[i];
+                }
+            }
+            while (num >= 10) {
+                num /= 10;
+            }
+            counter[num]++;
+        }
+        return ans;
     }
 
     @Override
--- problems/problems_422/Solution.java
+++ problems/problems_422/Solution.java
@@ -7,7 +7,20 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public boolean validWordSquare(List<String> words) {
-
+        for (int i = 0; i < words.size(); i++) {
+            String word = words.get(i);
+            if (word.length() > words.size()) {
+                return false;
+            }
+            for (int j = 0; j < i; j++) {
+                if ((word.length() <= j) != (words.get(j).length() <= i)) {
+                    return false;
+                } else if (word.length() > j && word.charAt(j) != words.get(j).charAt(i)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
