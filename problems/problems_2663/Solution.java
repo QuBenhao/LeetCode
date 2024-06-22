85b46eb91562ab4fde5085fefb6359c98552a8be
benhao <qubenhao2@gmail.com>
2024-06-22 09:16:02 +0800

    test: c++, golang, java, typescript 2663
    solution

--- problems/problems_2663/Solution.java
+++ problems/problems_2663/Solution.java
@@ -6,8 +6,27 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
-    public String smallestBeautifulString(String s, int k) {
-
+    public String smallestBeautifulString(String S, int k) {
+        k += 'a';
+        char[] s = S.toCharArray();
+        int n = s.length;
+        int i = n - 1; // 从最后一个字母开始
+        s[i]++; // 先加一
+        while (i < n) {
+            if (s[i] == k) { // 需要进位
+                if (i == 0) { // 无法进位
+                    return "";
+                }
+                // 进位
+                s[i] = 'a';
+                s[--i]++;
+            } else if (i > 0 && s[i] == s[i - 1] || i > 1 && s[i] == s[i - 2]) {
+                s[i]++; // 如果 s[i] 和左侧的字符形成回文串，就继续增加 s[i]
+            } else {
+                i++; // 反过来检查后面是否有回文串
+            }
+        }
+        return new String(s);
     }
 
     @Override
--- problems/problems_2663/solution.go
+++ problems/problems_2663/solution.go
@@ -6,8 +6,28 @@ import (
 	"strings"
 )
 
-func smallestBeautifulString(s string, k int) string {
-
+func smallestBeautifulString(S string, k int) string {
+	limit := 'a' + byte(k)
+	s := []byte(S)
+	n := len(s)
+	i := n - 1 // 从最后一个字母开始
+	s[i]++ // 先加一
+	for i < n {
+		if s[i] == limit { // 需要进位
+			if i == 0 { // 无法进位
+				return ""
+			}
+			// 进位
+			s[i] = 'a'
+			i--
+			s[i]++
+		} else if i > 0 && s[i] == s[i-1] || i > 1 && s[i] == s[i-2] {
+			s[i]++ // 如果 s[i] 和左侧的字符形成回文串，就继续增加 s[i]
+		} else {
+			i++ // 反过来检查后面是否有回文串
+		}
+	}
+	return string(s)
 }
 
 func Solve(input string) interface{} {
