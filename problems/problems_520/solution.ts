eee607d72097f5d34662b13521b392860080e2f0
benhao <qubenhao2@gmail.com>
2024-06-23 17:07:34 +0800

    test: typescript 520
    solution

--- problems/problems_520/solution.ts
+++ problems/problems_520/solution.ts
@@ -1,5 +1,24 @@
 function detectCapitalUse(word: string): boolean {
-    
+    if (word.charAt(word.length - 1).toUpperCase() == word.charAt(word.length - 1)) {
+		for (let i: number = 0; i < word.length - 1; i++) {
+			if (word.charAt(i).toLowerCase() == word.charAt(i)) {
+				return false;
+			}
+		}
+	} else if (word.charAt(0).toUpperCase() == word.charAt(0)) {
+		for (let i: number = 1; i < word.length - 1; i++) {
+			if (word.charAt(i).toUpperCase() == word.charAt(i)) {
+				return false;
+			}
+		}
+	} else {
+		for (let i: number = 1; i < word.length - 1; i++) {
+			if (word.charAt(i).toUpperCase() == word.charAt(i)) {
+				return false;
+			}
+		}
+	}
+	return true;
 };
 
 export function Solve(inputJsonElement: string): any {
