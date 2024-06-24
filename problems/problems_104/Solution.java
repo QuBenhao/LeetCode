761084a3695411fe8a27b112891157630a84d4be
benhao <qubenhao2@gmail.com>
2024-06-24 08:21:44 +0800

    test: java 503, 104
    solution

--- problems/problems_104/Solution.java
+++ problems/problems_104/Solution.java
@@ -22,7 +22,7 @@ import qubhjava.models.TreeNode;
 
 public class Solution extends BaseSolution {
     public int maxDepth(TreeNode root) {
-
+        return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 
     @Override
--- problems/problems_503/Solution.java
+++ problems/problems_503/Solution.java
@@ -7,7 +7,21 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int[] nextGreaterElements(int[] nums) {
-
+        int n = nums.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n * 2; i++) {
+            while (!stack.empty() && nums[stack.peek()] < nums[i % n]) {
+                ans[stack.pop()] = nums[i % n];
+            }
+            if (i < n) {
+                stack.push(i);
+            } else if (stack.empty()) {
+                break;
+            }
+        }
+        return ans;
     }
 
     @Override
