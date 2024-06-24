a0f2b4c372da43f353f1655873ba739a279cf0d6
benhao <qubenhao2@gmail.com>
2024-06-24 22:08:25 +0800

    fix: java object support
    Support Java object questions, solved 1472 and 1603

--- problems/problems_1603/Solution.java
+++ problems/problems_1603/Solution.java
@@ -3,14 +3,21 @@ package problems.problems_1603;
 import com.alibaba.fastjson.JSON;
 import java.util.*;
 import qubhjava.BaseSolution;
+
+
 class ParkingSystem {
+    private final int[] parks;
 
     public ParkingSystem(int big, int medium, int small) {
-
+        parks = new int[]{big, medium, small};
     }
     
     public boolean addCar(int carType) {
-
+        if (this.parks[--carType] <= 0) {
+            return false;
+        }
+        this.parks[carType]--;
+        return true;
     }
 }
 
@@ -25,7 +32,22 @@ public class Solution extends BaseSolution {
 
     @Override
     public Object solve(String[] values) {
-        
-        return JSON.toJSON();
+        String[] operators = jsonArrayToStringArray(values[0]);
+		String[][] opValues = jsonArrayToString2DArray(values[1]);
+		int big = Integer.parseInt(opValues[0][0]);
+		int medium = Integer.parseInt(opValues[0][1]);
+		int small = Integer.parseInt(opValues[0][2]);
+		ParkingSystem obj = new ParkingSystem(big, medium, small);
+		List<Object> ans = new ArrayList<>(operators.length);
+		ans.add(null);
+		for (int i = 1; i < operators.length; i++) {
+			if (operators[i].compareTo("addCar") == 0) {
+				int carType = Integer.parseInt(opValues[i][0]);
+				ans.add(obj.addCar(carType));
+				continue;
+			}
+			ans.add(null);
+		}
+        return JSON.toJSON(ans);
     }
 }
--- python/lc_libs/java_writer.py
+++ python/lc_libs/java_writer.py
@@ -1,5 +1,5 @@
 import os.path
-from collections import deque
+from collections import deque, defaultdict
 
 from python.constants import SOLUTION_TEMPLATE_JAVA
 
@@ -62,6 +62,36 @@ def __process_variable_type(input_name: str, variable_name: str, rt_type: str) -
     return f"{rt_type} {variable_name} = FIXME({input_name})"
 
 
+def __parse_java_method(strip_line: str):
+    variables = []
+    parse_input = []
+    additional_import = set()
+    return_func = strip_line.split("(")[0].split(" ")[-1]
+    return_type = strip_line.split("(")[0].split(" ")[1]
+    input_parts = strip_line.split("(")[1].split(")")[0].strip().split(",")
+    for i, input_part in enumerate(input_parts):
+        var_split = input_part.strip().split(" ")
+        rt_type, variable = var_split[0], var_split[1]
+        variables.append(variable)
+        parse_input.append(__process_variable_type(f"values[{i}]", variable, rt_type))
+        if "ListNode" in rt_type:
+            additional_import.add("import qubhjava.models.ListNode;")
+        elif "TreeNode" in rt_type:
+            additional_import.add("import qubhjava.models.TreeNode;")
+    if "ListNode" in return_type:
+        additional_import.add("import qubhjava.models.ListNode;")
+        return_part = "ListNode.LinkedListToIntArray({}({}))".format(return_func, ", ".join(variables))
+    elif "TreeNode" in return_type:
+        additional_import.add("import qubhjava.models.TreeNode;")
+        return_part = "TreeNode.TreeNodeToArray({}({}))".format(return_func, ", ".join(variables))
+    elif return_type == "void":
+        parse_input.append("{}({});".format(return_func, ", ".join(variables)))
+        return_part = variables[0]
+    else:
+        return_part = "{}({})".format(return_func, ", ".join(variables))
+    return variables, parse_input, additional_import, return_part, return_func, return_type
+
+
 def write_solution_java(code_default: str, code: str = None, problem_id: str = "", problem_folder: str = "") -> str:
     import_packages = []
     body = []
@@ -70,43 +100,66 @@ def write_solution_java(code_default: str, code: str = None, problem_id: str = "
     import_part = True
     variables = []
     code = code if code else code_default
-    for line in code.split("\n"):
-        if "class Solution {" in line:
-            import_part = False
-            continue
-        if import_part:
-            import_packages.append(line)
-        else:
-            if line == "}":
-                continue
+    if "object will be instantiated and called as such:" in code:
+        return_part = "ans"
+        parse_input.append("String[] operators = jsonArrayToStringArray(values[0]);")
+        parse_input.append("String[][] opValues = jsonArrayToString2DArray(values[1]);")
+        class_name = ""
+        all_return_parts = []
+        func_parse_input = defaultdict(list)
+        for line in code.split("\n"):
             strip_line = line.strip()
-            additional_import = set()
-            if strip_line.startswith("public ") and strip_line.endswith("{"):
-                return_func = strip_line.split("(")[0].split(" ")[-1]
-                return_type = strip_line.split("(")[0].split(" ")[1]
-                input_parts = strip_line.split("(")[1].split(")")[0].strip().split(",")
-                for i, input_part in enumerate(input_parts):
-                    var_split = input_part.strip().split(" ")
-                    rt_type, variable = var_split[0], var_split[1]
-                    variables.append(variable)
-                    parse_input.append(__process_variable_type(f"values[{i}]", variable, rt_type))
-                    if "ListNode" in rt_type:
-                        additional_import.add("import qubhjava.models.ListNode;")
-                    elif "TreeNode" in rt_type:
-                        additional_import.add("import qubhjava.models.TreeNode;")
-                if "ListNode" in return_type:
-                    additional_import.add("import qubhjava.models.ListNode;")
-                    return_part = "ListNode.LinkedListToIntArray({}({}))".format(return_func, ", ".join(variables))
-                elif "TreeNode" in return_type:
-                    additional_import.add("import qubhjava.models.TreeNode;")
-                    return_part = "TreeNode.TreeNodeToArray({}({}))".format(return_func, ", ".join(variables))
-                elif return_type == "void":
-                    parse_input.append("{}({});".format(return_func, ", ".join(variables)))
-                    return_part = variables[0]
+            if strip_line.startswith("class ") and strip_line.endswith("{"):
+                class_name = strip_line.split("{")[0].split("class ")[-1].strip()
+            elif strip_line.startswith("public ") and strip_line.endswith("{"):
+                vs, pi, ai, rp, func_name, rt = __parse_java_method(strip_line)
+                variables.extend(vs)
+                import_packages.extend(ai)
+                if func_name == class_name:
+                    parse_input.extend([p.replace("values", "opValues[0]") for p in pi])
+                    parse_input.append(f"{class_name} obj = new {rp};")
                 else:
-                    return_part = "{}({})".format(return_func, ", ".join(variables))
-            import_packages.extend(additional_import)
-            body.append(line)
+                    func_parse_input[func_name].extend(["\t\t" + p.replace("values", "opValues[i]") for p in pi])
+                    all_return_parts.append((func_name, rt, rp))
+        import_packages.append("")
+        import_packages.append("")
+        import_packages.extend(code.split("\n"))
+        parse_input.append("List<Object> ans = new ArrayList<>(operators.length);")
+        parse_input.append("ans.add(null);")
+        parse_input.append("for (int i = 1; i < operators.length; i++) {")
+        for func_name, rt, rp in all_return_parts:
+            parse_input.append("\tif (operators[i].compareTo(\"" + func_name + "\") == 0) {")
+            if rt == "void":
+                parse_input.extend(func_parse_input[func_name][:-1])
+                parse_input.append(f"\t\tobj.{func_name}({rp});")
+                parse_input.append(f"\t\tans.add(null);")
+            else:
+                parse_input.extend(func_parse_input[func_name])
+                parse_input.append(f"\t\tans.add(obj.{rp});")
+            parse_input.append("\t\tcontinue;")
+            parse_input.append("\t}")
+        parse_input.append("\tans.add(null);")
+        parse_input.append("}")
+    else:
+        for line in code.split("\n"):
+            if "class Solution {" in line:
+                import_part = False
+                continue
+            if import_part:
+                import_packages.append(line)
+            else:
+                if line == "}":
+                    continue
+                strip_line = line.strip()
+                additional_import = set()
+                if strip_line.startswith("public ") and strip_line.endswith("{"):
+                    vs, pi, ai, rp, _, _ = __parse_java_method(strip_line)
+                    variables.extend(vs)
+                    parse_input.extend(pi)
+                    additional_import.update(ai)
+                    return_part = rp
+                import_packages.extend(additional_import)
+                body.append(line)
 
     return SOLUTION_TEMPLATE_JAVA.format(
         problem_folder,
@@ -138,7 +191,11 @@ def get_solution_code_java(root_path, problem_folder: str, problem_id: str) -> (
         return "", problem_id
     final_codes = deque([])
     with open(file_path, 'r', encoding="utf-8") as f:
-        lines = f.read().split("\n")
+        content = f.read()
+        is_obj_question = False
+        if "object will be instantiated and called as such:" in content:
+            is_obj_question = True
+        lines = content.split("\n")
         solve_part = []
         for line in lines:
             if line.startswith("package "):
@@ -157,11 +214,19 @@ def get_solution_code_java(root_path, problem_folder: str, problem_id: str) -> (
                 for _ in range(line.count("}")):
                     solve_part.pop()
                 continue
-            # TODO: Object problem case, no any code needed from here
             if "public class Solution extends BaseSolution {" in line:
-                final_codes.append("class Solution {")
+                if not is_obj_question:
+                    final_codes.append("class Solution {")
                 continue
             final_codes.append(line)
     while final_codes and final_codes[0].strip() == '':
         final_codes.popleft()
+
+    if is_obj_question:
+        while final_codes and final_codes[-1].strip() == '':
+            final_codes.pop()
+        if final_codes and final_codes[-1].strip() == "}":
+            final_codes.pop()
+        while final_codes and final_codes[-1].strip() == '':
+            final_codes.pop()
     return "\n".join(final_codes), problem_id
