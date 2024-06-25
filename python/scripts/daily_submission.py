c504b0ec16e4c799690f8aae3ade9c9e9d2f6a49
benhao <qubenhao2@gmail.com>
2024-06-25 22:46:49 +0800

    fix: try empty problem folder
    daily script

--- python/scripts/daily_submission.py
+++ python/scripts/daily_submission.py
@@ -46,7 +46,7 @@ def main(user_slug: str, cookie: Optional[str], languages: list[str], problem_fo
             question_id = question_id.replace(" ", "_")
             cache = set()
             info = None
-            if problem_folder is not None:
+            if not problem_folder:
                 tmp_problem_folder = problem_folder
             elif question_id == daily_question:
                 tmp_problem_folder = get_default_folder()
