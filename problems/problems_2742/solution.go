e8a11f4c1a80f3fbded73f3f045370f5ae08fe26
benhao <qubenhao2@gmail.com>
2024-06-28 07:47:32 +0800

    test: golang 2742, 41
    solution

--- problems/problems_2742/solution.go
+++ problems/problems_2742/solution.go
@@ -3,11 +3,23 @@ package problem2742
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"strings"
 )
 
-func paintWalls(cost []int, time []int) int {
-
+func paintWalls(cost, time []int) int {
+	n := len(cost)
+	f := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		f[i] = math.MaxInt / 2 // 防止加法溢出
+	}
+	for i, c := range cost {
+		t := time[i] + 1 // 注意这里加一了
+		for j := n; j > 0; j-- {
+			f[j] = min(f[j], f[max(j-t, 0)]+c)
+		}
+	}
+	return f[n]
 }
 
 func Solve(input string) interface{} {
--- problems/problems_41/solution.go
+++ problems/problems_41/solution.go
@@ -6,8 +6,31 @@ import (
 	"strings"
 )
 
-func firstMissingPositive(nums []int) int {
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
 
+func firstMissingPositive(nums []int) int {
+	n := len(nums)
+	for i := 0; i < n; i++ {
+		if nums[i] <= 0 {
+			nums[i] = n + 1
+		}
+	}
+	for i := 0; i < n; i++ {
+		if num := abs(nums[i]); num <= n {
+			nums[num-1] = -abs(nums[num-1])
+		}
+	}
+	for i := 0; i < n; i++ {
+		if nums[i] > 0 {
+			return i + 1
+		}
+	}
+	return n + 1
 }
 
 func Solve(input string) interface{} {
