cd3c381a11b42f917d0ac62bb5846083c54a6112
qubh <qubh@xiaopeng.com>
2024-06-27 13:12:40 +0800

    test: c++ 2734
    solution

--- problems/problems_2734/Solution.cpp
+++ problems/problems_2734/Solution.cpp
@@ -8,7 +8,17 @@ using json = nlohmann::json;
 class Solution {
 public:
     string smallestString(string s) {
-        
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (s[i] > 'a') {
+                for (; i < n && s[i] > 'a'; i++) {
+                    s[i]--;
+                }
+                return s;
+            }
+        }
+        s.back() = 'z';
+        return s;
     }
 };
 
