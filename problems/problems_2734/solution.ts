e699aa3c435d796c173f3a7807a227b78a8c14dc
qubh <qubh@xiaopeng.com>
2024-06-27 15:26:00 +0800

    test: typescript 2734, 763
    solution

--- problems/problems_2734/solution.ts
+++ problems/problems_2734/solution.ts
@@ -1,5 +1,15 @@
 function smallestString(s: string): string {
-
+	const arr: string[] = s.split("");
+	for (let i: number = 0; i < arr.length; i++) {
+		if (arr[i] > 'a') {
+			for (; i < arr.length && arr[i] > 'a'; i++) {
+				arr[i] = String.fromCharCode(arr[i].charCodeAt(0) - 1);
+			}
+			return arr.join("");
+		}
+	}
+	arr[arr.length - 1] = 'z';
+	return arr.join("");
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_763/solution.ts
+++ problems/problems_763/solution.ts
@@ -1,5 +1,18 @@
 function partitionLabels(s: string): number[] {
-    
+	const last: number[] = new Array(26);
+	for (let i: number = 0; i < s.length; i++) {
+		last[s.charCodeAt(i) - 'a'.charCodeAt(0)] = i;
+	}
+	const partition: number[] = [];
+	let start: number = 0, end: number = 0;
+	for (let i: number = 0; i < s.length; i++) {
+		end = Math.max(end, last[s.charCodeAt(i) - 'a'.charCodeAt(0)]);
+		if (i === end) {
+			partition.push(end - start + 1);
+			start = end + 1;
+		}
+	}
+	return partition;    
 };
 
 export function Solve(inputJsonElement: string): any {
