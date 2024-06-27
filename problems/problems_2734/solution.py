0e65447c3064f8c83e2f8cd58641f9b574ac1700
benhao <qubenhao2@gmail.com>
2024-06-27 08:25:11 +0800

    test: python 2734, 763
    solution

--- problems/problems_2734/solution.py
+++ problems/problems_2734/solution.py
@@ -7,5 +7,14 @@ class Solution(solution.Solution):
         return self.smallestString(test_input)
 
     def smallestString(self, s: str) -> str:
-        pass
-
+        left = 0
+        while left < len(s) and s[left] == 'a':
+            left += 1
+        if left == len(s):
+            return s[:len(s) - 1] + 'z'
+        right = left
+        ans = s[:left]
+        while right < len(s) and s[right] != 'a':
+            ans += chr(ord(s[right]) - 1)
+            right += 1
+        return ans + s[right:]
--- problems/problems_763/solution.py
+++ problems/problems_763/solution.py
@@ -7,5 +7,12 @@ class Solution(solution.Solution):
         return self.partitionLabels(test_input)
 
     def partitionLabels(self, s: str) -> List[int]:
-        pass
-
+        idx_map = {c: i for i, c in enumerate(s)}
+        ans = []
+        start = end = 0
+        for i, c in enumerate(s):
+            end = max(end, idx_map[c])
+            if i == end:
+                ans.append(end - start + 1)
+                start = end + 1
+        return ans
