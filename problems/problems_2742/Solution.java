8bfdee770a9f0ba49092de7ba4be271073c4a680
qubh <qubh@xiaopeng.com>
2024-06-28 18:06:55 +0800

    test: java 2742, 41
    solution

--- problems/problems_2742/Solution.java
+++ problems/problems_2742/Solution.java
@@ -7,7 +7,17 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int paintWalls(int[] cost, int[] time) {
-
+        int n = cost.length;
+        int[] f = new int[n + 1];
+        Arrays.fill(f, Integer.MAX_VALUE / 2);
+        f[0] = 0;
+        for (int i = 0; i < n; i++) {
+            int t = time[i] + 1;
+            for (int j = n; j > 0; j--) {
+                f[j] = Math.min(f[j], f[Math.max(j - t, 0)] + cost[i]);
+            }
+        }
+        return f[n];
     }
 
     @Override
--- problems/problems_41/Solution.java
+++ problems/problems_41/Solution.java
@@ -7,7 +7,24 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int firstMissingPositive(int[] nums) {
-
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            int num = Math.abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -Math.abs(nums[num - 1]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
     }
 
     @Override
