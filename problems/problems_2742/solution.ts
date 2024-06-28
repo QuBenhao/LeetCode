b2e8ed462cb34a78bfd22d0859256314f2d88694
qubh <qubh@xiaopeng.com>
2024-06-28 18:13:40 +0800

    test: typescript 2742, 41
    solution

--- problems/problems_2742/solution.ts
+++ problems/problems_2742/solution.ts
@@ -1,5 +1,14 @@
 function paintWalls(cost: number[], time: number[]): number {
-
+	const n: number = cost.length;
+	const f: number[] = new Array(n + 1).fill(Number.MAX_SAFE_INTEGER / 2);
+	f[0] = 0;
+	for (let i = 0; i < n; i++) {
+		const t: number = time[i] + 1;
+		for (let j = n; j > 0; j--) {
+			f[j] = Math.min(f[j], f[Math.max(0, j - t)] + cost[i]);
+		}
+	}
+	return f[n];
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_41/solution.ts
+++ problems/problems_41/solution.ts
@@ -1,5 +1,22 @@
 function firstMissingPositive(nums: number[]): number {
-    
+    const n: number = nums.length;
+	for (let i: number = 0; i < n; i++) {
+		if (nums[i] <= 0) {
+			nums[i] = n + 1;
+		}
+	}
+	for (let i: number = 0; i < n; i++) {
+		const num: number = Math.abs(nums[i]);
+		if (num <= n) {
+			nums[num - 1] = -Math.abs(nums[num - 1]);
+		}
+	}
+	for (let i: number = 0; i < n; i++) {
+		if (nums[i] > 0) {
+			return i + 1;
+		}
+	}
+	return n + 1;
 };
 
 export function Solve(inputJsonElement: string): any {
