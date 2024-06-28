d57f5bec65099151cdd142cee44e25cba580c8e1
qubh <qubh@xiaopeng.com>
2024-06-28 17:31:08 +0800

    test: c++ 2742, 41
    solution

--- problems/problems_2742/Solution.cpp
+++ problems/problems_2742/Solution.cpp
@@ -8,7 +8,17 @@ using json = nlohmann::json;
 class Solution {
 public:
     int paintWalls(vector<int>& cost, vector<int>& time) {
-        
+        auto n = cost.size();
+        auto f = vector<int>(n + 1, INT32_MAX / 2);
+        f[0] = 0;
+        for (size_t i = 0; i < n; i++) {
+            auto t = time[i] + 1;
+            for (size_t j = n; j > 0; j--) {
+                f[j] =
+                    min(f[j], f[max(j - t, static_cast<size_t>(0))] + cost[i]);
+            }
+        }
+        return f[n];
     }
 };
 
--- problems/problems_41/Solution.cpp
+++ problems/problems_41/Solution.cpp
@@ -8,7 +8,24 @@ using json = nlohmann::json;
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-
+        size_t n = nums.size();
+        for (size_t i = 0; i < n; i++) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+        for (size_t i = 0; i < n; i++) {
+            int num = abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -abs(nums[num - 1]);
+            }
+        }
+        for (size_t i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
     }
 };
 
