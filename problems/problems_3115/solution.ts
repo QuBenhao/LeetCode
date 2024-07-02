54144fc9a3a9e70f7ba09b049e972149660e206c
benhao <qubenhao2@gmail.com>
2024-07-02 08:38:05 +0800

    test: typescript 3115
    solution

--- problems/problems_3115/solution.ts
+++ problems/problems_3115/solution.ts
@@ -1,5 +1,20 @@
 function maximumPrimeDifference(nums: number[]): number {
-    
+    const isPrime = (n: number): boolean => {
+		for (let i: number = 2; i * i <= n; i++) {
+			if (n % i === 0) {
+				return false;
+			}
+		}
+		return n >= 2;
+	}
+	let left: number = 0, right: number = nums.length - 1;
+	while (left < right && !isPrime(nums[left])) {
+		left++;
+	}
+	while (left < right && !isPrime(nums[right])) {
+		right--;
+	}
+	return right - left;
 };
 
 export function Solve(inputJsonElement: string): any {
