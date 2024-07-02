76d14407a1ea57d5469c6ad7f25242fa2bd62481
benhao <qubenhao2@gmail.com>
2024-07-02 08:19:56 +0800

    fix: submit script
    get_solution_code now based on writer instead of method directly

--- problems/problems_3115/solution.py
+++ problems/problems_3115/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+from math import isqrt
 
 
 class Solution(solution.Solution):
@@ -7,5 +8,16 @@ class Solution(solution.Solution):
         return self.maximumPrimeDifference(test_input)
 
     def maximumPrimeDifference(self, nums: List[int]) -> int:
-        pass
+        def is_prime(n: int) -> bool:
+            return n >= 2 and all(n % i for i in range(2, isqrt(n) + 1))
 
+        left, right = 0, len(nums) - 1
+        while left < right:
+            if is_prime(nums[left]):
+                break
+            left += 1
+        while left < right:
+            if is_prime(nums[right]):
+                break
+            right -= 1
+        return right - left
