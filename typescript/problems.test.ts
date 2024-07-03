45f9a91711975f5e20ab962ce118617e8f573e15
qubh <qubh@xiaopeng.com>
2024-07-03 14:33:57 +0800

    feat: typescript problems timeout
    each testcase timeout in 3s

--- typescript/problems.test.ts
+++ typescript/problems.test.ts
@@ -1,39 +1,49 @@
 import * as fs from 'fs';
-
+import * as ts from "typescript";
 var _ = require('lodash-contrib');
+const vm = require('node:vm');
 
 
 const PROBLEMS: string[][] = [['560', 'problems']];
 
 for (const [problemId, problemFolder] of PROBLEMS) {
     describe(`Test for problem ${problemId}`, () => {
-        let Solve: any;
         let inputJson: any;
         let outputJson: any;
+        let script: any;
 
         beforeAll(async () => {
             let testCasePath = `${problemFolder}/${problemFolder}_${problemId}/testcase`;
+            let solPath: string = `${problemFolder}/${problemFolder}_${problemId}/solution.ts`;
             if (!fs.existsSync(testCasePath)) {
                 console.log(`Problem in ${problemFolder} not found, try premiums...`);
                 testCasePath = `premiums/premiums_${problemId}/testcase`;
+                solPath = `premiums/premiums_${problemId}/solution.ts`;
             }
             const fileContent: string = fs.readFileSync(testCasePath, "utf-8");
             const splits: string[] = fileContent.split("\n");
             const inputs: string = splits[0], outputs: string = splits[1];
             inputJson = JSON.parse(inputs);
             outputJson = JSON.parse(outputs);
-
-            const solution = await import(`../${problemFolder}/${problemFolder}_${problemId}/solution`);
-            Solve = solution.Solve;
+            let solutionFileContent: string = fs.readFileSync(solPath, "utf-8");
+            solutionFileContent = solutionFileContent.replace("export function Solve", "function Solve");
+            solutionFileContent += "const execResult = Solve(testInputJsonString);"
+            let result = ts.transpileModule(solutionFileContent, { compilerOptions: { module: ts.ModuleKind.ES2022 }});
+            const codeText: string = result["outputText"];
+            script = new vm.Script(codeText);
         });
 
         test(`Test solution ${problemId}`, () => {
-            expect(Solve).toBeDefined();
+            expect(script).toBeDefined();
             for (let i: number = 0; i < inputJson.length; i++) {
+                const context = { testInputJsonString: inputJson[i], execResult: null};
+                vm.createContext(context); // Contextify the object.
+                script.runInContext(context, {timeout: 3000});
+                const result: any = context.execResult;
                 if (_.isFloat(outputJson[i])) {
-                    expect(Solve(inputJson[i])).toBeCloseTo(outputJson[i]);
+                    expect(result).toBeCloseTo(outputJson[i]);
                 } else {
-                    expect(Solve(inputJson[i])).toEqual(outputJson[i]);
+                    expect(result).toEqual(outputJson[i]);
                 }
             }
         })
