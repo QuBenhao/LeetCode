7bb2578fec069b26d667fc312a91d73e379c2037
benhao <qubenhao2@gmail.com>
2024-07-03 08:03:17 +0800

    test: typescript 3099, 560
    solution

--- problems/problems_3099/solution.ts
+++ problems/problems_3099/solution.ts
@@ -1,5 +1,9 @@
 function sumOfTheDigitsOfHarshadNumber(x: number): number {
-    
+    let s: number = 0;
+	for (let num: number = x; num > 0; num = Math.floor(num / 10)) {
+		s += num % 10;
+	}
+	return x % s == 0 ? s : -1;
 };
 
 export function Solve(inputJsonElement: string): any {
