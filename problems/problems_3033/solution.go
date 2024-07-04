c0ef7bf04231cf1b92b63b39fd9d1f3175d5ef7f
benhao <qubenhao2@gmail.com>
2024-07-05 07:34:21 +0800

    test: 3033, 283 golang
    solution

--- problems/problems_3033/solution.go
+++ problems/problems_3033/solution.go
@@ -7,7 +7,21 @@ import (
 )
 
 func modifiedMatrix(matrix [][]int) [][]int {
-
+	for m, n, j := len(matrix), len(matrix[0]), 0; j < n; j++ {
+		mx := -1
+		var remain []int
+		for i := 0; i < m; i++ {
+			if matrix[i][j] != -1 {
+				mx = max(mx, matrix[i][j])
+			} else {
+				remain = append(remain, i)
+			}
+		}
+		for _, i := range remain {
+			matrix[i][j] = mx
+		}
+	}
+	return matrix
 }
 
 func Solve(inputJsonValues string) interface{} {
