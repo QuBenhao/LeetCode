1d034893f6cdd2d0955620ddc527b7f239749b6b
benhao <qubenhao2@gmail.com>
2024-07-05 07:29:21 +0800

    test: 3033, 283 python
    solution

--- problems/problems_3033/solution.py
+++ problems/problems_3033/solution.py
@@ -7,5 +7,15 @@ class Solution(solution.Solution):
         return self.modifiedMatrix(test_input)
 
     def modifiedMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
-        pass
-
+        m, n = len(matrix), len(matrix[0])
+        for j in range(n):
+            mx = 0
+            remain = []
+            for i in range(m):
+                if matrix[i][j] == -1:
+                    remain.append(i)
+                    continue
+                mx = max(mx, matrix[i][j])
+            for i in remain:
+                matrix[i][j] = mx
+        return matrix
