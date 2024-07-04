b98dde6c3a94cad68b0ffb361d79142fbd3d3dc5
benhao <qubenhao2@gmail.com>
2024-07-05 07:48:45 +0800

    test: 3033, 283 java
    solution

--- problems/problems_3033/Solution.java
+++ problems/problems_3033/Solution.java
@@ -7,7 +7,21 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int[][] modifiedMatrix(int[][] matrix) {
-
+        for (int m = matrix.length, n = matrix[0].length, j = 0; j < n; j++) {
+            int mx = -1;
+            List<Integer> remain = new ArrayList<>();
+            for (int i = 0; i < m; i++) {
+                if (matrix[i][j] != -1) {
+                    mx = Math.max(mx, matrix[i][j]);
+                } else {
+                    remain.add(i);
+                }
+            }
+            for (int i: remain) {
+                matrix[i][j] = mx;
+            }
+        }
+        return matrix;
     }
 
     @Override
