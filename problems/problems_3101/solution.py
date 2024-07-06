c3f60c216b76a59ba28fa0b436562b3aa879b05d
benhao <qubenhao2@gmail.com>
2024-07-06 08:56:55 +0800

    test: python 3101, 3, 73
    solution

--- problems/problems_3101/solution.py
+++ problems/problems_3101/solution.py
@@ -7,5 +7,11 @@ class Solution(solution.Solution):
         return self.countAlternatingSubarrays(test_input)
 
     def countAlternatingSubarrays(self, nums: List[int]) -> int:
-        pass
-
+        ans = cnt = 0
+        for i in range(len(nums)):
+            if i == 0 or nums[i] != nums[i - 1]:
+                cnt += 1
+            else:
+                cnt = 1
+            ans += cnt
+        return ans
