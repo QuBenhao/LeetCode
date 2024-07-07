2d790620a6b5137fc3dcff191345f270ca0e60c1
benhao <qubenhao2@gmail.com>
2024-07-07 15:08:40 +0800

    test: golang 35, 62
    solution

--- problems/problems_62/solution.go
+++ problems/problems_62/solution.go
@@ -7,7 +7,16 @@ import (
 )
 
 func uniquePaths(m int, n int) int {
-
+	dp := make([]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = 1
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[j] += dp[j-1]
+		}
+	}
+	return dp[n-1]
 }
 
 func Solve(inputJsonValues string) interface{} {
