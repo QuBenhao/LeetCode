63df3b42e19fbf5f5ead272c1857863ecde03c64
benhao <qubenhao2@gmail.com>
2024-07-07 15:16:49 +0800

    test: typescript 35, 62
    solution

--- problems/problems_35/solution.ts
+++ problems/problems_35/solution.ts
@@ -1,5 +1,14 @@
 function searchInsert(nums: number[], target: number): number {
-    
+    let left: number = 0, right: number = nums.length;
+	while (left < right) {
+		const mid: number = left + Math.floor((right - left) / 2);
+		if (nums[mid] < target) {
+			left = mid + 1;
+		} else {
+			right = mid;
+		}
+	}
+	return left;
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_62/solution.ts
+++ problems/problems_62/solution.ts
@@ -1,5 +1,11 @@
 function uniquePaths(m: number, n: number): number {
-    
+	const dp: number[] = new Array(n).fill(1);
+	for (let i: number = 1; i < m; i++) {
+		for (let j: number = 1; j < n; j++) {
+			dp[j] += dp[j - 1];
+		}
+	}
+	return dp[n - 1];
 };
 
 export function Solve(inputJsonElement: string): any {
