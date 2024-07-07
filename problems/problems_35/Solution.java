1a2596b20a4a5c8835e624740c20cdf79d55c938
benhao <qubenhao2@gmail.com>
2024-07-07 15:14:32 +0800

    test: java 35, 62
    solution

--- problems/problems_35/Solution.java
+++ problems/problems_35/Solution.java
@@ -7,7 +7,16 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int searchInsert(int[] nums, int target) {
-
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
     }
 
     @Override
--- problems/problems_62/Solution.java
+++ problems/problems_62/Solution.java
@@ -7,7 +7,14 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int uniquePaths(int m, int n) {
-
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp[n - 1];
     }
 
     @Override
