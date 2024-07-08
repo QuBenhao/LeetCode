09b55b9d97bd2ca3663ca6f49022bf995b801e99
benhao <qubenhao2@gmail.com>
2024-07-08 08:08:36 +0800

    test: java 724, 70
    solution

--- problems/problems_70/Solution.java
+++ problems/problems_70/Solution.java
@@ -7,7 +7,13 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int climbStairs(int n) {
-
+        int[] dp = new int[3];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i % 3] = dp[(i - 1) % 3] + dp[(i - 2) % 3];
+        }
+        return dp[n % 3];
     }
 
     @Override
--- problems/problems_724/Solution.java
+++ problems/problems_724/Solution.java
@@ -7,7 +7,19 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int pivotIndex(int[] nums) {
-
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum -= nums[i];
+            if (leftSum == sum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
     }
 
     @Override
