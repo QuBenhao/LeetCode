e2b041b4733562008a4f8ba4758b45c761c65f59
benhao <qubenhao2@gmail.com>
2024-07-08 08:13:03 +0800

    test: typescript 724, 70
    solution

--- problems/problems_70/solution.ts
+++ problems/problems_70/solution.ts
@@ -1,5 +1,11 @@
 function climbStairs(n: number): number {
-    
+	const dp: number[] = new Array(3);
+	dp[1] = 1;
+	dp[2] = 2;
+	for (let i: number = 3; i <= n; i++) {
+		dp[i % 3] = dp[(i - 1) % 3] + dp[(i - 2) % 3];
+	}
+	return dp[n % 3];
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_724/solution.ts
+++ problems/problems_724/solution.ts
@@ -1,5 +1,14 @@
 function pivotIndex(nums: number[]): number {
-    
+    let sum: number = nums.reduce((a, b) => a + b, 0);
+	let leftSum: number = 0;
+	for (let i: number = 0; i < nums.length; i++) {
+		sum -= nums[i];
+		if (leftSum === sum) {
+			return i;
+		}
+		leftSum += nums[i];
+	}
+	return -1;
 };
 
 export function Solve(inputJsonElement: string): any {
