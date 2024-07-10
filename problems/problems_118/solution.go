e32dbf5d58332c8cf84c1ff52427b5037543401d
benhao <qubenhao2@gmail.com>
2024-07-10 08:15:56 +0800

    test: golang 2970, 118
    solution

--- problems/problems_118/solution.go
+++ problems/problems_118/solution.go
@@ -7,7 +7,16 @@ import (
 )
 
 func generate(numRows int) [][]int {
-
+	ans := make([][]int, numRows)
+	for i := 0; i < numRows; i++ {
+		ans[i] = make([]int, i+1)
+		ans[i][0], ans[i][i] = 1, 1
+		for j := 1; j < i/2+1; j++ {
+			ans[i][j] = ans[i-1][j-1] + ans[i-1][j]
+			ans[i][i-j] = ans[i][j]
+		}
+	}
+	return ans
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_2970/solution.go
+++ problems/problems_2970/solution.go
@@ -7,7 +7,24 @@ import (
 )
 
 func incremovableSubarrayCount(nums []int) int {
-
+	n := len(nums)
+	i := 0
+	for i < n-1 && nums[i] < nums[i+1] {
+		i++
+	}
+	if i == n-1 {
+		return n * (n + 1) / 2
+	}
+	ans := i + 2
+	j := n - 1
+	for j == n-1 || nums[j] < nums[j+1] {
+		for i >= 0 && nums[i] >= nums[j] {
+			i--
+		}
+		ans += i + 2
+		j--
+	}
+	return ans
 }
 
 func Solve(inputJsonValues string) interface{} {
