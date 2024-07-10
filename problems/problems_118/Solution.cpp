9b0ca59d21748395e87dc369597c0893c91edc8c
benhao <qubenhao2@gmail.com>
2024-07-10 08:21:21 +0800

    test: c++ 2970, 118
    solution

--- problems/problems_118/Solution.cpp
+++ problems/problems_118/Solution.cpp
@@ -8,7 +8,14 @@ using json = nlohmann::json;
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-
+        auto ans = vector<vector<int>>(numRows);
+        for (auto i = 0; i < numRows; i++) {
+            ans[i] = vector<int>(i + 1, 1);
+            for (auto j = 1; j < i / 2 + 1; j++) {
+                ans[i][j] = ans[i][i - j] = ans[i - 1][j - 1] + ans[i - 1][j];
+            }
+        }
+        return ans;
     }
 };
 
