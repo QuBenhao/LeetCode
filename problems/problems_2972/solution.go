48af38b37da66240c5a7f32b687941dd09ba6bb3
benhao <qubenhao2@gmail.com>
2024-07-11 07:22:38 +0800

    test: golang 2972, 46
    solution

--- problems/problems_2972/solution.go
+++ problems/problems_2972/solution.go
@@ -7,7 +7,24 @@ import (
 )
 
 func incremovableSubarrayCount(nums []int) int64 {
-
+	n := len(nums)
+	i := 0
+	for i < n-1 && nums[i] < nums[i+1] {
+		i++
+	}
+	if i == n-1 {
+		return int64(n) * int64(n+1) / 2
+	}
+	ans := int64(i + 2)
+	j := n - 1
+	for j == n-1 || nums[j] < nums[j+1] {
+		for i >= 0 && nums[i] >= nums[j] {
+			i--
+		}
+		ans += int64(i + 2)
+		j--
+	}
+	return ans
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_46/solution.go
+++ problems/problems_46/solution.go
@@ -6,8 +6,23 @@ import (
 	"strings"
 )
 
-func permute(nums []int) [][]int {
-
+func permute(nums []int) (ans [][]int) {
+	var backtrack func(int)
+	backtrack = func(idx int) {
+		if idx == len(nums) {
+			tmp := make([]int, len(nums))
+			copy(tmp, nums)
+			ans = append(ans, tmp)
+			return
+		}
+		for i := idx; i < len(nums); i++ {
+			nums[i], nums[idx] = nums[idx], nums[i]
+			backtrack(idx + 1)
+			nums[i], nums[idx] = nums[idx], nums[i]
+		}
+	}
+	backtrack(0)
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
