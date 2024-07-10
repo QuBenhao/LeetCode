fb8233ff037761449171ebce487b7ea99f232025
benhao <qubenhao2@gmail.com>
2024-07-10 08:31:14 +0800

    test: typescript 2970, 118
    solution

--- problems/problems_118/solution.ts
+++ problems/problems_118/solution.ts
@@ -1,5 +1,14 @@
 function generate(numRows: number): number[][] {
-    
+	const ans: number[][] = [[1]];
+	for (let i: number = 1; i < numRows; i++) {
+		const row: number[] = new Array(i + 1).fill(1);
+		for (let j: number = 1; j < Math.floor(i / 2) + 1; j++) {
+			row[j] = ans[i - 1][j - 1] + ans[i - 1][j];
+			row[i - j] = row[j];
+		}
+		ans.push(row);
+	}
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_2970/solution.ts
+++ problems/problems_2970/solution.ts
@@ -1,5 +1,22 @@
 function incremovableSubarrayCount(nums: number[]): number {
-    
+    const n: number = nums.length;
+	let i: number = 0;
+	while (i < n - 1 && nums[i] < nums[i + 1]) {
+		i++;
+	}
+	if (i === n - 1) {
+		return Math.floor(n * (n + 1) / 2);
+	}
+	let ans: number = i + 2;
+	let j: number = n - 1;
+	while (j == n - 1 || nums[j] < nums[j + 1]) {
+		while (i >= 0 && nums[i] >= nums[j]) {
+			i--;
+		}
+		ans += i + 2;
+		j--;
+	}
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
