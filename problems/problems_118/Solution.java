60b0b6b0f4218c584626c8b5d7db9cf77ddd2c51
benhao <qubenhao2@gmail.com>
2024-07-10 08:25:15 +0800

    test: java 2970, 118
    solution

--- problems/problems_118/Solution.java
+++ problems/problems_118/Solution.java
@@ -7,7 +7,19 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public List<List<Integer>> generate(int numRows) {
-
+        List<List<Integer>> ans = new ArrayList<>(numRows);
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>(i + 1), lastRow = i > 0 ? ans.get(i - 1) : null;
+            row.add(1);
+            for (int j = 1; j < i; j++) {
+                row.add(lastRow.get(j - 1) + lastRow.get(j));
+            }
+            if (i > 0) {
+                row.add(1);
+            }
+            ans.add(row);
+        }
+        return ans;
     }
 
     @Override
