0d2be57b266c4fb5b62354c13071f893b1c3987b
benhao <qubenhao2@gmail.com>
2024-07-11 07:59:24 +0800

    test: typescript 2972, 46
    solution

--- problems/problems_46/solution.ts
+++ problems/problems_46/solution.ts
@@ -1,5 +1,18 @@
 function permute(nums: number[]): number[][] {
-    
+	const ans: number[][] = [];
+    const backtrack: Function = (idx: number): void => {
+		if (idx === nums.length) {
+			ans.push([...nums]);
+			return;
+		}
+		for (let i: number = idx; i < nums.length; i++) {
+			[nums[i], nums[idx]] = [nums[idx], nums[i]];
+			backtrack(idx + 1);
+			[nums[i], nums[idx]] = [nums[idx], nums[i]];
+		}
+	}
+	backtrack(0);
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
