ad6f9685097036ac1a93f108a8a3e1f284ddc86e
benhao <qubenhao2@gmail.com>
2024-07-12 07:55:22 +0800

    test: typescript 2974
    solution

--- problems/problems_2974/solution.ts
+++ problems/problems_2974/solution.ts
@@ -1,5 +1,10 @@
 function numberGame(nums: number[]): number[] {
-    
+	nums.sort((a, b) => a - b);
+	const n: number = nums.length;
+	for (let i: number = 0; i < n; i += 2) {
+		[nums[i], nums[i + 1]] = [nums[i + 1], nums[i]]
+	}
+	return nums;
 };
 
 export function Solve(inputJsonElement: string): any {
