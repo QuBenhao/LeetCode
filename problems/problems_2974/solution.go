ba7f6839753da9e5ac745d84f60f1319b6b46c9a
benhao <qubenhao2@gmail.com>
2024-07-12 07:42:53 +0800

    test: golang 2974, 226
    solution

--- problems/problems_2974/solution.go
+++ problems/problems_2974/solution.go
@@ -3,11 +3,17 @@ package problem2974
 import (
 	"encoding/json"
 	"log"
+	"sort"
 	"strings"
 )
 
-func numberGame(nums []int) []int {
-
+func numberGame(nums []int) (ans []int) {
+	sort.Ints(nums)
+	for i := 1; i < len(nums); i += 2 {
+		ans = append(ans, nums[i])
+		ans = append(ans, nums[i-1])
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
