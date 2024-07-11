81d35bd48a401d07c4b439d90d5444c6ca7b9032
benhao <qubenhao2@gmail.com>
2024-07-12 07:48:06 +0800

    test: java 2974, 226
    solution

--- problems/problems_2974/Solution.java
+++ problems/problems_2974/Solution.java
@@ -7,7 +7,13 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int[] numberGame(int[] nums) {
-
+        Arrays.sort(nums);
+        int[] ans = new int[nums.length];
+        for (int i = 1; i < nums.length; i += 2) {
+            ans[i - 1] = nums[i];
+            ans[i] = nums[i - 1];
+        }
+        return ans;
     }
 
     @Override
