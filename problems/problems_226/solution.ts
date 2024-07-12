bd060821f0859bf04f94c43abf9dc46bfb866039
qubh <qubh@xiaopeng.com>
2024-07-12 09:10:13 +0800

    test: typescript 226
    solution

--- problems/problems_226/solution.ts
+++ problems/problems_226/solution.ts
@@ -15,7 +15,11 @@ import {TreeNode,TreeNodeToJSONArray,JSONArrayToTreeNode} from "../../typescript
  */
 
 function invertTree(root: TreeNode | null): TreeNode | null {
-    
+    if (root === null) {
+		return null;
+	}
+	[root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+	return root;
 };
 
 export function Solve(inputJsonElement: string): any {
