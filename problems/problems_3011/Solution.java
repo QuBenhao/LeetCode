b17bc26908d44667ea2de4d467df2b3b5b396a04
benhao <qubenhao2@gmail.com>
2024-07-13 08:51:02 +0800

    test: java 3011, 206, 74
    solution

--- problems/problems_3011/Solution.java
+++ problems/problems_3011/Solution.java
@@ -7,7 +7,15 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public boolean canSortArray(int[] nums) {
-
+        for (int i = 0, preMax = 0, n = nums.length; i < n; ) {
+            int ones = Integer.bitCount(nums[i]), curMax = nums[i];
+            for (; i < n && Integer.bitCount(nums[i]) == ones; i++) {
+                if (nums[i] < preMax) return false;
+                curMax = Math.max(curMax, nums[i]);
+            }
+            preMax = curMax;
+        }
+        return true;
     }
 
     @Override
--- problems/problems_74/Solution.java
+++ problems/problems_74/Solution.java
@@ -7,7 +7,14 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public boolean searchMatrix(int[][] matrix, int target) {
-
+        int m = matrix.length, n = matrix[0].length;
+        int row = 0, col = n - 1;
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target) return true;
+            if (matrix[row][col] < target) row++;
+            else col--;
+        }
+        return false;
     }
 
     @Override
