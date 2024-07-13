56896948c7b24dbdd4ffc9ac26fafabd2d5308e5
benhao <qubenhao2@gmail.com>
2024-07-13 08:47:48 +0800

    test: c++ 3011, 206, 74
    solution

--- problems/problems_3011/Solution.cpp
+++ problems/problems_3011/Solution.cpp
@@ -6,9 +6,28 @@ using namespace std;
 using json = nlohmann::json;
 
 class Solution {
+private:
+    int BitsCount(int n) {
+        int count = 0;
+        while (n) {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
 public:
     bool canSortArray(vector<int>& nums) {
-        
+        for (int i = 0, pre_max = 0, n = static_cast<int>(nums.size()); i < n;) {
+            auto ones = BitsCount(nums[i]), cur_max = 0;
+            for (; i < n && BitsCount(nums[i]) == ones; i++) {
+                if (nums[i] < pre_max) {
+                    return false;
+                }
+                cur_max = max(cur_max, nums[i]);
+            }
+            pre_max = cur_max;
+        }
+        return true;
     }
 };
 
--- problems/problems_74/Solution.cpp
+++ problems/problems_74/Solution.cpp
@@ -8,7 +8,18 @@ using json = nlohmann::json;
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-
+        int m = static_cast<int>(matrix.size()), n = static_cast<int>(matrix[0].size());
+        int row = 0, col = n - 1;
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return false;
     }
 };
 
