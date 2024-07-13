8b8a63c7a8fa74247d0c44fb83e61f35f6b6e954
benhao <qubenhao2@gmail.com>
2024-07-13 08:39:39 +0800

    test: golang 3011, 206, 74
    solution

--- problems/problems_3011/solution.go
+++ problems/problems_3011/solution.go
@@ -3,11 +3,23 @@ package problem3011
 import (
 	"encoding/json"
 	"log"
+	"math/bits"
 	"strings"
 )
 
 func canSortArray(nums []int) bool {
-
+	for i, preMax, n := 0, 0, len(nums); i < n; {
+		ones := bits.OnesCount32(uint32(uint(nums[i])))
+		curMax := nums[i]
+		for ; i < n && bits.OnesCount32(uint32(uint(nums[i]))) == ones; i++ {
+			if nums[i] < preMax {
+				return false
+			}
+			curMax = max(curMax, nums[i])
+		}
+		preMax = curMax
+	}
+	return true
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_74/solution.go
+++ problems/problems_74/solution.go
@@ -7,7 +7,22 @@ import (
 )
 
 func searchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) == 0 {
+		return false
+	}
 
+	row, col := 0, len(matrix[0])-1
+	for row < len(matrix) && col >= 0 {
+		if matrix[row][col] == target {
+			return true
+		}
+		if matrix[row][col] > target {
+			col--
+		} else {
+			row++
+		}
+	}
+	return false
 }
 
 func Solve(inputJsonValues string) interface{} {
