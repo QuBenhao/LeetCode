28f974007978f295c8769ac38c03029065e0c7ff
benhao <qubenhao2@gmail.com>
2024-07-16 06:49:56 +0800

    test: typescript 2956, 64
    solution

--- problems/problems_2956/solution.ts
+++ problems/problems_2956/solution.ts
@@ -1,5 +1,18 @@
 function findIntersectionValues(nums1: number[], nums2: number[]): number[] {
-    
+    const counter: Map<number, number> = new Map<number, number>();
+	for (const num of nums2) {
+		counter.set(num, (counter.get(num) || 0) + 1);
+	}
+	const ans: number[] = [0, 0];
+	for (const num of nums1) {
+		if (!counter.has(num)) {
+			continue;
+		}
+		ans[0]++;
+		ans[1] += counter.get(num);
+		counter.set(num, 0);
+	}
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_64/solution.ts
+++ problems/problems_64/solution.ts
@@ -1,5 +1,13 @@
 function minPathSum(grid: number[][]): number {
-    
+	const m: number = grid.length, n: number = grid[0].length;
+	const dp: number[] = new Array(n).fill(0);
+	for (let i: number = 0; i < m; i++) {
+		dp[0] += grid[i][0];
+		for (let j: number = 1; j < n; j++) {
+			dp[j] = (i === 0 ? dp[j - 1] : Math.min(dp[j - 1], dp[j])) + grid[i][j];
+		}
+	}
+	return dp[n - 1];
 };
 
 export function Solve(inputJsonElement: string): any {
