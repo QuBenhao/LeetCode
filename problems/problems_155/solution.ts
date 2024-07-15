a32a55d750f4dafe5f339edbdfab3c8d46d9f4b0
benhao <qubenhao2@gmail.com>
2024-07-15 08:13:40 +0800

    test: typescript 721, 155
    solution

--- problems/problems_155/solution.ts
+++ problems/problems_155/solution.ts
@@ -1,22 +1,31 @@
 class MinStack {
+	data: number[];
+	helper: number[];
     constructor() {
-        
+        this.data = [];
+		this.helper = [];
     }
 
     push(val: number): void {
-        
+        this.data.push(val);
+		if (this.helper.length > 0 && this.helper[this.helper.length - 1] < val) {
+			this.helper.push(this.helper[this.helper.length - 1]);
+		} else {
+			this.helper.push(val);
+		}
     }
 
     pop(): void {
-        
+        this.data.pop();
+		this.helper.pop();
     }
 
     top(): number {
-        
+		return this.data[this.data.length - 1];
     }
 
     getMin(): number {
-        
+        return this.helper[this.helper.length - 1];
     }
 }
 
--- problems/problems_721/solution.ts
+++ problems/problems_721/solution.ts
@@ -1,6 +1,56 @@
 function accountsMerge(accounts: string[][]): string[][] {
-    
+    let emailToIndex = new Map();
+    let emailToName = new Map();
+    let count = 0;
+    for (const account of accounts) {
+        for (let j = 1; j < account.length; j++) {
+            if (!emailToIndex.has(account[j])) {
+                emailToIndex.set(account[j], count++);
+                emailToName.set(account[j], account[0])
+            }
+        }
+    }
+    const uf = new UnionFind(count);
+    for (const account of accounts) {
+        const firstIndex = emailToIndex.get(account[1]);
+        for (let i = 2; i < account.length; i++) {
+            const nextIndex = emailToIndex.get(account[i]);
+            uf.union(firstIndex, nextIndex);
+        }
+    }
+    let indexToEmails = new Map();
+	for(const email of emailToIndex.keys()) {
+        const index = uf.find(emailToIndex.get(email));
+        const account = indexToEmails.get(index) || [];
+        account.push(email);
+        indexToEmails.set(index, account);
+    }
+    let result = [];
+	for(const emails of indexToEmails.values()) {
+        emails.sort();
+        const name = emailToName.get(emails[0]);
+        result.push([name, ...emails]);
+    }
+    return result;
 };
+
+class UnionFind {
+    parent: any;
+    constructor(n) {
+        this.parent = new Array(n).fill(0).map((value, index) => index);
+    }
+
+    union(index1, index2) {
+        this.parent[this.find(index2)] = this.find(index1);
+    }
+
+    find(index) {
+        if (this.parent[index] !== index) {
+            this.parent[index] = this.find(this.parent[index]);
+        }
+        return this.parent[index];
+    }
+}
 
 export function Solve(inputJsonElement: string): any {
 	const inputValues: string[] = inputJsonElement.split("\n");
