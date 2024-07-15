7a2719a6acbfd5eb86b57438c3fa311fe3c223ee
benhao <qubenhao2@gmail.com>
2024-07-16 06:44:32 +0800

    test: java 2956, 64
    solution

--- problems/problems_2956/Solution.java
+++ problems/problems_2956/Solution.java
@@ -1,19 +1,32 @@
 package problems.problems_2956;
 
 import com.alibaba.fastjson.JSON;
+
 import java.util.*;
+
 import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
-
+        int[] ans = new int[2];
+        Map<Integer, Integer> counter = new HashMap<>();
+        for (int num : nums1) {
+            counter.put(num, counter.getOrDefault(num, 0) + 1);
+        }
+        for (int num : nums2) {
+            if (!counter.containsKey(num)) continue;
+            ans[1]++;
+            ans[0] += counter.get(num);
+            counter.put(num, 0);
+        }
+        return ans;
     }
 
     @Override
     public Object solve(String[] inputJsonValues) {
         int[] nums1 = jsonArrayToIntArray(inputJsonValues[0]);
-		int[] nums2 = jsonArrayToIntArray(inputJsonValues[1]);
+        int[] nums2 = jsonArrayToIntArray(inputJsonValues[1]);
         return JSON.toJSON(findIntersectionValues(nums1, nums2));
     }
 }
--- problems/problems_64/Solution.java
+++ problems/problems_64/Solution.java
@@ -7,7 +7,15 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int minPathSum(int[][] grid) {
-
+        int m = grid.length, n = grid[0].length;
+        int[] dp = new int[n];
+        for (int i = 0; i < m; i++) {
+            dp[0] += grid[i][0];
+            for (int j = 1; j < n; j++) {
+                dp[j] = (i == 0 ? dp[j - 1] : Math.min(dp[j - 1], dp[j])) + grid[i][j];
+            }
+        }
+        return dp[n - 1];
     }
 
     @Override
