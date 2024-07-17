37fe8b561420fe7d628bf14b524f8f2944a69d91
benhao <qubenhao2@gmail.com>
2024-07-18 07:24:58 +0800

    feat: python 3112, 198
    solution

--- problems/problems_3112/solution.py
+++ problems/problems_3112/solution.py
@@ -1,5 +1,7 @@
 import solution
 from typing import *
+from collections import defaultdict
+from heapq import heappush, heappop
 
 
 class Solution(solution.Solution):
@@ -7,5 +9,21 @@ class Solution(solution.Solution):
         return self.minimumTime(*test_input)
 
     def minimumTime(self, n: int, edges: List[List[int]], disappear: List[int]) -> List[int]:
-        pass
+        graph = defaultdict(list)
+        for u, v, w in edges:
+            graph[u].append((v, w))
+            graph[v].append((u, w))
 
+        dis = [-1] * n
+        dis[0] = 0
+        pq = [(0, 0)]
+        while pq:
+            d, u = heappop(pq)
+            if dis[u] < d:
+                continue
+            for v, w in graph[u]:
+                new_d = d + w
+                if new_d < disappear[v] and (dis[v] == -1 or new_d < dis[v]):
+                    dis[v] = new_d
+                    heappush(pq, (new_d, v))
+        return dis
