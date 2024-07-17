86620dc78e7b9cae6708126ae5915032ff978c9d
benhao <qubenhao2@gmail.com>
2024-07-18 07:51:37 +0800

    feat: java 3112, 198
    solution

--- problems/problems_198/Solution.java
+++ problems/problems_198/Solution.java
@@ -7,7 +7,13 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int rob(int[] nums) {
-
+        int dp0 = 0, dp1 = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int newDp0 = Math.max(dp0, dp1);
+            dp1 = Math.max(dp0 + nums[i], dp1);
+            dp0 = newDp0;
+        }
+        return Math.max(dp0, dp1);
     }
 
     @Override
--- problems/problems_3112/Solution.java
+++ problems/problems_3112/Solution.java
@@ -7,7 +7,34 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int[] minimumTime(int n, int[][] edges, int[] disappear) {
-
+        List<int[]>[] graph = new ArrayList[n];
+        Arrays.setAll(graph, i -> new ArrayList<>());
+        for (int[] edge : edges) {
+            graph[edge[0]].add(new int[]{edge[1], edge[2]});
+            graph[edge[1]].add(new int[]{edge[0], edge[2]});
+        }
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+        ans[0] = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        pq.offer(new int[]{0, 0});
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int u = cur[1];
+            if (ans[u] < cur[0]) {
+                continue;
+            }
+            for (int[] next : graph[u]) {
+                int v = next[0];
+                int w = next[1];
+                int nd = cur[0] + w;
+                if (nd < disappear[v] && (ans[v] == -1 || ans[v] > nd)) {
+                    ans[v] = nd;
+                    pq.offer(new int[]{nd, v});
+                }
+            }
+        }
+        return ans;
     }
 
     @Override
