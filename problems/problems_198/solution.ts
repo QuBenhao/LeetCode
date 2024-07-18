44a69a6661266e2a22cf38a75d82a13841b10ec9
benhao <qubenhao2@gmail.com>
2024-07-18 08:12:23 +0800

    fix: typescript 3112, 198
    solution, and priority_queue version diff

--- problems/problems_198/solution.ts
+++ problems/problems_198/solution.ts
@@ -1,5 +1,9 @@
 function rob(nums: number[]): number {
-    
+    const dp: number[] = [0, nums[0]];
+	for (let i: number = 1; i < nums.length; i++) {
+		[dp[0], dp[1]] = [Math.max(dp[0], dp[1]), Math.max(dp[0] + nums[i], dp[1])];
+	}
+	return Math.max(dp[0], dp[1]);
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_3112/solution.ts
+++ problems/problems_3112/solution.ts
@@ -1,5 +1,29 @@
+import {MinPriorityQueue} from "@datastructures-js/priority-queue";
+
 function minimumTime(n: number, edges: number[][], disappear: number[]): number[] {
-    
+    const graph: number[][][] = Array.from({ length: n }, () => []);
+	for (const [u, v, w] of edges) {
+		graph[u].push([v, w]);
+		graph[v].push([u, w]);
+	}
+	const dis: number[] = Array(n).fill(-1);
+	dis[0] = 0;
+	const pq = new MinPriorityQueue();
+	pq.enqueue([0, 0], 0);
+	while (!pq.isEmpty()) {
+		const [d, u] = pq.dequeue().element;
+		if (dis[u] < d) {
+			continue;
+		}
+		for (const [v, w] of graph[u]) {
+			const nd: number = d + w;
+			if (nd < disappear[v] && (dis[v] === -1 || dis[v] > nd)) {
+				dis[v] = nd;
+				pq.enqueue([dis[v], v], dis[v]);
+			}
+		}
+	}
+	return dis;
 };
 
 export function Solve(inputJsonElement: string): any {
