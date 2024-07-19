8a8f749fdb6457d2608f8aca22fb3705cf125c2a
benhao <qubenhao2@gmail.com>
2024-07-20 06:30:01 +0800

    test: c++ 2850, 11, 54
    solution

--- problems/problems_2850/Solution.cpp
+++ problems/problems_2850/Solution.cpp
@@ -8,7 +8,28 @@ using json = nlohmann::json;
 class Solution {
 public:
     int minimumMoves(vector<vector<int>>& grid) {
-        
+        vector<pair<int, int>> source, target;
+        int m = static_cast<int>(grid.size()), n = static_cast<int>(grid[0].size());
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] > 1) {
+                    for (int k = 0; k < grid[i][j] - 1; k++) {
+                        source.emplace_back(i, j);
+                    }
+                } else if (grid[i][j] == 0) {
+                    target.emplace_back(i, j);
+                }
+            }
+        }
+        int ans = INT_MAX;
+        do {
+            int total = 0;
+            for (size_t i = 0; i < source.size(); i++) {
+                total += abs(source[i].first - target[i].first) + abs(source[i].second - target[i].second);
+            }
+            ans = min(ans, total);
+        } while (next_permutation(source.begin(), source.end()));
+        return ans == INT_MAX ? -1 : ans;
     }
 };
 
