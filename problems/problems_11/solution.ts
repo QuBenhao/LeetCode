99927dbf17fd03930a5ed203d338c0e1c4a8a2ba
benhao <qubenhao2@gmail.com>
2024-07-20 06:57:13 +0800

    test: typescript 2850, 11, 54
    solution

--- problems/problems_11/solution.ts
+++ problems/problems_11/solution.ts
@@ -1,5 +1,14 @@
 function maxArea(height: number[]): number {
-    
+    let ans: number = 0;
+	for (let left: number = 0, right: number = height.length - 1; left < right; ) {
+		ans = Math.max(ans, Math.min(height[left], height[right]) * (right - left));
+		if (height[left] < height[right]) {
+			left++;
+		} else {
+			right--;
+		}
+	}
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
