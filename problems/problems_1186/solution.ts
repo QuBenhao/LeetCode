1151436faab0866f7cdd156b6977c8b751fca412
benhao <qubenhao2@gmail.com>
2024-07-21 10:09:33 +0800

    test: typescript 1186, 994, 121
    solution

--- problems/problems_1186/solution.ts
+++ problems/problems_1186/solution.ts
@@ -1,5 +1,11 @@
 function maximumSum(arr: number[]): number {
-    
+    let ans: number = -Infinity, dp0: number = -Infinity, dp1: number = -Infinity;
+	for (const num of arr) {
+		dp1 = Math.max(dp1 + num, dp0);
+		dp0 = Math.max(dp0 + num, num);
+		ans = Math.max(ans, dp1, dp0);
+	}
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_121/solution.ts
+++ problems/problems_121/solution.ts
@@ -1,5 +1,10 @@
 function maxProfit(prices: number[]): number {
-    
+    let buy: number = -prices[0], sell: number = 0;
+	for (let i: number = 1; i < prices.length; i++) {
+		buy = Math.max(buy, -prices[i]);
+		sell = Math.max(sell, buy + prices[i]);
+	}
+	return sell;
 };
 
 export function Solve(inputJsonElement: string): any {
