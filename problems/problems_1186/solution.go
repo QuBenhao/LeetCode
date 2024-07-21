b375343c4d1af3697206449b3cf4a0a1de0630de
benhao <qubenhao2@gmail.com>
2024-07-21 09:04:02 +0800

    test: golang 1186, 994, 121
    solution

--- problems/problems_1186/solution.go
+++ problems/problems_1186/solution.go
@@ -3,11 +3,18 @@ package problem1186
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"strings"
 )
 
 func maximumSum(arr []int) int {
-
+	ans, dp0, dp1 := math.MinInt32, math.MinInt32, math.MinInt32
+	for _, v := range arr {
+		dp1 = max(dp0, dp1+v)
+		dp0 = max(dp0+v, v)
+		ans = max(ans, max(dp0, dp1))
+	}
+	return ans
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_121/solution.go
+++ problems/problems_121/solution.go
@@ -7,7 +7,12 @@ import (
 )
 
 func maxProfit(prices []int) int {
-
+	buy, sell := -prices[0], 0
+	for i := 1; i < len(prices); i++ {
+		buy = max(buy, -prices[i])
+		sell = max(sell, buy+prices[i])
+	}
+	return sell
 }
 
 func Solve(inputJsonValues string) interface{} {
