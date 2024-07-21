08c4ff31cc85bbe139e1b9827c6f5a17b1cfcec0
benhao <qubenhao2@gmail.com>
2024-07-22 07:07:18 +0800

    test: typescript 2101, 17
    solution

--- problems/problems_17/solution.ts
+++ problems/problems_17/solution.ts
@@ -1,5 +1,19 @@
 function letterCombinations(digits: string): string[] {
-    
+    const translator: string[] = ["abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
+	const result: string[] = [];
+	const dfs: Function = (index: number, path: string): void => {
+		if (index === digits.length) {
+			result.push(path);
+			return;
+		}
+		for (const c of translator[digits.charCodeAt(index) - "2".charCodeAt(0)]) {
+			dfs(index + 1, path + c);
+		}
+	};
+	if (digits.length > 0) {
+		dfs(0, "");
+	}
+	return result;
 };
 
 export function Solve(inputJsonElement: string): any {
