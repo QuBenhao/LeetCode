cb1b3730636bba1cb74de3423d8731fca814421b
benhao <qubenhao2@gmail.com>
2024-07-23 05:59:32 +0800

    test: java 543
    solution

--- problems/problems_543/Solution.java
+++ problems/problems_543/Solution.java
@@ -21,8 +21,19 @@ import qubhjava.BaseSolution;
 import qubhjava.models.TreeNode;
 
 public class Solution extends BaseSolution {
+    private int ans;
+    private int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = dfs(node.left), right = dfs(node.right);
+        ans = Math.max(ans, left + right);
+        return Math.max(left, right) + 1;
+    }
     public int diameterOfBinaryTree(TreeNode root) {
-
+        ans = 0;
+        dfs(root);
+        return ans;
     }
 
     @Override
