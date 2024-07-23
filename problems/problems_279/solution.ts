48b74b5309300363d8021fb8206cba166478ca50
benhao <qubenhao2@gmail.com>
2024-07-24 07:47:46 +0800

    test: typescript 2766, 279
    solution

--- problems/problems_279/solution.ts
+++ problems/problems_279/solution.ts
@@ -1,5 +1,26 @@
 function numSquares(n: number): number {
-    
+    const squares: Set<number> = new Set<number>();
+	for (let i: number = 1; i * i <= n; i++) {
+		squares.add(i * i);
+	}
+	const isDividedBy: Function = (n: number, count: number): boolean => {
+		if (count === 1) {
+			return squares.has(n);
+		}
+		// @ts-ignore
+		for (const square of squares) {
+			if (isDividedBy(n - square, count - 1)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	for (let count: number = 1; count <= n; count++) {
+		if (isDividedBy(n, count)) {
+			return count;
+		}
+	}
+	return n;
 };
 
 export function Solve(inputJsonElement: string): any {
