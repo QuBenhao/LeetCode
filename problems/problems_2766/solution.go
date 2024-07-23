28a680e889174b80125ee185eb0b891ee26a6eb1
benhao <qubenhao2@gmail.com>
2024-07-24 06:55:55 +0800

    test: golang 2766, 279
    solution

--- problems/problems_2766/solution.go
+++ problems/problems_2766/solution.go
@@ -3,11 +3,26 @@ package problem2766
 import (
 	"encoding/json"
 	"log"
+	"sort"
 	"strings"
 )
 
 func relocateMarbles(nums []int, moveFrom []int, moveTo []int) []int {
-
+	s := map[int]interface{}{}
+	for _, v := range nums {
+		s[v] = nil
+	}
+	for i := 0; i < len(moveFrom); i++ {
+		f, t := moveFrom[i], moveTo[i]
+		delete(s, f)
+		s[t] = nil
+	}
+	res := make([]int, 0, len(s))
+	for k := range s {
+		res = append(res, k)
+	}
+	sort.Ints(res)
+	return res
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_279/solution.go
+++ problems/problems_279/solution.go
@@ -7,7 +7,15 @@ import (
 )
 
 func numSquares(n int) int {
-
+	dp := make([]int, n+1)
+	dp[0] = 0
+	for i := 1; i <= n; i++ {
+		dp[i] = dp[i-1] + 1
+		for j := 2; j*j <= i; j++ {
+			dp[i] = min(dp[i], dp[i-j*j]+1)
+		}
+	}
+	return dp[n]
 }
 
 func Solve(inputJsonValues string) interface{} {
