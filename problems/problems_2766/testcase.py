9c9aa47375e5dca8a71e5047e7eaba3c63f09e68
benhao <qubenhao2@gmail.com>
2024-07-24 06:46:45 +0800

    test: python 2766, 279
    solution

--- problems/problems_2766/testcase.py
+++ problems/problems_2766/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=[[1, 6, 7, 8], [1, 7, 2], [2, 9, 5]], Output=[5, 6, 8, 9]))
 		self.testcases.append(case(Input=[[1, 1, 3, 3], [1, 3], [2, 2]], Output=[2]))
+		self.testcases.append(case(Input=[[3,4],[4,3,1,2,2,3,2,4,1],[3,1,2,2,3,2,4,1,1]], Output=[1]))
 
 	def get_testcases(self):
 		return self.testcases
