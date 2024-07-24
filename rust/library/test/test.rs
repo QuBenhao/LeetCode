ebba4414c52d121c78ef33a76204befb2670a89f
benhao <qubenhao2@gmail.com>
2024-07-25 06:42:06 +0800

    feat: Rust TreeNodeWithTargets
    add fn treeNode with targets

--- rust/library/test/test.rs
+++ rust/library/test/test.rs
@@ -19,4 +19,15 @@ mod test {
         assert_eq!(nums, res);
         println!("{:?}", root);
     }
+
+    #[test]
+    fn test_tree_node_with_targets() {
+        let nums = vec![Some(1), Some(2), Some(3), Some(4), Some(5)];
+        let targets = vec![1, 2, 4];
+        let nodes = tree_node::array_to_tree_with_targets(&nums, targets);
+        assert_eq!(nodes[0], nodes[1]);
+        assert_eq!(nodes[0].as_ref().unwrap().borrow().left, nodes[2]);
+        assert_eq!(nodes[0].as_ref().unwrap().borrow().left
+                       .as_ref().unwrap().borrow().left, nodes[3]);
+    }
 }
