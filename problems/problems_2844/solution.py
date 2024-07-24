45fd62c5fc374c5dd9fe25e79e8d79334335a099
benhao <qubenhao2@gmail.com>
2024-07-25 06:56:55 +0800

    test: python 2844, 49
    solution

--- problems/problems_2844/solution.py
+++ problems/problems_2844/solution.py
@@ -7,5 +7,14 @@ class Solution(solution.Solution):
         return self.minimumOperations(test_input)
 
     def minimumOperations(self, num: str) -> int:
-        pass
-
+        n = len(num)
+        zero = five = False
+        for i in range(n - 1, -1, -1):
+            c = num[i]
+            if (zero and c in "05") or (five and c in "27"):
+                return n - i - 2
+            if c == "0":
+                zero = True
+            if c == "5":
+                five = True
+        return n - zero
