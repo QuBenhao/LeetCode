aa459233d8f0c545f1259b3944bfbc882724077a
benhao <qubenhao2@gmail.com>
2024-07-25 06:46:49 +0800

    test: add rust 2844, 49
    daily problems

--- rust/test_executor/tests/solutions_test.rs
+++ rust/test_executor/tests/solutions_test.rs
@@ -1,21 +1,21 @@
-const PROBLEMS: [[&str; 2]; 1] = [["problems", "279"]];
-
+const PROBLEMS: [[&str; 2]; 1] = [["problems", "49"]];
 
 #[cfg(test)]
 mod test {
-    use test_executor::run_test::run_test;
-    use crate::PROBLEMS;
+	use test_executor::run_test::run_test;
+	use crate::PROBLEMS;
 
-    use solution_279 as solution0;
+	use solution_49 as solution0;
 
-    #[test]
-    fn test_solutions() {
-        for (i, problem) in PROBLEMS.iter().enumerate() {
-            let (folder, id) = (problem[0], problem[1]);
-            run_test(id, folder, match i {
-                0 => solution0::solve,
-                _ => panic!("Unknown solution"),
-            });
-        }
-    }
+	#[test]
+	fn test_solutions() {
+		for (i, problem) in PROBLEMS.iter().enumerate() {
+			let (folder, id) = (problem[0], problem[1]);
+			println!("Testing problem {}", id);
+			run_test(id, folder, match i {
+				0 => solution0::solve,
+				_ => panic!("Unknown solution"),
+			});
+		}
+	}
 }
