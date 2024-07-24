a98fb313cd64a649e0c60b0c921bcf23b28433fb
benhao <qubenhao2@gmail.com>
2024-07-25 07:40:53 +0800

    test: java 2844, 49
    solution

--- problems/problems_2844/Solution.java
+++ problems/problems_2844/Solution.java
@@ -7,7 +7,21 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int minimumOperations(String num) {
-
+        int n = num.length();
+        boolean zero = false, five = false;
+        for (int i = n - 1; i >= 0; i--) {
+            char c = num.charAt(i);
+            if (zero && (c == '0' || c == '5') || five && (c == '2' || c == '7')) {
+                return n - i - 2;
+            }
+            if (c == '0') {
+                zero = true;
+            }
+            if (c == '5') {
+                five = true;
+            }
+        }
+        return zero ? n - 1: n;
     }
 
     @Override
