148f899c9b4f12b0ee19b5f1d110e82cd223597a
benhao <qubenhao2@gmail.com>
2024-07-25 07:45:37 +0800

    test: typescript 2844, 49
    solution

--- problems/problems_2844/solution.ts
+++ problems/problems_2844/solution.ts
@@ -1,5 +1,19 @@
 function minimumOperations(num: string): number {
-
+	const n: number = num.length;
+	let zero: boolean = false, five: boolean = false;
+	for (let i: number = n - 1; i >= 0; i--) {
+		const c: string = num[i];
+		if (zero && (c === "0" || c === "5") || five && (c === "2" || c === "7")) {
+			return n - i - 2;
+		}
+		if (c === "0") {
+			zero = true;
+		}
+		if (c === "5") {
+			five = true;
+		}
+	}
+	return zero ? n - 1 : n;
 };
 
 export function Solve(inputJsonElement: string): any {
