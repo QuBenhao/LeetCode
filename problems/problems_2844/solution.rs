27fdc83ded96f4ba15328a832857f953adb89e77
benhao <qubenhao2@gmail.com>
2024-07-25 08:17:32 +0800

    test: rust 2844, 49
    solution

--- problems/problems_2844/solution.rs
+++ problems/problems_2844/solution.rs
@@ -4,7 +4,27 @@ pub struct Solution;
 
 impl Solution {
     pub fn minimum_operations(num: String) -> i32 {
-
+		let n: i32 = num.len() as i32;
+		let mut zero: bool = false;
+		let mut five: bool = false;
+		let mut idx = n - 1;
+		while idx >= 0 {
+			let c: char = num.chars().nth(idx as usize).unwrap();
+			if zero && (c == '0' || c == '5') || five && (c == '2' || c == '7') {
+				return n - idx - 2;
+			}
+			if c == '0' {
+				zero = true;
+			} else if c == '5' {
+				five = true;
+			}
+			idx -= 1;
+		}
+		if zero {
+			n - 1
+		} else {
+			n
+		}
     }
 }
 
