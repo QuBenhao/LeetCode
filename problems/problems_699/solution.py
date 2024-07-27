2758e3168e234748c9d7de69f4d3fab808077c86
benhao <qubenhao2@gmail.com>
2024-07-28 07:09:35 +0800

    test: python 699, 39, 215
    solution

--- problems/problems_699/solution.py
+++ problems/problems_699/solution.py
@@ -7,5 +7,18 @@ class Solution(solution.Solution):
         return self.fallingSquares(test_input)
 
     def fallingSquares(self, positions: List[List[int]]) -> List[int]:
-        pass
-
+        pos = set()
+        for a, length in positions:
+            pos.add(a)
+            pos.add(a + length - 1)
+        idx_map = {v: i for i, v in enumerate(sorted(pos))}
+        records = [0] * len(idx_map)
+        ans, cur = [], 0
+        for a, length in positions:
+            left, right = idx_map[a], idx_map[a + length - 1]
+            new_height = max(records[left:right + 1]) + length
+            for i in range(left, right + 1):
+                records[i] = new_height
+            cur = max(cur, new_height)
+            ans.append(cur)
+        return ans
