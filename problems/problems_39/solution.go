8774afb38a39b1c6f05a47b2b5922f42d584f4f9
benhao <qubenhao2@gmail.com>
2024-07-28 07:38:10 +0800

    test: golang 699, 39, 215
    solution

--- problems/problems_39/solution.go
+++ problems/problems_39/solution.go
@@ -6,8 +6,26 @@ import (
 	"strings"
 )
 
-func combinationSum(candidates []int, target int) [][]int {
+func combinationSum(candidates []int, target int) (ans [][]int) {
+	var dfs func([]int, int, int)
+	dfs = func(path []int, idx int, s int) {
+		if s == 0 {
+			ans = append(ans, append([]int(nil), path...))
+			return
+		}
+		if idx == len(candidates) {
+			return
+		}
+		if candidates[idx] <= s {
+			path = append(path, candidates[idx])
+			dfs(path, idx, s-candidates[idx])
+			path = path[:len(path)-1]
+		}
+		dfs(path, idx+1, s)
+	}
 
+	dfs([]int{}, 0, target)
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
