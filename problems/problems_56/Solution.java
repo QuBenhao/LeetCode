c11377eea029e9f4798c372e8f2bbb0a67e863ca
benhao <qubenhao2@gmail.com>
2024-07-29 06:57:38 +0800

    test: java 682, 56
    solution

--- problems/problems_56/Solution.java
+++ problems/problems_56/Solution.java
@@ -7,7 +7,16 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int[][] merge(int[][] intervals) {
-
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        List<int[]> ans = new ArrayList<>();
+        for (int[] interval: intervals) {
+            if (ans.isEmpty() || ans.getLast()[1] < interval[0]) {
+                ans.add(interval);
+            } else {
+                ans.getLast()[1] = Math.max(ans.getLast()[1], interval[1]);
+            }
+        }
+        return ans.toArray(new int[ans.size()][]);
     }
 
     @Override
