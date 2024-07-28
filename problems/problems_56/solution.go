5bfb085d1033fa73cf063356e7b9fa650a1cfb00
benhao <qubenhao2@gmail.com>
2024-07-29 06:51:41 +0800

    test: golang 682, 56
    solution

--- problems/problems_56/solution.go
+++ problems/problems_56/solution.go
@@ -3,11 +3,23 @@ package problem56
 import (
 	"encoding/json"
 	"log"
+	"sort"
 	"strings"
 )
 
-func merge(intervals [][]int) [][]int {
-
+func merge(intervals [][]int) (ans [][]int) {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	for _, interval := range intervals {
+		left, right := interval[0], interval[1]
+		if len(ans) == 0 || ans[len(ans)-1][1] < left {
+			ans = append(ans, interval)
+		} else {
+			ans[len(ans)-1][1] = max(ans[len(ans)-1][1], right)
+		}
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
