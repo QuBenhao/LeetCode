b1fe9ceb7e09fb9dfc30b440263bf073b20250b9
benhao <qubenhao2@gmail.com>
2024-07-29 07:01:29 +0800

    test: typescript 682, 56
    solution

--- problems/problems_56/solution.ts
+++ problems/problems_56/solution.ts
@@ -1,5 +1,14 @@
 function merge(intervals: number[][]): number[][] {
-    
+	intervals.sort((a, b) => a[0] - b[0]);
+	const merged: number[][] = [];
+	for (const interval of intervals) {
+		if (merged.length === 0 || merged[merged.length - 1][1] < interval[0]) {
+			merged.push(interval);
+		} else {
+			merged[merged.length - 1][1] = Math.max(merged[merged.length - 1][1], interval[1]);
+		}
+	}
+	return merged;
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_682/solution.ts
+++ problems/problems_682/solution.ts
@@ -1,5 +1,20 @@
 function calPoints(operations: string[]): number {
-    
+	let ans: number = 0;
+	const stack: number[] = [];
+	for (const op of operations) {
+		if (op === "C") {
+			ans -= stack.pop();
+			continue;
+		} else if (op === "D") {
+			stack.push(stack[stack.length - 1] * 2);
+		} else if (op === "+") {
+			stack.push(stack[stack.length - 1] + stack[stack.length - 2]);
+		} else {
+			stack.push(parseInt(op));
+		}
+		ans += stack[stack.length - 1];
+	}
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
