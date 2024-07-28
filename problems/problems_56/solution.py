cc18ae6b6576b964cb2bbd2d39ccc0c634c01569
benhao <qubenhao2@gmail.com>
2024-07-29 06:49:03 +0800

    test: python 682, 56
    solution

--- problems/problems_56/solution.py
+++ problems/problems_56/solution.py
@@ -7,5 +7,11 @@ class Solution(solution.Solution):
         return self.merge(test_input)
 
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        pass
-
+        intervals.sort()
+        ans = []
+        for left, right in intervals:
+            if ans and ans[-1][1] >= left:
+                ans[-1][1] = max(ans[-1][1], right)
+            else:
+                ans.append([left, right])
+        return ans
