ec6dcbedf0f56bd7b7c0e68a40c3408b62476d13
benhao <qubenhao2@gmail.com>
2024-07-29 06:54:18 +0800

    test: c++ 682, 56
    solution

--- problems/problems_56/Solution.cpp
+++ problems/problems_56/Solution.cpp
@@ -8,7 +8,16 @@ using json = nlohmann::json;
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-
+        vector<vector<int>> ans;
+        sort(intervals.begin(), intervals.end());
+        for (auto interval: intervals) {
+            if (ans.empty() || ans.back()[1] < interval[0]) {
+                ans.push_back(interval);
+            } else {
+                ans.back()[1] = max(ans.back()[1], interval[1]);
+            }
+        }
+        return ans;
     }
 };
 
