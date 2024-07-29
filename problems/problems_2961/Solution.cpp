9dbace6f4433524ce2f9a4fbd3dea50323b5055c
benhao <qubenhao2@gmail.com>
2024-07-30 07:37:59 +0800

    test: c++ 2961, 394
    solution

--- problems/problems_2961/Solution.cpp
+++ problems/problems_2961/Solution.cpp
@@ -6,9 +6,29 @@ using namespace std;
 using json = nlohmann::json;
 
 class Solution {
+private:
+    int fastPowMod(int base, int exp, int mod) {
+        int result = 1;
+        while (exp > 0) {
+            if (exp & 1) {
+                result = (result * base) % mod;
+            }
+            base = (base * base) % mod;
+            exp >>= 1;
+        }
+        return result;
+    }
 public:
     vector<int> getGoodIndices(vector<vector<int>>& variables, int target) {
-        
+        int n = static_cast<int>(variables.size());
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            auto a = variables[i][0], b = variables[i][1], c = variables[i][2], m = variables[i][3];
+            if (fastPowMod(fastPowMod(a, b, 10), c, m) == target) {
+                ans.emplace_back(i);
+            }
+        }
+        return ans;
     }
 };
 
--- problems/problems_394/Solution.cpp
+++ problems/problems_394/Solution.cpp
@@ -1,5 +1,8 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
+#include <stack>
+#include <sstream>
+#include <iostream>
 
 
 using namespace std;
@@ -8,7 +11,35 @@ using json = nlohmann::json;
 class Solution {
 public:
     string decodeString(string s) {
-
+        stack<int> num_stack;
+        stack<string> str_stack;
+        stringstream ss;
+        int times = 0;
+        for (auto c: s) {
+            if (c == '[') {
+                num_stack.push(times);
+                times = 0;
+                str_stack.push(ss.str());
+                ss.str("");
+            } else if (c == ']') {
+                auto t = num_stack.top();
+                auto last_str = str_stack.top();
+                num_stack.pop();
+                str_stack.pop();
+                stringstream tmp;
+                tmp << last_str;
+                for (int i = 0; i < t; i++) {
+                    tmp << ss.str();
+                }
+                ss.str("");
+                ss << tmp.str();
+            } else if (c >= '0' && c <= '9') {
+                times = times * 10 + c - '0';
+            } else {
+                ss << c;
+            }
+        }
+        return ss.str();
     }
 };
 
