41c9255ef166e39131af3a60a4add47572f8447e
benhao <qubenhao2@gmail.com>
2024-07-30 07:08:53 +0800

    test: golang 2961, 394
    solution

--- problems/problems_2961/solution.go
+++ problems/problems_2961/solution.go
@@ -6,8 +6,25 @@ import (
 	"strings"
 )
 
-func getGoodIndices(variables [][]int, target int) []int {
-
+func getGoodIndices(variables [][]int, target int) (ans []int) {
+	fastPowMod := func(a, b, mod int) int {
+		res := 1
+		for b > 0 {
+			if b&1 == 1 {
+				res = res * a % mod
+			}
+			a = a * a % mod
+			b >>= 1
+		}
+		return res
+	}
+	for i, v := range variables {
+		a, b, c, m := v[0], v[1], v[2], v[3]
+		if fastPowMod(fastPowMod(a, b, 10), c, m) == target {
+			ans = append(ans, i)
+		}
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_394/solution.go
+++ problems/problems_394/solution.go
@@ -7,7 +7,26 @@ import (
 )
 
 func decodeString(s string) string {
-
+	var stack [][]interface{}
+	var res string
+	var times int
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if c == '[' {
+			stack = append(stack, []interface{}{times, res})
+			times, res = 0, ""
+		} else if c == ']' {
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			t, r := top[0].(int), top[1].(string)
+			res = r + strings.Repeat(res, t)
+		} else if c >= '0' && c <= '9' {
+			times = times*10 + int(c-'0')
+		} else {
+			res += string(c)
+		}
+	}
+	return res
 }
 
 func Solve(inputJsonValues string) interface{} {
