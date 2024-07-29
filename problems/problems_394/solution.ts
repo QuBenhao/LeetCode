c0880a84fe696b1b65eaa8192014545b8008107a
benhao <qubenhao2@gmail.com>
2024-07-30 07:46:44 +0800

    test: typescript 2961, 394
    solution

--- problems/problems_394/solution.ts
+++ problems/problems_394/solution.ts
@@ -1,5 +1,22 @@
 function decodeString(s: string): string {
-    
+	const stack: Array<Array<any>> = [];
+	let str: string = "";
+	let num: number = 0;
+	for (let i: number = 0; i < s.length; i++) {
+		if (s[i] >= "0" && s[i] <= "9") {
+			num = num * 10 + parseInt(s[i]);
+		} else if (s[i] === "[") {
+			stack.push([str, num]);
+			str = "";
+			num = 0;
+		} else if (s[i] === "]") {
+			const [lastStr, lastNum] = stack.pop();
+			str = lastStr + str.repeat(lastNum);
+		} else {
+			str += s[i];
+		}
+	}
+	return str;
 };
 
 export function Solve(inputJsonElement: string): any {
