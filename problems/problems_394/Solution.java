13aea9405753126c0d42c888c7638118f657842c
benhao <qubenhao2@gmail.com>
2024-07-30 07:43:15 +0800

    test: Java 2961, 394
    solution

--- problems/problems_394/Solution.java
+++ problems/problems_394/Solution.java
@@ -7,7 +7,32 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public String decodeString(String s) {
-
+        Deque<String> stack = new ArrayDeque<>();
+        Deque<Integer> numStack = new ArrayDeque<>();
+        StringBuilder res = new StringBuilder();
+        int multi = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '[') {
+                numStack.push(multi);
+                stack.push(res.toString());
+                multi = 0;
+                res = new StringBuilder();
+            } else if (c == ']') {
+                int t = numStack.pop();
+                String last = stack.pop();
+                StringBuilder tmp = new StringBuilder(last);
+                for (int j = 0; j < t; j++) {
+                    tmp.append(res);
+                }
+                res = tmp;
+            } else if (c >= '0' && c <= '9') {
+                multi = multi * 10 + Integer.parseInt(String.valueOf(c));
+            } else {
+                res.append(c);
+            }
+        }
+        return res.toString();
     }
 
     @Override
