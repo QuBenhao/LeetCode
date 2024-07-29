ef9509c8d8f835e5fa61c130af0bca7f1c8fb920
benhao <qubenhao2@gmail.com>
2024-07-29 22:00:25 +0800

    test: rust 133
    solution

--- problems/problems_133/solution.rs
+++ problems/problems_133/solution.rs
@@ -20,9 +20,28 @@ pub struct Solution;
 // }
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::collections::HashSet;
 impl Solution {
-    pub fn clone_graph(node: Option<Rc<RefCell<Node>>>) -> Option<Rc<RefCell<Node>>> {
+    fn dfs(node: &Rc<RefCell<Node>>, visited: &mut HashSet<i32>, cloned_node: &mut Rc<RefCell<Node>>) {
+        if visited.contains(&node.borrow().val) {
+            return;
+        }
+        visited.insert(node.borrow().val);
+        for neighbor in node.borrow().neighbors.iter() {
+            let mut cloned_neighbor: Rc<RefCell<Node>> = Rc::new(RefCell::new(Node::new(neighbor.borrow().val)));
+            cloned_node.as_ref().borrow_mut().neighbors.push(cloned_neighbor.clone());
+            Solution::dfs(&neighbor.clone(), visited, &mut cloned_neighbor);
+        }
+    }
 
+    pub fn clone_graph(node: Option<Rc<RefCell<Node>>>) -> Option<Rc<RefCell<Node>>> {
+        if node.is_none() {
+            return None;
+        }
+        let cloned_node: Rc<RefCell<Node>> = Rc::new(RefCell::new(Node::new(node.as_ref().unwrap().borrow().val)));
+        let mut visited: HashSet<i32> = HashSet::new();
+        Solution::dfs(node.as_ref().unwrap(), &mut visited, &mut cloned_node.clone());
+        Some(cloned_node)
     }
 }
 
@@ -32,11 +51,10 @@ impl Solution {
  * let obj = Solution;
  * let data: Option<Rc<RefCell<Node>>> = obj.clone_graph(node);
  */
-
 #[cfg(feature = "solution_133")]
 pub fn solve(input_string: String) -> Value {
-	let input_values: Vec<String> = input_string.split('\n').map(|x| x.to_string()).collect();
-	let input_vec0: Vec<Vec<i32>> = serde_json::from_str(&input_values[0]).expect("Failed to parse input");
-	let node: Option<Rc<RefCell<Node>>> = array_to_node_neighbors(&input_vec0);
-	json!(node_neighbors_to_array(&Solution::clone_graph(node)))
+    let input_values: Vec<String> = input_string.split('\n').map(|x| x.to_string()).collect();
+    let input_vec0: Vec<Vec<i32>> = serde_json::from_str(&input_values[0]).expect("Failed to parse input");
+    let node: Option<Rc<RefCell<Node>>> = array_to_node_neighbors(&input_vec0);
+    json!(node_neighbors_to_array(&Solution::clone_graph(node)))
 }
