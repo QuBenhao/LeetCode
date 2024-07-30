7d0193fd213d9951f725a3d7076d2bc5fe69c3a3
benhao <qubenhao2@gmail.com>
2024-07-31 06:44:05 +0800

    test: golang 3111, 5
    solution

--- problems/problems_3111/solution.go
+++ problems/problems_3111/solution.go
@@ -3,11 +3,22 @@ package problem3111
 import (
 	"encoding/json"
 	"log"
+	"sort"
 	"strings"
 )
 
-func minRectanglesToCoverPoints(points [][]int, w int) int {
-
+func minRectanglesToCoverPoints(points [][]int, w int) (ans int) {
+	sort.Slice(points, func(i, j int) bool {
+		return points[i][0] < points[j][0]
+	})
+	for idx := 0; idx < len(points); {
+		ans++
+		cur := points[idx][0] + w
+		for idx < len(points) && points[idx][0] <= cur {
+			idx++
+		}
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_5/solution.go
+++ problems/problems_5/solution.go
@@ -7,7 +7,31 @@ import (
 )
 
 func longestPalindrome(s string) string {
-
+	if len(s) == 0 {
+		return s
+	}
+	start, end := 0, 0
+	for i := 0; i < len(s); i++ {
+		left, right := i, i
+		for left >= 0 && right < len(s) && s[left] == s[right] {
+			left--
+			right++
+		}
+		if right-left-2 > end-start {
+			start = left + 1
+			end = right - 1
+		}
+		left, right = i, i+1
+		for left >= 0 && right < len(s) && s[left] == s[right] {
+			left--
+			right++
+		}
+		if right-left-2 > end-start {
+			start = left + 1
+			end = right - 1
+		}
+	}
+	return s[start : end+1]
 }
 
 func Solve(inputJsonValues string) interface{} {
