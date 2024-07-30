08b24c9fa75c01cbfeddb5bb2bc2f5ff03a07bc2
benhao <qubenhao2@gmail.com>
2024-07-31 07:49:37 +0800

    test: rust 3111, 5
    solution

--- problems/problems_5/solution.rs
+++ problems/problems_5/solution.rs
@@ -3,8 +3,29 @@ use serde_json::{json, Value};
 pub struct Solution;
 
 impl Solution {
+    fn expand(s: &[u8], mut left: usize, mut right: usize) -> (usize, usize) {
+        while left > 0 && right < s.len() - 1 && s[left - 1] == s[right + 1] {
+            left -= 1;
+            right += 1;
+        }
+        (left , right)
+    }
     pub fn longest_palindrome(s: String) -> String {
-
+        let (mut start, mut end) = (0, 0);
+        let bytes = s.as_bytes();
+        let n = bytes.len();
+        for i in 0..n {
+            for j in (i..n).take(2) {
+                if bytes[i] != bytes[j] {
+                    continue;
+                }
+                let (l, r) = Self::expand(bytes, i, j);
+                if r - l > end - start {
+                    (start, end) = (l, r);
+                }
+            }
+        }
+        s[start..=end].to_string()
     }
 }
 
