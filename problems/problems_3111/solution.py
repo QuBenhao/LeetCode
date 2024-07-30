5ddb4409ffa1173932c7e226c201e3f09cf124e6
benhao <qubenhao2@gmail.com>
2024-07-31 06:33:01 +0800

    test: python 3111, 5
    solution

--- problems/problems_3111/solution.py
+++ problems/problems_3111/solution.py
@@ -7,5 +7,11 @@ class Solution(solution.Solution):
         return self.minRectanglesToCoverPoints(*test_input)
 
     def minRectanglesToCoverPoints(self, points: List[List[int]], w: int) -> int:
-        pass
-
+        poi = sorted(set(p[0] for p in points))
+        ans = idx = 0
+        while idx < len(poi):
+            ans += 1
+            cur = poi[idx] + w
+            while idx < len(poi) and poi[idx] <= cur:
+                idx += 1
+        return ans
