6b16829bf701f2b9f9be914a27cf66230c182d88
benhao <qubenhao2@gmail.com>
2024-07-31 07:44:51 +0800

    test: typescript 3111, 5
    solution

--- problems/problems_3111/solution.ts
+++ problems/problems_3111/solution.ts
@@ -1,5 +1,15 @@
 function minRectanglesToCoverPoints(points: number[][], w: number): number {
-    
+	points.sort((a, b) => a[0] - b[0]);
+	const n: number = points.length;
+	let ans: number = 0;
+	for (let idx: number = 0; idx < n; ) {
+		ans++;
+		const cur: number = points[idx][0] + w;
+		while (idx < n && points[idx][0] <= cur) {
+			idx++;
+		}
+	}
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
