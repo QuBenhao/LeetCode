ce86c36feee7ea7a086f4023f1c0a5543c6670f6
benhao <qubenhao2@gmail.com>
2024-08-01 06:40:41 +0800

    test: java LCP40, 169
    solution

--- problems/problems_LCP_40/Solution.java
+++ problems/problems_LCP_40/Solution.java
@@ -6,8 +6,31 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
+    private int replaceSum(int[] cards, int s, int cnt, int x) {
+        for (int i = cards.length - cnt - 1; i >= 0; i--) {
+            if (cards[i] % 2 != x % 2) {
+                return s - x + cards[i];
+            }
+        }
+        return 0;
+    }
     public int maxmiumScore(int[] cards, int cnt) {
-
+        Arrays.sort(cards);
+        int s = 0, n = cards.length;
+        for (int i = n - cnt; i < n; i++) {
+            s += cards[i];
+        }
+        if (s % 2 == 0) {
+            return s;
+        }
+        int cur = cards[n - cnt];
+        int ans = replaceSum(cards, s, cnt, cur);
+        for (int i = n -cnt + 1; i < n; i++) {
+            if (cards[i] % 2 != cur % 2) {
+                ans = Math.max(ans, replaceSum(cards, s, cnt, cards[i]));
+            }
+        }
+        return ans;
     }
 
     @Override
