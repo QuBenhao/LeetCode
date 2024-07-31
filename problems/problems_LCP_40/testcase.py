b2078225b0e6432229d5ead78c23e40dc18d4c22
benhao <qubenhao2@gmail.com>
2024-08-01 06:20:36 +0800

    test: python LCP40, 169
    solution

--- problems/problems_LCP_40/testcase.py
+++ problems/problems_LCP_40/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=[[1, 2, 8, 9], 3], Output=18))
 		self.testcases.append(case(Input=[[3, 3, 1], 1], Output=0))
+		self.testcases.append(case(Input=[[10,3],1], Output=10))
 
 	def get_testcases(self):
 		return self.testcases
