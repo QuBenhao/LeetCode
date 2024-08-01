c23cb8f5c63b40e5bb75636111afde8735f0a504
benhao <qubenhao2@gmail.com>
2024-08-02 07:36:58 +0800

    test: java 3128, 322
    solution

--- problems/problems_3128/Solution.java
+++ problems/problems_3128/Solution.java
@@ -7,7 +7,23 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public long numberOfRightTriangles(int[][] grid) {
-
+        int m = grid.length, n = grid[0].length;
+        int[] rowCount = new int[m], colCount = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                rowCount[i] += grid[i][j];
+                colCount[j] += grid[i][j];
+            }
+        }
+        long ans = 0L;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] > 0) {
+                    ans += (long)(rowCount[i] - 1) * (colCount[j] - 1);
+                }
+            }
+        }
+        return ans;
     }
 
     @Override
