d3d6c79b0f2a922d3a18ecb50768f70ddd9425ab
benhao <qubenhao2@gmail.com>
2024-08-03 07:51:12 +0800

    test: python 3143, 21, 108
    solution

--- problems/problems_108/solution.py
+++ problems/problems_108/solution.py
@@ -17,5 +17,10 @@ class Solution(solution.Solution):
         return tree_to_list(res)
 
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-        pass
-
+        if not nums:
+            return None
+        mid = len(nums) // 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid + 1:])
+        return root
--- problems/problems_3143/solution.py
+++ problems/problems_3143/solution.py
@@ -1,5 +1,7 @@
 import solution
 from typing import *
+from collections import defaultdict
+from math import inf
 
 
 class Solution(solution.Solution):
@@ -7,5 +9,16 @@ class Solution(solution.Solution):
         return self.maxPointsInsideSquare(*test_input)
 
     def maxPointsInsideSquare(self, points: List[List[int]], s: str) -> int:
-        pass
-
+        max_d = inf
+        explore = defaultdict(lambda: (inf, inf))
+        for (x, y), c in zip(points, s):
+            cur = max(abs(x), abs(y))
+            if c in explore:
+                v = sorted([cur, explore[c][0], explore[c][1]])
+                explore[c] = (v[0], v[1])
+                max_d = min(max_d, v[1] - 1)
+            else:
+                explore[c] = (cur, inf)
+        if max_d == inf:
+            return len(s)
+        return sum(1 for x, y in points if abs(x) <= max_d and abs(y) <= max_d)
