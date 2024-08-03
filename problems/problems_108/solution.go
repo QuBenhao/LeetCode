b92e6c321caeab6bef13e2cd8fb2e719406bef1a
benhao <qubenhao2@gmail.com>
2024-08-03 08:16:11 +0800

    test: golang 3143, 21, 108
    solution

--- problems/problems_108/solution.go
+++ problems/problems_108/solution.go
@@ -2,6 +2,7 @@ package problem108
 
 import (
 	"encoding/json"
+	. "leetCode/golang/models"
 	"log"
 	"strings"
 )
@@ -15,7 +16,14 @@ import (
  * }
  */
 func sortedArrayToBST(nums []int) *TreeNode {
-
+	if len(nums) == 0 {
+		return nil
+	}
+	mid := len(nums) / 2
+	root := &TreeNode{Val: nums[mid]}
+	root.Left = sortedArrayToBST(nums[:mid])
+	root.Right = sortedArrayToBST(nums[mid+1:])
+	return root
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_3143/solution.go
+++ problems/problems_3143/solution.go
@@ -3,11 +3,38 @@ package problem3143
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"strings"
 )
 
-func maxPointsInsideSquare(points [][]int, s string) int {
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
 
+func maxPointsInsideSquare(points [][]int, s string) (ans int) {
+	idxMap := make([]int, 26)
+	for i := 0; i < 26; i++ {
+		idxMap[i] = math.MaxInt
+	}
+	minD := math.MaxInt
+	for i, point := range points {
+		c := s[i] - 'a'
+		if cur := max(abs(point[0]), abs(point[1])); cur < idxMap[c] {
+			minD = min(minD, idxMap[c])
+			idxMap[c] = cur
+		} else {
+			minD = min(minD, cur)
+		}
+	}
+	for _, m := range idxMap {
+		if m < minD {
+			ans++
+		}
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
