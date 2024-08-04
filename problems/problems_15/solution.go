9e331a5d7682c3eeec4760416eaa71bf04f9453e
benhao <qubenhao2@gmail.com>
2024-08-05 07:42:04 +0800

    test: golang 600, 15
    solution

--- problems/problems_15/solution.go
+++ problems/problems_15/solution.go
@@ -3,11 +3,35 @@ package problem15
 import (
 	"encoding/json"
 	"log"
+	"sort"
 	"strings"
 )
 
-func threeSum(nums []int) [][]int {
-
+func threeSum(nums []int) (ans [][]int) {
+	sort.Ints(nums)
+	n := len(nums)
+	for i := 0; i < n-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		target := -nums[i]
+		k := n - 1
+		for j := i + 1; j < n-1; j++ {
+			if j > i+1 && nums[j] == nums[j-1] {
+				continue
+			}
+			for j < k && nums[j]+nums[k] > target {
+				k--
+			}
+			if j == k {
+				break
+			}
+			if nums[j]+nums[k] == target {
+				ans = append(ans, []int{nums[i], nums[j], nums[k]})
+			}
+		}
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_600/solution.go
+++ problems/problems_600/solution.go
@@ -6,8 +6,27 @@ import (
 	"strings"
 )
 
-func findIntegers(n int) int {
-
+func findIntegers(n int) (ans int) {
+	// dp[i] 表示长度为 i 的二进制不包含连续的 1 的个数
+	dp := make([]int, 31)
+	dp[0], dp[1] = 1, 2
+	for i := 2; i < 31; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	n++
+	pre := 0
+	for i := 29; i >= 0; i-- {
+		if n&(1<<i) != 0 {
+			ans += dp[i]
+			if pre == 1 {
+				return
+			}
+			pre = 1
+		} else {
+			pre = 0
+		}
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
