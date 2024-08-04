5f7cc6ca64a58d0f70b3b107f055627d547b8ffc
benhao <qubenhao2@gmail.com>
2024-08-05 07:51:38 +0800

    test: Java 600, 15
    solution

--- problems/problems_15/Solution.java
+++ problems/problems_15/Solution.java
@@ -7,7 +7,30 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public List<List<Integer>> threeSum(int[] nums) {
-
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int target = -nums[i], k = n - 1;
+            for (int j = i + 1; j < n - 1; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                while (j < k && nums[j] + nums[k] > target) {
+                    k--;
+                }
+                if (j == k) {
+                    break;
+                }
+                if (nums[j] + nums[k] == target) {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                }
+            }
+        }
+        return res;
     }
 
     @Override
--- problems/problems_600/Solution.java
+++ problems/problems_600/Solution.java
@@ -7,7 +7,26 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int findIntegers(int n) {
-
+        int[] dp = new int[31];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < 31; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        n++;
+        int ans = 0, pre = 0;
+        for (int i = 29; i >= 0; i--) {
+            if ((n & (1 << i)) > 0) {
+                ans += dp[i];
+                if (pre == 1) {
+                    break;
+                }
+                pre = 1;
+            } else {
+                pre = 0;
+            }
+        }
+        return ans;
     }
 
     @Override
