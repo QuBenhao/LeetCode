4b783a8fffb37c5c6e642b4cc486b29d2d5b979b
benhao <qubenhao2@gmail.com>
2024-08-04 21:31:22 +0800

    test: golang 2808
    solution

--- problems/problems_2808/solution.go
+++ problems/problems_2808/solution.go
@@ -7,7 +7,20 @@ import (
 )
 
 func minimumSeconds(nums []int) int {
-
+	idxMap := map[int][]int{}
+	for i, num := range nums {
+		idxMap[num] = append(idxMap[num], i)
+	}
+	n := len(nums)
+	ans := n
+	for _, idxes := range idxMap {
+		cur := idxes[0] + n - idxes[len(idxes)-1]
+		for i := 1; i < len(idxes); i++ {
+			cur = max(cur, idxes[i]-idxes[i-1])
+		}
+		ans = min(ans, cur)
+	}
+	return ans / 2
 }
 
 func Solve(inputJsonValues string) interface{} {
