06cdccba9514aff1eeb1a7d72056f79154394b73
benhao <qubenhao2@gmail.com>
2024-08-04 21:40:47 +0800

    test: Java 2808
    solution

--- problems/problems_2808/Solution.java
+++ problems/problems_2808/Solution.java
@@ -7,7 +7,23 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int minimumSeconds(List<Integer> nums) {
-
+        int n = nums.size();
+        Map<Integer, List<Integer>> idxMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (!idxMap.containsKey(nums.get(i))) {
+                idxMap.put(nums.get(i), new ArrayList<>());
+            }
+            idxMap.get(nums.get(i)).add(i);
+        }
+        int ans = n;
+        for (List<Integer> idxList : idxMap.values()) {
+            int cur = idxList.getFirst() + n - idxList.getLast();
+            for (int i = 1; i < idxList.size(); i++) {
+                cur = Math.max(cur, idxList.get(i) - idxList.get(i - 1));
+            }
+            ans = Math.min(ans, cur);
+        }
+        return ans / 2;
     }
 
     @Override
