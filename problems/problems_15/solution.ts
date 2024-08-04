11e0cc93b8c7974b38920da5493c68de175b693a
benhao <qubenhao2@gmail.com>
2024-08-05 07:57:22 +0800

    test: typescript 600, 15
    solution

--- problems/problems_15/solution.ts
+++ problems/problems_15/solution.ts
@@ -1,5 +1,29 @@
 function threeSum(nums: number[]): number[][] {
-    
+    const ans: number[][] = [];
+	nums.sort((a, b) => a - b);
+	const n: number = nums.length;
+	for (let i: number = 0; i < n; i++) {
+		if (i > 0 && nums[i] === nums[i - 1]) {
+			continue;
+		}
+		let k: number = n - 1;
+		for (let j: number = i + 1; j < n; j++) {
+			if (j > i + 1 && nums[j] === nums[j - 1]) {
+				continue;
+			}
+			const target: number = -nums[i] - nums[j];
+			while (j < k &&  nums[k] > target) {
+				k--;
+			}
+			if (j === k) {
+				break;
+			}
+			if (nums[k] === target) {
+				ans.push([nums[i], nums[j], nums[k]]);
+			}
+		}
+	}
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_600/solution.ts
+++ problems/problems_600/solution.ts
@@ -1,5 +1,24 @@
 function findIntegers(n: number): number {
-    
+    const dp: Array<number> = new Array<number>(31);
+	dp[0] = 1;
+	dp[1] = 2;
+	for (let i: number = 2; i < 31; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	n++;
+	let ans: number = 0, pre: number = 0;
+	for (let i = 29; i >= 0; i--) {
+		if ((n & (1 << i)) !== 0) {
+			ans += dp[i];
+			if (pre === 1) {
+				break;
+			}
+			pre = 1;
+		} else {
+			pre = 0;
+		}
+	}
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
