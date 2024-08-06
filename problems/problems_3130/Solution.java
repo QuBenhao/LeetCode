fd5a3cf7831cfc9e6c4af742ff576c46c9b727a9
benhao <qubenhao2@gmail.com>
2024-08-07 06:39:33 +0800

    test: Java 48
    solution

--- problems/problems_3130/Solution.java
+++ problems/problems_3130/Solution.java
@@ -6,8 +6,28 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
+    private static final int MOD = 1000000007;
     public int numberOfStableArrays(int zero, int one, int limit) {
-
+        int[][][] dp = new int[zero + 1][one + 1][2];
+        for (int i = 1; i <= Math.min(zero, limit); i++) {
+            dp[i][0][0] = 1;
+        }
+        for (int j = 1; j <= Math.min(one, limit); j++) {
+            dp[0][j][1] = 1;
+        }
+        for (int i = 1; i <= zero; i++) {
+            for (int j = 1; j <= one; j++) {
+                dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1]) % MOD;
+                if (i > limit) {
+                    dp[i][j][0] = (dp[i][j][0] - dp[i - limit - 1][j][1] + MOD) % MOD;
+                }
+                dp[i][j][1] = (dp[i][j - 1][0] + dp[i][j - 1][1]) % MOD;
+                if (j > limit) {
+                    dp[i][j][1] = (dp[i][j][1] - dp[i][j - limit - 1][0] + MOD) % MOD;
+                }
+            }
+        }
+        return (dp[zero][one][0] + dp[zero][one][1]) % MOD;
     }
 
     @Override
--- problems/problems_48/Solution.java
+++ problems/problems_48/Solution.java
@@ -7,7 +7,16 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public void rotate(int[][] matrix) {
-
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < (n + 1) / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = tmp;
+            }
+        }
     }
 
     @Override
