046d61e3980f5661cc889b8402cb59c27ace7e9f
benhao <qubenhao2@gmail.com>
2024-08-06 20:19:15 +0800

    test: golang 3129, 438
    solution

--- problems/problems_438/solution.go
+++ problems/problems_438/solution.go
@@ -6,8 +6,38 @@ import (
 	"strings"
 )
 
-func findAnagrams(s string, p string) []int {
+func findAnagrams(s string, p string) (ans []int) {
+	helper := func(counter []int, key int, val int) int {
+		before := counter[key] == 0
+		counter[key] += val
+		if before {
+			return 1
+		} else if counter[key] == 0 {
+			return -1
+		}
+		return 0
+	}
 
+	counter := make([]int, 26)
+	for _, c := range p {
+		counter[c-'a']--
+	}
+	diff := 0
+	for _, c := range counter {
+		if c != 0 {
+			diff++
+		}
+	}
+	for i, c := range s {
+		diff += helper(counter, int(c-'a'), 1)
+		if i >= len(p)-1 {
+			if diff == 0 {
+				ans = append(ans, i-len(p)+1)
+			}
+			diff += helper(counter, int(s[i-len(p)+1]-'a'), -1)
+		}
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
