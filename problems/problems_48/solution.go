ab8d9ba3533fc5177c7519759465b8d5ebf5c758
benhao <qubenhao2@gmail.com>
2024-08-07 06:18:50 +0800

    test: golang 48
    solution

--- problems/problems_48/solution.go
+++ problems/problems_48/solution.go
@@ -6,8 +6,14 @@ import (
 	"strings"
 )
 
-func rotate(matrix [][]int)  {
-
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n/2; i++ {
+		for j := 0; j < (n+1)/2; j++ {
+			matrix[i][j], matrix[j][n-i-1], matrix[n-i-1][n-j-1], matrix[n-j-1][i] =
+				matrix[n-j-1][i], matrix[i][j], matrix[j][n-i-1], matrix[n-i-1][n-j-1]
+		}
+	}
 }
 
 func Solve(inputJsonValues string) interface{} {
