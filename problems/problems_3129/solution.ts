d2438c3a111762ead748d7ed46dd74f797ef62ac
benhao <qubenhao2@gmail.com>
2024-08-06 21:38:03 +0800

    test: typescript 3129, 438
    solution

--- problems/problems_3129/solution.ts
+++ problems/problems_3129/solution.ts
@@ -1,5 +1,31 @@
 function numberOfStableArrays(zero: number, one: number, limit: number): number {
-    
+	const mod: number = 1000000007;
+	const dp: Array<Array<Array<number>>> = new Array<Array<Array<number>>>(zero + 1);
+	for (let i: number = 0; i < zero + 1; i++) {
+		dp[i] = new Array<Array<number>>(one + 1);
+		for (let j: number = 0; j < one + 1; j++) {
+			dp[i][j] = new Array<number>(2).fill(0);
+		}
+	}
+	for (let i: number = 1; i <= Math.min(zero, limit); i++) {
+		dp[i][0][0] = 1;
+	}
+	for (let i: number = 1; i <= Math.min(one, limit); i++) {
+		dp[0][i][1] = 1;
+	}
+	for (let i: number = 1; i <= zero; i++) {
+		for (let j: number = 1; j <= one; j++) {
+			dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1]) % mod;
+			if (i > limit) {
+				dp[i][j][0] = (dp[i][j][0] - dp[i - limit - 1][j][1] + mod) % mod;
+			}
+			dp[i][j][1] = (dp[i][j - 1][0] + dp[i][j - 1][1]) % mod;
+			if (j > limit) {
+				dp[i][j][1] = (dp[i][j][1] - dp[i][j - limit - 1][0] + mod) % mod;
+			}
+		}
+	}
+	return (dp[zero][one][0] + dp[zero][one][1]) % mod;
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_438/solution.ts
+++ problems/problems_438/solution.ts
@@ -1,5 +1,37 @@
 function findAnagrams(s: string, p: string): number[] {
-
+	const m: number = s.length, n: number = p.length;
+	const counter: number[] = new Array<number>(26).fill(0);
+	for (let i: number = 0; i < n; i++) {
+		counter[p.charCodeAt(i) - 97]--;
+	}
+	let diff: number = 0;
+	for (let i: number = 0; i < 26; i++) {
+		if (counter[i] !== 0) {
+			diff++;
+		}
+	}
+	const helper: Function = (key: number, val: number)=> {
+		const before: boolean = counter[key] === 0;
+		counter[key] += val;
+		if (before) {
+			return 1;
+		}
+		if (counter[key] === 0) {
+			return -1;
+		}
+		return 0;
+	}
+	const ans: number[] = [];
+	for (let i: number = 0; i < m; i++) {
+		diff += helper(s.charCodeAt(i) - 97, 1);
+		if (i >= n - 1) {
+			if (diff === 0) {
+				ans.push(i - n + 1);
+			}
+			diff += helper(s.charCodeAt(i - n + 1) - 97, -1);
+		}
+	}
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
