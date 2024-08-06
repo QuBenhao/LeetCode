794e6498ab03e0ce9474a6a76a60ed1eaa34aa38
benhao <qubenhao2@gmail.com>
2024-08-07 06:15:26 +0800

    test: 3130 same as 3129
    solution

--- problems/problems_3130/solution.go
+++ problems/problems_3130/solution.go
@@ -7,7 +7,30 @@ import (
 )
 
 func numberOfStableArrays(zero int, one int, limit int) int {
-
+	const mod = 1_000_000_007
+	dp := make([][][2]int, zero+1)
+	for i := range dp {
+		dp[i] = make([][2]int, one+1)
+	}
+	for i := 1; i <= min(zero, limit); i++ {
+		dp[i][0][0] = 1
+	}
+	for j := 1; j <= min(one, limit); j++ {
+		dp[0][j][1] = 1
+	}
+	for i := 1; i <= zero; i++ {
+		for j := 1; j <= one; j++ {
+			dp[i][j][0] = (dp[i-1][j][0] + dp[i-1][j][1]) % mod
+			if i > limit {
+				dp[i][j][0] = (dp[i][j][0] - dp[i-limit-1][j][1] + mod) % mod
+			}
+			dp[i][j][1] = (dp[i][j-1][0] + dp[i][j-1][1]) % mod
+			if j > limit {
+				dp[i][j][1] = (dp[i][j][1] - dp[i][j-limit-1][0] + mod) % mod
+			}
+		}
+	}
+	return (dp[zero][one][0] + dp[zero][one][1]) % mod
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_3130/solution.py
+++ problems/problems_3130/solution.py
@@ -7,5 +7,14 @@ class Solution(solution.Solution):
         return self.numberOfStableArrays(*test_input)
 
     def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:
-        pass
-
+        mod = 10 ** 9 + 7
+        dp = [[[0] * 2 for _ in range(one + 1)] for _ in range(zero + 1)]
+        for i in range(1, min(zero, limit) + 1):
+            dp[i][0][0] = 1
+        for j in range(1, min(one, limit) + 1):
+            dp[0][j][1] = 1
+        for i in range(1, zero + 1):
+            for j in range(1, one + 1):
+                dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1] - (dp[i - limit - 1][j][1] if i > limit else 0)) % mod
+                dp[i][j][1] = (dp[i][j - 1][0] + dp[i][j - 1][1] - (dp[i][j - limit - 1][0] if j > limit else 0)) % mod
+        return (dp[zero][one][0] + dp[zero][one][1]) % mod
