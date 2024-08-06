da4c00bc2528d278b51b0813f8cc603cff33cc69
benhao <qubenhao2@gmail.com>
2024-08-06 23:01:31 +0800

    test: Java 2398
    solution

--- problems/problems_2398/Solution.java
+++ problems/problems_2398/Solution.java
@@ -6,8 +6,39 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
+    private boolean check(int[] chargeTimes, int[] runningCosts, long budget, int mid) {
+        int n = chargeTimes.length;
+        long sum = 0L;
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            while (!deque.isEmpty() && chargeTimes[deque.peekLast()] <= chargeTimes[i]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+            sum += runningCosts[i];
+            if (i >= (deque.peekFirst() == null ? 0 : deque.peekFirst()) + mid) {
+                deque.pollFirst();
+            }
+            if (i >= mid - 1) {
+                if (sum * mid + chargeTimes[deque.peekFirst() == null ? 0 : deque.peekFirst()] <= budget) {
+                    return true;
+                }
+                sum -= runningCosts[i - mid + 1];
+            }
+        }
+        return false;
+    }
     public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
-
+        int left = 0, right = chargeTimes.length;
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2;
+            if (!check(chargeTimes, runningCosts, budget, mid)) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        return left;
     }
 
     @Override
