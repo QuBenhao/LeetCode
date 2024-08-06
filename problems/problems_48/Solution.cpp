aff4bd003e99807d3a46cdffdb37fe87d7b7c0df
benhao <qubenhao2@gmail.com>
2024-08-07 06:25:21 +0800

    test: c++ 48
    solution

--- problems/problems_48/Solution.cpp
+++ problems/problems_48/Solution.cpp
@@ -1,28 +1,37 @@
-//go:build ignore
+// go:build ignore
 #include "cpp/common/Solution.h"
-
 
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-
+  void rotate(vector<vector<int>> &matrix) {
+    int n = static_cast<int>(matrix.size());
+    for (int i = 0; i < n / 2; i++) {
+      for (int j = 0; j < (n + 1) / 2; j++) {
+        int temp = matrix[i][j];
+		matrix[i][j] = matrix[n - j - 1][i];
+		matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+		matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+		matrix[j][n - i - 1] = temp;
+      }
     }
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<vector<int>> matrix = json::parse(inputArray.at(0));
-	return solution.rotate(matrix);
+  Solution solution;
+  vector<vector<int>> matrix = json::parse(inputArray.at(0));
+  solution.rotate(matrix);
+  return matrix;
 }
