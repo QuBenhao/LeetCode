e2a4c6cd7d9875e7051a7ba7a68f7fd38fd95259
benhao <qubenhao2@gmail.com>
2024-08-07 06:45:21 +0800

    test: typescript 48
    solution

--- problems/problems_48/solution.ts
+++ problems/problems_48/solution.ts
@@ -2,7 +2,16 @@
  Do not return anything, modify matrix in-place instead.
  */
 function rotate(matrix: number[][]): void {
-    
+	const n: number = matrix.length;
+	for (let i: number = 0; i < Math.floor(n / 2); i++) {
+		for (let j: number = 0; j < Math.floor((n + 1) / 2); j++) {
+			const temp: number = matrix[i][j];
+			matrix[i][j] = matrix[n - j - 1][i];
+			matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+			matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+			matrix[j][n - i - 1] = temp;
+		}
+	}
 };
 
 export function Solve(inputJsonElement: string): any {
