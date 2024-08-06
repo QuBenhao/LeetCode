32097d9ce698b046c92afb92ee838eead797a0e1
benhao <qubenhao2@gmail.com>
2024-08-06 21:12:38 +0800

    test: c++ 3129, 438
    solution

--- problems/problems_438/Solution.cpp
+++ problems/problems_438/Solution.cpp
@@ -8,7 +8,37 @@ using json = nlohmann::json;
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-
+        vector<int> ans;
+        vector<int> counts(26, 0);
+        for (auto c: p) {
+            counts[c - 'a']--;
+        }
+        int diff = 0;
+        for (auto c: counts) {
+            diff += c != 0;
+        }
+        int m = static_cast<int>(s.size()), n = static_cast<int>(p.size());
+        auto helper = [&counts](int key, int val)-> int {
+            bool before = counts[key] == 0;
+            counts[key] += val;
+            if (before) {
+                return 1;
+            }
+            if (counts[key] == 0) {
+                return -1;
+            }
+            return 0;
+        };
+        for (int i = 0; i < m; i++) {
+            diff += helper(s[i] - 'a', 1);
+            if (i >= n - 1) {
+                if (diff == 0) {
+                    ans.push_back(i - n + 1);
+                }
+                diff += helper(s[i - n + 1] - 'a', -1);
+            }
+        }
+        return ans;
     }
 };
 
