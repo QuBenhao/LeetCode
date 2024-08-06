d2b6c794f88710e91c45d1a92da03e5931c331a0
benhao <qubenhao2@gmail.com>
2024-08-06 22:10:16 +0800

    test: rust 3129, 438
    solution

--- problems/problems_3129/solution.rs
+++ problems/problems_3129/solution.rs
@@ -2,9 +2,30 @@ use serde_json::{json, Value};
 
 pub struct Solution;
 
+use std::cmp::min;
 impl Solution {
     pub fn number_of_stable_arrays(zero: i32, one: i32, limit: i32) -> i32 {
-
+		let mod_num = 1_000_000_007;
+		let mut dp = vec![vec![vec![0; 2]; one as usize + 1]; zero as usize + 1];
+		for i in 1..=min(zero, limit) as usize {
+			dp[i][0][0] = 1;
+		}
+		for j in 1..=min(one, limit) as usize {
+			dp[0][j][1] = 1;
+		}
+		for i in 1..=zero as usize {
+			for j in 1..=one as usize {
+				dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1]) % mod_num;
+				if i as i32 > limit {
+					dp[i][j][0] = (dp[i][j][0] - dp[i - limit as usize - 1][j][1] + mod_num) % mod_num;
+				}
+				dp[i][j][1] = (dp[i][j - 1][0] + dp[i][j - 1][1]) % mod_num;
+				if j as i32 > limit {
+					dp[i][j][1] = (dp[i][j][1] - dp[i][j - limit as usize - 1][0] + mod_num) % mod_num;
+				}
+			}
+		}
+		(dp[zero as usize][one as usize][0] + dp[zero as usize][one as usize][1]) % mod_num
     }
 }
 
--- problems/problems_438/solution.rs
+++ problems/problems_438/solution.rs
@@ -4,14 +4,50 @@ pub struct Solution;
 
 impl Solution {
     pub fn find_anagrams(s: String, p: String) -> Vec<i32> {
-
+        let mut ans: Vec<i32> = Vec::new();
+        let m = s.len();
+        let n = p.len();
+        let mut counts = vec![0; 26];
+        for c in p.chars() {
+            counts[c as usize - 'a' as usize] -= 1;
+        }
+        let mut diff = 0;
+        for &v in counts.iter() {
+            if v != 0 {
+                diff += 1;
+            }
+        }
+        let mut helper = |key: usize, val: i32| -> i32 {
+            let before = counts[key] == 0;
+            counts[key] += val;
+            if before {
+                1
+            } else if counts[key] == 0 {
+                -1
+            } else {
+                0
+            }
+        };
+		let chars: Vec<char> = s.chars().collect();
+        for i in 0..m {
+            diff += helper(chars[i] as usize - 'a' as usize, 1);
+            if i >= n - 1 {
+                if diff == 0 {
+                    ans.push(i as i32 - n as i32 + 1);
+                }
+                if let Some(index) = (i as isize).checked_sub(n as isize - 1) {
+                    diff += helper(chars[index as usize] as usize - 'a' as usize, -1);
+                }
+            }
+        }
+        ans
     }
 }
 
 #[cfg(feature = "solution_438")]
 pub fn solve(input_string: String) -> Value {
-	let input_values: Vec<String> = input_string.split('\n').map(|x| x.to_string()).collect();
-	let s: String = serde_json::from_str(&input_values[0]).expect("Failed to parse input");
-	let p: String = serde_json::from_str(&input_values[1]).expect("Failed to parse input");
-	json!(Solution::find_anagrams(s, p))
+    let input_values: Vec<String> = input_string.split('\n').map(|x| x.to_string()).collect();
+    let s: String = serde_json::from_str(&input_values[0]).expect("Failed to parse input");
+    let p: String = serde_json::from_str(&input_values[1]).expect("Failed to parse input");
+    json!(Solution::find_anagrams(s, p))
 }
