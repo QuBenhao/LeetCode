b7f257c30507d4d0c54e223d03d073d519af0ce8
benhao <qubenhao2@gmail.com>
2024-08-07 07:39:01 +0800

    test: typescript 2398
    solution

--- problems/problems_2398/solution.ts
+++ problems/problems_2398/solution.ts
@@ -1,5 +1,36 @@
 function maximumRobots(chargeTimes: number[], runningCosts: number[], budget: number): number {
-    
+	const n: number = chargeTimes.length;
+	let left: number = 0, right: number = n;
+	const check = (mid: number): boolean => {
+		let s: number = 0;
+		let q: number[] = [];
+		for (let i: number = 0; i < n; i++) {
+			while (q.length > 0 && chargeTimes[q[q.length - 1]] <= chargeTimes[i]) {
+				q.pop();
+			}
+			q.push(i);
+			s += runningCosts[i];
+			if (i >= q[0] + mid) {
+				q.shift();
+			}
+			if (i >= mid - 1) {
+				if (s * mid + chargeTimes[q[0]] <= budget) {
+					return true;
+				}
+				s -= runningCosts[i - mid + 1];
+			}
+		}
+		return false;
+	}
+	while (left < right) {
+		const mid: number = left + right + 1 >> 1;
+		if (check(mid)) {
+			left = mid;
+		} else {
+			right = mid - 1;
+		}
+	}
+	return left;
 };
 
 export function Solve(inputJsonElement: string): any {
