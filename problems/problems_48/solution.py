94a587fc72f6699d5ac8b179d8b8267b9c3b1b9f
benhao <qubenhao2@gmail.com>
2024-08-07 06:17:32 +0800

    test: python 48
    solution

--- problems/problems_48/solution.py
+++ problems/problems_48/solution.py
@@ -11,5 +11,8 @@ class Solution(solution.Solution):
         """
         Do not return anything, modify matrix in-place instead.
         """
-        pass
-
+        n = len(matrix)
+        for i in range(n // 2):
+            for j in range((n + 1) // 2):
+                matrix[i][j], matrix[n - j - 1][i], matrix[n - i - 1][n - j - 1], matrix[j][n - i - 1] = \
+                    matrix[n - j - 1][i], matrix[n - i - 1][n - j - 1], matrix[j][n - i - 1], matrix[i][j]
