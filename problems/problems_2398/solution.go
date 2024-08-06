46f154b32a906ad6243d91828202889524f7da19
benhao <qubenhao2@gmail.com>
2024-08-07 07:09:03 +0800

    test: golang 2398
    solution

--- problems/problems_2398/solution.go
+++ problems/problems_2398/solution.go
@@ -7,7 +7,38 @@ import (
 )
 
 func maximumRobots(chargeTimes []int, runningCosts []int, budget int64) int {
-
+	n := len(chargeTimes)
+	left, right := 0, n
+	check := func(length int) bool {
+		var s int64
+		var q []int
+		for i := 0; i < n; i++ {
+			for len(q) > 0 && chargeTimes[q[len(q)-1]] <= chargeTimes[i] {
+				q = q[:len(q)-1]
+			}
+			q = append(q, i)
+			s += int64(runningCosts[i])
+			if i >= q[0]+length {
+				q = q[1:]
+			}
+			if i >= length-1 {
+				if s*int64(length)+int64(chargeTimes[q[0]]) <= budget {
+					return true
+				}
+				s -= int64(runningCosts[i-length+1])
+			}
+		}
+		return false
+	}
+	for left < right {
+		mid := left + (right-left+1)/2
+		if check(mid) {
+			left = mid
+		} else {
+			right = mid - 1
+		}
+	}
+	return left
 }
 
 func Solve(inputJsonValues string) interface{} {
