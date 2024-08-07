322d52bc48362bd2d4be586bef0b7e71004a9527
benhao <qubenhao2@gmail.com>
2024-08-07 21:45:21 +0800

    test: rust 950
    solution

--- problems/problems_950/solution.rs
+++ problems/problems_950/solution.rs
@@ -2,9 +2,20 @@ use serde_json::{json, Value};
 
 pub struct Solution;
 
+use std::collections::VecDeque;
 impl Solution {
     pub fn deck_revealed_increasing(deck: Vec<i32>) -> Vec<i32> {
-
+		let mut q: VecDeque<i32> = VecDeque::new();
+		let mut deck = deck;
+		deck.sort_unstable();
+		for i in (0..deck.len()).rev() {
+			if !q.is_empty() {
+				let last = q.pop_back().unwrap();
+				q.push_front(last);
+			}
+			q.push_front(deck[i]);
+		}
+		q.into_iter().collect()
     }
 }
 
