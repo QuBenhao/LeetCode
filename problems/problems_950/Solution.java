45cb0b0e5aea029814cf0e83ba27ad4fa0d315f4
benhao <qubenhao2@gmail.com>
2024-08-07 21:35:12 +0800

    test: Java 950
    solution

--- problems/problems_950/Solution.java
+++ problems/problems_950/Solution.java
@@ -7,7 +7,15 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int[] deckRevealedIncreasing(int[] deck) {
-
+        Deque<Integer> ans = new ArrayDeque<>();
+        Arrays.sort(deck);
+        for (int i = deck.length - 1; i >= 0; i--) {
+            if (!ans.isEmpty()) {
+                ans.addFirst(ans.pollLast());
+            }
+            ans.addFirst(deck[i]);
+        }
+        return ans.stream().mapToInt(Integer::intValue).toArray();
     }
 
     @Override
