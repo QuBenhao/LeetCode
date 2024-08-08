8c375ac77495f76f201a8a11164a4fe2afb39632
benhao <qubenhao2@gmail.com>
2024-08-08 21:38:45 +0800

    test: golang 2553
    solution

--- problems/problems_2553/solution.go
+++ problems/problems_2553/solution.go
@@ -6,8 +6,18 @@ import (
 	"strings"
 )
 
-func separateDigits(nums []int) []int {
-
+func separateDigits(nums []int) (ans []int) {
+	for _, num := range nums {
+		var cur []int
+		for num > 0 {
+			cur = append(cur, num%10)
+			num /= 10
+		}
+		for i := len(cur) - 1; i >= 0; i-- {
+			ans = append(ans, cur[i])
+		}
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
