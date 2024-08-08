9833752ed4d540fe4057063f5d598c54e7fb3a1c
benhao <qubenhao2@gmail.com>
2024-08-09 07:23:00 +0800

    test: typescript 3132, 22
    solution

--- problems/problems_22/solution.ts
+++ problems/problems_22/solution.ts
@@ -1,5 +1,19 @@
 function generateParenthesis(n: number): string[] {
-    
+    const ans: Array<string> = [];
+	const backtrack: Function = (s: string, left: number, right: number) => {
+		if (left === n && right === n) {
+			ans.push(s);
+			return;
+		}
+		if (left < n) {
+			backtrack(s + "(", left + 1, right);
+		}
+		if (right < left) {
+			backtrack(s + ")", left, right + 1);
+		}
+	}
+	backtrack("", 0, 0);
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_3132/solution.ts
+++ problems/problems_3132/solution.ts
@@ -1,5 +1,22 @@
 function minimumAddedInteger(nums1: number[], nums2: number[]): number {
-    
+    nums1.sort((a, b) => a - b);
+	nums2.sort((a, b) => a - b);
+	out:
+	for (let i: number = 2; i >= 1; i--) {
+		const diff: number = nums2[0] - nums1[i];
+		let quota: number = 2 - i, idx: number = i + 1;
+		for (let j: number = 1; j < nums2.length; j++) {
+			while (nums2[j] - nums1[idx] != diff) {
+				if (quota-- === 0) {
+					continue out;
+				}
+				idx++;
+			}
+			idx++;
+		}
+		return diff;
+	}
+	return nums2[0] - nums1[0];
 };
 
 export function Solve(inputJsonElement: string): any {
