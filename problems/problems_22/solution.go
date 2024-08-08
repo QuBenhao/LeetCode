e03bdfcba91289437022af59ac3a456312ecabf8
benhao <qubenhao2@gmail.com>
2024-08-09 06:47:37 +0800

    test: golang 3132, 22
    solution

--- problems/problems_22/solution.go
+++ problems/problems_22/solution.go
@@ -6,8 +6,26 @@ import (
 	"strings"
 )
 
-func generateParenthesis(n int) []string {
-
+func generateParenthesis(n int) (ans []string) {
+	var backtrack func(path []byte, left, right int)
+	backtrack = func(path []byte, left, right int) {
+		if left == n && right == n {
+			ans = append(ans, string(path))
+			return
+		}
+		if left < n {
+			path = append(path, '(')
+			backtrack(path, left+1, right)
+			path = path[:len(path)-1]
+		}
+		if right < left {
+			path = append(path, ')')
+			backtrack(path, left, right+1)
+			path = path[:len(path)-1]
+		}
+	}
+	backtrack([]byte{}, 0, 0)
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_3132/solution.go
+++ problems/problems_3132/solution.go
@@ -3,11 +3,34 @@ package problem3132
 import (
 	"encoding/json"
 	"log"
+	"sort"
 	"strings"
 )
 
 func minimumAddedInteger(nums1 []int, nums2 []int) int {
-
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	for i := 2; i >= 0; i-- {
+		quota, diff, idx, valid := 2-i, nums2[0]-nums1[i], i+1, true
+		for j := 1; j < len(nums2); j++ {
+			for nums2[j]-nums1[idx] != diff {
+				if quota == 0 {
+					valid = false
+					break
+				}
+				quota--
+				idx++
+			}
+			if !valid {
+				break
+			}
+			idx++
+		}
+		if valid {
+			return diff
+		}
+	}
+	return nums2[0] - nums1[0]
 }
 
 func Solve(inputJsonValues string) interface{} {
