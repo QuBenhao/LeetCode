4e00a62fa533ef0fc7cac6e668c20e25276d6daf
benhao <qubenhao2@gmail.com>
2024-08-08 21:51:23 +0800

    test: typescript 2553
    solution

--- problems/problems_2553/solution.ts
+++ problems/problems_2553/solution.ts
@@ -1,5 +1,17 @@
 function separateDigits(nums: number[]): number[] {
-    
+    const ans: Array<number> = [];
+	for (let i: number = 0; i < nums.length; i++) {
+		let num: number = nums[i];
+		const cur: Array<number> = [];
+		while (num > 0) {
+			cur.push(num % 10);
+			num = Math.floor(num / 10);
+		}
+		for (let j: number = cur.length - 1; j >= 0; j--) {
+			ans.push(cur[j]);
+		}
+	}
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
