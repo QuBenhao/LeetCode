4e2df2c52d1fe6050bf63e638d0341267d0d13a1
benhao <qubenhao2@gmail.com>
2024-08-10 07:51:37 +0800

    test: python 2940, 55, 139
    solution

--- problems/problems_2940/solution.py
+++ problems/problems_2940/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+from heapq import *
 
 
 class Solution(solution.Solution):
@@ -7,5 +8,20 @@ class Solution(solution.Solution):
         return self.leftmostBuildingQueries(*test_input)
 
     def leftmostBuildingQueries(self, heights: List[int], queries: List[List[int]]) -> List[int]:
-        pass
+        ans = [-1] * len(queries)
+        qs: List[List[Tuple[int, int]]] = [[] for _ in heights]
+        for i, (a, b) in enumerate(queries):
+            if a > b:
+                a, b = b, a
+            if a == b or heights[a] < heights[b]:
+                ans[i] = b
+            else:
+                qs[b].append((heights[a], i))
 
+        h = []
+        for i, x in enumerate(heights):
+            while h and h[0][0] < x:
+                ans[heappop(h)[1]] = i
+            for q in qs[i]:
+                heappush(h, q)
+        return ans
--- problems/problems_55/solution.py
+++ problems/problems_55/solution.py
@@ -7,5 +7,11 @@ class Solution(solution.Solution):
         return self.canJump(test_input)
 
     def canJump(self, nums: List[int]) -> bool:
-        pass
-
+        max_idx = 0
+        for i, dis in enumerate(nums):
+            max_idx = max(max_idx, i + dis)
+            if max_idx >= len(nums) - 1:
+                return True
+            if i >= max_idx:
+                return False
+        return max_idx >= len(nums) - 1
