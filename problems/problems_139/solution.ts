f5789c96ec8698e3b695798a3e77c8961ef8196f
benhao <qubenhao2@gmail.com>
2024-08-10 22:08:22 +0800

    test: typescript 2940, 55, 139
    solution

--- problems/problems_139/solution.ts
+++ problems/problems_139/solution.ts
@@ -1,5 +1,17 @@
 function wordBreak(s: string, wordDict: string[]): boolean {
-    
+    const words: Set<string> = new Set(wordDict);
+	const n: number = s.length;
+	const dp: boolean[] = Array(n + 1).fill(false);
+	dp[0] = true;
+	for (let i: number = 1; i <= n; i++) {
+		for (let j: number = 0; j < i; j++) {
+			if (dp[j] && words.has(s.substring(j, i))) {
+				dp[i] = true;
+				break;
+			}
+		}
+	}
+	return dp[n];
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_55/solution.ts
+++ problems/problems_55/solution.ts
@@ -1,5 +1,16 @@
 function canJump(nums: number[]): boolean {
-    
+	const n: number = nums.length;
+    let maxDis: number = 0;
+	for (let i: number = 0; i < n; i++) {
+		maxDis = Math.max(maxDis, i + nums[i]);
+		if (maxDis >= n - 1) {
+			return true;
+		}
+		if (i >= maxDis) {
+			return false;
+		}
+	}
+	return false;
 };
 
 export function Solve(inputJsonElement: string): any {
