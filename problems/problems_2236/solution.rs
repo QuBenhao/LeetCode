7442ae9532d374bd47c20ad40e32cf3322aed488
benhao <qubenhao2@gmail.com>
2024-08-11 21:59:48 +0800

    test: rust 2236
    solution

--- problems/problems_2236/solution.rs
+++ problems/problems_2236/solution.rs
@@ -24,7 +24,11 @@ use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn check_tree(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-
+        let root = root.unwrap();
+        let node = root.borrow();
+        let left = node.left.as_ref().unwrap().borrow();
+        let right = node.right.as_ref().unwrap().borrow();
+        node.val == left.val + right.val
     }
 }
 
--- rust/test_executor/tests/test.rs
+++ rust/test_executor/tests/test.rs
@@ -1,9 +1,9 @@
 const PROBLEM_FOLDER: &str = "problems";
-const PROBLEM_ID: &str = "1035";
+const PROBLEM_ID: &str = "2236";
 
 #[cfg(test)]
 mod test {
-	use solution_1035 as solution;
+	use solution_2236 as solution;
     use test_executor::run_test::run_test;
 
     use crate::{PROBLEM_FOLDER, PROBLEM_ID};
