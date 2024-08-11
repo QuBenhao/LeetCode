fbac92901fd198f1aefcdc7a1d0c9dd379c07cd6
benhao <qubenhao2@gmail.com>
2024-08-11 09:41:37 +0800

    test: c++ 1035, 19, 230
    solution

--- problems/problems_19/Solution.cpp
+++ problems/problems_19/Solution.cpp
@@ -18,7 +18,19 @@ using json = nlohmann::json;
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-
+		ListNode *slow = head, *fast = head;
+		for (int i = 0; i < n; i++) {
+			fast = fast->next;
+		}
+		if (fast == nullptr) {
+			return head->next;
+		}
+		while (fast->next != nullptr) {
+			slow = slow->next;
+			fast = fast->next;
+		}
+		slow->next = slow->next->next;
+		return head;
     }
 };
 
--- problems/problems_230/Solution.cpp
+++ problems/problems_230/Solution.cpp
@@ -1,6 +1,7 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 #include "cpp/models/TreeNode.h"
+#include <vector>
 
 using namespace std;
 using json = nlohmann::json;
@@ -17,9 +18,20 @@ using json = nlohmann::json;
  * };
  */
 class Solution {
+private:
+	void inorder(TreeNode* root, vector<int>& ans) {
+		if (root == nullptr) {
+			return;
+		}
+		inorder(root->left, ans);
+		ans.push_back(root->val);
+		inorder(root->right, ans);
+	}
 public:
     int kthSmallest(TreeNode* root, int k) {
-
+		vector<int> ans;
+		inorder(root, ans);
+		return ans[k - 1];
     }
 };
 
