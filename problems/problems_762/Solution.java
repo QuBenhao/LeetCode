c78f7d5d5e7c0c4a977ad6930169c3b933eb6c20
benhao <qubenhao2@gmail.com>
2024-08-11 22:29:59 +0800

    test: Java 762
    solution

--- problems/problems_762/Solution.java
+++ problems/problems_762/Solution.java
@@ -6,8 +6,23 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
+    static boolean[] hash = new boolean[40];
+    static {
+        int[] nums = new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
+        for (int x : nums) hash[x] = true;
+    }
     public int countPrimeSetBits(int left, int right) {
-
+        int ans = 0;
+        for (int i = left; i <= right; i++) {
+            int x = i;
+            x = (x & 0x55555555) + ((x >>> 1)  & 0x55555555);
+            x = (x & 0x33333333) + ((x >>> 2)  & 0x33333333);
+            x = (x & 0x0f0f0f0f) + ((x >>> 4)  & 0x0f0f0f0f);
+            x = (x & 0x00ff00ff) + ((x >>> 8)  & 0x00ff00ff);
+            x = (x & 0x0000ffff) + ((x >>> 16) & 0x0000ffff);
+            if (hash[x]) ans++;
+        }
+        return ans;
     }
 
     @Override
