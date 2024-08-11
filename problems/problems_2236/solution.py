aeff0f8b0b43340d68c9698bb5b3f5ace05f49a4
benhao <qubenhao2@gmail.com>
2024-08-11 21:47:32 +0800

    test: add 2236
    python solution

---
+++ problems/problems_2236/solution.py
@@ -0,0 +1,20 @@
+import solution
+from typing import *
+from python.object_libs import list_to_tree
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        nums0 = test_input
+        root0 = list_to_tree(nums0)
+        return self.checkTree(root0)
+
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        return root.val == root.left.val + root.right.val
