146e051db5ec3efd01afb62caf55f8fcd929ba5a
benhao <qubenhao2@gmail.com>
2024-08-13 07:27:26 +0800

    test: rust 1139
    solution

--- problems/problems_1139/solution.rs
+++ problems/problems_1139/solution.rs
@@ -1,10 +1,33 @@
 use serde_json::{json, Value};
 
 pub struct Solution;
-
+use std::cmp::min;
 impl Solution {
     pub fn largest1_bordered_square(grid: Vec<Vec<i32>>) -> i32 {
-
+		let m: usize = grid.len();
+		let n: usize = grid[0].len();
+		let mut pre_row: Vec<Vec<i32>> = vec![vec![0; n + 1]; m];
+		let mut pre_col: Vec<Vec<i32>> = vec![vec![0; m + 1]; n];
+		for i in 0..m {
+			for j in 0..n {
+				pre_row[i][j + 1] = pre_row[i][j] + grid[i][j];
+				pre_col[j][i + 1] = pre_col[j][i] + grid[i][j];
+			}
+		}
+		for d in (1..=min(m, n)).rev() {
+			for i in 0..m - d + 1 {
+				for j in 0..n - d + 1 {
+					if pre_row[i][j + d] - pre_row[i][j] == d as i32
+						&& pre_row[i + d - 1][j + d] - pre_row[i + d - 1][j] == d as i32
+						&& pre_col[j][i + d] - pre_col[j][i] == d as i32
+						&& pre_col[j + d - 1][i + d] - pre_col[j + d - 1][i] == d as i32
+					{
+						return d as i32 * d as i32;
+					}
+				}
+			}
+		}
+		0
     }
 }
 
