3d6a8a1d36d5fc4c1b96cc9c86e198066ad7261d
benhao <qubenhao2@gmail.com>
2024-08-14 07:47:52 +0800

    test: Java 3152, 199
    solution

--- problems/problems_199/Solution.java
+++ problems/problems_199/Solution.java
@@ -22,7 +22,28 @@ import qubhjava.models.TreeNode;
 
 public class Solution extends BaseSolution {
     public List<Integer> rightSideView(TreeNode root) {
-
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            TreeNode node = null;
+            for (int i = 0; i < n; i++) {
+                node = queue.poll();
+                assert node != null;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            ans.add(node.val);
+        }
+        return ans;
     }
 
     @Override
--- problems/problems_3152/Solution.java
+++ problems/problems_3152/Solution.java
@@ -7,7 +7,17 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public boolean[] isArraySpecial(int[] nums, int[][] queries) {
-
+        int n = nums.length;
+        int[] preSum = new int[n];
+        for (int i = 0; i < n - 1; i++) {
+            preSum[i + 1] = preSum[i] + ((nums[i] & 1) != (nums[i + 1] & 1) ? 1 : 0);
+        }
+        boolean[] ans = new boolean[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int l = queries[i][0], r = queries[i][1];
+            ans[i] = preSum[r] - preSum[l] == r - l;
+        }
+        return ans;
     }
 
     @Override
