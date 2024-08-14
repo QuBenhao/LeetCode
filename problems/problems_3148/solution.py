e3dbdee0f8485b9accf380be759dff9f04e75a39
benhao <qubenhao2@gmail.com>
2024-08-15 07:47:49 +0800

    test: python 3148, 739
    solution

--- problems/problems_3148/solution.py
+++ problems/problems_3148/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+from math import inf
 
 
 class Solution(solution.Solution):
@@ -7,5 +8,14 @@ class Solution(solution.Solution):
         return self.maxScore(test_input)
 
     def maxScore(self, grid: List[List[int]]) -> int:
-        pass
-
+        # ans = (b - a) + (c - b) + (d -c) + ... = last - first
+        m, n = len(grid), len(grid[0])
+        ans = -inf
+        cols_min = [inf] * n
+        for row in grid:
+            pre_min = inf
+            for j, v in enumerate(row):
+                ans = max(ans, v - min(pre_min, cols_min[j]))
+                cols_min[j] = min(cols_min[j], v)
+                pre_min = min(pre_min, cols_min[j])
+        return ans
--- problems/problems_739/solution.py
+++ problems/problems_739/solution.py
@@ -7,5 +7,11 @@ class Solution(solution.Solution):
         return self.dailyTemperatures(test_input)
 
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        pass
-
+        stack = []
+        ans = [0] * len(temperatures)
+        for i, t in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < t:
+                j = stack.pop()
+                ans[j] = i - j
+            stack.append(i)
+        return ans
