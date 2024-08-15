b5d2da22be5025b3bcdb28b0b3fdccc6aa17e8c7
qubh <qubh@xiaopeng.com>
2024-08-15 09:44:51 +0800

    test: typescript 3148, 739
    solution

--- problems/problems_3148/solution.ts
+++ problems/problems_3148/solution.ts
@@ -1,5 +1,16 @@
 function maxScore(grid: number[][]): number {
-    
+	let ans: number = Number.MIN_SAFE_INTEGER;
+	const n: number = grid[0].length;
+	const colsMin: Array<number> = new Array<number>(n).fill(Number.MAX_SAFE_INTEGER);
+	for (const row of grid) {
+		let preMin: number = Number.MAX_SAFE_INTEGER;
+		for (let j: number = 0; j < n; j++) {
+			ans = Math.max(ans, row[j] - Math.min(preMin, colsMin[j]));
+			colsMin[j] = Math.min(colsMin[j], row[j]);
+			preMin = Math.min(preMin, colsMin[j]);
+		}
+	}
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_739/solution.ts
+++ problems/problems_739/solution.ts
@@ -1,5 +1,15 @@
 function dailyTemperatures(temperatures: number[]): number[] {
-    
+	const n: number = temperatures.length;
+	const ans: Array<number> = new Array<number>(n).fill(0);
+	const s: Array<number> = [];
+	for (let i: number = 0; i < n; i++) {
+		while (s.length > 0 && temperatures[s[s.length - 1]] < temperatures[i]) {
+			const prev: number = s.pop()!!;
+			ans[prev] = i - prev;
+		}
+		s.push(i);
+	}
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
