dbce189f180abbddb64f059e669d94159f6c6725
qubh <qubh@xiaopeng.com>
2024-08-15 10:07:37 +0800

    test: rust 3148, 739
    solution

--- problems/problems_3148/solution.rs
+++ problems/problems_3148/solution.rs
@@ -2,15 +2,28 @@ use serde_json::{json, Value};
 
 pub struct Solution;
 
+use std::i32::{MAX, MIN};
 impl Solution {
     pub fn max_score(grid: Vec<Vec<i32>>) -> i32 {
-
+        let mut ans: i32 = MIN;
+        let n: usize = grid[0].len();
+        let mut cols_min: Vec<i32> = vec![MAX; n];
+        for row in grid {
+            let mut pre_min: i32 = MAX;
+            for j in 0..n {
+                ans = ans.max(row[j] - pre_min.min(cols_min[j]));
+                cols_min[j] = cols_min[j].min(row[j]);
+                pre_min = pre_min.min(cols_min[j]);
+            }
+        }
+        return ans;
     }
 }
 
 #[cfg(feature = "solution_3148")]
 pub fn solve(input_string: String) -> Value {
-	let input_values: Vec<String> = input_string.split('\n').map(|x| x.to_string()).collect();
-	let grid: Vec<Vec<i32>> = serde_json::from_str(&input_values[0]).expect("Failed to parse input");
-	json!(Solution::max_score(grid))
+    let input_values: Vec<String> = input_string.split('\n').map(|x| x.to_string()).collect();
+    let grid: Vec<Vec<i32>> =
+        serde_json::from_str(&input_values[0]).expect("Failed to parse input");
+    json!(Solution::max_score(grid))
 }
--- problems/problems_739/solution.rs
+++ problems/problems_739/solution.rs
@@ -1,16 +1,26 @@
 use serde_json::{json, Value};
 
 pub struct Solution;
-
 impl Solution {
     pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
-
+        let n: usize = temperatures.len();
+        let mut ans: Vec<i32> = vec![0; n];
+        let mut s: Vec<usize> = Vec::new();
+        for i in 0..n {
+            while !s.is_empty() && temperatures[*s.last().unwrap()] < temperatures[i] {
+                let prev = s.pop().unwrap();
+                ans[prev] = (i - prev) as i32;
+            }
+            s.push(i);
+        }
+        ans
     }
 }
 
 #[cfg(feature = "solution_739")]
 pub fn solve(input_string: String) -> Value {
-	let input_values: Vec<String> = input_string.split('\n').map(|x| x.to_string()).collect();
-	let temperatures: Vec<i32> = serde_json::from_str(&input_values[0]).expect("Failed to parse input");
-	json!(Solution::daily_temperatures(temperatures))
+    let input_values: Vec<String> = input_string.split('\n').map(|x| x.to_string()).collect();
+    let temperatures: Vec<i32> =
+        serde_json::from_str(&input_values[0]).expect("Failed to parse input");
+    json!(Solution::daily_temperatures(temperatures))
 }
