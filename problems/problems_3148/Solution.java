555aee5eb7c937cd5fab62a80efd63cb3c40bc5c
qubh <qubh@xiaopeng.com>
2024-08-15 09:35:20 +0800

    test: java 3148, 739
    solution

--- problems/problems_3148/Solution.java
+++ problems/problems_3148/Solution.java
@@ -1,18 +1,33 @@
 package problems.problems_3148;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.alibaba.fastjson.JSON;
-import java.util.*;
+
 import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
     public int maxScore(List<List<Integer>> grid) {
-
+        int ans = Integer.MIN_VALUE;
+        int n = grid.get(0).size();
+        int[] colsMin = new int[n];
+        Arrays.fill(colsMin, Integer.MAX_VALUE);
+        for (List<Integer> row: grid) {
+            int preMin = Integer.MAX_VALUE;
+            for (int j = 0; j < n; j++) {
+                ans = Math.max(ans, row.get(j) - Math.min(preMin, colsMin[j]));
+                colsMin[j] = Math.min(colsMin[j], row.get(j));
+                preMin = Math.min(preMin, colsMin[j]);
+            }
+        }
+        return ans;
     }
 
     @Override
     public Object solve(String[] inputJsonValues) {
-        List<List<Integer>> grid = FIXME(inputJsonValues[0])
+        List<List<Integer>> grid = jsonArrayTo2DIntList(inputJsonValues[0]);
         return JSON.toJSON(maxScore(grid));
     }
 }
--- problems/problems_739/Solution.java
+++ problems/problems_739/Solution.java
@@ -1,13 +1,25 @@
 package problems.problems_739;
 
+import java.util.Stack;
+
 import com.alibaba.fastjson.JSON;
-import java.util.*;
+
 import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
     public int[] dailyTemperatures(int[] temperatures) {
-
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int prev = stack.pop();
+                ans[prev] = i - prev;
+            }
+            stack.add(i);
+        }
+        return ans;
     }
 
     @Override
