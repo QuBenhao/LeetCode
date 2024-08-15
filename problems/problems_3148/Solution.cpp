b74bb4bffc974f9ac01a5e933afb2cd50ad5f90a
qubh <qubh@xiaopeng.com>
2024-08-15 09:19:45 +0800

    test: c++ 3148, 739
    solution

--- problems/problems_3148/Solution.cpp
+++ problems/problems_3148/Solution.cpp
@@ -1,28 +1,37 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    int maxScore(vector<vector<int>>& grid) {
-        
+  int maxScore(vector<vector<int>> &grid) {
+    int ans = INT32_MIN;
+    vector<int> cols_min(grid[0].size(), INT32_MAX);
+    for (auto row : grid) {
+      int pre_min = INT32_MAX;
+      for (size_t j = 0; j < row.size(); j++) {
+        ans = max(ans, row[j] - min(pre_min, cols_min[j]));
+        cols_min[j] = min(cols_min[j], row[j]);
+        pre_min = min(pre_min, cols_min[j]);
+      }
     }
+    return ans;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<vector<int>> grid = json::parse(inputArray.at(0));
-	return solution.maxScore(grid);
+  Solution solution;
+  vector<vector<int>> grid = json::parse(inputArray.at(0));
+  return solution.maxScore(grid);
 }
--- problems/problems_739/Solution.cpp
+++ problems/problems_739/Solution.cpp
@@ -1,28 +1,39 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
-
+#include <stack>
 
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        
+  vector<int> dailyTemperatures(vector<int> &temperatures) {
+    int n = static_cast<int>(temperatures.size());
+    vector<int> ans(n, 0);
+    stack<int> s;
+    for (int i = 0; i < n; i++) {
+      while (!s.empty() && temperatures[s.top()] < temperatures[i]) {
+        int prev = s.top();
+        s.pop();
+        ans[prev] = i - prev;
+      }
+      s.push(i);
     }
+    return ans;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<int> temperatures = json::parse(inputArray.at(0));
-	return solution.dailyTemperatures(temperatures);
+  Solution solution;
+  vector<int> temperatures = json::parse(inputArray.at(0));
+  return solution.dailyTemperatures(temperatures);
 }
