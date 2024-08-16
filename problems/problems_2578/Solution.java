1d926650570cdb489455e25ac8648fd7a6a7fa6e
benhao <qubenhao2@gmail.com>
2024-08-16 21:52:21 +0800

    test: Java 2578
    solution

--- problems/problems_2578/Solution.java
+++ problems/problems_2578/Solution.java
@@ -7,7 +7,24 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int splitNum(int num) {
-
+        List<Integer> nums = new ArrayList<>();
+        while (num > 0) {
+            int r = num % 10;
+            if (r != 0) {
+                nums.add(r);
+            }
+            num /= 10;
+        }
+        Collections.sort(nums);
+        int a = 0, b = 0;
+        for (int v: nums) {
+            if (a <= b) {
+                a = a * 10 + v;
+            } else {
+                b = b * 10 + v;
+            }
+        }
+        return a + b;
     }
 
     @Override
