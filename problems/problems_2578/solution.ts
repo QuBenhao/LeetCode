24ac88cbc5684092bc6dee024e5904953f209cd4
benhao <qubenhao2@gmail.com>
2024-08-16 22:29:34 +0800

    test: typescript 2578
    solution

--- problems/problems_2578/solution.ts
+++ problems/problems_2578/solution.ts
@@ -1,5 +1,22 @@
 function splitNum(num: number): number {
-    
+    let nums: Array<number> = [];
+	while (num > 0) {
+		const r: number = num % 10;
+		if (r !== 0) {
+			nums.push(r);
+		}
+		num = Math.floor(num / 10);
+	}
+	nums.sort((a, b) => a - b);
+	let a: number = 0, b: number = 0;
+	for (let i: number = 0; i < nums.length; i++) {
+		if (i % 2 === 0) {
+			a = a * 10 + nums[i];
+		} else {
+			b = b * 10 + nums[i];
+		}
+	}
+	return a + b;
 };
 
 export function Solve(inputJsonElement: string): any {
