fe855237fa15ca305b8b39ae79e9e1acefd95e25
benhao <qubenhao2@gmail.com>
2024-08-16 22:19:29 +0800

    test: golang 2578
    solution

--- problems/problems_2578/solution.go
+++ problems/problems_2578/solution.go
@@ -3,11 +3,28 @@ package problem2578
 import (
 	"encoding/json"
 	"log"
+	"sort"
 	"strings"
 )
 
 func splitNum(num int) int {
-
+	var nums []int
+	for num > 0 {
+		if r := num % 10; r != 0 {
+			nums = append(nums, r)
+		}
+		num /= 10
+	}
+	sort.Ints(nums)
+	var a, b int
+	for i, v := range nums {
+		if i&1 == 1 {
+			b = b*10 + v
+		} else {
+			a = a*10 + v
+		}
+	}
+	return a + b
 }
 
 func Solve(inputJsonValues string) interface{} {
