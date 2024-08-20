9f1595f002097d6aff52f78aa9a26a0b154c1863
benhao <qubenhao2@gmail.com>
2024-08-21 06:54:42 +0800

    test: python 3007, 114, LCR 088
    solution

--- problems/problems_3007/solution.py
+++ problems/problems_3007/solution.py
@@ -7,5 +7,15 @@ class Solution(solution.Solution):
         return self.findMaximumNumber(*test_input)
 
     def findMaximumNumber(self, k: int, x: int) -> int:
-        pass
-
+        num = pre_one = 0
+        for i in range(((k + 1) << x).bit_length() - 1, -1, -1):
+            # 当前i位为1的话，会增加:
+            # 左边 2^i * pre_one 个价值
+            # 右边 1到2^i-1里出现多少个x倍数位为1 的价值 (即 i//x * 2^(i-1)个1)
+            # pre_one的价值会在遇到x倍数时增加
+            cur = (pre_one << i) + (i // x << i >> 1)
+            if cur <= k:
+                k -= cur
+                num |= 1 << i
+                pre_one += (i + 1) % x == 0
+        return num - 1
--- problems/problems_LCR_088/solution.py
+++ problems/problems_LCR_088/solution.py
@@ -7,5 +7,8 @@ class Solution(solution.Solution):
         return self.minCostClimbingStairs(test_input)
 
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        pass
-
+        n = len(cost)
+        dp = [0] * (n + 1)
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+        return dp[-1]
