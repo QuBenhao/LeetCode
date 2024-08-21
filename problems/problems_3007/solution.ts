397d17a2de75f75b69db12afb593421815017a38
benhao <qubenhao2@gmail.com>
2024-08-21 08:05:13 +0800

    test: typescript 3007, 114, LCR 088
    solution

--- problems/problems_3007/solution.ts
+++ problems/problems_3007/solution.ts
@@ -1,6 +1,33 @@
 function findMaximumNumber(k: number, x: number): number {
-    
+    let l = 1n, r = (BigInt(k) + 1n) << BigInt(x);
+    while (l < r) {
+        let m = (l + r + 1n) / 2n;
+        if (accumulatedPrice(x, m) > k) {
+            r = m - 1n;
+        } else {
+            l = m;
+        }
+    }
+    return Number(l);
 };
+
+function accumulatedBitPrice(x: number, num: bigint): bigint {
+    const period = 1n << BigInt(x);
+    let res = period / 2n * (num / period);
+    if (num % period >= period / 2n) {
+        res += num % period - (period / 2n - 1n);
+    }
+    return res;
+}
+
+function accumulatedPrice(x: number, num: bigint): bigint {
+    let res = 0n;
+    const length = 64 - Math.clz32(Number(num >> 32n));
+    for (let i = x; i <= length; i += x) {
+        res += accumulatedBitPrice(i, num);
+    }
+    return res;
+}
 
 export function Solve(inputJsonElement: string): any {
 	const inputValues: string[] = inputJsonElement.split("\n");
--- problems/problems_LCR_088/solution.ts
+++ problems/problems_LCR_088/solution.ts
@@ -1,5 +1,10 @@
 function minCostClimbingStairs(cost: number[]): number {
-
+	const n: number = cost.length;
+	const dp: number[] = new Array(3).fill(0);
+	for (let i: number = 2; i < n + 1; i++) {
+		dp[i % 3] = Math.min(dp[(i - 1) % 3] + cost[i - 1], dp[(i - 2) % 3] + cost[i - 2]);
+	}
+	return dp[n % 3];
 };
 
 export function Solve(inputJsonElement: string): any {
