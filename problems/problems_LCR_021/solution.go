4fdf23b3cc086c6f8aeff732c2277d1602f56aa4
benhao <qubenhao2@gmail.com>
2024-08-23 07:01:28 +0800

    test: golang 3145, 295, LCR 021
    solution

--- problems/problems_LCR_021/solution.go
+++ problems/problems_LCR_021/solution.go
@@ -15,7 +15,16 @@ import (
  * }
  */
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-
+	dummy := &ListNode{Next: head}
+	slow, fast := dummy, dummy
+	for i := 0; i < n; i++ {
+		fast = fast.Next
+	}
+	for fast.Next != nil {
+		slow, fast = slow.Next, fast.Next
+	}
+	slow.Next = slow.Next.Next
+	return dummy.Next
 }
 
 func Solve(inputJsonValues string) interface{} {
