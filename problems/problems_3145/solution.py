49207c5a9e4cafc25669809f6ef2a2e000c717cf
benhao <qubenhao2@gmail.com>
2024-08-23 06:57:34 +0800

    test: python 3145, 295, LCR 021
    solution

--- problems/problems_3145/solution.py
+++ problems/problems_3145/solution.py
@@ -7,5 +7,26 @@ class Solution(solution.Solution):
         return self.findProductsOfElements(test_input)
 
     def findProductsOfElements(self, queries: List[List[int]]) -> List[int]:
-        pass
+        def sum_e(k: int) -> int:
+            res = n = cnt1 = sum_i = 0
+            for i in range((k + 1).bit_length() - 1, 0, -1):
+                c = (cnt1 << i) + (i << (i - 1))  # 新增的幂次个数
+                if c <= k:
+                    k -= c
+                    res += (sum_i << i) + ((i * (i - 1) // 2) << (i - 1))
+                    sum_i += i  # 之前填的 1 的幂次之和
+                    cnt1 += 1  # 之前填的 1 的个数
+                    n |= 1 << i  # 填 1
+            # 最低位单独计算
+            if cnt1 <= k:
+                k -= cnt1
+                res += sum_i
+                n |= 1  # 最低位填 1
+            # 剩余的 k 个幂次，由 n 的低 k 个 1 补充
+            for _ in range(k):
+                lb = n & -n
+                res += lb.bit_length() - 1
+                n ^= lb  # 去掉最低位的 1（置为 0）
+            return res
 
+        return [pow(2, sum_e(r + 1) - sum_e(l), mod) for l, r, mod in queries]
--- problems/problems_LCR_021/solution.py
+++ problems/problems_LCR_021/solution.py
@@ -17,5 +17,12 @@ class Solution(solution.Solution):
         return linked_list_to_list(res)
 
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        pass
-
+        dummy = ListNode(next=head)
+        fast = slow = dummy
+        for _ in range(n):
+            fast = fast.next
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return dummy.next
