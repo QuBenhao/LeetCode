69cd0475b66d943a03026a254de1c92d80abaecb
benhao <qubenhao2@gmail.com>
2024-08-24 22:33:50 +0800

    test: rust 138
    solution

--- problems/problems_138/solution.rs
+++ problems/problems_138/solution.rs
@@ -28,38 +28,32 @@ impl Solution {
 
         // Step 1: Interleave copied nodes with original nodes
         while let Some(node) = current {
-            let new_node = Rc::new(RefCell::new(Node::new(node.borrow().val)));
-            new_node.borrow_mut().next = node.borrow().next.clone();
-            node.borrow_mut().next = Some(new_node.clone());
-            current = new_node.borrow().next.clone();
+            let copied = Rc::new(RefCell::new(Node::new(node.borrow().val)));
+            copied.borrow_mut().next = node.borrow().next.clone();
+            node.borrow_mut().next = Some(copied.clone());
+            current = copied.borrow().next.clone();
         }
 
         // Step 2: Assign random pointers for the copied nodes
         current = head.clone();
         while let Some(node) = current {
-            if let Some(random) = &node.borrow().random {
-                node.borrow().next.as_ref()?.borrow_mut().random = random.borrow().next.clone();
+            if let Some(random) = node.borrow().random.clone() {
+                node.borrow().next.clone()?.borrow_mut().random = random.borrow().next.clone();
             }
-            current = node.borrow().next.as_ref()?.borrow().next.clone();
+            let next = node.borrow().next.clone();
+            current = next?.borrow().next.clone();
         }
 
         // Step 3: Separate the copied list from the original list
         current = head.clone();
-        let mut new_head = None;
-        let mut new_current = None;
+        let new_head = head.clone()?.borrow().next.clone();
 
         while let Some(node) = current {
-            let copied_node = node.borrow().next.clone()?;
-            node.borrow_mut().next = copied_node.borrow().next.clone();
-
-            if new_head.is_none() {
-                new_head = Some(copied_node.clone());
-                new_current = Some(copied_node);
-            } else {
-                new_current.as_ref()?.borrow_mut().next = Some(copied_node.clone());
-                new_current = Some(copied_node);
+            let next = node.borrow().next.clone();
+            node.borrow_mut().next = next.clone()?.borrow().next.clone();
+            if let Some(node_next) = node.borrow().next.clone() {
+                next?.borrow_mut().next = node_next.borrow().next.clone();
             }
-
             current = node.borrow().next.clone();
         }
 
