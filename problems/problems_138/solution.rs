487010efdef92e19b9d90ffc829f86b80bd350be
benhao <qubenhao2@gmail.com>
2024-08-24 21:39:35 +0800

    test: rust 138
    solution

--- problems/problems_138/solution.rs
+++ problems/problems_138/solution.rs
@@ -24,7 +24,46 @@ use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn copy_random_list(head: Option<Rc<RefCell<Node>>>) -> Option<Rc<RefCell<Node>>> {
-		return head;
+		let mut current = head.clone();
+
+        // Step 1: Interleave copied nodes with original nodes
+        while let Some(node) = current {
+            let new_node = Rc::new(RefCell::new(Node::new(node.borrow().val)));
+            new_node.borrow_mut().next = node.borrow().next.clone();
+            node.borrow_mut().next = Some(new_node.clone());
+            current = new_node.borrow().next.clone();
+        }
+
+        // Step 2: Assign random pointers for the copied nodes
+        current = head.clone();
+        while let Some(node) = current {
+            if let Some(random) = &node.borrow().random {
+                node.borrow().next.as_ref()?.borrow_mut().random = random.borrow().next.clone();
+            }
+            current = node.borrow().next.as_ref()?.borrow().next.clone();
+        }
+
+        // Step 3: Separate the copied list from the original list
+        current = head.clone();
+        let mut new_head = None;
+        let mut new_current = None;
+
+        while let Some(node) = current {
+            let copied_node = node.borrow().next.clone()?;
+            node.borrow_mut().next = copied_node.borrow().next.clone();
+
+            if new_head.is_none() {
+                new_head = Some(copied_node.clone());
+                new_current = Some(copied_node);
+            } else {
+                new_current.as_ref()?.borrow_mut().next = Some(copied_node.clone());
+                new_current = Some(copied_node);
+            }
+
+            current = node.borrow().next.clone();
+        }
+
+        new_head
     }
 }
 
