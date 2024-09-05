3325ac721824364a469a99de25dce4685c674d7d
benhao <qubenhao2@gmail.com>
2024-09-06 07:56:57 +0800

    test: python 3176, 236, LCR 106
    solution

--- problems/problems_3176/solution.py
+++ problems/problems_3176/solution.py
@@ -7,5 +7,13 @@ class Solution(solution.Solution):
         return self.maximumLength(*test_input)
 
     def maximumLength(self, nums: List[int], k: int) -> int:
-        pass
-
+        fs = {}
+        mx = [0] * (k + 2)
+        for x in nums:
+            if x not in fs:
+                fs[x] = [0] * (k + 1)
+            f = fs[x]
+            for j in range(k, -1, -1):
+                f[j] = max(f[j], mx[j]) + 1
+                mx[j + 1] = max(mx[j + 1], f[j])
+        return mx[-1]
--- problems/problems_LCR_106/solution.py
+++ problems/problems_LCR_106/solution.py
@@ -7,5 +7,15 @@ class Solution(solution.Solution):
         return self.isBipartite(test_input)
 
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        pass
+        def dfs(node, c):
+            if color[node] != -1:
+                return color[node] == c
+            color[node] = c
+            for nei in graph[node]:
+                if not dfs(nei, c ^ 1):
+                    return False
+            return True
 
+        n = len(graph)
+        color = [-1] * n
+        return all(color[i] != -1 or dfs(i, 0) for i in range(n))
