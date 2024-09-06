5806dff68f1bdbc0c2f1674a49cf0ea0b6dacab7
benhao <qubenhao2@gmail.com>
2024-09-06 21:02:57 +0800

    test: 317 solution
    golang, c++, Java, typescript, rust

--- problems/problems_3176/Solution.cpp
+++ problems/problems_3176/Solution.cpp
@@ -1,29 +1,38 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    int maximumLength(vector<int>& nums, int k) {
-        
+  int maximumLength(vector<int> &nums, int k) {
+    unordered_map<int, vector<int>> fs;
+    vector<int> mx(k + 2);
+    for (int x : nums) {
+      auto &f = fs[x];
+      f.resize(k + 1);
+      for (int j = k; j >= 0; j--) {
+        f[j] = max(f[j], mx[j]) + 1;
+        mx[j + 1] = max(mx[j + 1], f[j]);
+      }
     }
+    return mx[k + 1];
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<int> nums = json::parse(inputArray.at(0));
-	int k = json::parse(inputArray.at(1));
-	return solution.maximumLength(nums, k);
+  Solution solution;
+  vector<int> nums = json::parse(inputArray.at(0));
+  int k = json::parse(inputArray.at(1));
+  return solution.maximumLength(nums, k);
 }
--- problems/problems_3176/solution.rs
+++ problems/problems_3176/solution.rs
@@ -1,17 +1,37 @@
 use serde_json::{json, Value};
 
 pub struct Solution;
+use std::collections::HashMap;
 
 impl Solution {
     pub fn maximum_length(nums: Vec<i32>, k: i32) -> i32 {
+        let mut dp = HashMap::new();
+        let mut zd = vec![0; k as usize + 1];
 
+        for &v in &nums {
+            let tmp = dp.entry(v).or_insert(vec![0; k as usize + 1]);
+            for j in 0..=k as usize {
+                tmp[j] += 1;
+                if j > 0 {
+                    tmp[j] = tmp[j].max(zd[j - 1] + 1);
+                }
+            }
+
+            for j in 0..=k as usize {
+                zd[j] = zd[j].max(tmp[j]);
+                if j > 0 {
+                    zd[j] = zd[j].max(zd[j - 1]);
+                }
+            }
+        }
+        zd[k as usize]
     }
 }
 
 #[cfg(feature = "solution_3176")]
 pub fn solve(input_string: String) -> Value {
-	let input_values: Vec<String> = input_string.split('\n').map(|x| x.to_string()).collect();
-	let nums: Vec<i32> = serde_json::from_str(&input_values[0]).expect("Failed to parse input");
-	let k: i32 = serde_json::from_str(&input_values[1]).expect("Failed to parse input");
-	json!(Solution::maximum_length(nums, k))
+    let input_values: Vec<String> = input_string.split('\n').map(|x| x.to_string()).collect();
+    let nums: Vec<i32> = serde_json::from_str(&input_values[0]).expect("Failed to parse input");
+    let k: i32 = serde_json::from_str(&input_values[1]).expect("Failed to parse input");
+    json!(Solution::maximum_length(nums, k))
 }
