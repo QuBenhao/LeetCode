9163074edf96e7868a14c2345e55f9424d1e687e
benhao <qubenhao2@gmail.com>
2024-09-11 07:44:57 +0800

    test: Java 2555, LCR 032
    solution

--- problems/problems_2555/Solution.java
+++ problems/problems_2555/Solution.java
@@ -7,7 +7,17 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int maximizeWin(int[] prizePositions, int k) {
-
+        int ans = 0;
+        int n = prizePositions.length;
+        int[] dp = new int[n + 1];
+        for (int left = 0, right = 0; right < n; right++) {
+            while (prizePositions[right] - prizePositions[left] > k) {
+                left++;
+            }
+            dp[right + 1] = Math.max(dp[right], right - left + 1);
+            ans = Math.max(ans, dp[left] + right - left + 1);
+        }
+        return ans;
     }
 
     @Override
--- problems/problems_LCR_032/Solution.java
+++ problems/problems_LCR_032/Solution.java
@@ -7,7 +7,21 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public boolean isAnagram(String s, String t) {
-
+        if (s.length() != t.length() || s.compareTo(t) == 0) {
+            return false;
+        }
+        int n = s.length();
+        int[] cnt = new int[26];
+        for (int i = 0; i < n; i++) {
+            cnt[s.charAt(i) - 'a']++;
+            cnt[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
