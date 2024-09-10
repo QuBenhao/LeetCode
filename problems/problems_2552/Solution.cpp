27af03bee31810db3ea62bdff9c2b09c5fede9c4
benhao <qubenhao2@gmail.com>
2024-09-10 20:25:30 +0800

    test: c++ 2552, LCR 002
    solution

--- problems/problems_2552/Solution.cpp
+++ problems/problems_2552/Solution.cpp
@@ -1,42 +1,41 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    long long countQuadruplets(vector<int>& nums) {
-		int64_t cnt4 = 0;
-		size_t n = nums.size();
-		vector<int64_t> cnt3(n);
-		for (size_t l = 2; l < n; l++) {
-			int64_t cnt2 = 0;
-			for (size_t j = 0; j < l; j++) {
-				if (nums[j] < nums[l]) {
-					cnt4 += cnt3[j];
-					cnt2++;
-				} else {
-					cnt3[j] += cnt2;
-				}
-			}
-		}
-		return cnt4;
+  long long countQuadruplets(vector<int> &nums) {
+    int64_t cnt4 = 0;
+    size_t n = nums.size();
+    vector<int64_t> cnt3(n);
+    for (size_t l = 2; l < n; l++) {
+      int64_t cnt2 = 0;
+      for (size_t j = 0; j < l; j++) {
+        if (nums[j] < nums[l]) {
+          cnt4 += cnt3[j];
+          cnt2++;
+        } else {
+          cnt3[j] += cnt2;
+        }
+      }
     }
+    return cnt4;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<int> nums = json::parse(inputArray.at(0));
-	return solution.countQuadruplets(nums);
+  Solution solution;
+  vector<int> nums = json::parse(inputArray.at(0));
+  return solution.countQuadruplets(nums);
 }
--- problems/problems_LCR_002/Solution.cpp
+++ problems/problems_LCR_002/Solution.cpp
@@ -1,29 +1,47 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
-
+#include <sstream>
 
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    string addBinary(string a, string b) {
-
+  string addBinary(string a, string b) {
+    stringstream ss;
+    int carry = 0;
+    if (a.size() < b.size()) {
+      swap(a, b);
     }
+    int i = static_cast<int>(a.size()) - 1;
+    int diff = static_cast<int>(a.size()) - static_cast<int>(b.size());
+    while (i >= 0) {
+      int sum = a[i] - '0' + (i - diff >= 0 ? b[i - diff] - '0' : 0) + carry;
+      ss << sum % 2;
+      carry = sum / 2;
+			i--;
+    }
+    if (carry) {
+      ss << carry;
+    }
+    string result = ss.str();
+    reverse(result.begin(), result.end());
+    return result;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	string a = json::parse(inputArray.at(0));
-	string b = json::parse(inputArray.at(1));
-	return solution.addBinary(a, b);
+  Solution solution;
+  string a = json::parse(inputArray.at(0));
+  string b = json::parse(inputArray.at(1));
+  return solution.addBinary(a, b);
 }
