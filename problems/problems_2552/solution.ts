ebe1fda84cf14179199467001bfcf326c67e95e4
benhao <qubenhao2@gmail.com>
2024-09-10 20:40:11 +0800

    test: typescript 2552, LCR 002
    solution

--- problems/problems_2552/solution.ts
+++ problems/problems_2552/solution.ts
@@ -1,5 +1,19 @@
 function countQuadruplets(nums: number[]): number {
-    
+	const n: number = nums.length;
+	let cnt4: number = 0;
+	const cn3: number[] = new Array(n).fill(0);
+	for (let l: number = 2; l < n; l++) {
+		let cnt2: number = 0;
+		for (let j: number = 0; j < l; j++) {
+			if (nums[j] < nums[l]) {
+				cnt4 += cn3[j];
+				cnt2++;
+			} else {
+				cn3[j] += cnt2;
+			}
+		}
+	}
+	return cnt4;
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_LCR_002/solution.ts
+++ problems/problems_LCR_002/solution.ts
@@ -1,5 +1,19 @@
 function addBinary(a: string, b: string): string {
-
+	if (a.length < b.length) {
+		return addBinary(b, a);
+	}
+	const result: string[] = [];
+	let carry: number = 0;
+	const d: number = a.length - b.length;
+	for (let i: number = a.length - 1; i >= 0; i--) {
+		const sum: number = parseInt(a[i]) + (i - d >= 0 ? parseInt(b[i - d]) : 0) + carry;
+		result.push((sum % 2).toString());
+		carry = Math.floor(sum / 2);
+	}
+	if (carry) {
+		result.push("1");
+	}
+	return result.reverse().join("");
 };
 
 export function Solve(inputJsonElement: string): any {
