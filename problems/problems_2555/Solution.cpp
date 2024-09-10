58dac067d676d5a93f51e3a926247f113923551c
benhao <qubenhao2@gmail.com>
2024-09-11 07:42:08 +0800

    test: c++ 2555, LCR 032
    solution

--- problems/problems_2555/Solution.cpp
+++ problems/problems_2555/Solution.cpp
@@ -1,29 +1,38 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    int maximizeWin(vector<int>& prizePositions, int k) {
-        
+  int maximizeWin(vector<int> &prizePositions, int k) {
+    int ans = 0;
+    int n = static_cast<int>(prizePositions.size());
+    vector<int> dp(n + 1, 0);
+    for (int left = 0, right = 0; right < n; ++right) {
+      while (prizePositions[right] - prizePositions[left] > k) {
+        ++left;
+      }
+      dp[right + 1] = max(dp[right], right - left + 1);
+      ans = max(ans, dp[left] + right - left + 1);
     }
+    return ans;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<int> prizePositions = json::parse(inputArray.at(0));
-	int k = json::parse(inputArray.at(1));
-	return solution.maximizeWin(prizePositions, k);
+  Solution solution;
+  vector<int> prizePositions = json::parse(inputArray.at(0));
+  int k = json::parse(inputArray.at(1));
+  return solution.maximizeWin(prizePositions, k);
 }
--- problems/problems_LCR_032/Solution.cpp
+++ problems/problems_LCR_032/Solution.cpp
@@ -1,29 +1,41 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-
+  bool isAnagram(string s, string t) {
+    if (s.size() != t.size() || s == t) {
+      return false;
     }
+    vector<int> count(26, 0);
+    for (size_t i = 0; i < s.size(); ++i) {
+      ++count[s[i] - 'a'];
+      --count[t[i] - 'a'];
+    }
+    for (int i = 0; i < 26; ++i) {
+      if (count[i] != 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	string s = json::parse(inputArray.at(0));
-	string t = json::parse(inputArray.at(1));
-	return solution.isAnagram(s, t);
+  Solution solution;
+  string s = json::parse(inputArray.at(0));
+  string t = json::parse(inputArray.at(1));
+  return solution.isAnagram(s, t);
 }
