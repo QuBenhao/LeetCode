5770bf61c30f216bbb30c6ae709de740066ffaf7
benhao <qubenhao2@gmail.com>
2024-09-12 21:29:47 +0800

    test: golang 2576, LCR 043
    solution

--- problems/problems_2576/solution.go
+++ problems/problems_2576/solution.go
@@ -3,11 +3,19 @@ package problem2576
 import (
 	"encoding/json"
 	"log"
+	"sort"
 	"strings"
 )
 
 func maxNumOfMarkedIndices(nums []int) int {
-
+	sort.Ints(nums)
+	left := 0
+	for right := (len(nums) + 1) / 2; right < len(nums); right++ {
+		if nums[right] >= 2*nums[left] {
+			left++
+		}
+	}
+	return left << 1
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_LCR_043/solution.go
+++ problems/problems_LCR_043/solution.go
@@ -2,7 +2,9 @@ package problemLCR_043
 
 import (
 	"encoding/json"
+	. "leetCode/golang/models"
 	"log"
+	"math/bits"
 	"strings"
 )
 
@@ -14,25 +16,54 @@ import (
  *     Right *TreeNode
  * }
  */
+
 type CBTInserter struct {
-
+	root *TreeNode
+	n    int
 }
-
 
 func Constructor(root *TreeNode) CBTInserter {
-
+	n := 0
+	var q []*TreeNode
+	if root != nil {
+		q = append(q, root)
+		for len(q) > 0 {
+			node := q[0]
+			q = q[1:]
+			n++
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+		}
+	}
+	return CBTInserter{root: root, n: n}
 }
 
-
-func (this *CBTInserter) Insert(v int) int {
-
+func (c *CBTInserter) Insert(val int) int {
+	c.n++
+	child := &TreeNode{Val: val}
+	node := c.root
+	for i := bits.Len(uint(c.n)) - 2; i > 0; i-- {
+		if c.n>>i&1 == 0 {
+			node = node.Left
+		} else {
+			node = node.Right
+		}
+	}
+	if c.n&1 == 0 {
+		node.Left = child
+	} else {
+		node.Right = child
+	}
+	return node.Val
 }
 
-
-func (this *CBTInserter) Get_root() *TreeNode {
-
+func (c *CBTInserter) Get_root() *TreeNode {
+	return c.root
 }
-
 
 /**
  * Your CBTInserter object will be instantiated and called as such:
@@ -54,7 +85,7 @@ func Solve(inputJsonValues string) interface{} {
 		log.Println(err)
 		return nil
 	}
-	obj := Constructor(opValues[0][0].(*TreeNode))
+	obj := Constructor(InterfaceArrayToTree(opValues[0][0].([]interface{})))
 	ans = append(ans, nil)
 	for i := 1; i < len(operators); i++ {
 		var res interface{}
@@ -62,13 +93,12 @@ func Solve(inputJsonValues string) interface{} {
 		case "insert", "Insert":
 			res = obj.Insert(int(opValues[i][0].(float64)))
 		case "get_root", "Get_root":
-			res = obj.Get_root()
+			res = TreeToArray(obj.Get_root())
 		default:
 			res = nil
 		}
 		ans = append(ans, res)
 	}
-
 
 	return ans
 }
