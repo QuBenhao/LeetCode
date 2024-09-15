889a641767c29547d28d2dc94881be9ece7d654a
benhao <qubenhao2@gmail.com>
2024-09-15 22:41:42 +0800

    Revert "test: [20240915] Add daily problem solution"
    This reverts commit f4c3a0270bfaaa0e9884725fddc1bb8d1217d555.

--- problems/problems_LCR_060/Solution.cpp
+++ problems/problems_LCR_060/Solution.cpp
@@ -1,6 +1,7 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
-
+#include <queue>
+#include <unordered_map>
 
 using namespace std;
 using json = nlohmann::json;
@@ -31,17 +32,17 @@ public:
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<int> nums = json::parse(inputArray.at(0));
-	int k = json::parse(inputArray.at(1));
-	return solution.topKFrequent(nums, k);
+  Solution solution;
+  vector<int> nums = json::parse(inputArray.at(0));
+  int k = json::parse(inputArray.at(1));
+  return solution.topKFrequent(nums, k);
 }
