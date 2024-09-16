0a265a63755bc603ffe2e293ab74531fdd782e9a
benhao <qubenhao2@gmail.com>
2024-09-16 09:19:51 +0800

    test: python 1184, LCR 082
    solution

--- problems/problems_1184/solution.py
+++ problems/problems_1184/solution.py
@@ -7,5 +7,12 @@ class Solution(solution.Solution):
         return self.distanceBetweenBusStops(*test_input)
 
     def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
-        pass
-
+        n = len(distance)
+        distance += distance
+        clock = 0
+        for i in range(start, destination if destination > start else destination + n):
+            clock += distance[i]
+        clockwise = 0
+        for i in range(destination, start if start > destination else start + n):
+            clockwise += distance[i]
+        return min(clock, clockwise)
--- problems/problems_LCR_082/solution.py
+++ problems/problems_LCR_082/solution.py
@@ -7,5 +7,19 @@ class Solution(solution.Solution):
         return self.combinationSum2(*test_input)
 
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        pass
-
+        ans = []
+        def backtrack(remain, path, idx):
+            if remain == 0:
+                ans.append(list(path))
+                return
+            if idx == len(candidates) or remain < 0:
+                return
+            path.append(candidates[idx])
+            backtrack(remain - candidates[idx], path, idx + 1)
+            path.pop()
+            while idx < len(candidates) - 1 and candidates[idx] == candidates[idx + 1]:
+                idx += 1
+            backtrack(remain, path, idx + 1)
+        candidates.sort()
+        backtrack(target, [], 0)
+        return ans
