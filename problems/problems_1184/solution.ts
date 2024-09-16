a8da55f4d65e1a5a49e6efba3c6202217dafe520
benhao <qubenhao2@gmail.com>
2024-09-16 15:01:19 +0800

    test: typescript 1184, LCR 082
    solution

--- problems/problems_1184/solution.ts
+++ problems/problems_1184/solution.ts
@@ -1,5 +1,14 @@
 function distanceBetweenBusStops(distance: number[], start: number, destination: number): number {
-
+	const n: number = distance.length, min: number = Math.min(start, destination), max: number = Math.max(start, destination);
+	let clockwise: number = 0, counterclockwise: number = 0;
+	for (let i: number = 0; i < n; i++) {
+		if (i >= min && i < max) {
+			clockwise += distance[i];
+		} else {
+			counterclockwise += distance[i];
+		}
+	}
+	return Math.min(clockwise, counterclockwise);
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_LCR_082/solution.ts
+++ problems/problems_LCR_082/solution.ts
@@ -1,5 +1,23 @@
 function combinationSum2(candidates: number[], target: number): number[][] {
-
+	candidates.sort((a, b) => a - b);
+	const result: number[][] = [];
+	const dfs = (index: number, target: number, path: number[]) => {
+		if (target < 0) {
+			return;
+		}
+		if (target === 0) {
+			result.push(path);
+			return;
+		}
+		for (let i: number = index; i < candidates.length; i++) {
+			if (i > index && candidates[i] === candidates[i - 1]) {
+				continue;
+			}
+			dfs(i + 1, target - candidates[i], [...path, candidates[i]]);
+		}
+	};
+	dfs(0, target, []);
+	return result;
 };
 
 export function Solve(inputJsonElement: string): any {
