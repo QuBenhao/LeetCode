783077ba5d9131a118b6a36f600f92aa03012cb4
benhao <qubenhao2@gmail.com>
2024-09-16 14:17:36 +0800

    test: c++ 1184, LCR 082
    solution

--- problems/problems_1184/Solution.cpp
+++ problems/problems_1184/Solution.cpp
@@ -1,30 +1,39 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
-
+  int distanceBetweenBusStops(vector<int> &distance, int start,
+                              int destination) {
+    int n = static_cast<int>(distance.size()), mn = min(start, destination),
+        mx = max(start, destination);
+    int clock = 0, counterclock = 0;
+    for (int i = mn; i < mx; i++) {
+      clock += distance[i];
     }
+    for (int i = mx; i < n + mn; i++) {
+      counterclock += distance[i % n];
+    }
+    return min(clock, counterclock);
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<int> distance = json::parse(inputArray.at(0));
-	int start = json::parse(inputArray.at(1));
-	int destination = json::parse(inputArray.at(2));
-	return solution.distanceBetweenBusStops(distance, start, destination);
+  Solution solution;
+  vector<int> distance = json::parse(inputArray.at(0));
+  int start = json::parse(inputArray.at(1));
+  int destination = json::parse(inputArray.at(2));
+  return solution.distanceBetweenBusStops(distance, start, destination);
 }
--- problems/problems_LCR_082/Solution.cpp
+++ problems/problems_LCR_082/Solution.cpp
@@ -1,29 +1,50 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
-
+#include <vector>
 
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-
-    }
+  vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
+    vector<vector<int>> res;
+    vector<int> path;
+    sort(candidates.begin(), candidates.end());
+    function<void(int, int)> dfs = [&](int idx, int remain) {
+      if (remain == 0) {
+        res.push_back(path);
+        return;
+      }
+      if (idx == candidates.size() || remain < 0) {
+        return;
+      }
+      for (int i = idx; i < candidates.size(); i++) {
+        if (i > idx && candidates[i] == candidates[i - 1]) {
+          continue;
+        }
+        path.push_back(candidates[i]);
+        dfs(i + 1, remain - candidates[i]);
+        path.pop_back();
+      }
+    };
+    dfs(0, target);
+    return res;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<int> candidates = json::parse(inputArray.at(0));
-	int target = json::parse(inputArray.at(1));
-	return solution.combinationSum2(candidates, target);
+  Solution solution;
+  vector<int> candidates = json::parse(inputArray.at(0));
+  int target = json::parse(inputArray.at(1));
+  return solution.combinationSum2(candidates, target);
 }
