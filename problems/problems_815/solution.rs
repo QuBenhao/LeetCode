360987af8f3c84a1d35987ccf45e8d5d18e59bf0
benhao <qubenhao2@gmail.com>
2024-09-17 10:23:02 +0800

    test: rust 815, LCR 094
    solution

--- problems/problems_815/solution.rs
+++ problems/problems_815/solution.rs
@@ -4,7 +4,45 @@ pub struct Solution;
 
 impl Solution {
     pub fn num_buses_to_destination(routes: Vec<Vec<i32>>, source: i32, target: i32) -> i32 {
-
+		if source == target {
+			return 0;
+		}
+		let mut stop_to_buses = std::collections::HashMap::new();
+		for (bus, stops) in routes.iter().enumerate() {
+			for &stop in stops {
+				stop_to_buses.entry(stop).or_insert(vec![]).push(bus);
+			}
+		}
+		let mut visited_buses = vec![false; routes.len()];
+		let mut visited_stops = vec![false; 1000001];
+		let mut queue = std::collections::VecDeque::new();
+		queue.push_back(source);
+		let mut level = 0;
+		while !queue.is_empty() {
+			level += 1;
+			let mut size = queue.len();
+			while size > 0 {
+				size -= 1;
+				let stop = queue.pop_front().unwrap();
+				for &bus in stop_to_buses.get(&stop).unwrap_or(&vec![]) {
+					if visited_buses[bus] {
+						continue;
+					}
+					visited_buses[bus] = true;
+					for &next_stop in routes[bus].iter() {
+						if visited_stops[next_stop as usize] {
+							continue;
+						}
+						visited_stops[next_stop as usize] = true;
+						if next_stop == target {
+							return level;
+						}
+						queue.push_back(next_stop);
+					}
+				}
+			}
+		}
+		-1
     }
 }
 
--- problems/problems_LCR_094/solution.rs
+++ problems/problems_LCR_094/solution.rs
@@ -5,7 +5,24 @@ pub struct Solution;
 
 impl Solution {
     pub fn min_cut(s: String) -> i32 {
-
+		let n = s.len();
+		let s = s.chars().collect::<Vec<char>>();
+		let mut dp = vec![vec![false; n]; n];
+		for i in (0..n).rev() {
+			for j in i..n {
+				dp[i][j] = s[i] == s[j] && (j - i < 2 || dp[i + 1][j - 1]);
+			}
+		}
+		let mut cut = vec![n as i32; n + 1];
+		cut[0] = -1;
+		for i in 0..n {
+			for j in 0..=i {
+				if dp[j][i] {
+					cut[i + 1] = cut[i + 1].min(cut[j] + 1);
+				}
+			}
+		}
+		cut[n]
     }
 }
 
