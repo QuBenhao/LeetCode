603f0c3e8047d8f650c54eb395b4d6ab2d454f37
benhao <qubenhao2@gmail.com>
2024-09-17 10:02:50 +0800

    test: golang 815, LCR 094
    solution

--- problems/problems_815/solution.go
+++ problems/problems_815/solution.go
@@ -7,7 +7,57 @@ import (
 )
 
 func numBusesToDestination(routes [][]int, source int, target int) int {
+	if source == target {
+		return 0
+	}
 
+	n := len(routes)
+	edge := make([][]bool, n)
+	for i := range edge {
+		edge[i] = make([]bool, n)
+	}
+
+	rec := make(map[int][]int)
+	for i, route := range routes {
+		for _, site := range route {
+			for _, j := range rec[site] {
+				edge[i][j] = true
+				edge[j][i] = true
+			}
+			rec[site] = append(rec[site], i)
+		}
+	}
+
+	dis := make([]int, n)
+	for i := range dis {
+		dis[i] = -1
+	}
+	var q []int
+	for _, site := range rec[source] {
+		dis[site] = 1
+		q = append(q, site)
+	}
+	for len(q) > 0 {
+		x := q[0]
+		q = q[1:]
+		for y, b := range edge[x] {
+			if b && dis[y] == -1 {
+				dis[y] = dis[x] + 1
+				q = append(q, y)
+			}
+		}
+	}
+
+	ret := 1 << 31
+	for _, site := range rec[target] {
+		if dis[site] != -1 && dis[site] < ret {
+			ret = dis[site]
+		}
+	}
+	if ret == 1<<31 {
+		ret = -1
+	}
+	return ret
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_LCR_094/solution.go
+++ problems/problems_LCR_094/solution.go
@@ -7,7 +7,43 @@ import (
 )
 
 func minCut(s string) int {
+	n := len(s)
+	if n == 0 {
+		return 0
+	}
 
+	isPalindrome := make([][]bool, n)
+	for i := range isPalindrome {
+		isPalindrome[i] = make([]bool, n)
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		for j := i; j < n; j++ {
+			if s[i] == s[j] && (j-i < 2 || isPalindrome[i+1][j-1]) {
+				isPalindrome[i][j] = true
+			}
+		}
+	}
+
+	dp := make([]int, n)
+	for i := range dp {
+		dp[i] = i
+	}
+
+	for i := 0; i < n; i++ {
+		if isPalindrome[0][i] {
+			dp[i] = 0
+			continue
+		}
+
+		for j := 0; j < i; j++ {
+			if isPalindrome[j+1][i] {
+				dp[i] = min(dp[i], dp[j]+1)
+			}
+		}
+	}
+
+	return dp[n-1]
 }
 
 func Solve(inputJsonValues string) interface{} {
