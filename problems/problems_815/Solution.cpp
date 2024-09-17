19e7bf6fb235b6f06a7b42e4fb1f096d1a9e4ce4
benhao <qubenhao2@gmail.com>
2024-09-17 10:07:49 +0800

    test: c++ 815, LCR 094
    solution

--- problems/problems_815/Solution.cpp
+++ problems/problems_815/Solution.cpp
@@ -1,30 +1,66 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
-        
+  int numBusesToDestination(vector<vector<int>> &routes, int source,
+                            int target) {
+    if (source == target)
+      return 0;
+    int n = routes.size();
+    vector<vector<int>> g(n);
+    unordered_map<int, vector<int>> to_routes;
+    for (int i = 0; i < n; ++i) {
+      for (int site : routes[i]) {
+        for (int j : to_routes[site]) {
+          g[i].push_back(j);
+          g[j].push_back(i);
+        }
+        to_routes[site].push_back(i);
+      }
     }
+    vector<int> dis(n, -1);
+    queue<int> q;
+    for (int i : to_routes[source]) {
+      dis[i] = 1;
+      q.push(i);
+    }
+    while (!q.empty()) {
+      int x = q.front();
+      q.pop();
+      for (int y : g[x]) {
+        if (dis[y] == -1) {
+          dis[y] = dis[x] + 1;
+          q.push(y);
+        }
+      }
+    }
+    int ans = INT_MAX;
+    for (int i : to_routes[target]) {
+      if (dis[i] != -1) {
+        ans = min(ans, dis[i]);
+      }
+    }
+    return ans == INT_MAX ? -1 : ans;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<vector<int>> routes = json::parse(inputArray.at(0));
-	int source = json::parse(inputArray.at(1));
-	int target = json::parse(inputArray.at(2));
-	return solution.numBusesToDestination(routes, source, target);
+  Solution solution;
+  vector<vector<int>> routes = json::parse(inputArray.at(0));
+  int source = json::parse(inputArray.at(1));
+  int target = json::parse(inputArray.at(2));
+  return solution.numBusesToDestination(routes, source, target);
 }
--- problems/problems_LCR_094/Solution.cpp
+++ problems/problems_LCR_094/Solution.cpp
@@ -1,28 +1,52 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    int minCut(string s) {
-
+  int minCut(string s) {
+    int n = static_cast<int>(s.size());
+    if (n == 0) {
+      return 0;
     }
+    vector<vector<bool>> isPal(n, vector<bool>(n, false));
+    for (int i = 0; i < n; i++) {
+      isPal[i][i] = true;
+      for (int j = 0; j < i; j++) {
+        if (s[j] == s[i] && (j + 1 >= i - 1 || isPal[j + 1][i - 1])) {
+          isPal[j][i] = true;
+        }
+      }
+    }
+    vector<int> dp(n, INT_MAX / 2);
+    for (int i = 0; i < n; i++) {
+      if (isPal[0][i]) {
+        dp[i] = 0;
+      } else {
+        for (int j = 0; j < i; j++) {
+          if (isPal[j + 1][i]) {
+            dp[i] = min(dp[i], dp[j] + 1);
+          }
+        }
+      }
+    }
+    return dp[n - 1];
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	string s = json::parse(inputArray.at(0));
-	return solution.minCut(s);
+  Solution solution;
+  string s = json::parse(inputArray.at(0));
+  return solution.minCut(s);
 }
