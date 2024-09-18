3a058b61e27cb68bd8a7a9853965e7e43d61481e
benhao <qubenhao2@gmail.com>
2024-09-18 21:56:09 +0800

    test: golang 2332, LCR 108
    solution

--- problems/problems_2332/solution.go
+++ problems/problems_2332/solution.go
@@ -3,11 +3,32 @@ package problem2332
 import (
 	"encoding/json"
 	"log"
+	"sort"
 	"strings"
 )
 
-func latestTimeCatchTheBus(buses []int, passengers []int, capacity int) int {
-
+func latestTimeCatchTheBus(buses []int, passengers []int, capacity int) (ans int) {
+	sort.Ints(buses)
+	sort.Ints(passengers)
+	j, c := 0, 0
+	for _, bus := range buses {
+		c = capacity
+		for j < len(passengers) && c > 0 && passengers[j] <= bus {
+			c--
+			j++
+		}
+	}
+	j--
+	if c != 0 {
+		ans = buses[len(buses)-1]
+	} else {
+		ans = passengers[j]
+	}
+	for j >= 0 && ans == passengers[j] {
+		ans--
+		j--
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_LCR_108/solution.go
+++ problems/problems_LCR_108/solution.go
@@ -3,11 +3,89 @@ package problemLCR_108
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"strings"
 )
 
 func ladderLength(beginWord string, endWord string, wordList []string) int {
+	wordId := map[string]int{}
+	graph := [][]int{}
+	addWord := func(word string) int {
+		id, has := wordId[word]
+		if !has {
+			id = len(wordId)
+			wordId[word] = id
+			graph = append(graph, []int{})
+		}
+		return id
+	}
+	addEdge := func(word string) int {
+		id1 := addWord(word)
+		s := []byte(word)
+		for i, b := range s {
+			s[i] = '*'
+			id2 := addWord(string(s))
+			graph[id1] = append(graph[id1], id2)
+			graph[id2] = append(graph[id2], id1)
+			s[i] = b
+		}
+		return id1
+	}
 
+	for _, word := range wordList {
+		addEdge(word)
+	}
+	beginId := addEdge(beginWord)
+	endId, has := wordId[endWord]
+	if !has {
+		return 0
+	}
+
+	const inf int = math.MaxInt64
+	distBegin := make([]int, len(wordId))
+	for i := range distBegin {
+		distBegin[i] = inf
+	}
+	distBegin[beginId] = 0
+	queueBegin := []int{beginId}
+
+	distEnd := make([]int, len(wordId))
+	for i := range distEnd {
+		distEnd[i] = inf
+	}
+	distEnd[endId] = 0
+	queueEnd := []int{endId}
+
+	for len(queueBegin) > 0 && len(queueEnd) > 0 {
+		q := queueBegin
+		queueBegin = nil
+		for _, v := range q {
+			if distEnd[v] < inf {
+				return (distBegin[v]+distEnd[v])/2 + 1
+			}
+			for _, w := range graph[v] {
+				if distBegin[w] == inf {
+					distBegin[w] = distBegin[v] + 1
+					queueBegin = append(queueBegin, w)
+				}
+			}
+		}
+
+		q = queueEnd
+		queueEnd = nil
+		for _, v := range q {
+			if distBegin[v] < inf {
+				return (distBegin[v]+distEnd[v])/2 + 1
+			}
+			for _, w := range graph[v] {
+				if distEnd[w] == inf {
+					distEnd[w] = distEnd[v] + 1
+					queueEnd = append(queueEnd, w)
+				}
+			}
+		}
+	}
+	return 0
 }
 
 func Solve(inputJsonValues string) interface{} {
