9368af053da7ed5b08941ab73ec0f6da6cc3e7de
qubh <qubh@xiaopeng.com>
2024-09-18 18:30:46 +0800

    test: python 2332
    solution

--- problems/problems_2332/solution.py
+++ problems/problems_2332/solution.py
@@ -7,5 +7,19 @@ class Solution(solution.Solution):
         return self.latestTimeCatchTheBus(*test_input)
 
     def latestTimeCatchTheBus(self, buses: List[int], passengers: List[int], capacity: int) -> int:
-        pass
-
+        ans = -1
+        buses.sort()
+        passengers.sort()
+        n = len(passengers)
+        j = 0
+        for t in buses:
+            c = capacity
+            while c and j < n and passengers[j] <= t:
+                c -= 1
+                j += 1
+        j -= 1
+        ans = buses[-1] if c else passengers[j]
+        while j >= 0 and ans == passengers[j]:
+            ans -= 1
+            j -= 1
+        return ans
--- problems/problems_2332/testcase.py
+++ problems/problems_2332/testcase.py
@@ -9,6 +9,9 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=[[10, 20], [2, 17, 18, 19], 2], Output=16))
 		self.testcases.append(case(Input=[[20, 30, 10], [19, 13, 26, 4, 25, 11, 21], 2], Output=20))
+		self.testcases.append(case(Input=[[3],[2,4],2], Output=3))
+		self.testcases.append(case(Input=[[2],[2],2], Output=1))
+		self.testcases.append(case(Input=[[8,7],[7,3,8,5,2],3], Output=6))
 
 	def get_testcases(self):
 		return self.testcases
