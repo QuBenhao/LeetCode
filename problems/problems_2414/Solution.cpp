9ff7c94716b33224d1843f9a69ddb9c60da5e601
benhao <qubenhao2@gmail.com>
2024-09-19 07:49:54 +0800

    test: c++ 2414, LCR 038
    solution

--- problems/problems_2414/Solution.cpp
+++ problems/problems_2414/Solution.cpp
@@ -1,28 +1,36 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    int longestContinuousSubstring(string s) {
-        
+  int longestContinuousSubstring(string s) {
+    int ans = 1, n = static_cast<int>(s.size());
+    for (int i = 0, cur = 1; i < n - 1; i++) {
+      if (s[i + 1] - s[i] == 1) {
+        cur++;
+        ans = max(ans, cur);
+      } else {
+        cur = 1;
+      }
     }
+    return ans;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	string s = json::parse(inputArray.at(0));
-	return solution.longestContinuousSubstring(s);
+  Solution solution;
+  string s = json::parse(inputArray.at(0));
+  return solution.longestContinuousSubstring(s);
 }
