17c992c65a879aa3eec0db51eed935649267b2b1
benhao <qubenhao2@gmail.com>
2024-09-19 08:23:25 +0800

    test: typescript 2414, LCR 038
    solution

--- problems/problems_2414/solution.ts
+++ problems/problems_2414/solution.ts
@@ -1,5 +1,15 @@
 function longestContinuousSubstring(s: string): number {
-    
+	const n: number = s.length;
+	let ans: number = 1;
+	for (let i: number = 0, cur: number = 1; i < n - 1; i++) {
+		if (s.charCodeAt(i + 1) - s.charCodeAt(i) === 1) {
+			cur++;
+			ans = Math.max(ans, cur);
+		} else {
+			cur = 1;
+		}
+	}
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_LCR_038/solution.ts
+++ problems/problems_LCR_038/solution.ts
@@ -1,5 +1,15 @@
 function dailyTemperatures(temperatures: number[]): number[] {
-
+	const n: number = temperatures.length;
+	const ans: number[] = new Array(n).fill(0);
+	const stack: number[] = [];
+	for (let i: number = 0; i < n; i++) {
+		while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+			const top: number = stack.pop();
+			ans[top] = i - top;
+		}
+		stack.push(i);
+	}
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
