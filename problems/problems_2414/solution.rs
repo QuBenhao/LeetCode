f9c73803d45fce709584c88b9dc1f97384a7225e
benhao <qubenhao2@gmail.com>
2024-09-19 08:27:48 +0800

    test: rust 2414, LCR 038
    solution

--- problems/problems_2414/solution.rs
+++ problems/problems_2414/solution.rs
@@ -4,7 +4,18 @@ pub struct Solution;
 
 impl Solution {
     pub fn longest_continuous_substring(s: String) -> i32 {
-
+		let mut ans = 1;
+		let mut cur = 1;
+		let chars = s.chars().collect::<Vec<char>>();
+		for i in 0..s.len()-1 {
+			if chars[i] as u8 + 1 == chars[i+1] as u8 {
+				cur += 1;
+				ans = ans.max(cur);
+			} else {
+				cur = 1;
+			}
+		}
+		ans
     }
 }
 
--- problems/problems_LCR_038/solution.rs
+++ problems/problems_LCR_038/solution.rs
@@ -5,7 +5,17 @@ pub struct Solution;
 
 impl Solution {
     pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
-
+		let n = temperatures.len();
+		let mut ans = vec![0; n];
+		let mut stack = Vec::new();
+		for i in 0..n {
+			while !stack.is_empty() && temperatures[i] > temperatures[*stack.last().unwrap()] {
+				let j = stack.pop().unwrap();
+				ans[j] = (i - j) as i32;
+			}
+			stack.push(i);
+		}
+		ans
     }
 }
 
