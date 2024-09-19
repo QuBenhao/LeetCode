2f9b59211d4a148e5e4d8b7eda04477bd8095687
benhao <qubenhao2@gmail.com>
2024-09-19 08:21:05 +0800

    test: Java 2414, LCR 038
    solution

--- problems/problems_2414/Solution.java
+++ problems/problems_2414/Solution.java
@@ -7,7 +7,16 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int longestContinuousSubstring(String s) {
-
+        int ans = 1, n = s.length();
+        for (int i = 0, cur = 1; i < n - 1; i++) {
+            if (s.charAt(i + 1) - s.charAt(i) == 1) {
+                cur++;
+                ans = Math.max(ans, cur);
+            } else {
+                cur = 1;
+            }
+        }
+        return ans;
     }
 
     @Override
--- problems/problems_LCR_038/Solution.java
+++ problems/problems_LCR_038/Solution.java
@@ -7,7 +7,17 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int[] dailyTemperatures(int[] temperatures) {
-
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int preIndex = stack.pop();
+                ans[preIndex] = i - preIndex;
+            }
+            stack.push(i);
+        }
+        return ans;
     }
 
     @Override
