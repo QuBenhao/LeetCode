40314e46d814e109a1d736c13c2590c4b6ea9018
benhao <qubenhao2@gmail.com>
2024-09-21 11:43:27 +0800

    test: Java 2374, LCR 076, LCR 095
    solution

--- problems/problems_2374/Solution.java
+++ problems/problems_2374/Solution.java
@@ -7,7 +7,17 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int edgeScore(int[] edges) {
-
+        int ans = 0, n = edges.length;
+        long[] counter = new long[n];
+        for (int i = 0; i < n; i++) {
+            counter[edges[i]] += i;
+            if (counter[edges[i]] > counter[ans]) {
+                ans = edges[i];
+            } else if (counter[edges[i]] == counter[ans]) {
+                ans = Math.min(ans, edges[i]);
+            }
+        }
+        return ans;
     }
 
     @Override
--- problems/problems_LCR_076/Solution.java
+++ problems/problems_LCR_076/Solution.java
@@ -7,7 +7,8 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int findKthLargest(int[] nums, int k) {
-
+        Arrays.sort(nums);
+        return nums[nums.length - k];
     }
 
     @Override
