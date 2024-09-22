86c3b31970e44cc95898d6ec12a0fffb0c0217bb
benhao <qubenhao2@gmail.com>
2024-09-23 07:41:04 +0800

    test: c++ 1014, LCR 010
    solution

--- problems/problems_1014/Solution.cpp
+++ problems/problems_1014/Solution.cpp
@@ -1,28 +1,32 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    int maxScoreSightseeingPair(vector<int>& values) {
-
+  int maxScoreSightseeingPair(vector<int> &values) {
+    int ans = 0, left = values[0], n = static_cast<int>(values.size());
+    for (int i = 1; i < n; i++) {
+      ans = max(ans, left + values[i] - i);
+      left = max(left, values[i] + i);
     }
+    return ans;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<int> values = json::parse(inputArray.at(0));
-	return solution.maxScoreSightseeingPair(values);
+  Solution solution;
+  vector<int> values = json::parse(inputArray.at(0));
+  return solution.maxScoreSightseeingPair(values);
 }
--- problems/problems_LCR_010/Solution.cpp
+++ problems/problems_LCR_010/Solution.cpp
@@ -1,29 +1,39 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
-
+#include <unordered_map>
 
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    int subarraySum(vector<int>& nums, int k) {
-
+  int subarraySum(vector<int> &nums, int k) {
+    int ans = 0, s = 0, n = static_cast<int>(nums.size());
+    unordered_map<int, int> mp;
+    mp[0] = 1;
+    for (int i = 0; i < n; i++) {
+      s += nums[i];
+      if (mp.find(s - k) != mp.end()) {
+        ans += mp[s - k];
+      }
+      mp[s]++;
     }
+    return ans;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<int> nums = json::parse(inputArray.at(0));
-	int k = json::parse(inputArray.at(1));
-	return solution.subarraySum(nums, k);
+  Solution solution;
+  vector<int> nums = json::parse(inputArray.at(0));
+  int k = json::parse(inputArray.at(1));
+  return solution.subarraySum(nums, k);
 }
