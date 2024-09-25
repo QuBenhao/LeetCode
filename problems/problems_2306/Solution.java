fa1904aa0608501d2cacc1c2123c9716ffbc0dc0
benhao <qubenhao2@gmail.com>
2024-09-25 21:05:59 +0800

    test: Java 2306, LCR 083
    solution

--- problems/problems_2306/Solution.java
+++ problems/problems_2306/Solution.java
@@ -7,7 +7,25 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public long distinctNames(String[] ideas) {
+        Set<String>[] groups = new HashSet[26];
+        Arrays.setAll(groups, i -> new HashSet<>());
+        for (String s : ideas) {
+            groups[s.charAt(0) - 'a'].add(s.substring(1)); // 按照首字母分组
+        }
 
+        long ans = 0;
+        for (int a = 1; a < 26; a++) { // 枚举所有组对
+            for (int b = 0; b < a; b++) {
+                int m = 0; // 交集的大小
+                for (String s : groups[a]) {
+                    if (groups[b].contains(s)) {
+                        m++;
+                    }
+                }
+                ans += (long) (groups[a].size() - m) * (groups[b].size() - m);
+            }
+        }
+        return ans * 2; // 乘 2 放到最后
     }
 
     @Override
--- problems/problems_LCR_083/Solution.java
+++ problems/problems_LCR_083/Solution.java
@@ -7,7 +7,25 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
+        for (int num : nums) {
+            output.add(num);
+        }
+        int n = nums.length;
+        backtrack(n, output, res, 0);
+        return res;
+    }
 
+    private void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+        if (first == n) {
+            res.add(new ArrayList<>(output));
+        }
+        for (int i = first; i < n; i++) {
+            Collections.swap(output, first, i);
+            backtrack(n, output, res, first + 1);
+            Collections.swap(output, first, i);
+        }
     }
 
     @Override
