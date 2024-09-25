8b3fafce92a665c9c513be706d514924874423d6
benhao <qubenhao2@gmail.com>
2024-09-25 21:17:00 +0800

    test: rust 2306, LCR 083
    solution

--- problems/problems_2306/solution.rs
+++ problems/problems_2306/solution.rs
@@ -2,9 +2,23 @@ use serde_json::{json, Value};
 
 pub struct Solution;
 
+use std::collections::HashSet;
+
 impl Solution {
     pub fn distinct_names(ideas: Vec<String>) -> i64 {
+        let mut groups = vec![HashSet::new(); 26];
+        for s in ideas {
+            groups[(s.as_bytes()[0] - b'a') as usize].insert(s[1..].to_string()); // 按照首字母分组
+        }
 
+        let mut ans = 0i64;
+        for a in 1..26 { // 枚举所有组对
+            for b in 0..a {
+                let m = groups[a].iter().filter(|&s| groups[b].contains(s)).count(); // 交集的大小
+                ans += (groups[a].len() - m) as i64 * (groups[b].len() - m) as i64;
+            }
+        }
+        ans * 2 // 乘 2 放到最后
     }
 }
 
--- problems/problems_LCR_083/solution.rs
+++ problems/problems_LCR_083/solution.rs
@@ -5,7 +5,24 @@ pub struct Solution;
 
 impl Solution {
     pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
-
+		let mut res = vec![];
+		let mut path = vec![];
+		fn backtrack(nums: &Vec<i32>, path: &mut Vec<i32>, res: &mut Vec<Vec<i32>>) {
+			if path.len() == nums.len() {
+				res.push(path.clone());
+				return;
+			}
+			for i in 0..nums.len() {
+				if path.contains(&nums[i]) {
+					continue;
+				}
+				path.push(nums[i]);
+				backtrack(nums, path, res);
+				path.pop();
+			}
+		}
+		backtrack(&nums, &mut path, &mut res);
+		res
     }
 }
 
