7a68c40005e7790e2cf6c46ee21f1b8d741d976a
benhao <qubenhao2@gmail.com>
2024-09-25 20:55:53 +0800

    test: c++ 2306, LCR 083
    solution

--- problems/problems_2306/Solution.cpp
+++ problems/problems_2306/Solution.cpp
@@ -1,28 +1,42 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    long long distinctNames(vector<string>& ideas) {
-        
+  long long distinctNames(vector<string> &ideas) {
+    unordered_set<string> groups[26];
+    for (auto &s : ideas) {
+      groups[s[0] - 'a'].insert(s.substr(1));  // 按照首字母分组
     }
+
+    int64_t ans = 0;
+    for (int a = 1; a < 26; a++) {  // 枚举所有组对
+      for (int b = 0; b < a; b++) {
+        int m = 0;  // 交集的大小
+        for (auto &s : groups[a]) {
+          m += groups[b].count(s);
+        }
+        ans += (int64_t)(groups[a].size() - m) * (groups[b].size() - m);
+      }
+    }
+    return ans * 2;  // 乘 2 放到最后
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<string> ideas = json::parse(inputArray.at(0));
-	return solution.distinctNames(ideas);
+  Solution solution;
+  vector<string> ideas = json::parse(inputArray.at(0));
+  return solution.distinctNames(ideas);
 }
--- problems/problems_LCR_083/Solution.cpp
+++ problems/problems_LCR_083/Solution.cpp
@@ -1,28 +1,44 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-
-    }
+  vector<vector<int>> permute(vector<int> &nums) {
+    vector<vector<int>> res;
+    vector<int> path;
+    function<void(int)> backtracking = [&](int index) {
+      if (index == nums.size()) {
+        res.push_back(path);
+        return;
+      }
+      for (int i = 0; i < nums.size(); i++) {
+        if (find(path.begin(), path.end(), nums[i]) != path.end()) {
+          continue;
+        }
+        path.push_back(nums[i]);
+        backtracking(index + 1);
+        path.pop_back();
+      }
+    };
+    backtracking(0);
+    return res;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<int> nums = json::parse(inputArray.at(0));
-	return solution.permute(nums);
+  Solution solution;
+  vector<int> nums = json::parse(inputArray.at(0));
+  return solution.permute(nums);
 }
