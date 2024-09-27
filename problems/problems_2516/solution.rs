84edf5976baf5d1351934d8340d8ac4850e182ed
benhao <qubenhao2@gmail.com>
2024-09-27 21:44:23 +0800

    test: rust 2516, LCR 003
    solution

--- problems/problems_2516/solution.rs
+++ problems/problems_2516/solution.rs
@@ -4,7 +4,27 @@ pub struct Solution;
 
 impl Solution {
     pub fn take_characters(s: String, k: i32) -> i32 {
+        let mut cnt = [0; 3];
+        for c in s.bytes() {
+            cnt[(c - b'a') as usize] += 1; // 一开始，把所有字母都取走
+        }
+        if cnt[0] < k || cnt[1] < k || cnt[2] < k {
+            return -1; // 字母个数不足 k
+        }
 
+        let mut mx = 0;
+        let mut left = 0;
+        let s = s.as_bytes();
+        for (right, &c) in s.iter().enumerate() {
+            let c = (c - b'a') as usize;
+            cnt[c] -= 1; // 移入窗口，相当于不取走 c
+            while cnt[c] < k { // 窗口之外的 c 不足 k
+                cnt[(s[left] - b'a') as usize] += 1; // 移出窗口，相当于取走 s[left]
+                left += 1;
+            }
+            mx = mx.max(right - left + 1);
+        }
+        (s.len() - mx) as _
     }
 }
 
--- problems/problems_LCR_003/solution.rs
+++ problems/problems_LCR_003/solution.rs
@@ -5,7 +5,11 @@ pub struct Solution;
 
 impl Solution {
     pub fn count_bits(n: i32) -> Vec<i32> {
-
+		let mut res = vec![0; (n + 1) as usize];
+		for i in 1..=n as usize {
+			res[i] = res[i >> 1] + (i & 1) as i32;
+		}
+		res
     }
 }
 
