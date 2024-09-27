d7ac4bd4184d9a0d21c2eda407d590ad0a4bb77e
benhao <qubenhao2@gmail.com>
2024-09-27 21:41:07 +0800

    test: typescript 2516, LCR 003
    solution

--- problems/problems_2516/solution.ts
+++ problems/problems_2516/solution.ts
@@ -1,6 +1,24 @@
 function takeCharacters(s: string, k: number): number {
-    
-};
+    const idx = (c: string) => c.charCodeAt(0) - 97;
+    const cnt: number[] = Array(3).fill(0);
+    for (const c of s) {
+        ++cnt[idx(c)];
+    }
+    if (cnt.some(v => v < k)) {
+        return -1;
+    }
+    const n = s.length;
+    let [mx, j] = [0, 0];
+    for (let i = 0; i < n; ++i) {
+        const c = idx(s[i]);
+        --cnt[c];
+        while (cnt[c] < k) {
+            ++cnt[idx(s[j++])];
+        }
+        mx = Math.max(mx, i - j + 1);
+    }
+    return n - mx;
+}
 
 export function Solve(inputJsonElement: string): any {
 	const inputValues: string[] = inputJsonElement.split("\n");
--- problems/problems_LCR_003/solution.ts
+++ problems/problems_LCR_003/solution.ts
@@ -1,5 +1,9 @@
 function countBits(n: number): number[] {
-
+	const bits: number[] = Array(n + 1).fill(0);
+	for (let i = 1; i <= n; ++i) {
+		bits[i] = bits[i >> 1] + (i & 1);
+	}
+	return bits;
 };
 
 export function Solve(inputJsonElement: string): any {
