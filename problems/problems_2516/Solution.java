4488f55e4d519992863dba623959465998fbd8f6
benhao <qubenhao2@gmail.com>
2024-09-27 21:39:08 +0800

    test: Java 2516, LCR 003
    solution

--- problems/problems_2516/Solution.java
+++ problems/problems_2516/Solution.java
@@ -6,8 +6,28 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
-    public int takeCharacters(String s, int k) {
+    public int takeCharacters(String S, int k) {
+        char[] s = S.toCharArray();
+        int[] cnt = new int[3];
+        for (char c : s) {
+            cnt[c - 'a']++; // 一开始，把所有字母都取走
+        }
+        if (cnt[0] < k || cnt[1] < k || cnt[2] < k) {
+            return -1; // 字母个数不足 k
+        }
 
+        int mx = 0; // 子串最大长度
+        int left = 0;
+        for (int right = 0; right < s.length; right++) {
+            int c = s[right] - 'a';
+            cnt[c]--; // 移入窗口，相当于不取走 c
+            while (cnt[c] < k) { // 窗口之外的 c 不足 k
+                cnt[s[left] - 'a']++; // 移出窗口，相当于取走 s[left]
+                left++;
+            }
+            mx = Math.max(mx, right - left + 1);
+        }
+        return s.length - mx;
     }
 
     @Override
--- problems/problems_LCR_003/Solution.java
+++ problems/problems_LCR_003/Solution.java
@@ -7,7 +7,11 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int[] countBits(int n) {
-
+        int[] bits = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            bits[i] = bits[i >> 1] + (i & 1);
+        }
+        return bits;
     }
 
     @Override
