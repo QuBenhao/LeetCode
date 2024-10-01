2ddb1efd57026ae33ae8064053fc20bf7894377d
benhao <qubenhao2@gmail.com>
2024-10-01 14:22:04 +0800

    test: python 983
    solution

--- problems/problems_983/solution.py
+++ problems/problems_983/solution.py
@@ -1,5 +1,7 @@
 import solution
 from typing import *
+from functools import lru_cache
+from math import inf
 
 
 class Solution(solution.Solution):
@@ -7,5 +9,21 @@ class Solution(solution.Solution):
         return self.mincostTickets(*test_input)
 
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        pass
+        n = len(days)
 
+        @lru_cache(None)
+        def dfs(i):
+            if i >= n:
+                return 0
+            ans = inf
+            ans = min(ans, dfs(i+1) + costs[0])
+            j = i
+            while j < len(days) and days[j] < days[i] + 7:
+                j += 1
+            ans = min(ans, dfs(j) + costs[1])
+            while j < len(days) and days[j] < days[i] + 30:
+                j += 1
+            ans = min(ans, dfs(j) + costs[2])
+            return ans
+
+        return dfs(0)
