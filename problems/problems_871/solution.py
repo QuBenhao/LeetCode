d01262250b88e405306e884a1cf2da55232d9535
benhao <qubenhao2@gmail.com>
2024-10-07 17:03:02 +0800

    test: 871
    solution

--- problems/problems_871/solution.py
+++ problems/problems_871/solution.py
@@ -7,5 +7,13 @@ class Solution(solution.Solution):
         return self.minRefuelStops(*test_input)
 
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
-        pass
-
+        n = len(stations)
+        dp = [startFuel] + [0] * n
+        for i, (location, capacity) in enumerate(stations):
+            for t in range(i, -1, -1):
+                if dp[t] >= location:
+                    dp[t + 1] = max(dp[t + 1], dp[t] + capacity)
+        for i, d in enumerate(dp):
+            if d >= target:
+                return i
+        return -1
