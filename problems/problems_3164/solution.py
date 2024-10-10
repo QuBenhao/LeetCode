46e838ffbba2a0f635ad6ff8a7739dd5ef51b978
GitHub Action <action@github.com>
2024-10-10 16:07:17 +0000

    test: [20241011] Add daily LeetCode problem

---
+++ problems/problems_3164/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.numberOfPairs(*test_input)
+
+    def numberOfPairs(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        pass
+
