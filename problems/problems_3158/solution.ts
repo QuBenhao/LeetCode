828eec9e46c64385f17bf48bbdc235494f9cf92b
benhao <qubenhao2@gmail.com>
2024-10-12 23:09:14 +0800

    test: 3158, LCR 065 solution
    typescript

--- problems/problems_3158/solution.ts
+++ problems/problems_3158/solution.ts
@@ -1,5 +1,14 @@
 function duplicateNumbersXOR(nums: number[]): number {
-    
+    let ans: number = 0;
+	const explored: Set<number> = new Set<number>();
+	for (const num of nums) {
+		if (explored.has(num)) {
+			ans ^= num;
+		} else {
+			explored.add(num);
+		}
+	}
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_LCR_065/solution.ts
+++ problems/problems_LCR_065/solution.ts
@@ -1,5 +1,16 @@
 function minimumLengthEncoding(words: string[]): number {
-
+	const n: number = words.length;
+	const reversedWords: string[] = words.map((word: string) => word.split("").reverse().join(""));
+	reversedWords.sort();
+	let ans: number = 0;
+	for (let i: number = 0; i < n - 1; i++) {
+		if (reversedWords[i + 1].startsWith(reversedWords[i])) {
+			continue;
+		}
+		ans += reversedWords[i].length + 1;
+	}
+	ans += reversedWords[n - 1].length + 1;
+	return ans;
 };
 
 export function Solve(inputJsonElement: string): any {
