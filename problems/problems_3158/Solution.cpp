6fe3aee6a3580e317ed6620d83278d392bbd4f4a
benhao <qubenhao2@gmail.com>
2024-10-12 23:04:01 +0800

    test: 3158, LCR 065 solution
    c++

--- problems/problems_3158/Solution.cpp
+++ problems/problems_3158/Solution.cpp
@@ -1,28 +1,37 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
-
+#include <unordered_set>
 
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    int duplicateNumbersXOR(vector<int>& nums) {
-        
+  int duplicateNumbersXOR(vector<int> &nums) {
+    int ans = 0;
+    unordered_set<int> s;
+    for (int num : nums) {
+      if (s.find(num) != s.end()) {
+        ans ^= num;
+      } else {
+        s.insert(num);
+      }
     }
+    return ans;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<int> nums = json::parse(inputArray.at(0));
-	return solution.duplicateNumbersXOR(nums);
+  Solution solution;
+  vector<int> nums = json::parse(inputArray.at(0));
+  return solution.duplicateNumbersXOR(nums);
 }
--- problems/problems_LCR_065/Solution.cpp
+++ problems/problems_LCR_065/Solution.cpp
@@ -1,28 +1,41 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    int minimumLengthEncoding(vector<string>& words) {
-
+  int minimumLengthEncoding(vector<string> &words) {
+    int n = static_cast<int>(words.size());
+    vector<string> revertWords = vector<string>(n);
+    for (int i = 0; i < n; i++) {
+      revertWords[i] = words[i];
+      reverse(revertWords[i].begin(), revertWords[i].end());
     }
+    sort(revertWords.begin(), revertWords.end());
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+      if (i + 1 < n && revertWords[i + 1].find(revertWords[i]) == 0) {
+        continue;
+      }
+      ans += revertWords[i].size() + 1;
+    }
+    return ans;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<string> words = json::parse(inputArray.at(0));
-	return solution.minimumLengthEncoding(words);
+  Solution solution;
+  vector<string> words = json::parse(inputArray.at(0));
+  return solution.minimumLengthEncoding(words);
 }
