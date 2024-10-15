64ff631110f3387cbe199584d2ce3a1c88b43d48
benhao <qubenhao2@gmail.com>
2024-10-15 21:30:46 +0800

    test: 3200 solution
    py, golang

--- problems/problems_3200/solution.go
+++ problems/problems_3200/solution.go
@@ -3,11 +3,21 @@ package problem3200
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"strings"
 )
 
-func maxHeightOfTriangle(red int, blue int) int {
+func f(n, m int) int {
+	odd := int(math.Sqrt(float64(n)))
+	even := int((math.Sqrt(float64(m*4+1)) - 1) / 2)
+	if odd > even {
+		return even*2 + 1
+	}
+	return odd * 2
+}
 
+func maxHeightOfTriangle(red, blue int) int {
+	return max(f(red, blue), f(blue, red))
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_3200/solution.py
+++ problems/problems_3200/solution.py
@@ -1,3 +1,5 @@
+from math import isqrt, sqrt
+
 import solution
 from typing import *
 
@@ -7,5 +9,11 @@ class Solution(solution.Solution):
         return self.maxHeightOfTriangle(*test_input)
 
     def maxHeightOfTriangle(self, red: int, blue: int) -> int:
-        pass
+        # if odd lines, the first line has 1,3,5,...,2n-1; which sum is n^2
+        # if even lines, the second line has 2,4,6,...,2n; which sum is n(n+1);
+        def f(n, m):
+            odd = isqrt(n)
+            even = int((sqrt(m * 4 + 1) - 1) / 2)
+            return even * 2 + 1 if odd > even else odd * 2
 
+        return max(f(red, blue), f(blue, red))
