f805498ffee8c6acd40e405f802e884459d1efc1
benhao <qubenhao2@gmail.com>
2024-10-16 21:30:31 +0800

    test: 3194 solution
    golang

--- problems/problems_3194/solution.go
+++ problems/problems_3194/solution.go
@@ -3,11 +3,18 @@ package problem3194
 import (
 	"encoding/json"
 	"log"
+	"sort"
 	"strings"
 )
 
 func minimumAverage(nums []int) float64 {
-
+	sort.Ints(nums)
+	n := len(nums)
+	ans := nums[0] + nums[n-1]
+	for i := 1; i < n/2; i++ {
+		ans = min(ans, nums[n-1-i]+nums[i])
+	}
+	return float64(ans) / 2
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_3194/solution.py
+++ problems/problems_3194/solution.py
@@ -9,4 +9,4 @@ class Solution(solution.Solution):
     def minimumAverage(self, nums: List[int]) -> float:
         nums.sort()
         n = len(nums)
-        return min((nums[i] + nums[n - i - 1]) / 2 for i in range(n // 2))
+        return min(nums[i] + nums[n - i - 1] for i in range(n // 2)) / 2
