b713ab37a3fabc65a1ec484f164795220fdf96af
benhao <qubenhao2@gmail.com>
2024-10-20 11:43:39 +0800

    test: 908, LCR 084 solution
    c++

--- problems/problems_908/Solution.cpp
+++ problems/problems_908/Solution.cpp
@@ -1,29 +1,33 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    int smallestRangeI(vector<int>& nums, int k) {
-        
+  int smallestRangeI(vector<int> &nums, int k) {
+    int min_val = nums[0], max_val = nums[0];
+    for (int num : nums) {
+      min_val = min(min_val, num);
+      max_val = max(max_val, num);
     }
+    return max(0, max_val - min_val - 2 * k);
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<int> nums = json::parse(inputArray.at(0));
-	int k = json::parse(inputArray.at(1));
-	return solution.smallestRangeI(nums, k);
+  Solution solution;
+  vector<int> nums = json::parse(inputArray.at(0));
+  int k = json::parse(inputArray.at(1));
+  return solution.smallestRangeI(nums, k);
 }
--- problems/problems_LCR_084/Solution.cpp
+++ problems/problems_LCR_084/Solution.cpp
@@ -1,28 +1,46 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        
+  vector<vector<int>> permuteUnique(vector<int> &nums) {
+    dfs(nums, 0);
+    return res;
+  }
+
+private:
+  vector<vector<int>> res;
+  void dfs(vector<int> nums, int x) {
+    if (x == nums.size() - 1) {
+      res.push_back(nums);  // 添加排列方案
+      return;
     }
+    set<int> st;
+    for (int i = x; i < nums.size(); i++) {
+      if (st.find(nums[i]) != st.end())
+        continue;  // 重复，因此剪枝
+      st.insert(nums[i]);
+      swap(nums[i], nums[x]);  // 交换，将 nums[i] 固定在第 x 位
+      dfs(nums, x + 1);       // 开启固定第 x + 1 位元素
+      swap(nums[i], nums[x]);  // 恢复交换
+    }
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<int> nums = json::parse(inputArray.at(0));
-	return solution.permuteUnique(nums);
+  Solution solution;
+  vector<int> nums = json::parse(inputArray.at(0));
+  return solution.permuteUnique(nums);
 }
