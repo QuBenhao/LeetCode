0fbebf0ddd195bea6a9a1512dffe79bef9fde4eb
benhao <qubenhao2@gmail.com>
2024-10-20 11:53:51 +0800

    test: 908, LCR 084 solution
    Java

--- problems/problems_908/Solution.java
+++ problems/problems_908/Solution.java
@@ -7,7 +7,12 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int smallestRangeI(int[] nums, int k) {
-        
+        int min = nums[0], max = nums[0];
+        for (int num: nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        return Math.max(0, max - min - 2 * k);
     }
 
     @Override
--- problems/problems_LCR_084/Solution.java
+++ problems/problems_LCR_084/Solution.java
@@ -6,8 +6,32 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
+    boolean[] vis;
+
     public int[][] permuteUnique(int[] nums) {
-        
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> perm = new ArrayList<>();
+        vis = new boolean[nums.length];
+        Arrays.sort(nums);
+        backtrack(nums, ans, 0, perm);
+        return ans.stream().map(permList -> permList.stream().mapToInt(i -> i).toArray()).toArray(int[][]::new);
+    }
+
+    private void backtrack(int[] nums, List<List<Integer>> ans, int idx, List<Integer> perm) {
+        if (idx == nums.length) {
+            ans.add(new ArrayList<>(perm));
+            return;
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (vis[i] || (i > 0 && nums[i] == nums[i - 1] && !vis[i - 1])) {
+                continue;
+            }
+            perm.add(nums[i]);
+            vis[i] = true;
+            backtrack(nums, ans, idx + 1, perm);
+            vis[i] = false;
+            perm.remove(idx);
+        }
     }
 
     @Override
