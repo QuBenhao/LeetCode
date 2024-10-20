8ed996beaebad211ea26c87a75255d36dfaadb73
benhao <qubenhao2@gmail.com>
2024-10-20 11:56:06 +0800

    test: 908, LCR 084 solution
    typescript

--- problems/problems_908/solution.ts
+++ problems/problems_908/solution.ts
@@ -1,5 +1,10 @@
 function smallestRangeI(nums: number[], k: number): number {
-    
+    let min: number = nums[0], max: number = nums[0];
+	for (const num of nums) {
+		min = Math.min(min, num);
+		max = Math.max(max, num);
+	}
+	return Math.max(0, max - min - 2 * k);
 };
 
 export function Solve(inputJsonElement: string): any {
--- problems/problems_LCR_084/solution.ts
+++ problems/problems_LCR_084/solution.ts
@@ -1,5 +1,27 @@
 function permuteUnique(nums: number[]): number[][] {
-    
+    const ansMap = new Map();//用于去重
+    const dfs = (path:number[],indexMap:Map<number,number>) => {
+        if (path.length === nums.length) {
+            const key = path.join('-');
+            if (ansMap.has(key) === false) {
+                ansMap.set(key,[...path]);
+            }
+            return;
+        }
+
+        for (let i = 0; i < nums.length; i++) {
+            if (indexMap.has(i) === false) {
+                indexMap.set(i,1);
+                path.push(nums[i]);
+                dfs(path,indexMap);
+                indexMap.delete(i);
+                path.pop();
+            }
+        }
+    }
+    dfs([],new Map());
+    //返回二维数组
+    return Array.from(ansMap.values());
 };
 
 export function Solve(inputJsonElement: string): any {
