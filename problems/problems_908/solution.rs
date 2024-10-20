ac8114a193e18c93c7af10deb4248fac1079d29d
benhao <qubenhao2@gmail.com>
2024-10-20 11:58:31 +0800

    test: 908, LCR 084 solution
    rust

--- problems/problems_908/solution.rs
+++ problems/problems_908/solution.rs
@@ -1,10 +1,16 @@
 use serde_json::{json, Value};
 
 pub struct Solution;
-
+use std::cmp::{max, min};
 impl Solution {
     pub fn smallest_range_i(nums: Vec<i32>, k: i32) -> i32 {
-        
+		let mut min_val = nums[0];
+		let mut max_val = nums[0];
+		for i in 1..nums.len() {
+			min_val = min(min_val, nums[i]);
+			max_val = max(max_val, nums[i]);
+		}
+		max(0, max_val - min_val - 2 * k)
     }
 }
 
--- problems/problems_LCR_084/solution.rs
+++ problems/problems_LCR_084/solution.rs
@@ -3,9 +3,30 @@ use serde_json::{json, Value};
 
 pub struct Solution;
 
+use std::collections::HashSet;
 impl Solution {
     pub fn permute_unique(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        
+        fn dfs(a: &Vec<i32>, used: &mut Vec<bool>, tmp: &mut Vec<i32>, ans: &mut HashSet<Vec<i32>>) {
+            if ans.get(tmp).is_some() {
+                return;
+            } else {
+                ans.insert(tmp.clone());
+            }
+            for i in 0..a.len() {
+                if !used[i] {
+                    used[i] = true;
+                    tmp.push(a[i]);
+                    dfs(a, used, tmp, ans);
+                    tmp.pop();
+                    used[i] = false;
+                }
+            }
+        }
+        let mut ans = HashSet::with_capacity(1 << (nums.len() + 2));
+        let mut used = vec![false; nums.len()];
+        let mut tmp = Vec::with_capacity(nums.len());
+        dfs(&nums, &mut used, &mut tmp, &mut ans);
+        ans.into_iter().filter(|t| t.len() == nums.len()).collect()
     }
 }
 
