b930fd419782e0a37b45763ca7a1d77a514eb756
benhao <qubenhao2@gmail.com>
2024-10-20 11:35:08 +0800

    test: 908, LCR 084 solution
    python

--- problems/problems_LCR_084/testcase.py
+++ problems/problems_LCR_084/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=[1, 1, 2], Output=[[1, 1, 2], [1, 2, 1], [2, 1, 1]]))
 		self.testcases.append(case(Input=[1, 2, 3], Output=[[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]))
+		self.testcases.append(case(Input=[2,2,1,1], Output=[[1,1,2,2],[1,2,1,2],[1,2,2,1],[2,1,1,2],[2,1,2,1],[2,2,1,1]]))
 
 	def get_testcases(self):
 		return self.testcases
