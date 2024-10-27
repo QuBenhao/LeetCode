929db6b6b310244621787e861ae74a2f4cf63242
benhao <qubenhao2@gmail.com>
2024-10-27 20:14:48 +0800

    test: 684, LCR 053 solution
    python

--- problems/problems_684/solution.py
+++ problems/problems_684/solution.py
@@ -7,5 +7,19 @@ class Solution(solution.Solution):
         return self.findRedundantConnection(test_input)
 
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        pass
+        n = len(edges)
+        parent = [i for i in range(n + 1)]
 
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(x, y):
+            parent[find(x)] = find(y)
+
+        for x, y in edges:
+            if find(x) == find(y):
+                return [x, y]
+            union(x, y)
+        return []
--- problems/problems_LCR_053/solution.py
+++ problems/problems_LCR_053/solution.py
@@ -20,5 +20,20 @@ class Solution(solution.Solution):
         return tree_to_list(res)
 
     def inorderSuccessor(self, root: 'TreeNode', p: 'TreeNode') -> 'TreeNode':
-        pass
-
+        if not root:
+            return None
+        if p.right:
+            p = p.right
+            while p.left:
+                p = p.left
+            return p
+        stack = []
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if root == p:
+                return stack[-1] if stack else None
+            root = root.right
+        return None
