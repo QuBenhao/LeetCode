f34c273ca87d83e5b98a9ddcb1d65f32635825d2
benhao <qubenhao2@gmail.com>
2024-10-27 20:59:16 +0800

    test: 684, LCR 053 solution
    Java

--- problems/problems_684/Solution.java
+++ problems/problems_684/Solution.java
@@ -6,8 +6,33 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
+    private int find(int[] parent, int node) {
+        if (parent[node] != node) {
+            parent[node] = find(parent, parent[node]);
+        }
+        return parent[node];
+    }
+
+    private void union(int[] parent, int node1, int node2) {
+        parent[find(parent, node1)] = find(parent, node2);
+    }
+
     public int[] findRedundantConnection(int[][] edges) {
-        
+        int n = edges.length;
+        int[] parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+        for (int[] edge : edges) {
+            int node1 = edge[0];
+            int node2 = edge[1];
+            if (find(parent, node1) != find(parent, node2)) {
+                union(parent, node1, node2);
+            } else {
+                return edge;
+            }
+        }
+        return new int[0];
     }
 
     @Override
--- problems/problems_LCR_053/Solution.java
+++ problems/problems_LCR_053/Solution.java
@@ -16,7 +16,28 @@ import qubhjava.models.TreeNode;
 
 public class Solution extends BaseSolution {
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-        
+        if (p.right != null) {
+            p = p.right;
+            while (p.left != null) {
+                p = p.left;
+            }
+            return p;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (pre == p) {
+                return root;
+            }
+            pre = root;
+            root = root.right;
+        }
+        return null;
     }
 
     @Override
