ea496e1f387fb6d4a9394bc58e79163ab2983ec5
benhao <qubenhao2@gmail.com>
2024-10-27 20:47:27 +0800

    test: 684, LCR 053 solution
    c++

--- problems/problems_684/Solution.cpp
+++ problems/problems_684/Solution.cpp
@@ -1,28 +1,48 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
-public:
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        
+private:
+  int find(vector<int> &parent, int x) {
+    if (parent[x] != x) {
+      parent[x] = find(parent, parent[x]);
     }
+    return parent[x];
+  }
+
+public:
+  vector<int> findRedundantConnection(vector<vector<int>> &edges) {
+    int n = edges.size();
+    vector<int> parent(n + 1);
+    for (int i = 1; i <= n; i++) {
+      parent[i] = i;
+    }
+    for (auto &edge : edges) {
+      int x = edge[0], y = edge[1];
+      if (find(parent, x) != find(parent, y)) {
+        parent[find(parent, x)] = find(parent, y);
+      } else {
+        return edge;
+      }
+    }
+    return {};
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<vector<int>> edges = json::parse(inputArray.at(0));
-	return solution.findRedundantConnection(edges);
+  Solution solution;
+  vector<vector<int>> edges = json::parse(inputArray.at(0));
+  return solution.findRedundantConnection(edges);
 }
--- problems/problems_LCR_053/Solution.cpp
+++ problems/problems_LCR_053/Solution.cpp
@@ -16,25 +16,43 @@ using json = nlohmann::json;
  */
 class Solution {
 public:
-    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
-        
+  TreeNode *inorderSuccessor(TreeNode *root, TreeNode *p) {
+    if (p->right) {
+      p = p->right;
+      while (p->left) {
+        p = p->left;
+      }
+      return p;
     }
+    TreeNode *successor = nullptr;
+    while (root) {
+      if (root->val > p->val) {
+        successor = root;
+        root = root->left;
+      } else if (root->val < p->val) {
+        root = root->right;
+      } else {
+        break;
+      }
+    }
+    return successor;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	json root_array = json::parse(inputArray.at(0));
-	int p_val = json::parse(inputArray.at(1));
-	auto nodes = JsonArrayToTreeNodeWithTargets(root_array, {p_val});
-	TreeNode *root = nodes[0], *p = nodes[1];
-	return TreeNodeToJsonArray(solution.inorderSuccessor(root, p));
+  Solution solution;
+  json root_array = json::parse(inputArray.at(0));
+  int p_val = json::parse(inputArray.at(1));
+  auto nodes = JsonArrayToTreeNodeWithTargets(root_array, {p_val});
+  TreeNode *root = nodes[0], *p = nodes[1];
+  return TreeNodeToJsonArray(solution.inorderSuccessor(root, p));
 }
