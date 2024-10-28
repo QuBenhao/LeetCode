fef40dad8a3bcfa160b673eb4c70117f4e7a9d99
benhao <qubenhao2@gmail.com>
2024-10-28 22:04:25 +0800

    test: 685 solution
    py

--- problems/problems_685/solution.ts
+++
@@ -1,9 +0,0 @@
-function findRedundantDirectedConnection(edges: number[][]): number[] {
-    
-};
-
-export function Solve(inputJsonElement: string): any {
-	const inputValues: string[] = inputJsonElement.split("\n");
-	const edges: number[][] = JSON.parse(inputValues[0]);
-	return findRedundantDirectedConnection(edges);
-}
