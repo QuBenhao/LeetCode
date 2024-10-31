297b29581e1b5d592dba1f1acac49bb110bfc305
benhao <qubenhao2@gmail.com>
2024-10-31 22:22:39 +0800

    test: 3165 solution
    py

--- problems/problems_3165/solution.py
+++ problems/problems_3165/solution.py
@@ -7,5 +7,48 @@ class Solution(solution.Solution):
         return self.maximumSumSubsequence(*test_input)
 
     def maximumSumSubsequence(self, nums: List[int], queries: List[List[int]]) -> int:
-        pass
+        n = len(nums)
+        # 4 个数分别保存 f00, f01, f10, f11
+        t = [[0] * 4 for _ in range(2 << n.bit_length())]
 
+        # 手写 max，效率更高
+        def max(a: int, b: int) -> int:
+            return b if b > a else a
+
+        # 合并左右儿子
+        def maintain(o: int):
+            a, b = t[o * 2], t[o * 2 + 1]
+            t[o][0] = max(a[0] + b[2], a[1] + b[0])
+            t[o][1] = max(a[0] + b[3], a[1] + b[1])
+            t[o][2] = max(a[2] + b[2], a[3] + b[0])
+            t[o][3] = max(a[2] + b[3], a[3] + b[1])
+
+        # 用 nums 初始化线段树
+        def build(o: int, l: int, r: int) -> None:
+            if l == r:
+                t[o][3] = max(nums[l], 0)
+                return
+            m = (l + r) // 2
+            build(o * 2, l, m)
+            build(o * 2 + 1, m + 1, r)
+            maintain(o)
+
+        # 把 nums[i] 改成 val
+        def update(o: int, l: int, r: int, i: int, val: int) -> None:
+            if l == r:
+                t[o][3] = max(val, 0)
+                return
+            m = (l + r) // 2
+            if i <= m:
+                update(o * 2, l, m, i, val)
+            else:
+                update(o * 2 + 1, m + 1, r, i, val)
+            maintain(o)
+
+        build(1, 0, n - 1)
+
+        ans = 0
+        for i, x in queries:
+            update(1, 0, n - 1, i, x)
+            ans += t[1][3]  # 注意 f11 没有任何限制，也就是整个数组的打家劫舍
+        return ans % 1_000_000_007
