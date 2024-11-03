3e9c886964ac96371d4bc9fd8db6d7ccdc48a524
benhao <qubenhao2@gmail.com>
2024-11-03 21:33:02 +0800

    test: 638, LCR 018 solution
    py

--- problems/problems_638/solution.py
+++ problems/problems_638/solution.py
@@ -7,5 +7,16 @@ class Solution(solution.Solution):
         return self.shoppingOffers(*test_input)
 
     def shoppingOffers(self, price: List[int], special: List[List[int]], needs: List[int]) -> int:
-        pass
+        def dfs(needs):
+            if tuple(needs) in memo:
+                return memo[tuple(needs)]
+            res = sum(needs[i] * price[i] for i in range(len(needs)))
+            for s in special:
+                if all(needs[i] >= s[i] for i in range(len(needs))):
+                    res = min(res, s[-1] + dfs([needs[i] - s[i] for i in range(len(needs))]))
+            memo[tuple(needs)] = res
+            return res
+
+        memo = {}
+        return dfs(needs)
 
--- problems/problems_LCR_018/solution.py
+++ problems/problems_LCR_018/solution.py
@@ -7,5 +7,16 @@ class Solution(solution.Solution):
         return self.isPalindrome(test_input)
 
     def isPalindrome(self, s: str) -> bool:
-        pass
-
+        n = len(s)
+        left, right = 0, n - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if left < right:
+                if s[left].lower() != s[right].lower():
+                    return False
+                left += 1
+                right -= 1
+        return True
