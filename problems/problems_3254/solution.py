7c4fc0d225c4389995f4a1c799a93801801f08d2
benhao <qubenhao2@gmail.com>
2024-11-06 22:00:10 +0800

    test: 3254 solution
    py

--- problems/problems_3254/solution.py
+++ problems/problems_3254/solution.py
@@ -7,5 +7,11 @@ class Solution(solution.Solution):
         return self.resultsArray(*test_input)
 
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
-        pass
-
+        n = len(nums)
+        ans = [-1] * (n - k + 1)
+        cnt = 0
+        for i, num in enumerate(nums):
+            cnt = cnt + 1 if i == 0 or num > nums[i - 1] else 1
+            if cnt >= k:
+                ans[i - k + 1] = num
+        return ans
