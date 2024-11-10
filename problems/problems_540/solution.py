970181f25c0250cfc776521563500a7b5274e76f
benhao <qubenhao2@gmail.com>
2024-11-10 17:31:35 +0800

    test: 540, LCR 039 solution
    py

--- problems/problems_540/solution.py
+++ problems/problems_540/solution.py
@@ -7,5 +7,14 @@ class Solution(solution.Solution):
         return self.singleNonDuplicate(test_input)
 
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        pass
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if mid % 2 == 1:
+                mid -= 1
+            if nums[mid] == nums[mid + 1]:
+                l = mid + 2
+            else:
+                r = mid
+        return nums[l]
 
--- problems/problems_LCR_039/solution.py
+++ problems/problems_LCR_039/solution.py
@@ -7,5 +7,17 @@ class Solution(solution.Solution):
         return self.largestRectangleArea(test_input)
 
     def largestRectangleArea(self, heights: List[int]) -> int:
-        pass
+        stack = []
+        max_area = 0
+        for i, h in enumerate(heights):
+            while stack and h < heights[stack[-1]]:
+                height = heights[stack.pop()]
+                width = i if not stack else i - stack[-1] - 1
+                max_area = max(max_area, height * width)
+            stack.append(i)
+        while stack:
+            height = heights[stack.pop()]
+            width = len(heights) if not stack else len(heights) - stack[-1] - 1
+            max_area = max(max_area, height * width)
+        return max_area
 
