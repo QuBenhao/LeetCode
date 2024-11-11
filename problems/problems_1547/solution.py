9d55be8d3a6e7b2b42d2a586dee025878547e8ed
benhao <qubenhao2@gmail.com>
2024-11-11 20:25:06 +0800

    test: 1547 solution
    py

--- problems/problems_1547/solution.py
+++ problems/problems_1547/solution.py
@@ -7,5 +7,12 @@ class Solution(solution.Solution):
         return self.minCost(*test_input)
 
     def minCost(self, n: int, cuts: List[int]) -> int:
-        pass
+        cuts.sort()
+        cuts = [0] + cuts + [n]
+        m = len(cuts)
+        dp = [[0] * m for _ in range(m)]
+        for i in range(m - 2, -1, -1):
+            for j in range(i + 2, m):
+                dp[i][j] = min(dp[i][k] + dp[k][j] for k in range(i + 1, j)) + cuts[j] - cuts[i]
+        return dp[0][m - 1]
 
