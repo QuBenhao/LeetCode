d6198468e3de8c3996b28eea06c98299cfaeb2bb
benhao <qubenhao2@gmail.com>
2024-11-12 21:27:22 +0800

    test: 3258 solution
    py

--- problems/problems_3258/solution.py
+++ problems/problems_3258/solution.py
@@ -7,5 +7,12 @@ class Solution(solution.Solution):
         return self.countKConstraintSubstrings(*test_input)
 
     def countKConstraintSubstrings(self, s: str, k: int) -> int:
-        pass
-
+        ans = left = 0
+        counts = [0, 0]
+        for i, c in enumerate(s):
+            counts[ord(c) & 1] += 1
+            while counts[0] > k and counts[1] > k:
+                counts[ord(s[left]) & 1] -= 1
+                left += 1
+            ans += i - left + 1
+        return ans
