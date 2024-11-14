8cc9312a73a77102dba1d37f2211f9d5a710240d
benhao <qubenhao2@gmail.com>
2024-11-14 20:08:06 +0800

    test: 3249 solution
    py

--- problems/problems_3249/solution.py
+++ problems/problems_3249/solution.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import solution
 from typing import *
 
@@ -7,5 +9,28 @@ class Solution(solution.Solution):
         return self.countGoodNodes(test_input)
 
     def countGoodNodes(self, edges: List[List[int]]) -> int:
-        pass
+        n = len(edges) + 1
+        g = [[] for _ in range(n)]
+        for x, y in edges:
+            g[x].append(y)
+            g[y].append(x)
 
+        ans = 0
+
+        def dfs(x: int, fa: int) -> int:
+            size, sz0, ok = 1, 0, True
+            for y in g[x]:
+                if y == fa:
+                    continue  # 不能递归到父节点
+                sz = dfs(y, x)
+                if sz0 == 0:
+                    sz0 = sz  # 记录第一个儿子子树的大小
+                elif sz != sz0:  # 存在大小不一样的儿子子树
+                    ok = False  # 注意不能 break，其他子树 y 仍然要递归
+                size += sz
+            nonlocal ans
+            ans += ok
+            return size
+
+        dfs(0, -1)
+        return ans
--- problems/problems_3249/testcase.py
+++ problems/problems_3249/testcase.py
@@ -10,6 +10,7 @@ class Testcase(testcase.Testcase):
 		self.testcases.append(case(Input=[[0, 1], [0, 2], [1, 3], [1, 4], [2, 5], [2, 6]], Output=7))
 		self.testcases.append(case(Input=[[0, 1], [1, 2], [2, 3], [3, 4], [0, 5], [1, 6], [2, 7], [3, 8]], Output=6))
 		self.testcases.append(case(Input=[[0, 1], [1, 2], [1, 3], [1, 4], [0, 5], [5, 6], [6, 7], [7, 8], [0, 9], [9, 10], [9, 12], [10, 11]], Output=12))
+		self.testcases.append(case(Input=[[2,0],[4,2],[1,2],[3,1],[5,1]], Output=5))
 
 	def get_testcases(self):
 		return self.testcases
