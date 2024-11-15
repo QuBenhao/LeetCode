19f988a2cffbb6cef2df948a8de8696a95bf535f
benhao <qubenhao2@gmail.com>
2024-11-15 22:35:17 +0800

    test: 3239 solution
    py

--- problems/problems_3239/solution.py
+++ problems/problems_3239/solution.py
@@ -7,5 +7,16 @@ class Solution(solution.Solution):
         return self.minFlips(test_input)
 
     def minFlips(self, grid: List[List[int]]) -> int:
-        pass
+        diff_row = 0
+        for row in grid:
+            for j in range(len(row) // 2):
+                if row[j] != row[-1 - j]:
+                    diff_row += 1
 
+        diff_col = 0
+        for col in zip(*grid):
+            for i in range(len(grid) // 2):
+                if col[i] != col[-1 - i]:
+                    diff_col += 1
+
+        return min(diff_row, diff_col)
