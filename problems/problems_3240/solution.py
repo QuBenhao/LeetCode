e886e357d2ab07c6e91eac44394ddc270d225674
GitHub Action <action@github.com>
2024-11-16 14:02:19 +0000

    test: [20241116] Add daily problem solution

--- problems/problems_3240/solution.py
+++ problems/problems_3240/solution.py
@@ -6,6 +6,34 @@ class Solution(solution.Solution):
     def solve(self, test_input=None):
         return self.minFlips(test_input)
 
-    def minFlips(self, grid: List[List[int]]) -> int:
-        pass
+    def minFlips(self, a: List[List[int]]) -> int:
+        m, n = len(a), len(a[0])
+        ans = 0
+        for i in range(m // 2):
+            row, row2 = a[i], a[-1 - i]
+            for j in range(n // 2):
+                cnt1 = row[j] + row[-1 - j] + row2[j] + row2[-1 - j]
+                ans += min(cnt1, 4 - cnt1)  # 全为 1 或全为 0
 
+        if m % 2 and n % 2:
+            # 正中间的数必须是 0
+            ans += a[m // 2][n // 2]
+
+        diff = cnt1 = 0
+        if m % 2:
+            # 统计正中间这一排
+            row = a[m // 2]
+            for j in range(n // 2):
+                if row[j] != row[-1 - j]:
+                    diff += 1
+                else:
+                    cnt1 += row[j] * 2
+        if n % 2:
+            # 统计正中间这一列
+            for i in range(m // 2):
+                if a[i][n // 2] != a[-1 - i][n // 2]:
+                    diff += 1
+                else:
+                    cnt1 += a[i][n // 2] * 2
+
+        return ans + (diff if diff else cnt1 % 4)        
--- problems/problems_LCR_077/solution.py
+++ problems/problems_LCR_077/solution.py
@@ -17,5 +17,62 @@ class Solution(solution.Solution):
         return linked_list_to_list(res)
 
     def sortList(self, head: ListNode) -> ListNode:
-        pass
-
+        def merge(head1: ListNode, head2: ListNode) -> ListNode:
+            dummyHead = ListNode(0)
+            temp, temp1, temp2 = dummyHead, head1, head2
+            while temp1 and temp2:
+                if temp1.val <= temp2.val:
+                    temp.next = temp1
+                    temp1 = temp1.next
+                else:
+                    temp.next = temp2
+                    temp2 = temp2.next
+                temp = temp.next
+            if temp1:
+                temp.next = temp1
+            elif temp2:
+                temp.next = temp2
+            return dummyHead.next
+        
+        if not head:
+            return head
+        
+        length = 0
+        node = head
+        while node:
+            length += 1
+            node = node.next
+        
+        dummyHead = ListNode(0, head)
+        subLength = 1
+        while subLength < length:
+            prev, curr = dummyHead, dummyHead.next
+            while curr:
+                head1 = curr
+                for i in range(1, subLength):
+                    if curr.next:
+                        curr = curr.next
+                    else:
+                        break
+                head2 = curr.next
+                curr.next = None
+                curr = head2
+                for i in range(1, subLength):
+                    if curr and curr.next:
+                        curr = curr.next
+                    else:
+                        break
+                
+                succ = None
+                if curr:
+                    succ = curr.next
+                    curr.next = None
+                
+                merged = merge(head1, head2)
+                prev.next = merged
+                while prev.next:
+                    prev = prev.next
+                curr = succ
+            subLength <<= 1
+        
+        return dummyHead.next
