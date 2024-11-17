f8b13db5d6ce2572267e68e47bea0c5adecb5fb2
benhao <qubenhao2@gmail.com>
2024-11-17 17:08:31 +0800

    test: 825, LCR 072 solution
    py

--- problems/problems_825/solution.py
+++ problems/problems_825/solution.py
@@ -1,3 +1,5 @@
+from bisect import bisect_left
+
 import solution
 from typing import *
 
@@ -7,5 +9,16 @@ class Solution(solution.Solution):
         return self.numFriendRequests(test_input)
 
     def numFriendRequests(self, ages: List[int]) -> int:
-        pass
+        cnt = [0] * 121
+        for age in ages:
+            cnt[age] += 1
 
+        ans = cnt_window = age_y = 0
+        for age_x, c in enumerate(cnt):
+            cnt_window += c
+            if age_y * 2 <= age_x + 14:  # 不能发送好友请求
+                cnt_window -= cnt[age_y]
+                age_y += 1
+            if cnt_window:  # 存在可以发送好友请求的用户
+                ans += c * cnt_window - c
+        return ans
--- problems/problems_LCR_072/solution.py
+++ problems/problems_LCR_072/solution.py
@@ -7,5 +7,15 @@ class Solution(solution.Solution):
         return self.mySqrt(test_input)
 
     def mySqrt(self, x: int) -> int:
-        pass
-
+        if x == 0:
+            return 0
+        left, right = 1, x
+        while left < right:
+            mid = left + (right - left) // 2
+            if mid > x // mid:
+                right = mid
+            else:
+                if mid + 1 > x // (mid + 1):
+                    return mid
+                left = mid + 1
+        return left
