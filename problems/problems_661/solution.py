ebf05eeb9669075725e1a9d93f26bdcfc3d9d254
benhao <qubenhao2@gmail.com>
2024-11-18 21:40:24 +0800

    test: 661 solution
    py

--- problems/problems_661/solution.py
+++ problems/problems_661/solution.py
@@ -7,5 +7,15 @@ class Solution(solution.Solution):
         return self.imageSmoother(test_input)
 
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
-        pass
-
+        m, n = len(img), len(img[0])
+        ans = [[0] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                count = 0
+                for x in range(i - 1, i + 2):
+                    for y in range(j - 1, j + 2):
+                        if 0 <= x < m and 0 <= y < n:
+                            ans[i][j] += img[x][y]
+                            count += 1
+                ans[i][j] //= count
+        return ans
