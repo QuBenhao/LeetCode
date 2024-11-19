f77398edcb609bcf7c27ee0fdbb1a085a69405f8
benhao <qubenhao2@gmail.com>
2024-11-19 20:48:13 +0800

    test: 3243 solution
    py

--- problems/problems_3243/solution.py
+++ problems/problems_3243/solution.py
@@ -1,3 +1,5 @@
+from math import inf
+
 import solution
 from typing import *
 
@@ -7,5 +9,14 @@ class Solution(solution.Solution):
         return self.shortestDistanceAfterQueries(*test_input)
 
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
-        pass
-
+        frm = [[] for _ in range(n)]
+        f = list(range(n))
+        ans = []
+        for l, r in queries:
+            frm[r].append(l)
+            if f[l] + 1 < f[r]:
+                f[r] = f[l] + 1
+                for i in range(r + 1, n):
+                    f[i] = min(f[i], f[i - 1] + 1, min((f[j] for j in frm[i]), default=inf) + 1)
+            ans.append(f[-1])
+        return ans
