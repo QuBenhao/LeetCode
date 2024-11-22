9d8ab08d481877c3596943c1675d4d48ddcb7d1a
GitHub Action <action@github.com>
2024-11-22 15:01:03 +0000

    test: [20241122] Add daily problem solution

--- problems/problems_3233/solution.py
+++ problems/problems_3233/solution.py
@@ -7,5 +7,14 @@ class Solution(solution.Solution):
         return self.nonSpecialCount(*test_input)
 
     def nonSpecialCount(self, l: int, r: int) -> int:
-        pass
+        n = int(math.sqrt(r))
+        v = [0] * (n + 1)
+        res = r - l + 1
+        for i in range(2, n + 1):
+            if v[i] == 0:
+                if l <= i * i <= r:
+                    res -= 1
+                for j in range(i * 2, n + 1, i):
+                    v[j] = 1
+        return res
 
