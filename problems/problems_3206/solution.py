807816f72776e7acd79b9eca05ba0cc2443c0d4e
benhao <qubenhao2@gmail.com>
2024-11-26 21:32:12 +0800

    test: 3206 solution
    py

--- problems/problems_3206/solution.py
+++ problems/problems_3206/solution.py
@@ -7,5 +7,8 @@ class Solution(solution.Solution):
         return self.numberOfAlternatingGroups(test_input)
 
     def numberOfAlternatingGroups(self, colors: List[int]) -> int:
-        pass
-
+        ans, n = 0, len(colors)
+        for i in range(n):
+            if colors[i] != colors[i - 1] and colors[i] != colors[(i + 1) % n]:
+                ans += 1
+        return ans
