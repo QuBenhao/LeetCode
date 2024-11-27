f4f238ea0b0a2ea3854867b3bb3c88def6f1fc5f
benhao <qubenhao2@gmail.com>
2024-11-27 20:39:36 +0800

    test: 3208 solution
    py

--- problems/problems_3208/solution.py
+++ problems/problems_3208/solution.py
@@ -7,5 +7,13 @@ class Solution(solution.Solution):
         return self.numberOfAlternatingGroups(*test_input)
 
     def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
-        pass
-
+        n = len(colors)
+        ans, cnt = 0, 0
+        for i in range(1, n + k - 1):
+            if colors[i % n] == colors[(i - 1) % n]:
+                cnt = 0
+            else:
+                cnt += 1
+            if cnt >= k - 1:
+                ans += 1
+        return ans
--- problems/problems_3208/testcase.py
+++ problems/problems_3208/testcase.py
@@ -10,6 +10,7 @@ class Testcase(testcase.Testcase):
 		self.testcases.append(case(Input=[[0, 1, 0, 1, 0], 3], Output=3))
 		self.testcases.append(case(Input=[[0, 1, 0, 0, 1, 0, 1], 6], Output=2))
 		self.testcases.append(case(Input=[[1, 1, 0, 1], 4], Output=0))
+		self.testcases.append(case(Input=[[0,1,1],3], Output=1))
 
 	def get_testcases(self):
 		return self.testcases
