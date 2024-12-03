50dfe037ce3ebffaef26858fedc5d782358cea36
benhao <qubenhao2@gmail.com>
2024-12-03 21:53:51 +0800

    test: 3274 solution
    py

--- problems/problems_3274/solution.py
+++ problems/problems_3274/solution.py
@@ -7,5 +7,4 @@ class Solution(solution.Solution):
         return self.checkTwoChessboards(*test_input)
 
     def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
-        pass
-
+        return (ord(coordinate1[0]) + ord(coordinate1[1])) % 2 == (ord(coordinate2[0]) + ord(coordinate2[1])) % 2
