7d53141e5221b38b5bd3b1d8203f5545e44d304d
benhao <qubenhao2@gmail.com>
2024-12-12 22:28:23 +0800

    test: 2931 solution
    py

--- problems/problems_2931/solution.py
+++ problems/problems_2931/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+import heapq
 
 
 class Solution(solution.Solution):
@@ -7,5 +8,16 @@ class Solution(solution.Solution):
         return self.maxSpending(test_input)
 
     def maxSpending(self, values: List[List[int]]) -> int:
-        pass
-
+        m, n = len(values), len(values[0])
+        pq = []
+        for i in range(m):
+            heapq.heappush(pq, (values[i][-1], i, n - 1))
+        ans, d = 0, 1
+        while pq:
+            val, i, j = heapq.heappop(pq)
+            ans += d * val
+            d += 1
+            if j == 0:
+                continue
+            heapq.heappush(pq, (values[i][j - 1], i, j - 1))
+        return ans
