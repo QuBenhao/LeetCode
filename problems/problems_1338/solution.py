90ecb6cf14cdcbf48b9a002e1560b01210fb996e
benhao <qubenhao2@gmail.com>
2024-12-15 10:24:10 +0800

    test: 1338 solution
    py

--- problems/problems_1338/solution.py
+++ problems/problems_1338/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+from collections import Counter
 
 
 class Solution(solution.Solution):
@@ -7,5 +8,12 @@ class Solution(solution.Solution):
         return self.minSetSize(test_input)
 
     def minSetSize(self, arr: List[int]) -> int:
-        pass
-
+        counter = Counter(arr)
+        n = len(arr)
+        cur = n
+        ans = 0
+        sorted_counter = sorted(counter.items(), key=lambda x: -x[1])
+        while cur > n // 2:
+            cur -= sorted_counter[ans][1]
+            ans += 1
+        return ans
