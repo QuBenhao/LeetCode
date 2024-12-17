ca7d2e79bf6fb590effcdbadb7e74cb140e7cbca
benhao <qubenhao2@gmail.com>
2024-12-17 21:01:11 +0800

    test: 3291 solution
    py

--- problems/problems_3291/solution.py
+++ problems/problems_3291/solution.py
@@ -1,3 +1,5 @@
+from random import randint
+
 import solution
 from typing import *
 
@@ -7,5 +9,41 @@ class Solution(solution.Solution):
         return self.minValidStrings(*test_input)
 
     def minValidStrings(self, words: List[str], target: str) -> int:
-        pass
+        n = len(target)
 
+        # 多项式字符串哈希（方便计算子串哈希值）
+        # 哈希函数 hash(s) = s[0] * BASE^(n-1) + s[1] * BASE^(n-2) + ... + s[n-2] * BASE + s[n-1]
+        MOD = 1_070_777_777
+        BASE = randint(8 * 10 ** 8, 9 * 10 ** 8)  # 随机 BASE，防止 hack
+        pow_base = [1] + [0] * n  # pow_base[i] = BASE^i
+        pre_hash = [0] * (n + 1)  # 前缀哈希值 pre_hash[i] = hash(s[:i])
+        for i, b in enumerate(target):
+            pow_base[i + 1] = pow_base[i] * BASE % MOD
+            pre_hash[i + 1] = (pre_hash[i] * BASE + ord(b)) % MOD  # 秦九韶算法计算多项式哈希
+
+        # 计算子串 target[l:r] 的哈希值，注意这是左闭右开区间 [l,r)
+        # 计算方法类似前缀和
+        def sub_hash(l: int, r: int) -> int:
+            return (pre_hash[r] - pre_hash[l] * pow_base[r - l]) % MOD
+
+        # 保存每个 words[i] 的每个前缀的哈希值，按照长度分组
+        max_len = max(map(len, words))
+        sets = [set() for _ in range(max_len)]
+        for w in words:
+            h = 0
+            for j, b in enumerate(w):
+                h = (h * BASE + ord(b)) % MOD
+                sets[j].add(h)  # 注意 j 从 0 开始
+
+        ans = 0
+        cur_r = 0  # 已建造的桥的右端点
+        nxt_r = 0  # 下一座桥的右端点的最大值
+        for i in range(n):
+            while nxt_r < n and nxt_r - i < max_len and sub_hash(i, nxt_r + 1) in sets[nxt_r - i]:
+                nxt_r += 1  # 尽量伸长
+            if i == cur_r:  # 到达已建造的桥的右端点
+                if i == nxt_r:  # 无论怎么造桥，都无法从 i 到 i+1
+                    return -1
+                cur_r = nxt_r  # 建造下一座桥
+                ans += 1
+        return ans
