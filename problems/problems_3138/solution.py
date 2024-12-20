4cde8600ae61f75b653c3f0569cf52d3862647c2
benhao <qubenhao2@gmail.com>
2024-12-20 21:06:59 +0800

    test: 3138 solution
    py

--- problems/problems_3138/solution.py
+++ problems/problems_3138/solution.py
@@ -7,5 +7,11 @@ class Solution(solution.Solution):
         return self.minAnagramLength(test_input)
 
     def minAnagramLength(self, s: str) -> int:
-        pass
-
+        n = len(s)
+        for k in range(1, n // 2 + 1):
+            if n % k:
+                continue
+            t = sorted(s[:k])
+            if all(sorted(s[i - k: i]) == t for i in range(k * 2, n + 1, k)):
+                return k
+        return n
