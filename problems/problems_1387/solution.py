f45084b15c719cf13d7c6d504757f748f12145df
benhao <qubenhao2@gmail.com>
2024-12-22 21:36:28 +0800

    test: 1387 solution
    py

--- problems/problems_1387/solution.py
+++ problems/problems_1387/solution.py
@@ -7,5 +7,15 @@ class Solution(solution.Solution):
         return self.getKth(*test_input)
 
     def getKth(self, lo: int, hi: int, k: int) -> int:
-        pass
+        def power(x):
+            if x == 1:
+                return 0
+            if x % 2:
+                return power(3 * x + 1) + 1
+            return power(x // 2) + 1
 
+        power_map = {1: 0}
+        for i in range(lo, hi + 1):
+            power_map[i] = power(i)
+
+        return sorted(range(lo, hi + 1), key=lambda x: (power_map[x], x))[k - 1]
