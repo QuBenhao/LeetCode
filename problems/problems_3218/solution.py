3593c7dd9232f5ee0c2695ea42e69f055ff8ea48
benhao <qubenhao2@gmail.com>
2024-12-25 20:05:53 +0800

    test: 3218 solution
    py

--- problems/problems_3218/solution.py
+++ problems/problems_3218/solution.py
@@ -7,5 +7,14 @@ class Solution(solution.Solution):
         return self.minimumCost(*test_input)
 
     def minimumCost(self, m: int, n: int, horizontalCut: List[int], verticalCut: List[int]) -> int:
-        pass
-
+        horizontalCut.sort()
+        verticalCut.sort()
+        ans = i = j = 0
+        for _ in range(m + n - 2):
+            if j == n - 1 or i < m - 1 and horizontalCut[i] < verticalCut[j]:
+                ans += horizontalCut[i] * (n - j)  # 上下连边
+                i += 1
+            else:
+                ans += verticalCut[j] * (m - i)  # 左右连边
+                j += 1
+        return ans
