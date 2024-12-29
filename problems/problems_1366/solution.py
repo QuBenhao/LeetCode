749877d15b1b02576e8e0e39edd9ee44dfab8b85
benhao <qubenhao2@gmail.com>
2024-12-29 22:27:52 +0800

    test: 1366 solution
    py

--- problems/problems_1366/solution.py
+++ problems/problems_1366/solution.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import solution
 from typing import *
 
@@ -7,5 +9,9 @@ class Solution(solution.Solution):
         return self.rankTeams(test_input)
 
     def rankTeams(self, votes: List[str]) -> str:
-        pass
-
+        m = len(votes[0])
+        counter = defaultdict(lambda: [0] * m)
+        for vote in votes:
+            for i, ch in enumerate(vote):
+                counter[ch][i] -= 1
+        return ''.join(sorted(counter, key=lambda c: (counter[c], c)))
