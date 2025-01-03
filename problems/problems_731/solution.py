e1a5c86e5b454183f1301ee1e3ef73bbe2054d11
benhao <qubenhao2@gmail.com>
2025-01-03 17:11:43 +0800

    test: 731 solution
    py

--- problems/problems_731/solution.py
+++ problems/problems_731/solution.py
@@ -12,8 +12,27 @@ class Solution(solution.Solution):
 
 class MyCalendarTwo:
     def __init__(self):
-        pass
+        self.tree = {}
 
-    def book(self, startTime: int, endTime: int) -> bool:
-        pass
+    def update(self, start: int, end: int, val: int, l: int, r: int, idx: int) -> None:
+        if r < start or end < l:
+            return
+        if start <= l and r <= end:
+            p = self.tree.get(idx, [0, 0])
+            p[0] += val
+            p[1] += val
+            self.tree[idx] = p
+            return
+        mid = (l + r) // 2
+        self.update(start, end, val, l, mid, 2 * idx)
+        self.update(start, end, val, mid + 1, r, 2 * idx + 1)
+        p = self.tree.get(idx, [0, 0])
+        p[0] = p[1] + max(self.tree.get(2 * idx, (0,))[0], self.tree.get(2 * idx + 1, (0,))[0])
+        self.tree[idx] = p
 
+    def book(self, start: int, end: int) -> bool:
+        self.update(start, end - 1, 1, 0, 10 ** 9, 1)
+        if self.tree[1][0] > 2:
+            self.update(start, end - 1, -1, 0, 10 ** 9, 1)
+            return False
+        return True
