86e721393d2a4f08ee66cb8b74be21c71d030fca
benhao <qubenhao2@gmail.com>
2025-01-04 21:01:24 +0800

    test: 732 solution
    py

--- problems/problems_732/solution.py
+++ problems/problems_732/solution.py
@@ -1,6 +1,7 @@
 import solution
 from typing import *
 from python.object_libs import call_method
+from collections import defaultdict
 
 
 class Solution(solution.Solution):
@@ -12,8 +13,21 @@ class Solution(solution.Solution):
 
 class MyCalendarThree:
     def __init__(self):
-        pass
+        self.tree = defaultdict(int)
+        self.lazy = defaultdict(int)
 
-    def book(self, startTime: int, endTime: int) -> int:
-        pass
+    def update(self, start: int, end: int, l: int, r: int, idx: int):
+        if r < start or end < l:
+            return
+        if start <= l and r <= end:
+            self.tree[idx] += 1
+            self.lazy[idx] += 1
+        else:
+            mid = (l + r) // 2
+            self.update(start, end, l, mid, idx * 2)
+            self.update(start, end, mid + 1, r, idx * 2 + 1)
+            self.tree[idx] = self.lazy[idx] + max(self.tree[idx * 2], self.tree[idx * 2 + 1])
 
+    def book(self, start: int, end: int) -> int:
+        self.update(start, end - 1, 0, 10 ** 9, 1)
+        return self.tree[1]
