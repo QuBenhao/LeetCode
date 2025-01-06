033ac89fbadbd2834728d2547983ce7075635596
benhao <qubenhao2@gmail.com>
2025-01-06 21:01:31 +0800

    test: 2274 solution
    py

--- problems/problems_2274/solution.py
+++ problems/problems_2274/solution.py
@@ -1,3 +1,5 @@
+from itertools import pairwise
+
 import solution
 from typing import *
 
@@ -7,5 +9,8 @@ class Solution(solution.Solution):
         return self.maxConsecutive(*test_input)
 
     def maxConsecutive(self, bottom: int, top: int, special: List[int]) -> int:
-        pass
-
+        special.sort()
+        ans = max(special[0] - bottom, top - special[-1])
+        for a, b in pairwise(special):
+            ans = max(ans, b - a - 1)
+        return ans
