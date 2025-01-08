fbf1f989fe43bf4668cd9a9a8cc6dc61f7fffa84
benhao <qubenhao2@gmail.com>
2025-01-08 21:31:09 +0800

    test: 2264 solution
    py

--- problems/problems_2264/solution.py
+++ problems/problems_2264/solution.py
@@ -7,5 +7,8 @@ class Solution(solution.Solution):
         return self.largestGoodInteger(test_input)
 
     def largestGoodInteger(self, num: str) -> str:
-        pass
-
+        ans = ""
+        for i in range(len(num) - 2):
+            if num[i] == num[i + 1] == num[i + 2]:
+                ans = max(ans, num[i:i + 3])
+        return ans
