67d34c2f4feb9fedd0937e39fff12005cd554142
benhao <qubenhao2@gmail.com>
2025-01-13 21:08:09 +0800

    test: 2270 solution
    py

--- problems/problems_2270/solution.py
+++ problems/problems_2270/solution.py
@@ -7,5 +7,11 @@ class Solution(solution.Solution):
         return self.waysToSplitArray(test_input)
 
     def waysToSplitArray(self, nums: List[int]) -> int:
-        pass
-
+        presum = [0] * (len(nums) + 1)
+        for i in range(len(nums)):
+            presum[i + 1] = presum[i] + nums[i]
+        ans, s = 0, presum[-1] / 2
+        for i in range(len(nums) - 1):
+            if presum[i + 1] >= s:
+                ans += 1
+        return ans
--- problems/problems_2270/testcase.py
+++ problems/problems_2270/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=[10, 4, -8, 7], Output=2))
 		self.testcases.append(case(Input=[2, 3, 1, 0], Output=2))
+		self.testcases.append(case(Input=[0,0], Output=1))
 
 	def get_testcases(self):
 		return self.testcases
