6dcb04b20dcbd34f630ccbbf8218bbde50572aee
benhao <qubenhao2@gmail.com>
2025-01-17 21:02:05 +0800

    test: 3097 solution
    py

--- problems/problems_3097/solution.py
+++ problems/problems_3097/solution.py
@@ -1,3 +1,5 @@
+from math import inf
+
 import solution
 from typing import *
 
@@ -7,5 +9,15 @@ class Solution(solution.Solution):
         return self.minimumSubarrayLength(*test_input)
 
     def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
-        pass
+        ans = inf
+        for i, x in enumerate(nums):
+            if x >= k:
+                return 1
+            j = i - 1
+            while j >= 0 and nums[j] | x != nums[j]:
+                nums[j] |= x
+                if nums[j] >= k:
+                    ans = min(ans, i - j + 1)
+                j -= 1
+        return ans if ans < inf else -1
 
