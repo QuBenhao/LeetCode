ab676a7f716562228234b7182725dbab8de084db
benhao <qubenhao2@gmail.com>
2025-02-04 22:13:40 +0800

    test: 922 solution
    py

--- problems/problems_922/solution.py
+++ problems/problems_922/solution.py
@@ -7,5 +7,13 @@ class Solution(solution.Solution):
         return self.sortArrayByParityII(test_input)
 
     def sortArrayByParityII(self, nums: List[int]) -> List[int]:
-        pass
-
+        n = len(nums)
+        i, j = 0, 1
+        while i < n and j < n:
+            while i < n and nums[i] % 2 == 0:
+                i += 2
+            while j < n and nums[j] % 2:
+                j += 2
+            if i < n and j < n:
+                nums[i], nums[j] = nums[j], nums[i]
+        return nums
