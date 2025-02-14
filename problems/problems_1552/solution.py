44b7369e6a52067bbd8e6a9fe16ba9aad0198c90
benhao <qubenhao2@gmail.com>
2025-02-14 17:30:40 +0800

    test: 1552 solution
    py

--- problems/problems_1552/solution.py
+++ problems/problems_1552/solution.py
@@ -7,5 +7,23 @@ class Solution(solution.Solution):
         return self.maxDistance(*test_input)
 
     def maxDistance(self, position: List[int], m: int) -> int:
-        pass
+        position.sort()
+        n = len(position)
 
+        def check(d):
+            cnt = 1
+            pre = position[0]
+            for i in range(1, n):
+                if position[i] - pre >= d:
+                    cnt += 1
+                    pre = position[i]
+            return cnt >= m
+
+        l, r = 0, position[-1] - position[0]
+        while l < r:
+            mid = (l + r + 1) // 2
+            if check(mid):
+                l = mid
+            else:
+                r = mid - 1
+        return l
