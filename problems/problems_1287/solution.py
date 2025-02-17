c3465afaa9f8d977641d20f02d6c09ef223043f8
benhao <qubenhao2@gmail.com>
2025-02-17 22:31:11 +0800

    test: 1287 solution
    py

--- problems/problems_1287/solution.py
+++ problems/problems_1287/solution.py
@@ -7,5 +7,14 @@ class Solution(solution.Solution):
         return self.findSpecialInteger(test_input)
 
     def findSpecialInteger(self, arr: List[int]) -> int:
-        pass
+        n = len(arr)
+        count = 1
+        for i in range(1, n):
+            if arr[i] == arr[i - 1]:
+                count += 1
+                if count > n // 4:
+                    return arr[i]
+            else:
+                count = 1
+        return arr[0]
 
