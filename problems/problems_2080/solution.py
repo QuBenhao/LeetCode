611c7f636fda46c7a3d51dd1af570a83fecac55d
benhao <qubenhao2@gmail.com>
2025-02-18 22:49:46 +0800

    test: 2080 solution
    py

--- problems/problems_2080/solution.py
+++ problems/problems_2080/solution.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from bisect import bisect_left, bisect_right
+
 import solution
 from typing import *
 from python.object_libs import call_method
@@ -12,8 +15,9 @@ class Solution(solution.Solution):
 
 class RangeFreqQuery:
     def __init__(self, arr: List[int]):
-        pass
+        self.map = defaultdict(list)
+        for i, num in enumerate(arr):
+            self.map[num].append(i)
 
     def query(self, left: int, right: int, value: int) -> int:
-        pass
-
+        return bisect_right(self.map[value], right) - bisect_left(self.map[value], left)
