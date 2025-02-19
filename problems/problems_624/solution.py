c5f8d114ae35b00d7452aa115100fee5b1759eb4
benhao <qubenhao2@gmail.com>
2025-02-19 21:43:14 +0800

    test: 624 solution
    py

--- problems/problems_624/solution.py
+++ problems/problems_624/solution.py
@@ -1,11 +1,16 @@
 import solution
 from typing import *
-
+from math import inf
 
 class Solution(solution.Solution):
     def solve(self, test_input=None):
         return self.maxDistance(test_input)
 
     def maxDistance(self, arrays: List[List[int]]) -> int:
-        pass
-
+        min_val, max_val = inf, -inf
+        ans = 0
+        for arr in arrays:
+            ans = max(ans, max(arr) - min_val, max_val - min(arr))
+            min_val = min(min_val, arr[0])
+            max_val = max(max_val, arr[-1])
+        return ans
