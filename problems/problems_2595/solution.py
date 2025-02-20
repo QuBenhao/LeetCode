5f768de5c9c042b807b1404c846d52b1a0b90f81
benhao <qubenhao2@gmail.com>
2025-02-20 20:50:21 +0800

    test: 2595 solution

--- problems/problems_2595/solution.py
+++ problems/problems_2595/solution.py
@@ -7,5 +7,13 @@ class Solution(solution.Solution):
         return self.evenOddBit(test_input)
 
     def evenOddBit(self, n: int) -> List[int]:
-        pass
-
+        a, b, length = 0, 0, 0
+        while n:
+            if n & 1:
+                if length & 1:
+                    b += 1
+                else:
+                    a += 1
+            length += 1
+            n >>= 1
+        return [a,b]
--- problems/problems_2595/testcase.py
+++ problems/problems_2595/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=50, Output=[1, 2]))
 		self.testcases.append(case(Input=2, Output=[0, 1]))
+		self.testcases.append(case(Input=5, Output=[2,0]))
 
 	def get_testcases(self):
 		return self.testcases
