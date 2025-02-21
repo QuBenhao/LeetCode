98ae0d73dc3b102201c8a8501accf570fbc57861
benhao <qubenhao2@gmail.com>
2025-02-21 22:01:36 +0800

    test: 2209 solution

--- problems/problems_2209/solution.py
+++ problems/problems_2209/solution.py
@@ -1,3 +1,5 @@
+from itertools import accumulate
+
 import solution
 from typing import *
 
@@ -7,5 +9,15 @@ class Solution(solution.Solution):
         return self.minimumWhiteTiles(*test_input)
 
     def minimumWhiteTiles(self, floor: str, numCarpets: int, carpetLen: int) -> int:
-        pass
+        m = len(floor)
+        if numCarpets * carpetLen >= m:
+            return 0
 
+        floor = list(map(int, floor))
+        f = list(accumulate(floor))
+        for i in range(1, numCarpets + 1):
+            nf = [0] * m
+            for j in range(carpetLen * i, m):
+                nf[j] = min(nf[j - 1] + floor[j], f[j - carpetLen])
+            f = nf
+        return f[-1]
