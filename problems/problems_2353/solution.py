4b258f084d55e9e52ed55df58fc48ba638686e38
benhao <qubenhao2@gmail.com>
2025-02-28 22:21:03 +0800

    test: 2353 solution

--- problems/problems_2353/solution.py
+++ problems/problems_2353/solution.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+
+from sortedcontainers import SortedList
+
 import solution
 from typing import *
 from python.object_libs import call_method
@@ -12,11 +16,20 @@ class Solution(solution.Solution):
 
 class FoodRatings:
     def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
-        pass
+        self.food_map = {}
+        self.cuisine_map = defaultdict(SortedList)  # sortedcontainers
+        for food, cuisine, rating in zip(foods, cuisines, ratings):
+            self.food_map[food] = [rating, cuisine]
+            # 取负号，保证 rating 相同时，字典序更小的 food 排在前面
+            self.cuisine_map[cuisine].add((-rating, food))
 
     def changeRating(self, food: str, newRating: int) -> None:
-        pass
+        rating, cuisine = self.food_map[food]
+        sl = self.cuisine_map[cuisine]
+        sl.discard((-rating, food))  # 移除旧数据
+        sl.add((-newRating, food))  # 添加新数据
+        self.food_map[food][0] = newRating  # 更新 food 的 rating
 
     def highestRated(self, cuisine: str) -> str:
-        pass
+        return self.cuisine_map[cuisine][0][1]
 
