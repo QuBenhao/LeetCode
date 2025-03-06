deb0d8c940674cf1c7dbcb00ace80539e0222e25
benhao <qubenhao2@gmail.com>
2025-03-06 20:33:02 +0800

    test: 2588 solution

--- problems/problems_2588/solution.py
+++ problems/problems_2588/solution.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import solution
 from typing import *
 
@@ -7,5 +9,11 @@ class Solution(solution.Solution):
         return self.beautifulSubarrays(test_input)
 
     def beautifulSubarrays(self, nums: List[int]) -> int:
-        pass
-
+        ans = s = 0
+        cnt = defaultdict(int)
+        cnt[0] = 1
+        for x in nums:
+            s ^= x
+            ans += cnt[s]
+            cnt[s] += 1
+        return ans
