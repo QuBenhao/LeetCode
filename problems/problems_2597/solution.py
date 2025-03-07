f12a149950e38a7785bf2094a15a8db49a6da7b2
benhao <qubenhao2@gmail.com>
2025-03-07 20:37:08 +0800

    test: 2597 solution

--- problems/problems_2597/solution.py
+++ problems/problems_2597/solution.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import solution
 from typing import *
 
@@ -7,5 +9,23 @@ class Solution(solution.Solution):
         return self.beautifulSubsets(*test_input)
 
     def beautifulSubsets(self, nums: List[int], k: int) -> int:
-        pass
+        groups = defaultdict(Counter)
+        for x in nums:
+            # 模 k 同余的数分到同一组，记录元素 x 及其出现次数
+            groups[x % k][x] += 1
 
+        ans = 1
+        for cnt in groups.values():
+            # 计算这一组的方案数
+            a = sorted(cnt.items())
+            m = len(a)
+            f = [0] * (m + 1)
+            f[0] = 1
+            f[1] = 1 << a[0][1]
+            for i in range(1, m):
+                if a[i][0] - a[i - 1][0] == k:
+                    f[i + 1] = f[i] + f[i - 1] * ((1 << a[i][1]) - 1)
+                else:
+                    f[i + 1] = f[i] << a[i][1]
+            ans *= f[m]  # 每组方案数相乘
+        return ans - 1  # 去掉空集
