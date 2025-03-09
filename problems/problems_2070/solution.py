15ad0508c6543281b9de79410b6184c54fa2a82f
benhao <qubenhao2@gmail.com>
2025-03-09 14:59:26 +0800

    test: 2070 solution

--- problems/problems_2070/solution.py
+++ problems/problems_2070/solution.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import solution
 from typing import *
 
@@ -7,5 +9,16 @@ class Solution(solution.Solution):
         return self.maximumBeauty(*test_input)
 
     def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
-        pass
+        items.sort(key=lambda item: item[0])
+        idx = sorted(range(len(queries)), key=lambda i: queries[i])
 
+        ans = [0] * len(queries)
+        max_beauty = j = 0
+        for i in idx:
+            q = queries[i]
+            # 增量地遍历满足 queries[i-1] < price <= queries[i] 的物品
+            while j < len(items) and items[j][0] <= q:
+                max_beauty = max(max_beauty, items[j][1])
+                j += 1
+            ans[i] = max_beauty
+        return ans
