e7953cc7b666dd473faa17e44ce8246aa424fe87
benhao <qubenhao2@gmail.com>
2025-03-10 21:51:29 +0800

    test: 2269 solution

--- problems/problems_2269/solution.py
+++ problems/problems_2269/solution.py
@@ -7,5 +7,15 @@ class Solution(solution.Solution):
         return self.divisorSubstrings(*test_input)
 
     def divisorSubstrings(self, num: int, k: int) -> int:
-        pass
-
+        num_str = str(num)
+        cur = 0
+        ans = 0
+        for i in range(k):
+            cur = cur * 10 + int(num_str[i])
+        if cur and num % cur == 0:
+            ans += 1
+        for i in range(k, len(num_str)):
+            cur = cur * 10 + int(num_str[i]) - int(num_str[i - k]) * 10 ** k
+            if cur and num % cur == 0:
+                ans += 1
+        return ans
