06c0d1e833bf271537728eec943173e216656bf4
benhao <qubenhao2@gmail.com>
2025-03-11 21:51:25 +0800

    test: 2012 solution

--- problems/problems_2012/solution.py
+++ problems/problems_2012/solution.py
@@ -7,5 +7,16 @@ class Solution(solution.Solution):
         return self.sumOfBeauties(test_input)
 
     def sumOfBeauties(self, nums: List[int]) -> int:
-        pass
-
+        ans = 0
+        n = len(nums)
+        suf_min = [nums[-1]] * n
+        for i in range(n - 2, 0, -1):
+            suf_min[i] = min(nums[i], suf_min[i + 1])
+        pre_max = nums[0]
+        for i in range(1, n - 1):
+            if pre_max < nums[i] < suf_min[i + 1]:
+                ans += 2
+            elif nums[i - 1] < nums[i] < nums[i + 1]:
+                ans += 1
+            pre_max = max(pre_max, nums[i])
+        return ans
