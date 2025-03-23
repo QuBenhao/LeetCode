fb1262f078d8c64ad8edea4b564dc2cc6b05ca03
benhao <qubenhao2@gmail.com>
2025-03-23 11:53:33 +0800

    test: 2116 solution

--- problems/problems_2116/solution.py
+++ problems/problems_2116/solution.py
@@ -7,5 +7,19 @@ class Solution(solution.Solution):
         return self.canBeValid(*test_input)
 
     def canBeValid(self, s: str, locked: str) -> bool:
-        pass
-
+        if len(s) % 2:
+            return False
+        mn = mx = 0
+        for b, lock in zip(s, locked):
+            if lock == '1':  # 不能改
+                d = 1 if b == '(' else -1
+                mx += d
+                if mx < 0:  # c 不能为负
+                    return False
+                mn += d
+            else:  # 可以改
+                mx += 1  # 改成左括号，c 加一
+                mn -= 1  # 改成右括号，c 减一
+            if mn < 0:  # c 不能为负
+                mn = 1  # 此时 c 的取值范围都是奇数，最小的奇数是 1
+        return mn == 0  # 说明最终 c 能是 0
