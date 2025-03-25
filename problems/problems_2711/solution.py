ebca91d5c67ab3da27a48455e3f3bccd1da866b2
benhao <qubenhao2@gmail.com>
2025-03-25 22:00:26 +0800

    test: 2711 solution

--- problems/problems_2711/solution.py
+++ problems/problems_2711/solution.py
@@ -7,5 +7,22 @@ class Solution(solution.Solution):
         return self.differenceOfDistinctValues(test_input)
 
     def differenceOfDistinctValues(self, grid: List[List[int]]) -> List[List[int]]:
-        pass
+        m, n = len(grid), len(grid[0])
+        ans = [[0] * n for _ in range(m)]
 
+        for k in range(1, m + n):
+            min_j = max(n - k, 0)
+            max_j = min(m + n - 1 - k, n - 1)
+
+            st = 0
+            for j in range(min_j, max_j + 1):
+                i = k + j - n
+                ans[i][j] = st.bit_count()  # 计算 st 中 1 的个数
+                st |= 1 << grid[i][j]  # 把 grid[i][j] 加到 st 中
+
+            st = 0
+            for j in range(max_j, min_j - 1, -1):
+                i = k + j - n
+                ans[i][j] = abs(ans[i][j] - st.bit_count())
+                st |= 1 << grid[i][j]
+        return ans
--- problems/problems_2711/testcase.py
+++ problems/problems_2711/testcase.py
@@ -7,8 +7,8 @@ case = namedtuple("Testcase", ["Input", "Output"])
 class Testcase(testcase.Testcase):
 	def __init__(self):
 		self.testcases = []
-		self.testcases.append(case(Input=[[1, 2, 3], [3, 1, 5], [3, 2, 1]], Output=None))
-		self.testcases.append(case(Input=[[1]], Output=None))
+		self.testcases.append(case(Input=[[1, 2, 3], [3, 1, 5], [3, 2, 1]], Output=[[1,1,0],[1,0,1],[0,1,1]]))
+		self.testcases.append(case(Input=[[1]], Output=[[0]]))
 
 	def get_testcases(self):
 		return self.testcases
