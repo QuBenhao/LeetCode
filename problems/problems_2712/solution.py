7ea6da1e0a2fb3d35856481fa9e871ecf171c90f
benhao <qubenhao2@gmail.com>
2025-03-27 22:30:45 +0800

    test: 2712 solution
    python

--- problems/problems_2712/solution.py
+++ problems/problems_2712/solution.py
@@ -7,5 +7,9 @@ class Solution(solution.Solution):
         return self.minimumCost(test_input)
 
     def minimumCost(self, s: str) -> int:
-        pass
-
+        n = len(s)
+        ans = 0
+        for i in range(1, n):
+            if s[i - 1] != s[i]:
+                ans += min(i, n - i)
+        return ans
