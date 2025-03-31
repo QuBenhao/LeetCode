2d3371685e905ed00895ce16dc9882f5fb15141d
benhao <qubenhao2@gmail.com>
2025-03-31 20:02:46 +0800

    test: 2278 solution
    java

--- problems/problems_2278/Solution.java
+++ problems/problems_2278/Solution.java
@@ -7,7 +7,13 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public int percentageLetter(String s, char letter) {
-        
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == letter) {
+                count++;
+            }
+        }
+        return (int) Math.floor((double) count / s.length() * 100);
     }
 
     @Override
