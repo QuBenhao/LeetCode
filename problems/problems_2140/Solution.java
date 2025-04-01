355d1c790cf30d802e0ad2026392fb3e037ccb7b
benhao <qubenhao2@gmail.com>
2025-04-01 20:54:32 +0800

    test: 2140 solution
    java, rs

--- problems/problems_2140/Solution.java
+++ problems/problems_2140/Solution.java
@@ -7,7 +7,14 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public long mostPoints(int[][] questions) {
-        
+        int n = questions.length;
+        long[] dp = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+            int next = Math.min(n, i + questions[i][1] + 1);
+            dp[next] = Math.max(dp[next], dp[i] + questions[i][0]);
+        }
+        return dp[n];
     }
 
     @Override
--- problems/problems_2140/solution.rs
+++ problems/problems_2140/solution.rs
@@ -4,7 +4,16 @@ pub struct Solution;
 
 impl Solution {
     pub fn most_points(questions: Vec<Vec<i32>>) -> i64 {
-        
+		use std::cmp::{max, min};
+        let n = questions.len();
+		let mut dp = vec![0i64; n + 1];
+		for i in 0..n {
+			let (points, brainpower) = (questions[i][0], questions[i][1]);
+			dp[i + 1] = max(dp[i + 1], dp[i]);
+			let next = min(i + brainpower as usize + 1, n);
+			dp[next] = max(dp[next], dp[i] + points as i64);
+		}
+		dp[n]
     }
 }
 
