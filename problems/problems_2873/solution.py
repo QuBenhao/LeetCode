5959a5eb09ff2af7b1867b7aab90c1f5e5b8b09b
benhao <qubenhao2@gmail.com>
2025-04-02 20:29:09 +0800

    test: 2873 solution
    java, rs

--- problems/problems_2873/solution.py
+++ problems/problems_2873/solution.py
@@ -7,5 +7,13 @@ class Solution(solution.Solution):
         return self.maximumTripletValue(test_input)
 
     def maximumTripletValue(self, nums: List[int]) -> int:
-        pass
-
+        n = len(nums)
+        right_max = [nums[-1]] * n
+        ans = 0
+        for i in range(n - 2, 0, -1):
+            right_max[i] = max(right_max[i + 1], nums[i])
+        pre_max = nums[0]
+        for j in range(1, n - 1):
+            ans = max(ans, (pre_max - nums[j]) * right_max[j + 1])
+            pre_max = max(pre_max, nums[j])
+        return ans
--- problems/problems_2873/testcase.py
+++ problems/problems_2873/testcase.py
@@ -10,6 +10,8 @@ class Testcase(testcase.Testcase):
 		self.testcases.append(case(Input=[12, 6, 1, 2, 7], Output=77))
 		self.testcases.append(case(Input=[1, 10, 3, 4, 19], Output=133))
 		self.testcases.append(case(Input=[1, 2, 3], Output=0))
+		self.testcases.append(case(Input=[8,6,3,13,2,12,19,5,19,6,10,11,9], Output=266))
+		self.testcases.append(case(Input=[10,13,6,2], Output=14))
 
 	def get_testcases(self):
 		return self.testcases
