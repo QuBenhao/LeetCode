d80adc56400caf209c626bf1cff8a19e37c576b0
benhao <qubenhao2@gmail.com>
2025-04-02 20:37:02 +0800

    test: 2873 solution
    py, java, rs

--- problems/problems_2873/solution.rs
+++ problems/problems_2873/solution.rs
@@ -4,7 +4,20 @@ pub struct Solution;
 
 impl Solution {
     pub fn maximum_triplet_value(nums: Vec<i32>) -> i64 {
-        
+		use std::cmp::max;
+        let mut ans = 0i64;
+		let n = nums.len();
+		let mut suf_max = vec![0; n];
+		suf_max[n - 1] = nums[n - 1];
+		for i in (0..n - 1).rev() {
+			suf_max[i] = max(suf_max[i + 1], nums[i]);
+		}
+		let mut pre_max = nums[0];
+		for j in 1..n - 1 {
+			ans = max(ans, (pre_max - nums[j]) as i64 * suf_max[j + 1] as i64);
+			pre_max = max(pre_max, nums[j]);
+		}
+		ans
     }
 }
 
