b8fb79b2d8f672c43d2b8d39d2257e3bcd1abd39
benhao <qubenhao2@gmail.com>
2025-04-03 07:57:39 +0800

    test: 2874 solution
    py, java, rs

--- problems/problems_2874/solution.py
+++ problems/problems_2874/solution.py
@@ -7,5 +7,13 @@ class Solution(solution.Solution):
         return self.maximumTripletValue(test_input)
 
     def maximumTripletValue(self, nums: List[int]) -> int:
-        pass
-
+        n = len(nums)
+        suffix_max = [nums[-1]] * n
+        for i in range(n - 2, -1, -1):
+            suffix_max[i] = max(suffix_max[i + 1], nums[i])
+        ans = 0
+        pre_max = nums[0]
+        for j in range(1, n - 1):
+            pre_max = max(pre_max, nums[j - 1])
+            ans = max(ans, (pre_max - nums[j]) * suffix_max[j + 1])
+        return ans
