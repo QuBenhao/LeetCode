e0f54da12ba415a027c99cec5f5f4007799d4b9f
benhao <qubenhao2@gmail.com>
2025-04-09 21:41:49 +0800

    test: 3375 solution
    py

--- problems/problems_3375/solution.py
+++ problems/problems_3375/solution.py
@@ -7,5 +7,10 @@ class Solution(solution.Solution):
         return self.minOperations(*test_input)
 
     def minOperations(self, nums: List[int], k: int) -> int:
-        pass
-
+        ans, m = set(), nums[0]
+        for num in nums:
+            ans.add(num)
+            m = min(m, num)
+        if k > m:
+            return -1
+        return len(ans) - 1 if k in ans else len(ans)
