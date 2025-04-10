7c5f884560d3966f7431bb84a4155be06eb0f5a8
benhao <qubenhao2@gmail.com>
2025-04-10 17:05:20 +0800

    test: 2999 solution
    py

--- problems/problems_2999/solution.py
+++ problems/problems_2999/solution.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 import solution
 from typing import *
 
@@ -7,5 +9,29 @@ class Solution(solution.Solution):
         return self.numberOfPowerfulInt(*test_input)
 
     def numberOfPowerfulInt(self, start: int, finish: int, limit: int, s: str) -> int:
-        pass
+        high = list(map(int, str(finish)))  # 避免在 dfs 中频繁调用 int()
+        n = len(high)
+        low = list(map(int, str(start).zfill(n)))  # 补前导零，和 high 对齐
+        diff = n - len(s)
 
+        @cache
+        def dfs(i: int, limit_low: bool, limit_high: bool) -> int:
+            if i == n:
+                return 1
+
+            # 第 i 个数位可以从 lo 枚举到 hi
+            # 如果对数位还有其它约束，应当只在下面的 for 循环做限制，不应修改 lo 或 hi
+            lo = low[i] if limit_low else 0
+            hi = high[i] if limit_high else 9
+
+            res = 0
+            if i < diff:  # 枚举这个数位填什么
+                for d in range(lo, min(hi, limit) + 1):
+                    res += dfs(i + 1, limit_low and d == lo, limit_high and d == hi)
+            else:  # 这个数位只能填 s[i-diff]
+                x = int(s[i - diff])
+                if lo <= x <= hi:  # 题目保证 x <= limit，无需判断
+                    res = dfs(i + 1, limit_low and x == lo, limit_high and x == hi)
+            return res
+
+        return dfs(0, True, True)
