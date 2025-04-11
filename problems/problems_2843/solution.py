5bfbc67bdf2c90a29d1e46eee229f93f22e3fabb
benhao <qubenhao2@gmail.com>
2025-04-11 20:15:26 +0800

    test: 2843 solution
    py

--- problems/problems_2843/solution.py
+++ problems/problems_2843/solution.py
@@ -7,5 +7,11 @@ class Solution(solution.Solution):
         return self.countSymmetricIntegers(*test_input)
 
     def countSymmetricIntegers(self, low: int, high: int) -> int:
-        pass
-
+        ans = 0
+        for i in range(low, high + 1):
+            s = str(i)
+            if len(s) % 2 == 0:
+                n = len(s) // 2
+                if sum(map(int, s[:n])) == sum(map(int, s[n:])):
+                    ans += 1
+        return ans
