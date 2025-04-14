99c9cf9b22df078bac0898c1b561dce4593d0546
benhao <qubenhao2@gmail.com>
2025-04-14 21:25:28 +0800

    test: 1534 solution
    py

--- problems/problems_1534/solution.py
+++ problems/problems_1534/solution.py
@@ -7,5 +7,14 @@ class Solution(solution.Solution):
         return self.countGoodTriplets(*test_input)
 
     def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
-        pass
-
+        n = len(arr)
+        ans = 0
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                if abs(arr[i] - arr[j]) > a:
+                    continue
+                for k in range(j + 1, n):
+                    if abs(arr[j] - arr[k]) > b or abs(arr[i] - arr[k]) > c:
+                        continue
+                    ans += 1
+        return ans
