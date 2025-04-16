f72469e04dc13b1c287eda575e221295a946c119
benhao <qubenhao2@gmail.com>
2025-04-16 21:36:04 +0800

    test: 2537 solution
    py

--- problems/problems_2537/solution.py
+++ problems/problems_2537/solution.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import solution
 from typing import *
 
@@ -7,5 +9,14 @@ class Solution(solution.Solution):
         return self.countGood(*test_input)
 
     def countGood(self, nums: List[int], k: int) -> int:
-        pass
-
+        cnt = defaultdict(int)  # 比 Counter() 快
+        ans = left = pairs = 0
+        for x in nums:
+            pairs += cnt[x]
+            cnt[x] += 1
+            while pairs >= k:
+                cnt[nums[left]] -= 1
+                pairs -= cnt[nums[left]]
+                left += 1
+            ans += left
+        return ans
