07880175fbd70f85124ca91c2768a955a62532c3
benhao <qubenhao2@gmail.com>
2025-04-19 10:26:02 +0800

    test: 2563 solution
    py, go

--- problems/problems_2563/solution.go
+++ problems/problems_2563/solution.go
@@ -3,11 +3,24 @@ package problem2563
 import (
 	"encoding/json"
 	"log"
+	"slices"
 	"strings"
 )
 
-func countFairPairs(nums []int, lower int, upper int) int64 {
-    
+func countFairPairs(nums []int, lower int, upper int) (ans int64) {
+	slices.Sort(nums)
+	left, right := len(nums)-1, len(nums)-1
+	for i, num := range nums {
+		left_bound, right_bound := lower-num, upper-num
+		for right > i && nums[right] > right_bound {
+			right--
+		}
+		for left > i && nums[left] >= left_bound {
+			left--
+		}
+		ans += int64(max(right, i) - max(left, i))
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_2563/solution.py
+++ problems/problems_2563/solution.py
@@ -1,3 +1,5 @@
+from bisect import bisect_left, bisect_right
+
 import solution
 from typing import *
 
@@ -7,5 +9,24 @@ class Solution(solution.Solution):
         return self.countFairPairs(*test_input)
 
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
-        pass
+        nums.sort()
 
+        # # bisect
+        # ans, n = 0, len(nums)
+        # for i, num in enumerate(nums):
+        #     l = bisect_left(nums, lower - num, i + 1, n)
+        #     r = bisect_right(nums, upper - num, i + 1, n)
+        #     ans += r - l
+        # return ans
+
+        # # two pointers
+        ans = 0
+        left = right = len(nums) - 1
+        for i, num in enumerate(nums):
+            left_bound, right_bound = lower - num, upper - num
+            while right > i and nums[right] > right_bound:
+                right -= 1
+            while left > i and nums[left] >= left_bound:
+                left -= 1
+            ans += max(right, i) - max(left, i)
+        return ans
