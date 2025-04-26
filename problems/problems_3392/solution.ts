297ff4e30664782daf081c76c5c7b9bbdaa66e32
benhao <qubenhao2@gmail.com>
2025-04-27 07:54:05 +0800

    test: delete questions
    for debugging daily github
    
    (cherry picked from commit 671f79be8b76478f829745ebdbf4d70d58dd0fdf)

--- problems/problems_3392/solution.ts
+++
@@ -1,9 +0,0 @@
-function countSubarrays(nums: number[]): number {
-    
-};
-
-export function Solve(inputJsonElement: string): any {
-	const inputValues: string[] = inputJsonElement.split("\n");
-	const nums: number[] = JSON.parse(inputValues[0]);
-	return countSubarrays(nums);
-}
