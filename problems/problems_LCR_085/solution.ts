e1b169de651897777140d2cb8fc04b09876cd174
benhao <qubenhao2@gmail.com>
2025-04-27 07:54:05 +0800

    test: delete questions
    for debugging daily github
    
    (cherry picked from commit 671f79be8b76478f829745ebdbf4d70d58dd0fdf)
    (cherry picked from commit 297ff4e30664782daf081c76c5c7b9bbdaa66e32)

--- problems/problems_LCR_085/solution.ts
+++
@@ -1,9 +0,0 @@
-function generateParenthesis(n: number): string[] {
-
-};
-
-export function Solve(inputJsonElement: string): any {
-	const inputValues: string[] = inputJsonElement.split("\n");
-	const n: number = JSON.parse(inputValues[0]);
-	return generateParenthesis(n);
-}
