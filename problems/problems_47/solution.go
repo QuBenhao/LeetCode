fca085346fa472b02370efd1a76875c633735947
benhao <qubenhao2@gmail.com>
2025-04-28 20:20:56 +0800

    test: 47 solution
    py, go

--- problems/problems_47/solution.go
+++ problems/problems_47/solution.go
@@ -3,11 +3,41 @@ package problem47
 import (
 	"encoding/json"
 	"log"
+	"sort"
 	"strings"
 )
 
-func permuteUnique(nums []int) [][]int {
-    
+func NextPermutation(nums []int) {
+	n := len(nums)
+	i := n - 2
+	for i >= 0 && nums[i] >= nums[i+1] {
+		i--
+	}
+	for l, r := i+1, n-1; l < r; l, r = l+1, r-1 {
+		nums[l], nums[r] = nums[r], nums[l]
+	}
+	if i < 0 {
+		return
+	}
+	j := i + 1
+	for j < n && nums[j] <= nums[i] {
+		j++
+	}
+	nums[i], nums[j] = nums[j], nums[i]
+}
+
+func permuteUnique(nums []int) (ans [][]int) {
+	// Sort the array to ensure we can skip duplicates
+	sort.Ints(nums)
+	ans = append(ans, append([]int{}, nums...))
+	for {
+		NextPermutation(nums)
+		if sort.IntsAreSorted(nums) {
+			break
+		}
+		ans = append(ans, append([]int{}, nums...))
+	}
+	return ans
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_47/solution.py
+++ problems/problems_47/solution.py
@@ -7,5 +7,28 @@ class Solution(solution.Solution):
         return self.permuteUnique(test_input)
 
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        pass
+        def next_permutation(arr):
+            n = len(arr)
+            i = n - 2
+            while i >= 0 and arr[i] >= arr[i + 1]:
+                i -= 1
+            left, right = i + 1, n - 1
+            while left < right:
+                arr[left], arr[right] = arr[right], arr[left]
+                left += 1
+                right -= 1
+            if i < 0:
+                return
+            j = i + 1
+            while j < n and arr[j] <= arr[i]:
+                j += 1
+            arr[i], arr[j] = arr[j], arr[i]
 
+        nums.sort()
+        ans = [list(nums)]
+        while True:
+            next_permutation(nums)
+            if nums == ans[0]:
+                break
+            ans.append(list(nums))
+        return ans
