dddb51ae473f619b6e25fd11e74b298119b43d92
qubh <qubh@xiaopeng.com>
2025-04-28 17:44:47 +0800

    test: 47 testcase
    failed cases

--- problems/problems_47/testcase.py
+++ problems/problems_47/testcase.py
@@ -9,6 +9,8 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=[1, 1, 2], Output=[[1, 1, 2], [1, 2, 1], [2, 1, 1]]))
 		self.testcases.append(case(Input=[1, 2, 3], Output=[[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]))
+		self.testcases.append(case(Input=[2,2,1,1], Output=[[1,1,2,2],[1,2,1,2],[1,2,2,1],[2,1,1,2],[2,1,2,1],[2,2,1,1]]))
+		self.testcases.append(case(Input=[0,1,0,0,9], Output=[[0,0,0,1,9],[0,0,0,9,1],[0,0,1,0,9],[0,0,1,9,0],[0,0,9,0,1],[0,0,9,1,0],[0,1,0,0,9],[0,1,0,9,0],[0,1,9,0,0],[0,9,0,0,1],[0,9,0,1,0],[0,9,1,0,0],[1,0,0,0,9],[1,0,0,9,0],[1,0,9,0,0],[1,9,0,0,0],[9,0,0,0,1],[9,0,0,1,0],[9,0,1,0,0],[9,1,0,0,0]]))
 
 	def get_testcases(self):
 		return self.testcases
