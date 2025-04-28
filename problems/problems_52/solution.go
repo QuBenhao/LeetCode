da010c00b4a7873ecc6a25b64d76baa71b941859
qubh <qubh@xiaopeng.com>
2025-04-28 16:25:14 +0800

    test: 52 solution
    go

--- problems/problems_52/solution.go
+++ problems/problems_52/solution.go
@@ -6,8 +6,41 @@ import (
 	"strings"
 )
 
-func totalNQueens(n int) int {
-    
+func totalNQueens(n int) (ans int) {
+	cols := map[int]interface{}{}
+	rowCols := map[int]interface{}{}
+	colRows := map[int]interface{}{}
+
+	var backtrack func()
+	backtrack = func() {
+		r := len(cols)
+		if r == n {
+			ans++
+			return
+		}
+		for c := 0; c < n; c++ {
+			if _, ok := cols[c]; ok {
+				continue
+			}
+			rc := r + c
+			if _, ok := rowCols[rc]; ok {
+				continue
+			}
+			cr := r - c
+			if _, ok := colRows[cr]; ok {
+				continue
+			}
+			cols[c] = nil
+			rowCols[rc] = nil
+			colRows[cr] = nil
+			backtrack()
+			delete(cols, c)
+			delete(rowCols, rc)
+			delete(colRows, cr)
+		}
+	}
+	backtrack()
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
