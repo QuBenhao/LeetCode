6c8ad5d76d99cfeaa4a1fb0e01f0a9bf5ff8b100
benhao <qubenhao2@gmail.com>
2025-04-30 07:29:41 +0800

    test: 638 solution
    go

--- problems/problems_638/solution.go
+++ problems/problems_638/solution.go
@@ -7,7 +7,38 @@ import (
 )
 
 func shoppingOffers(price []int, special [][]int, needs []int) int {
-    
+	n := len(price)
+	columns := make([]int, n+1)
+	columns[0] = 1
+	for i, need := range needs {
+		// need + 1 because we need to consider the case where we don't buy any of this item
+		columns[i+1] = columns[i] * (need + 1)
+	}
+	dp := make([]int, columns[n]+1)
+	for i := 1; i <= columns[n]; i++ {
+		curNeeds := make([]int, n)
+		for j := 0; j < n; j++ {
+			curNeeds[j] = (i % columns[j+1]) / columns[j]
+		}
+		for j, p := range price {
+			dp[i] += curNeeds[j] * p
+		}
+		for _, offer := range special {
+			prev := 0
+			for j := 0; j < n; j++ {
+				if curNeeds[j] < offer[j] {
+					prev = -1
+					break
+				}
+				prev += offer[j] * columns[j]
+			}
+			if prev == -1 {
+				continue
+			}
+			dp[i] = min(dp[i], dp[i-prev]+offer[n])
+		}
+	}
+	return dp[columns[n]-1]
 }
 
 func Solve(inputJsonValues string) interface{} {
