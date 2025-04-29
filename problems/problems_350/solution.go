bc0855b7afc0fa94b676112a1769c4366db53ff1
qubh <qubh@xiaopeng.com>
2025-04-29 13:54:41 +0800

    test: 350 solution
    py, go

--- problems/problems_350/solution.go
+++ problems/problems_350/solution.go
@@ -3,11 +3,44 @@ package problem350
 import (
 	"encoding/json"
 	"log"
+	"sort"
 	"strings"
 )
 
-func intersect(nums1 []int, nums2 []int) []int {
-    
+func intersect(nums1 []int, nums2 []int) (ans []int) {
+	// 解法一： 排序双指针
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	n1, n2 := len(nums1), len(nums2)
+	for idx1, idx2 := 0, 0; idx1 < n1 && idx2 < n2; {
+		if d := nums1[idx1] - nums2[idx2]; d == 0 {
+			ans = append(ans, nums1[idx1])
+			idx1++
+			idx2++
+		} else if d > 0 {
+			idx2++
+		} else {
+			idx1++
+		}
+	}
+	return
+
+	// // 解法二： 哈希
+	// counter1, counter2 := map[int]int{}, map[int]int{}
+	// for _, num := range nums1 {
+	// 	counter1[num]++
+	// }
+	// for _, num := range nums2 {
+	// 	counter2[num]++
+	// }
+	// for k, c1 := range counter1 {
+	// 	if c2, ok := counter2[k]; ok {
+	// 		for i := 0; i < min(c1, c2); i++ {
+	// 			ans = append(ans, k)
+	// 		}
+	// 	}
+	// }
+	// return
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_350/solution.py
+++ problems/problems_350/solution.py
@@ -7,5 +7,26 @@ class Solution(solution.Solution):
         return self.intersect(*test_input)
 
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        pass
-
+        # # 解法一： 排序后双指针对比
+        # nums1.sort()
+        # nums2.sort()
+        # ans = []
+        # idx1 = idx2 = 0
+        # n1, n2 = len(nums1), len(nums2)
+        # while idx1 < n1 and idx2 < n2:
+        #     if nums1[idx1] == nums2[idx2]:
+        #         ans.append(nums1[idx1])
+        #         idx1 += 1
+        #         idx2 += 1
+        #     elif nums1[idx1] > nums2[idx2]:
+        #         idx2 += 1
+        #     else:
+        #         idx1 += 1
+        # return ans
+    
+        # 解法二： 哈希计数对比
+        c1, c2 = Counter(nums1), Counter(nums2)
+        ans = []
+        for k in c1 & c2:
+            ans.extend([k] * min(c1[k], c2[k]))
+        return ans
