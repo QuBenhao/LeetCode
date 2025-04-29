2d5eb66a3846d9d2721c948d197cb724d7978696
qubh <qubh@xiaopeng.com>
2025-04-29 16:13:29 +0800

    test: 624 solution
    go

--- problems/problems_624/solution.go
+++ problems/problems_624/solution.go
@@ -3,11 +3,19 @@ package problem624
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"strings"
 )
 
-func maxDistance(arrays [][]int) int {
-    
+func maxDistance(arrays [][]int) (ans int) {
+	maxVal, minVal := arrays[0][len(arrays[0])-1], arrays[0][0]
+	for i := 1; i < len(arrays); i++ {
+		minCur, maxCur := arrays[i][0], arrays[i][len(arrays[i])-1]
+		ans = max(ans, int(math.Abs(float64(maxVal-minCur))), int(math.Abs(float64(maxCur-minVal))))
+		maxVal = max(maxVal, maxCur)
+		minVal = min(minVal, minCur)
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
