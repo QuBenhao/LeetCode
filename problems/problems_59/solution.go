d7327502377f2c9b278a81383bfba2cc2bec0c92
qubh <qubh@xiaopeng.com>
2025-04-29 10:04:40 +0800

    test: 59 solution
    go

--- problems/problems_59/solution.go
+++ problems/problems_59/solution.go
@@ -7,7 +7,39 @@ import (
 )
 
 func generateMatrix(n int) [][]int {
-    
+	directions := [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+	ans := make([][]int, n)
+	for i := 0; i < n; i++ {
+		ans[i] = make([]int, n)
+	}
+	num := 1
+	for i := 0; i < n/2; i++ {
+		ans[i][i] = num
+		num++
+		x, y := i, i+1
+		minBound, maxBound := i-1, n-i
+		dIdx := 0
+		for x != i || y != i {
+			ans[x][y] = num
+			num++
+			nx, ny := x+directions[dIdx][0], y+directions[dIdx][1]
+			if nx <= minBound || nx >= maxBound || ny <= minBound || ny >= maxBound {
+				dIdx++
+				if dIdx < len(directions) {
+					x += directions[dIdx][0]
+					y += directions[dIdx][1]
+				}
+			} else {
+				x = nx
+				y = ny
+			}
+		}
+	}
+	if n%2 == 1 {
+		ans[n/2][n/2] = num
+	}
+
+	return ans
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_59/testcase.py
+++ problems/problems_59/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
         self.testcases = []
         self.testcases.append(case(Input=3, Output=[[1,2,3],[8,9,4],[7,6,5]]))
         self.testcases.append(case(Input=1, Output=[[1]]))
+        self.testcases.append(case(Input=2, Output=[[1,2],[4,3]]))
 
     def get_testcases(self):
         return self.testcases
