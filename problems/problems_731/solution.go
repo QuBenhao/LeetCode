e08e26deda05f7829c5603ca4bda2c5b9bf1f3cd
qubh <qubh@xiaopeng.com>
2025-04-30 12:48:41 +0800

    test: 731 solution
    py, go

--- problems/problems_731/solution.go
+++ problems/problems_731/solution.go
@@ -6,20 +6,97 @@ import (
 	"strings"
 )
 
-type MyCalendarTwo struct {
-    
+type Node struct {
+	left, right *Node
+	val, lazy   int
 }
 
+type DynamicSegmentTree struct {
+	root       *Node
+	start, end int
+}
+
+func NewDynamicSegmentTree(start, end int) *DynamicSegmentTree {
+	return &DynamicSegmentTree{
+		root:  &Node{},
+		start: start,
+		end:   end,
+	}
+}
+
+func (dst *DynamicSegmentTree) pushDown(node *Node, l, r int) {
+	if node.left == nil {
+		node.left = &Node{}
+	}
+	if node.right == nil {
+		node.right = &Node{}
+	}
+	if node.lazy != 0 {
+		// 更新左子节点
+		node.left.val += node.lazy
+		node.left.lazy += node.lazy
+		// 更新右子节点
+		node.right.val += node.lazy
+		node.right.lazy += node.lazy
+		node.lazy = 0
+	}
+}
+
+func (dst *DynamicSegmentTree) update(node *Node, l, r, ul, ur, val int) {
+	if ul <= l && r <= ur {
+		node.val += val
+		node.lazy += val
+		return
+	}
+	dst.pushDown(node, l, r)
+	mid := (l + r) / 2
+	if ul <= mid {
+		dst.update(node.left, l, mid, ul, ur, val)
+	}
+	if ur > mid {
+		dst.update(node.right, mid+1, r, ul, ur, val)
+	}
+	node.val = max(node.left.val, node.right.val)
+}
+
+func (dst *DynamicSegmentTree) UpdateRange(l, r, val int) {
+	dst.update(dst.root, dst.start, dst.end, l, r, val)
+}
+
+func (dst *DynamicSegmentTree) query(node *Node, l, r, ql, qr int) int {
+	if qr < l || r < ql {
+		return 0
+	}
+	if ql <= l && r <= qr {
+		return node.val
+	}
+	dst.pushDown(node, l, r)
+	mid := (l + r) / 2
+	return max(dst.query(node.left, l, mid, ql, qr),
+		dst.query(node.right, mid+1, r, ql, qr))
+}
+
+func (dst *DynamicSegmentTree) QueryRange(l, r int) int {
+	return dst.query(dst.root, dst.start, dst.end, l, r)
+}
+
+type MyCalendarTwo struct {
+	tree DynamicSegmentTree
+}
 
 func Constructor() MyCalendarTwo {
-    
+	return MyCalendarTwo{
+		*NewDynamicSegmentTree(0, 1e9),
+	}
 }
-
 
 func (this *MyCalendarTwo) Book(startTime int, endTime int) bool {
-    
+	if this.tree.QueryRange(startTime, endTime-1) >= 2 {
+		return false
+	}
+	this.tree.UpdateRange(startTime, endTime-1, 1)
+	return true
 }
-
 
 /**
  * Your MyCalendarTwo object will be instantiated and called as such:
@@ -52,7 +129,6 @@ func Solve(inputJsonValues string) interface{} {
 		}
 		ans = append(ans, res)
 	}
-
 
 	return ans
 }
