243b15b71d471214633b7e6593620bb1d0232cd2
qubh <qubh@xiaopeng.com>
2025-04-30 15:59:07 +0800

    test: 782 remove
    remove

--- problems/problems_782/solution.ts
+++
@@ -1,9 +0,0 @@
-function movesToChessboard(board: number[][]): number {
-    
-};
-
-export function Solve(inputJsonElement: string): any {
-	const inputValues: string[] = inputJsonElement.split("\n");
-	const board: number[][] = JSON.parse(inputValues[0]);
-	return movesToChessboard(board);
-}
