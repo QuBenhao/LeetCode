b666be6a87f12206c6674a23c55c260be93fd351
qubh <qubh@xiaopeng.com>
2025-04-30 16:20:06 +0800

    test: 825 solution
    go

--- problems/problems_825/solution.go
+++ problems/problems_825/solution.go
@@ -6,8 +6,24 @@ import (
 	"strings"
 )
 
-func numFriendRequests(ages []int) int {
-    
+func numFriendRequests(ages []int) (ans int) {
+	/*
+		ages[y] <= 0.5 * ages[x] + 7
+		ages[y] > ages[x]
+	*/
+	cnts := make([]int, 121)
+	for _, age := range ages {
+		cnts[age]++
+	}
+	prefixSum := make([]int, 122)
+	for i := 0; i < 121; i++ {
+		prefixSum[i+1] = prefixSum[i] + cnts[i]
+	}
+	for age := 0; age < 121; age++ {
+		left := age/2 + 7
+		ans += max(0, cnts[age]*(prefixSum[age+1]-prefixSum[left+1]-1))
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
