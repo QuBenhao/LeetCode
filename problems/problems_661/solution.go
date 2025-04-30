de89d47d729a9695bf63f07ff7901c68ec829612
qubh <qubh@xiaopeng.com>
2025-04-30 09:51:26 +0800

    test: 661 solution
    go

--- problems/problems_661/solution.go
+++ problems/problems_661/solution.go
@@ -7,7 +7,23 @@ import (
 )
 
 func imageSmoother(img [][]int) [][]int {
-    
+	m, n := len(img), len(img[0])
+	prefixSum := make([][]int, m+1)
+	for i := 0; i <= m; i++ {
+		prefixSum[i] = make([]int, n+1)
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			prefixSum[i+1][j+1] = prefixSum[i+1][j] + prefixSum[i][j+1] - prefixSum[i][j] + img[i][j]
+		}
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			a, b, c, d := max(0, i-1), max(0, j-1), min(m-1, i+1), min(n-1, j+1)
+			img[i][j] = (prefixSum[c+1][d+1] + prefixSum[a][b] - prefixSum[c+1][b] - prefixSum[a][d+1]) / ((c - a + 1) * (d - b + 1))
+		}
+	}
+	return img
 }
 
 func Solve(inputJsonValues string) interface{} {
