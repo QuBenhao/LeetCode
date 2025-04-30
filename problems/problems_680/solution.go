ce3946381d1288415047a3879c236bb3d9b6a034
qubh <qubh@xiaopeng.com>
2025-04-30 10:11:15 +0800

    test: 680 solution
    py, go

--- problems/problems_680/solution.go
+++ problems/problems_680/solution.go
@@ -7,7 +7,23 @@ import (
 )
 
 func validPalindrome(s string) bool {
-    
+	isPalindrome := func(str string, l, r int) bool {
+		for l < r {
+			if str[l] != str[r] {
+				return false
+			}
+			l++
+			r--
+		}
+		return true
+	}
+	for left, right := 0, len(s)-1; left < right; left++ {
+		if s[left] != s[right] {
+			return isPalindrome(s, left+1, right) || isPalindrome(s, left, right-1)
+		}
+		right--
+	}
+	return true
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_680/solution.py
+++ problems/problems_680/solution.py
@@ -7,5 +7,14 @@ class Solution(solution.Solution):
         return self.validPalindrome(test_input)
 
     def validPalindrome(self, s: str) -> bool:
-        pass
+        def is_palindrome(string: str) -> bool:
+            return string == string[::-1]
+        
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return is_palindrome(s[left + 1:right+1]) or is_palindrome(s[left:right])
+            left += 1
+            right -= 1
+        return True
 
