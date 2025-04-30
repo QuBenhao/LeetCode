fc5b0e2c333b33f7c26cf43bb76218fc5b8642f3
qubh <qubh@xiaopeng.com>
2025-04-30 11:02:57 +0800

    test: 688 solution
    py, go

--- problems/problems_688/solution.go
+++ problems/problems_688/solution.go
@@ -7,7 +7,30 @@ import (
 )
 
 func knightProbability(n int, k int, row int, column int) float64 {
-    
+	dirs := [][]int{{-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {-2, -1}, {-2, 1}, {2, -1}, {2, 1}}
+	dp := make([][][]float64, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([][]float64, n)
+		for j := 0; j < n; j++ {
+			dp[i][j] = make([]float64, 2)
+			dp[i][j][0] = 1
+		}
+	}
+	for p := 1; p <= k; p++ {
+		for i := 0; i < n; i++ {
+			for j := 0; j < n; j++ {
+				dp[i][j][p%2] = 0
+				for _, dir := range dirs {
+					nx, ny := i+dir[0], j+dir[1]
+					if nx < 0 || nx >= n || ny < 0 || ny >= n {
+						continue
+					}
+					dp[i][j][p%2] += dp[nx][ny][(p+1)%2] / 8
+				}
+			}
+		}
+	}
+	return dp[row][column][k%2]
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_688/solution.py
+++ problems/problems_688/solution.py
@@ -7,5 +7,15 @@ class Solution(solution.Solution):
         return self.knightProbability(*test_input)
 
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
-        pass
+        dirs = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]]
+        dp = [[[1.0, 0.0] for _ in range(n)] for _ in range(n)]
+        for p in range(1, k+1):
+            for i in range(n):
+                for j in range(n):
+                    dp[i][j][p%2]=0
+                    for dx, dy in dirs:
+                        if (nx := i + dx) < 0 or nx >= n or (ny := j + dy) < 0 or ny >= n:
+                            continue
+                        dp[i][j][p%2] += dp[nx][ny][(p+1)%2] / 8
+        return dp[row][column][k%2]
 
