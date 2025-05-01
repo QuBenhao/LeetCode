dcdc38f3e18612b7dcbf68dd1b4649d93b92cbba
benhao <qubenhao2@gmail.com>
2025-05-01 08:56:43 +0800

    test: 2071 solution
    py, go

--- problems/problems_2071/solution.go
+++ problems/problems_2071/solution.go
@@ -3,11 +3,48 @@ package problem2071
 import (
 	"encoding/json"
 	"log"
+	"sort"
 	"strings"
 )
 
+func helper(tasks []int, workers []int, pills int, strength int, k int) bool {
+	i := 0
+	var validTasks []int
+	for idx := len(workers) - k; idx < len(workers); idx++ {
+		for i < k && tasks[i] <= workers[idx]+strength {
+			validTasks = append(validTasks, tasks[i])
+			i++
+		}
+		if len(validTasks) == 0 {
+			return false
+		}
+		if validTasks[0] <= workers[idx] {
+			validTasks = validTasks[1:]
+			continue
+		}
+		if pills == 0 {
+			return false
+		}
+		pills--
+		validTasks = validTasks[:len(validTasks)-1]
+	}
+	return true
+}
+
 func maxTaskAssign(tasks []int, workers []int, pills int, strength int) int {
-    
+	sort.Ints(tasks)
+	sort.Ints(workers)
+
+	left, right := 0, min(len(tasks), len(workers))
+	for left < right {
+		mid := left + (right-left+1)/2
+		if helper(tasks, workers, pills, strength, mid) {
+			left = mid
+		} else {
+			right = mid - 1
+		}
+	}
+	return left
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_2071/solution.py
+++ problems/problems_2071/solution.py
@@ -1,3 +1,6 @@
+from bisect import bisect_left
+from collections import deque
+
 import solution
 from typing import *
 
@@ -7,5 +10,32 @@ class Solution(solution.Solution):
         return self.maxTaskAssign(*test_input)
 
     def maxTaskAssign(self, tasks: List[int], workers: List[int], pills: int, strength: int) -> int:
-        pass
+        tasks.sort()
+        workers.sort()
 
+        def check(k: int) -> bool:
+            k += 1  # 二分最小的无法完成的 k+1，那么最终的 k 就是最大的可以完成的 k
+            # 贪心：用最强的 k 名工人，完成最简单的 k 个任务
+            i, p = 0, pills
+            valid_tasks = deque()
+            for w in workers[-k:]:  # 枚举工人
+                # 在吃药的情况下，把能完成的任务记录到 valid_tasks 中
+                while i < k and tasks[i] <= w + strength:
+                    valid_tasks.append(tasks[i])
+                    i += 1
+                # 即使吃药也无法完成任务
+                if not valid_tasks:
+                    return True
+                # 无需吃药就能完成（最简单的）任务
+                if w >= valid_tasks[0]:
+                    valid_tasks.popleft()
+                    continue
+                # 必须吃药
+                if p == 0:  # 没药了
+                    return True
+                p -= 1
+                # 完成（能完成的）最难的任务
+                valid_tasks.pop()
+            return False
+
+        return bisect_left(range(min(len(tasks), len(workers))), True, key=check)
