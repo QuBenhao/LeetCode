373a731f84f6c6842b85b7ff93063ef4691fc07a
benhao <qubenhao2@gmail.com>
2025-05-03 09:20:12 +0800

    test: 1007 solution
    py, go

--- problems/problems_1007/solution.go
+++ problems/problems_1007/solution.go
@@ -3,11 +3,48 @@ package problem1007
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"strings"
 )
 
 func minDominoRotations(tops []int, bottoms []int) int {
-    
+	n, ans1, ans2, ans3, ans4 := len(tops), 0, 0, 0, 0
+	tp, bt := tops[0], bottoms[0]
+	for i := 0; i < n; i++ {
+		t, b := tops[i], bottoms[i]
+		if t != tp && b != tp {
+			ans1 = -1
+			ans3 = -1
+		} else {
+			if t != tp && ans1 != -1 {
+				ans1++
+			}
+			if b != tp && ans3 != -1 {
+				ans3++
+			}
+		}
+		if t != bt && b != bt {
+			ans2 = -1
+			ans4 = -1
+		} else {
+			if t != bt && ans2 != -1 {
+				ans2++
+			}
+			if b != bt && ans4 != -1 {
+				ans4++
+			}
+		}
+		if ans1 == -1 && ans2 == -1 && ans3 == -1 && ans4 == -1 {
+			return -1
+		}
+	}
+	convert := func(x int) int {
+		if x == -1 {
+			return math.MaxInt
+		}
+		return x
+	}
+	return min(convert(ans1), convert(ans2), convert(ans3), convert(ans4))
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_1007/solution.py
+++ problems/problems_1007/solution.py
@@ -1,3 +1,5 @@
+from math import inf
+
 import solution
 from typing import *
 
@@ -7,5 +9,26 @@ class Solution(solution.Solution):
         return self.minDominoRotations(*test_input)
 
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
-        pass
-
+        tp, bt =  tops[0], bottoms[0]
+        ans1 = ans2 = 0
+        ans3 = ans4 = 0
+        for t, b in zip(tops, bottoms):
+            if t != tp and b != tp:
+                ans1 = inf
+                ans3 = inf
+            else:
+                if t != tp:
+                    ans1 += 1
+                if b != tp:
+                    ans3 += 1
+            if t != bt and b != bt:
+                ans2 = inf
+                ans4 = inf
+            else:
+                if b != bt:
+                    ans2 += 1
+                if t != bt:
+                    ans4 += 1
+            if ans1 == ans2 == ans3 == ans4 == inf:
+                break
+        return -1 if (a := min(ans1, ans2, ans3, ans4)) == inf else a
