695a09ec4361160a2477e67dce70c1cb7fd6e784
benhao <qubenhao2@gmail.com>
2025-05-05 10:05:26 +0800

    test: 790 solution
    py, go

--- problems/problems_790/solution.go
+++ problems/problems_790/solution.go
@@ -6,8 +6,23 @@ import (
 	"strings"
 )
 
+const MOD = 1000000007
+
 func numTilings(n int) int {
-    
+	dp := make([][]int, 2)
+	for i := range 2 {
+		dp[i] = make([]int, 4)
+	}
+	dp[1][0] = 1
+	dp[1][3] = 1
+	for i := 2; i <= n; i++ {
+		cur, prev := i%2, (i+1)%2
+		dp[cur][0] = dp[prev][3] % MOD
+		dp[cur][1] = (dp[prev][0] + dp[prev][2]) % MOD
+		dp[cur][2] = (dp[prev][0] + dp[prev][1]) % MOD
+		dp[cur][3] = ((dp[cur][1]+dp[prev][1])%MOD + dp[prev][3]) % MOD
+	}
+	return dp[n%2][3] % MOD
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_790/solution.py
+++ problems/problems_790/solution.py
@@ -7,5 +7,14 @@ class Solution(solution.Solution):
         return self.numTilings(test_input)
 
     def numTilings(self, n: int) -> int:
-        pass
-
+        mod = 10**9 + 7
+        # dp[i][j], i代表第i列, j代表第i列的状态 0: 空 1: 上面填了 2: 下面填了 3: 上下都填了
+        dp = [[0] * 4 for _ in range(2)]
+        dp[1][0] = dp[1][3] = 1
+        for i in range(2, n + 1):
+            prev, cur = (i - 1) % 2, i % 2
+            dp[cur][0] = dp[prev][3] % mod
+            dp[cur][1] = (dp[prev][0] + dp[prev][2]) % mod
+            dp[cur][2] = (dp[prev][0] + dp[prev][1]) % mod
+            dp[cur][3] = sum(v for v in dp[prev]) % mod
+        return dp[n % 2][3] % mod
