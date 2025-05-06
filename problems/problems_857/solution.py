45165adcadd3633a46c0a693c8750a45f2b986aa
qubh <qubh@xiaopeng.com>
2025-05-06 10:42:35 +0800

    test: 857 solution
    py

--- problems/problems_857/solution.py
+++ problems/problems_857/solution.py
@@ -1,5 +1,8 @@
-import solution
+import heapq
 from typing import *
+from math import inf
+
+import solution
 
 
 class Solution(solution.Solution):
@@ -7,5 +10,13 @@ class Solution(solution.Solution):
         return self.mincostToHireWorkers(*test_input)
 
     def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
-        pass
-
+        # 性价比=w/q
+        ans, total, pq = inf, 0, []
+        for q, w in sorted(zip(quality, wage), key=lambda x: (x[1] / x[0])):
+            total += q
+            heapq.heappush(pq, -q)
+            if len(pq) > k:
+                total += heapq.heappop(pq)
+            if len(pq) == k:
+                ans = min(ans, total * w / q)
+        return ans
