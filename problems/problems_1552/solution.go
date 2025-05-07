849e42b8e8dd7fbd3a4e4314bc20665685f0f4ea
qubh <qubh@xiaopeng.com>
2025-05-07 18:31:40 +0800

    test: 1552 solution
    go

--- problems/problems_1552/solution.go
+++ problems/problems_1552/solution.go
@@ -3,11 +3,33 @@ package problem1552
 import (
 	"encoding/json"
 	"log"
+	"sort"
 	"strings"
 )
 
 func maxDistance(position []int, m int) int {
-    
+	helper := func(d int) bool {
+		count, last := 0, -d
+		for _, p := range position {
+			if p-last >= d {
+				count++
+				last = p
+			}
+		}
+		return count >= m
+	}
+
+	sort.Ints(position)
+	left, right := 1, position[len(position)-1]-position[0]
+	for left < right {
+		mid := left + (right-left+1)/2
+		if helper(mid) {
+			left = mid
+		} else {
+			right = mid - 1
+		}
+	}
+	return left
 }
 
 func Solve(inputJsonValues string) interface{} {
