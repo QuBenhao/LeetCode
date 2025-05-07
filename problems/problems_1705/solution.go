25089d526b478fd8c63e98de4457e7413072d07d
benhao <qubenhao2@gmail.com>
2025-05-07 20:40:02 +0800

    test: 1705 solution
    go

--- problems/problems_1705/solution.go
+++ problems/problems_1705/solution.go
@@ -1,13 +1,60 @@
 package problem1705
 
 import (
+	"container/heap"
 	"encoding/json"
 	"log"
 	"strings"
 )
 
-func eatenApples(apples []int, days []int) int {
-    
+func eatenApples(apples []int, days []int) (ans int) {
+	pq := &maxHeap{}
+	heap.Init(pq)
+	n := len(apples)
+	for i := 0; i < n; i++ {
+		heap.Push(pq, tuple{apples[i], days[i] + i})
+		for pq.Len() > 0 && (*pq)[0].day <= i {
+			heap.Pop(pq)
+		}
+		if pq.Len() > 0 {
+			ans++
+			(*pq)[0].apple--
+			if (*pq)[0].apple == 0 {
+				heap.Pop(pq)
+			}
+		}
+	}
+	cur := n
+	for pq.Len() > 0 {
+		for pq.Len() > 0 && (*pq)[0].day <= cur {
+			heap.Pop(pq)
+		}
+		if pq.Len() == 0 {
+			break
+		}
+		item := heap.Pop(pq).(tuple)
+		diff := min(item.day-cur, item.apple)
+		ans += diff
+		cur += diff
+	}
+	return
+}
+
+type tuple struct{ apple, day int }
+type maxHeap []tuple
+
+func (h maxHeap) Len() int           { return len(h) }
+func (h maxHeap) Less(i, j int) bool { return h[i].day < h[j].day }
+func (h maxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *maxHeap) Push(x interface{}) {
+	*h = append(*h, x.(tuple))
+}
+func (h *maxHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_1705/testcase.py
+++ problems/problems_1705/testcase.py
@@ -9,6 +9,8 @@ class Testcase(testcase.Testcase):
         self.testcases = []
         self.testcases.append(case(Input=([1, 2, 3, 5, 2], [3, 2, 1, 4, 2]), Output=7))
         self.testcases.append(case(Input=([3, 0, 0, 0, 0, 2], [3, 0, 0, 0, 0, 2]), Output=5))
+        self.testcases.append(case(Input=[[9,10,1,7,0,2,1,4,1,7,0,11,0,11,0,0,9,11,11,2,0,5,5],[3,19,1,14,0,4,1,8,2,7,0,13,0,13,0,0,2,2,13,1,0,3,7]], Output=31))
+        self.testcases.append(case(Input=[[2,1,10],[2,10,1]], Output=4))
 
     def get_testcases(self):
         return self.testcases
