557ea7c57b70f9cdaeb78f729c1672be33770fd5
qubh <qubh@xiaopeng.com>
2025-05-07 13:57:02 +0800

    test: 1366 solution
    go

--- problems/problems_1366/solution.go
+++ problems/problems_1366/solution.go
@@ -1,13 +1,29 @@
 package problem1366
 
 import (
+	"cmp"
 	"encoding/json"
 	"log"
+	"maps"
+	"slices"
 	"strings"
 )
 
 func rankTeams(votes []string) string {
-    
+	n := len(votes[0])
+	counter := map[rune][]int{}
+	for _, ch := range votes[0] {
+		counter[ch] = make([]int, n)
+	}
+	for _, vote := range votes {
+		for i, ch := range vote {
+			counter[ch][i]++
+		}
+	}
+	sorted := slices.SortedFunc(maps.Keys(counter), func(a, b rune) int {
+		return cmp.Or(slices.Compare(counter[b], counter[a]), cmp.Compare(a, b))
+	})
+	return string(sorted)
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_1366/testcase.py
+++ problems/problems_1366/testcase.py
@@ -10,6 +10,7 @@ class Testcase(testcase.Testcase):
 		self.testcases.append(case(Input=['ABC', 'ACB', 'ABC', 'ACB', 'ACB'], Output="ACB"))
 		self.testcases.append(case(Input=['WXYZ', 'XYZW'], Output="XWYZ"))
 		self.testcases.append(case(Input=['ZMNAGUEDSJYLBOPHRQICWFXTVK'], Output="ZMNAGUEDSJYLBOPHRQICWFXTVK"))
+		self.testcases.append(case(Input=["BCA","CAB","CBA","ABC","ACB","BAC"], Output="ABC"))
 
 	def get_testcases(self):
 		return self.testcases
