a3cf52b32c8dda31a14445174e8314fb99111304
qubh <qubh@xiaopeng.com>
2025-05-07 18:36:44 +0800

    test: 1561 solution
    go

--- problems/problems_1561/solution.go
+++ problems/problems_1561/solution.go
@@ -3,11 +3,18 @@ package problem1561
 import (
 	"encoding/json"
 	"log"
+	"sort"
 	"strings"
 )
 
-func maxCoins(piles []int) int {
-    
+func maxCoins(piles []int) (ans int) {
+	sort.Ints(piles)
+	n := len(piles)
+	for i, j := n-2, 0; j < n/3; j++ {
+		ans += piles[i]
+		i -= 2
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
