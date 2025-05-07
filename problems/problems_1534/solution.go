ce2320ba1cdda8fb8689aceccc0d18039772f073
qubh <qubh@xiaopeng.com>
2025-05-07 16:28:54 +0800

    test: 1534 solution
    go

--- problems/problems_1534/solution.go
+++ problems/problems_1534/solution.go
@@ -3,11 +3,29 @@ package problem1534
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"strings"
 )
 
-func countGoodTriplets(arr []int, a int, b int, c int) int {
-    
+func countGoodTriplets(arr []int, a int, b int, c int) (ans int) {
+	n := len(arr)
+	for i := range n - 2 {
+		for j := i + 1; j < n-1; j++ {
+			if math.Abs(float64(arr[i]-arr[j])) > float64(a) {
+				continue
+			}
+			for k := j + 1; k < n; k++ {
+				if math.Abs(float64(arr[j]-arr[k])) > float64(b) {
+					continue
+				}
+				if math.Abs(float64(arr[i]-arr[k])) > float64(c) {
+					continue
+				}
+				ans++
+			}
+		}
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
