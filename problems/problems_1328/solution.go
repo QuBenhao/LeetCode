d2853a2f365d828be3caf39fb2a072cc7dc62bff
qubh <qubh@xiaopeng.com>
2025-05-07 11:43:21 +0800

    test: 1328 solution
    go

--- problems/problems_1328/solution.go
+++ problems/problems_1328/solution.go
@@ -7,7 +7,17 @@ import (
 )
 
 func breakPalindrome(palindrome string) string {
-    
+	n := len(palindrome)
+	if n == 1 {
+		return ""
+	}
+	mid := n / 2
+	for i := range mid {
+		if palindrome[i] != 'a' {
+			return palindrome[:i] + "a" + palindrome[i+1:]
+		}
+	}
+	return palindrome[:n-1] + "b"
 }
 
 func Solve(inputJsonValues string) interface{} {
