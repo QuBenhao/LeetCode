26b151bf10eaa7d0431c787aa766a0bd00ebf96a
qubh <qubh@xiaopeng.com>
2025-05-07 12:47:32 +0800

    test: 1338 solution
    go

--- problems/problems_1338/solution.go
+++ problems/problems_1338/solution.go
@@ -3,11 +3,25 @@ package problem1338
 import (
 	"encoding/json"
 	"log"
+	"maps"
+	"slices"
 	"strings"
 )
 
 func minSetSize(arr []int) int {
-    
+	counter := map[int]int{}
+	for _, num := range arr {
+		counter[num]++
+	}
+	cnt := slices.SortedFunc(maps.Values(counter), func(a, b int) int { return b - a })
+	s := 0
+	for i, c := range cnt {
+		s += c
+		if s >= len(arr)/2 {
+			return i + 1
+		}
+	}
+	return -1
 }
 
 func Solve(inputJsonValues string) interface{} {
