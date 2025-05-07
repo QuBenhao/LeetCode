46b835985dbde06b2ed544b7e0bbe228f567e994
benhao <qubenhao2@gmail.com>
2025-05-07 22:25:27 +0800

    doc: digit dp template
    数位dp模板

--- problems/problems_1742/solution.go
+++ problems/problems_1742/solution.go
@@ -6,8 +6,23 @@ import (
 	"strings"
 )
 
-func countBalls(lowLimit int, highLimit int) int {
-    
+func countBalls(lowLimit int, highLimit int) (ans int) {
+	convert := func(n int) (sum int) {
+		for n > 0 {
+			sum += n % 10
+			n /= 10
+		}
+		return
+	}
+	counter := map[int]int{}
+	for i := lowLimit; i <= highLimit; i++ {
+		c := convert(i)
+		counter[c]++
+		if counter[c] > ans {
+			ans = counter[c]
+		}
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
