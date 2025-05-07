2dfe71add7649501f9db1435e2092a27dd0c0f10
qubh <qubh@xiaopeng.com>
2025-05-07 17:51:59 +0800

    test: 1547 solution
    go

--- problems/problems_1547/solution.go
+++ problems/problems_1547/solution.go
@@ -3,11 +3,32 @@ package problem1547
 import (
 	"encoding/json"
 	"log"
+	"math"
+	"sort"
 	"strings"
 )
 
 func minCost(n int, cuts []int) int {
-    
+	cp := make([]int, len(cuts)+2)
+	copy(cp, cuts)
+	cp[len(cuts)] = 0
+	cp[len(cuts)+1] = n
+	sort.Ints(cp)
+	m := len(cp)
+	dp := make([][]int, m)
+	for i := range dp {
+		dp[i] = make([]int, m)
+	}
+	for i := m - 2; i >= 0; i-- {
+		for j := i + 2; j < m; j++ {
+			dp[i][j] = math.MaxInt
+			for k := i + 1; k < j; k++ {
+				dp[i][j] = min(dp[i][j], dp[i][k]+dp[k][j])
+			}
+			dp[i][j] += cp[j] - cp[i]
+		}
+	}
+	return dp[0][m-1]
 }
 
 func Solve(inputJsonValues string) interface{} {
