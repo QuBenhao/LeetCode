589ad1ac0ccf7cdfb5c503e09b0d68e6ac3053af
benhao <qubenhao2@gmail.com>
2025-05-08 07:57:29 +0800

    test: 3342 solution
    py, go

--- problems/problems_3342/solution.go
+++ problems/problems_3342/solution.go
@@ -1,13 +1,70 @@
 package problem3342
 
 import (
+	"container/heap"
 	"encoding/json"
 	"log"
+	"math"
 	"strings"
 )
 
 func minTimeToReach(moveTime [][]int) int {
-    
+	m, n := len(moveTime), len(moveTime[0])
+	directions := [][2]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+	explored := make([][]int, m)
+	for i := range explored {
+		explored[i] = make([]int, n)
+		for j := range explored[i] {
+			explored[i][j] = math.MaxInt
+		}
+	}
+	pq := &queue{}
+	heap.Init(pq)
+	heap.Push(pq, tuple{0, 0, 0})
+	explored[0][0] = 0
+	for pq.Len() > 0 {
+		tp := heap.Pop(pq).(tuple)
+		time, x, y := tp.time, tp.x, tp.y
+		if x == m-1 && y == n-1 {
+			return time
+		}
+		if time > explored[x][y] {
+			continue
+		}
+		for _, d := range directions {
+			nx, ny := x+d[0], y+d[1]
+			if nx < 0 || nx >= m || ny < 0 || ny >= n {
+				continue
+			}
+			nt := max(time, moveTime[nx][ny]) + (x+y)%2 + 1
+			if nt < explored[nx][ny] {
+				explored[nx][ny] = nt
+				heap.Push(pq, tuple{nt, nx, ny})
+			}
+		}
+	}
+	return -1
+}
+
+type tuple struct{ time, x, y int }
+type queue []tuple
+
+func (q queue) Len() int { return len(q) }
+func (q queue) Less(i, j int) bool {
+	return q[i].time < q[j].time
+}
+func (q queue) Swap(i, j int) {
+	q[i], q[j] = q[j], q[i]
+}
+func (q *queue) Push(x interface{}) {
+	*q = append(*q, x.(tuple))
+}
+func (q *queue) Pop() interface{} {
+	old := *q
+	n := len(old)
+	x := old[n-1]
+	*q = old[0 : n-1]
+	return x
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_3342/solution.py
+++ problems/problems_3342/solution.py
@@ -1,5 +1,8 @@
-import solution
+import heapq
+from math import inf
 from typing import *
+
+import solution
 
 
 class Solution(solution.Solution):
@@ -7,5 +10,21 @@ class Solution(solution.Solution):
         return self.minTimeToReach(test_input)
 
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
-        pass
-
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        pq = [(0, 0, 0)] # (time, x, y)
+        m, n = len(moveTime), len(moveTime[0])
+        explored = [[inf] * n for _ in range(m)]
+        explored[0][0] = 0
+        while pq:
+            time, x, y = heapq.heappop(pq)
+            if x == m - 1 and y == n - 1:
+                return time
+            if time > explored[x][y]:
+                continue
+            for dx, dy in directions:
+                if 0 <= (nx := x + dx) < m and 0 <= (ny := y + dy) < n:
+                    nt = max(time, moveTime[nx][ny]) + (x+y)%2 + 1
+                    if explored[nx][ny] > nt:
+                        explored[nx][ny] = nt
+                        heapq.heappush(pq, (nt, nx, ny))
+        return -1
