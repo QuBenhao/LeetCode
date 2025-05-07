b309ac55e476166e0234e17d5dc1fa4b8f808f51
qubh <qubh@xiaopeng.com>
2025-05-07 09:51:48 +0800

    test: LCR 055 solution
    go

--- problems/problems_LCR_055/solution.go
+++ problems/problems_LCR_055/solution.go
@@ -16,18 +16,31 @@ import (
  * }
  */
 type BSTIterator struct {
+	stack []*TreeNode
 }
 
 func Constructor(root *TreeNode) BSTIterator {
-
+	stack := []*TreeNode{}
+	for root != nil {
+		stack = append(stack, root)
+		root = root.Left
+	}
+	return BSTIterator{stack}
 }
 
 func (this *BSTIterator) Next() int {
-
+	node := this.stack[len(this.stack)-1]
+	this.stack = this.stack[:len(this.stack)-1]
+	nxt := node.Right
+	for nxt != nil {
+		this.stack = append(this.stack, nxt)
+		nxt = nxt.Left
+	}
+	return node.Val
 }
 
 func (this *BSTIterator) HasNext() bool {
-
+	return len(this.stack) > 0
 }
 
 /**
