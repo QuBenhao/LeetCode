4a51e58737835bd3cdd4f6d5afca17188e63feeb
qubh <qubh@xiaopeng.com>
2025-05-08 11:03:04 +0800

    test: 1812 solution
    go

--- problems/problems_1812/solution.go
+++ problems/problems_1812/solution.go
@@ -7,7 +7,7 @@ import (
 )
 
 func squareIsWhite(coordinates string) bool {
-    
+	return (int(coordinates[0]-'a')+int(coordinates[1]-'1'))%2 == 1
 }
 
 func Solve(inputJsonValues string) interface{} {
