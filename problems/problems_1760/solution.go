b34fc1359f6f8cf97c50b87c8d1fccc5716218a2
qubh <qubh@xiaopeng.com>
2025-05-08 10:58:19 +0800

    test: 1760 solution
    go

--- problems/problems_1760/solution.go
+++ problems/problems_1760/solution.go
@@ -7,7 +7,28 @@ import (
 )
 
 func minimumSize(nums []int, maxOperations int) int {
-    
+	helper := func(mx int) bool {
+		op := 0
+		for _, num := range nums {
+			if num > mx {
+				op += (num - 1) / mx
+				if op > maxOperations {
+					return true
+				}
+			}
+		}
+		return false
+	}
+	left, right := 1, int(1e9)
+	for left < right {
+		mid := left + (right-left)/2
+		if helper(mid) {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	return left
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_1760/testcase.py
+++ problems/problems_1760/testcase.py
@@ -11,6 +11,7 @@ class Testcase(testcase.Testcase):
         self.testcases.append(case(Input=([2, 4, 8, 2], 4), Output=2))
         self.testcases.append(case(Input=([7, 17], 2), Output=7))
         self.testcases.append(case(Input=([1], 1), Output=1))
+        self.testcases.append(case(Input=[[7,17],2], Output=7))
 
     def get_testcases(self):
         return self.testcases
