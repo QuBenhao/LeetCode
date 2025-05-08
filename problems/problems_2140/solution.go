6ae007f70203ddeb13bbcac0885d4795fd83feed
benhao <qubenhao2@gmail.com>
2025-05-08 22:49:37 +0800

    test: 2140 solution
    go

--- problems/problems_2140/solution.go
+++ problems/problems_2140/solution.go
@@ -7,7 +7,14 @@ import (
 )
 
 func mostPoints(questions [][]int) int64 {
-    
+	n := len(questions)
+	dp := make([]int64, n+1)
+	for i, question := range questions {
+		dp[i+1] = max(dp[i+1], dp[i])
+		nxt := min(n, i+question[1]+1)
+		dp[nxt] = max(dp[nxt], dp[i]+int64(question[0]))
+	}
+	return dp[n]
 }
 
 func Solve(inputJsonValues string) interface{} {
