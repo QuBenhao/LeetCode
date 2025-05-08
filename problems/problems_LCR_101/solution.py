17ceb0e63ec4a7f75361e7cbe4c80530c7bec935
benhao <qubenhao2@gmail.com>
2025-05-08 08:31:05 +0800

    test: LCR 101 solution
    py

--- problems/problems_LCR_101/solution.py
+++ problems/problems_LCR_101/solution.py
@@ -7,5 +7,14 @@ class Solution(solution.Solution):
         return self.canPartition(test_input)
 
     def canPartition(self, nums: List[int]) -> bool:
-        pass
+        s = sum(nums)
+        if s % 2 != 0:
+            return False
 
+        s //= 2
+        dp = [False] * (s + 1)
+        dp[0] = True
+        for num in nums:
+            for i in range(s, num - 1, -1):
+                dp[i] = dp[i] or dp[i - num]
+        return dp[s]
