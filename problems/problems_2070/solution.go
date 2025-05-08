cf5281c2d763e3e98904943f7386b00ab881473b
qubh <qubh@xiaopeng.com>
2025-05-08 17:24:15 +0800

    test: 2070 solution
    go

--- problems/problems_2070/solution.go
+++ problems/problems_2070/solution.go
@@ -3,11 +3,29 @@ package problem2070
 import (
 	"encoding/json"
 	"log"
+	"slices"
 	"strings"
 )
 
 func maximumBeauty(items [][]int, queries []int) []int {
-    
+	slices.SortFunc(items, func(a, b []int) int { return a[0] - b[0] })
+	n := len(queries)
+	ans := make([]int, n)
+	idxes := make([]int, n)
+	for i := range idxes {
+		idxes[i] = i
+	}
+	slices.SortFunc(idxes, func(a, b int) int { return queries[a] - queries[b] })
+	i, curMax := 0, 0
+	for _, idx := range idxes {
+		q := queries[idx]
+		for i < len(items) && items[i][0] <= q {
+			curMax = max(curMax, items[i][1])
+			i++
+		}
+		ans[idx] = curMax
+	}
+	return ans
 }
 
 func Solve(inputJsonValues string) interface{} {
