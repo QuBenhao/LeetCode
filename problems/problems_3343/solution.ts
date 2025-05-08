accee543fcc282f7abf4b5e2649720c6e6dba302
benhao <qubenhao2@gmail.com>
2025-05-09 06:41:39 +0800

    test: remove 3343
    deleted

--- problems/problems_3343/solution.ts
+++
@@ -1,9 +0,0 @@
-function countBalancedPermutations(num: string): number {
-    
-};
-
-export function Solve(inputJsonElement: string): any {
-	const inputValues: string[] = inputJsonElement.split("\n");
-	const num: string = JSON.parse(inputValues[0]);
-	return countBalancedPermutations(num);
-}
