22f97d48671eed2659d88ca78e0363e792cec628
qubh <qubh@xiaopeng.com>
2025-05-08 13:56:08 +0800

    test: 2012 solution
    go

--- problems/problems_2012/solution.go
+++ problems/problems_2012/solution.go
@@ -6,8 +6,23 @@ import (
 	"strings"
 )
 
-func sumOfBeauties(nums []int) int {
-    
+func sumOfBeauties(nums []int) (ans int) {
+	n := len(nums)
+	sufMin := make([]int, n)
+	sufMin[n-1] = nums[n-1]
+	for i := n - 2; i >= 0; i-- {
+		sufMin[i] = min(sufMin[i+1], nums[i])
+	}
+	preMax := nums[0]
+	for i := 1; i < n-1; i++ {
+		if preMax < nums[i] && nums[i] < sufMin[i+1] {
+			ans += 2
+		} else if nums[i-1] < nums[i] && nums[i] < nums[i+1] {
+			ans++
+		}
+		preMax = max(preMax, nums[i])
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
