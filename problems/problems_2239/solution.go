72234e7f61a439f5a2a68206d630bdbc9fc1361b
benhao <qubenhao2@gmail.com>
2025-05-09 22:55:09 +0800

    test: 2239 solution
    go

--- problems/problems_2239/solution.go
+++ problems/problems_2239/solution.go
@@ -3,11 +3,24 @@ package problem2239
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"strings"
 )
 
-func findClosestNumber(nums []int) int {
-    
+func findClosestNumber(nums []int) (ans int) {
+	minDist := math.MaxInt
+	for _, num := range nums {
+		if num >= 0 {
+			if num <= minDist {
+				ans = num
+				minDist = num
+			}
+		} else if -num < minDist {
+			ans = num
+			minDist = -num
+		}
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
