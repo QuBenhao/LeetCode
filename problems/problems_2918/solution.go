6de5e2d025383905beaf8cf4179db004888d6335
benhao <qubenhao2@gmail.com>
2025-05-10 07:30:33 +0800

    test: 2918 solution
    py, go

--- problems/problems_2918/solution.go
+++ problems/problems_2918/solution.go
@@ -7,7 +7,26 @@ import (
 )
 
 func minSum(nums1 []int, nums2 []int) int64 {
-    
+	var s1, s2 int64
+	var c1, c2 int
+	for _, num := range nums1 {
+		s1 += int64(num)
+		if num == 0 {
+			s1++
+			c1++
+		}
+	}
+	for _, num := range nums2 {
+		s2 += int64(num)
+		if num == 0 {
+			s2++
+			c2++
+		}
+	}
+	if (s1 < s2 && c1 == 0) || (s1 > s2 && c2 == 0) {
+		return -1
+	}
+	return max(s1, s2)
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_2918/solution.py
+++ problems/problems_2918/solution.py
@@ -7,5 +7,11 @@ class Solution(solution.Solution):
         return self.minSum(*test_input)
 
     def minSum(self, nums1: List[int], nums2: List[int]) -> int:
-        pass
-
+        s1, s2, c1, c2 = sum(nums1), sum(nums2), nums1.count(0), nums2.count(0)
+        if c1 == 0 and c2 == 0:
+            return s1 if s1 == s2 else -1
+        if c1 == 0:
+            return s1 if s2 + c2 <= s1 else -1
+        if c2 == 0:
+            return s2 if s1 + c1 <= s2 else -1
+        return max(s1 + c1, s2 + c2)
