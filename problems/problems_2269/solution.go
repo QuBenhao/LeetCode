ae9a167a868707f0ff2f00b0256dfb1cde8ed4ae
benhao <qubenhao2@gmail.com>
2025-05-10 00:00:22 +0800

    test: 2269 solution
    go

--- problems/problems_2269/solution.go
+++ problems/problems_2269/solution.go
@@ -3,11 +3,29 @@ package problem2269
 import (
 	"encoding/json"
 	"log"
+	"math"
+	"slices"
 	"strings"
 )
 
-func divisorSubstrings(num int, k int) int {
-    
+func divisorSubstrings(num int, k int) (ans int) {
+	power := int(math.Pow(10, float64(k-1)))
+	var nums []int
+	for val := num; val > 0; val /= 10 {
+		nums = append(nums, val%10)
+	}
+	slices.Reverse(nums)
+	cur := 0
+	for i, v := range nums {
+		cur = cur*10 + v
+		if i >= k-1 {
+			if cur != 0 && num%cur == 0 {
+				ans++
+			}
+			cur -= nums[i+1-k] * power
+		}
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
