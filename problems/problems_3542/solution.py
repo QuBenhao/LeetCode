5069dc58622a78be3f160e6568bc4004c921bbbd
benhao <qubenhao2@gmail.com>
2025-05-11 01:36:48 +0800

    test: 3542 solution
    py

--- problems/problems_3542/solution.py
+++ problems/problems_3542/solution.py
@@ -1,3 +1,5 @@
+from math import inf
+
 import solution
 from typing import *
 
@@ -7,5 +9,22 @@ class Solution(solution.Solution):
         return self.minOperations(test_input)
 
     def minOperations(self, nums: List[int]) -> int:
-        pass
-
+        n = len(nums)
+        left_idx = [-1] * n
+        right_idx = [n] * n
+        min_stack = []
+        for i, num in enumerate(nums):
+            while min_stack and num < nums[min_stack[-1]]:
+                idx = min_stack.pop()
+                right_idx[idx] = i
+            if num == 0:
+                left_idx[i] = inf
+            min_stack.append(i)
+        min_stack = []
+        for i in range(n-1, -1, -1):
+            num = nums[i]
+            while min_stack and num < nums[min_stack[-1]]:
+                idx = min_stack.pop()
+                left_idx[idx] = i
+            min_stack.append(i)
+        return len(set((l, r) for l, r in zip(left_idx, right_idx) if l != inf))
--- problems/problems_3542/testcase.py
+++ problems/problems_3542/testcase.py
@@ -10,6 +10,7 @@ class Testcase(testcase.Testcase):
 		self.testcases.append(case(Input=[0, 2], Output=1))
 		self.testcases.append(case(Input=[3, 1, 2, 1], Output=3))
 		self.testcases.append(case(Input=[1, 2, 1, 2, 1, 2], Output=4))
+		self.testcases.append(case(Input=[3,5,4], Output=3))
 
 	def get_testcases(self):
 		return self.testcases
