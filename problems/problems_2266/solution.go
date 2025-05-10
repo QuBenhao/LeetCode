4cd19e1d596533c6d42e8463350c4761aa948679
benhao <qubenhao2@gmail.com>
2025-05-10 08:12:40 +0800

    test: 2266 solution
    py, go

--- problems/problems_2266/solution.go
+++ problems/problems_2266/solution.go
@@ -6,8 +6,24 @@ import (
 	"strings"
 )
 
+const MOD = 1000_000_007
+
+var POSSIBILITIES = map[byte]int{'2': 3, '3': 3, '4': 3, '5': 3, '6': 3, '7': 4, '8': 3, '9': 4}
+
 func countTexts(pressedKeys string) int {
-    
+	n := len(pressedKeys)
+	dp := make([]int, n+1)
+	dp[0] = 1
+	for i := range n {
+		dp[i+1] = dp[i]
+		for j := i - 1; j >= 0 && i-j < POSSIBILITIES[pressedKeys[i]]; j-- {
+			if pressedKeys[i] != pressedKeys[j] {
+				break
+			}
+			dp[i+1] = (dp[i+1] + dp[j]) % MOD
+		}
+	}
+	return dp[n]
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_2266/solution.py
+++ problems/problems_2266/solution.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 import solution
 from typing import *
 
@@ -7,5 +9,20 @@ class Solution(solution.Solution):
         return self.countTexts(test_input)
 
     def countTexts(self, pressedKeys: str) -> int:
-        pass
+        mod = 10 ** 9 + 7
+        possibilities = {'2': 3, '3': 3, '4': 3, '5': 3, '6': 3, '7': 4, '8': 3, '9': 4}
+        n = len(pressedKeys)
 
+        @cache
+        def dfs(i: int) -> int:
+            if i == n:
+                return 1
+            cur = possibilities[pressedKeys[i]]
+            ans = 0
+            for nxt in range(i, min(i + cur, n)):
+                if pressedKeys[nxt] != pressedKeys[i]:
+                    break
+                ans = (ans + dfs(nxt + 1)) % mod
+            return ans
+
+        return dfs( 0)
