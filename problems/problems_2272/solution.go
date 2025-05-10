145bc14851b1618bdf053a5749cb7fa8e0d98a15
benhao <qubenhao2@gmail.com>
2025-05-10 10:38:17 +0800

    test: 2272 solution
    py, go

--- problems/problems_2272/solution.go
+++ problems/problems_2272/solution.go
@@ -3,11 +3,35 @@ package problem2272
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"strings"
 )
 
-func largestVariance(s string) int {
-    
+func largestVariance(s string) (ans int) {
+	var f0, f1 [26][26]int
+	for i := range f1 {
+		for j := range f1[i] {
+			f1[i][j] = math.MinInt
+		}
+	}
+
+	for _, ch := range s {
+		ch -= 'a'
+		// 遍历到 ch 时，只需计算 a=ch 或者 b=ch 的状态，其他状态和 ch 无关，f 值不变
+		for i := range 26 {
+			if i == int(ch) {
+				continue
+			}
+			// 假设出现次数最多的字母 a=ch，更新所有 b=i 的状态
+			f0[ch][i] = max(f0[ch][i], 0) + 1
+			f1[ch][i]++
+			// 假设出现次数最少的字母 b=ch，更新所有 a=i 的状态
+			f0[i][ch] = max(f0[i][ch], 0) - 1
+			f1[i][ch] = f0[i][ch]
+			ans = max(ans, f1[ch][i], f1[i][ch])
+		}
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_2272/solution.py
+++ problems/problems_2272/solution.py
@@ -1,3 +1,5 @@
+from math import inf
+
 import solution
 from typing import *
 
@@ -7,5 +9,18 @@ class Solution(solution.Solution):
         return self.largestVariance(test_input)
 
     def largestVariance(self, s: str) -> int:
-        pass
-
+        # 按小写字母枚举
+        f0 = [[0] * 26 for _ in range(26)] # 表示最大子数组和中, -1不一定出现
+        f1 = [[-inf] * 26 for _ in range(26)] # 表示最大子数组和中, -1一定出现
+        ans = 0
+        for ch in map(ord, s):
+            ch -= ord("a")
+            for i in range(26):
+                if i == ch:
+                    continue
+                f0[ch][i] = max(0, f0[ch][i]) + 1
+                f1[ch][i] += 1
+                f1[i][ch] = f0[i][ch] = max(f0[i][ch], 0) - 1
+                # 循环内更新最大方便统计了所有子串
+                ans = max(ans, f1[ch][i], f1[i][ch])
+        return ans
