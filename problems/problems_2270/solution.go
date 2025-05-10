7bfa7eee28d4fc3ea16a4368d4d41e5240a474fc
benhao <qubenhao2@gmail.com>
2025-05-10 08:16:44 +0800

    test: 2270 solution
    go

--- problems/problems_2270/solution.go
+++ problems/problems_2270/solution.go
@@ -6,8 +6,20 @@ import (
 	"strings"
 )
 
-func waysToSplitArray(nums []int) int {
-    
+func waysToSplitArray(nums []int) (ans int) {
+	n := len(nums)
+	s := 0
+	for _, num := range nums {
+		s += num
+	}
+	leftSum := 0
+	for i := range n - 1 {
+		leftSum += nums[i]
+		if leftSum >= s-leftSum {
+			ans++
+		}
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
