8999dfdb3f5b47a62125edd9a11815e660497498
benhao <qubenhao2@gmail.com>
2025-05-11 12:19:44 +0800

    feat: python contest template
    fast debugging

---
+++ python/contest.py
@@ -0,0 +1,22 @@
+import heapq
+from bisect import *
+from typing import *
+from functools import *
+from itertools import *
+from sortedcontainers import *
+from collections import *
+from heapq import *
+from math import *
+from object_libs.linked_list import ListNode
+from object_libs.tree import TreeNode
+
+
+class Solution:
+    pass
+
+def main():
+    sol = Solution()
+    # print()
+
+if __name__ == '__main__':
+    main()
