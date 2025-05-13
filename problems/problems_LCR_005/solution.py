66f5500dbff3201936dd8a1f3d7ce58fbb76c845
benhao <qubenhao2@gmail.com>
2025-05-13 09:22:33 +0800

    test: LCR 005 solution
    py

--- problems/problems_LCR_005/solution.py
+++ problems/problems_LCR_005/solution.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from itertools import combinations
+
 import solution
 from typing import *
 
@@ -7,5 +10,14 @@ class Solution(solution.Solution):
         return self.maxProduct(test_input)
 
     def maxProduct(self, words: List[str]) -> int:
-        pass
-
+        ans = 0
+        word_dict = defaultdict(int)
+        for i, word in enumerate(words):
+            cur = 0
+            for c in word:
+                cur |= 1 << (ord(c) - ord('a'))
+            word_dict[cur] = max(word_dict[cur], len(word))
+        for k1, k2 in combinations(word_dict.keys(), 2):
+            if k1 & k2 == 0:
+                ans = max(ans, word_dict[k1] * word_dict[k2])
+        return ans
