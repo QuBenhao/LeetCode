190cffb801916fe121565ee7d67b15f232e9be70
benhao <qubenhao2@gmail.com>
2025-05-13 09:59:59 +0800

    test: 3335, 3337 solution
    go

--- problems/problems_3335/solution.go
+++ problems/problems_3335/solution.go
@@ -6,8 +6,34 @@ import (
 	"strings"
 )
 
-func lengthAfterTransformations(s string, t int) int {
-    
+const MOD = 1000000007
+
+func lengthAfterTransformations(s string, t int) (ans int) {
+	count := make([]int, 26)
+	for _, r := range s {
+		count[r-'a']++
+	}
+	remain := t % 26
+	for range remain {
+		extra := count[25]
+		for i := 25; i > 0; i-- {
+			count[i] = count[i-1]
+		}
+		count[0] = extra
+		count[1] = (count[1] + extra) % MOD
+	}
+	for range (t - remain) / 26 {
+		extra := count[25]
+		for i := 25; i > 0; i-- {
+			count[i] = (count[i] + count[i-1]) % MOD
+		}
+		count[0] = (count[0] + extra) % MOD
+		count[1] = (count[1] + extra) % MOD
+	}
+	for _, c := range count {
+		ans = (ans + c) % MOD
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_3337/solution.go
+++ problems/problems_3337/solution.go
@@ -6,8 +6,59 @@ import (
 	"strings"
 )
 
-func lengthAfterTransformations(s string, t int, nums []int) int {
-    
+const MOD = 1000000007
+
+func multiply(a, b [][]int) [][]int {
+	n := len(a)
+	m := len(b[0])
+	p := len(b)
+	res := make([][]int, n)
+	for i := range res {
+		res[i] = make([]int, m)
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			for k := 0; k < p; k++ {
+				res[i][j] = (res[i][j] + a[i][k]*b[k][j]) % MOD
+			}
+		}
+	}
+	return res
+}
+
+func matrixPower(matrix [][]int, n int, res [][]int) [][]int {
+	for n > 0 {
+		if n%2 == 1 {
+			res = multiply(matrix, res)
+		}
+		matrix = multiply(matrix, matrix)
+		n /= 2
+	}
+	return res
+}
+
+func lengthAfterTransformations(s string, t int, nums []int) (ans int) {
+	matrix := make([][]int, 26)
+	for i := range matrix {
+		matrix[i] = make([]int, 26)
+	}
+	for i, c := range nums {
+		for j := i + 1; j < i+c+1; j++ {
+			matrix[j%26][i]++
+		}
+	}
+	f0 := make([][]int, 26)
+	for i := range f0 {
+		f0[i] = make([]int, 1)
+	}
+	for _, r := range s {
+		f0[r-'a'][0]++
+	}
+	res := matrixPower(matrix, t, f0)
+	for i := range 26 {
+		ans = (ans + res[i][0]) % MOD
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
