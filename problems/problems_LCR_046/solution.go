737b7b0b259b2fd1686300d3c7c025080757ff7a
benhao <qubenhao2@gmail.com>
2025-05-15 18:28:17 +0800

    test: 2900 solution
    go

--- problems/problems_LCR_046/solution.go
+++ problems/problems_LCR_046/solution.go
@@ -13,8 +13,26 @@ import (
  *     Right *TreeNode
  * }
  */
-func rightSideView(root *TreeNode) []int {
-
+func rightSideView(root *TreeNode) (ans []int) {
+	if root == nil {
+		return
+	}
+	q := []*TreeNode{root}
+	for len(q) > 0 {
+		n := len(q)
+		ans = append(ans, q[0].Val)
+		for i := range n {
+			node := q[i]
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+		}
+		q = q[n:]
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
