0aa0b83831297f746371922126b75f90603a6373
benhao <qubenhao2@gmail.com>
2025-05-15 18:15:49 +0800

    test: 2900 solution
    go

--- problems/problems_2900/solution.go
+++ problems/problems_2900/solution.go
@@ -7,7 +7,20 @@ import (
 )
 
 func getLongestSubsequence(words []string, groups []int) []string {
-    
+	var dp0, dp1 []string
+	for i, word := range words {
+		if len(dp0)%2 == groups[i] {
+			dp0 = append(dp0, word)
+		}
+		if len(dp1)%2 == groups[i]^1 {
+			dp1 = append(dp1, word)
+		}
+	}
+	if len(dp0) > len(dp1) {
+		return dp0
+	} else {
+		return dp1
+	}
 }
 
 func Solve(inputJsonValues string) interface{} {
