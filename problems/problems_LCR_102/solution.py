98fe2ff41a9fdf64a0151f07bd3321ce501d8217
benhao <qubenhao2@gmail.com>
2025-05-16 07:48:58 +0800

    test: LCR 102 solution
    py

--- problems/problems_LCR_102/solution.py
+++ problems/problems_LCR_102/solution.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 import solution
 from typing import *
 
@@ -7,5 +9,12 @@ class Solution(solution.Solution):
         return self.findTargetSumWays(*test_input)
 
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        pass
+        @cache
+        def dfs(i: int, cur_sum: int):
+            if i == len(nums):
+                return cur_sum == target
+            ans = dfs(i + 1, cur_sum + nums[i])
+            ans += dfs(i + 1, cur_sum - nums[i])
+            return ans
 
+        return dfs(0, 0)
--- problems/problems_LCR_102/testcase.py
+++ problems/problems_LCR_102/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=[[1, 1, 1, 1, 1], 3], Output=5))
 		self.testcases.append(case(Input=[[1], 1], Output=1))
+		self.testcases.append(case(Input=[[43,9,26,24,39,40,20,11,18,13,14,30,48,47,37,24,32,32,2,26],47], Output=5844))
 
 	def get_testcases(self):
 		return self.testcases
