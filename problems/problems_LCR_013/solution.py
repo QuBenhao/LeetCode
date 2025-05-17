63405a23c558904e0b50522b031ad16096433730
benhao <qubenhao2@gmail.com>
2025-05-18 07:50:55 +0800

    test: LCR 067, LCR 013 solution
    py

--- problems/problems_LCR_013/solution.py
+++ problems/problems_LCR_013/solution.py
@@ -12,8 +12,13 @@ class Solution(solution.Solution):
 
 class NumMatrix:
     def __init__(self, matrix: List[List[int]]):
-        pass
+        m, n = len(matrix), len(matrix[0])
+        prefix_sum = [[0] * (n + 1) for _ in range(m + 1)]
+        for i, row in enumerate(matrix):
+            for j, num in enumerate(row):
+                prefix_sum[i + 1][j + 1] = prefix_sum[i][j + 1] + prefix_sum[i + 1][j] - prefix_sum[i][j] + num
+        self.prefix_sum = prefix_sum
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        pass
+        return self.prefix_sum[row2+1][col2+1] - self.prefix_sum[row1][col2+1] - self.prefix_sum[row2+1][col1] + self.prefix_sum[row1][col1]
 
--- problems/problems_LCR_067/solution.py
+++ problems/problems_LCR_067/solution.py
@@ -7,5 +7,34 @@ class Solution(solution.Solution):
         return self.findMaximumXOR(test_input)
 
     def findMaximumXOR(self, nums: List[int]) -> int:
-        pass
+        root = {}
 
+        def insert(word):
+            node = root
+            for char in word:
+                if char not in node:
+                    node[char] = {}
+                node = node[char]
+            node['#'] = word
+
+        def search(word) -> Optional[str]:
+            node = root
+            for char in word:
+                c = '1' if char == '0' else '0'
+                if c in node:
+                    node = node[c]
+                    continue
+                if char not in node:
+                    return None
+                node = node[char]
+            return node.get('#', None)
+
+        ans = 0
+        b = len(bin(max(nums))) - 2
+        for num in nums:
+            wd = bin(num)[2:].zfill(b)
+            if mx := search(wd):
+                ans = max(ans, int(mx, 2) ^ num)
+            insert(wd)
+
+        return ans
