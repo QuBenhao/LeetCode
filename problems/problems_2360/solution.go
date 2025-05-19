b38bc02298c7d94f7847b53075aa01de7333bf14
qubh <qubh@xiaopeng.com>
2025-05-19 17:48:06 +0800

    test: 2360 solution
    go

--- problems/problems_2360/solution.go
+++ problems/problems_2360/solution.go
@@ -6,8 +6,23 @@ import (
 	"strings"
 )
 
-func longestCycle(edges []int) int {
-    
+func longestCycle(edges []int) (ans int) {
+	n := len(edges)
+	visited := make([]int, n)
+	ans = -1
+	curTime := 1
+	for i := range n {
+		startTime := curTime // 本轮出发时间
+		for i != -1 && visited[i] == 0 {
+			visited[i] = curTime
+			curTime++
+			i = edges[i]
+		}
+		if i != -1 && visited[i] >= startTime { // 被走过多次的点一定在环内, 环长为最大的时间差
+			ans = max(ans, curTime-visited[i])
+		}
+	}
+	return
 }
 
 func Solve(inputJsonValues string) interface{} {
