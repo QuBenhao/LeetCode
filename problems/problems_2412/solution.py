8d57b0f0ecc1af121172dc9ec192d65e184a7298
qubh <qubh@xiaopeng.com>
2025-05-19 18:39:36 +0800

    test: 2412 solution
    py

--- problems/problems_2412/solution.py
+++ problems/problems_2412/solution.py
@@ -7,5 +7,12 @@ class Solution(solution.Solution):
         return self.minimumMoney(test_input)
 
     def minimumMoney(self, transactions: List[List[int]]) -> int:
-        pass
-
+        # 最坏交易顺序
+        total_lose = 0
+        mx = 0
+        for cost, cashback in transactions:
+            total_lose += max(cost - cashback, 0)
+            # 如果是亏钱的，那么至少需要在所有亏钱后依然足够，即 init >= total_lose + cost - (cost - cashback) = total_lose + cashback
+            # 如果是赚钱的，那么至少需要在所有亏钱后依然足够，即 init >= total_lose + cost
+            mx = max(mx, min(cost, cashback))
+        return total_lose + mx
--- problems/problems_2412/testcase.py
+++ problems/problems_2412/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=[[2, 1], [5, 0], [4, 2]], Output=10))
 		self.testcases.append(case(Input=[[3, 0], [0, 3]], Output=3))
+		self.testcases.append(case(Input=[[6,10],[10,4],[8,2],[5,2],[9,4],[6,6],[5,5],[7,3]], Output=30))
 
 	def get_testcases(self):
 		return self.testcases
