3d6e204b121855bf35e7964692e39a34631a88ff
qubh <qubh@xiaopeng.com>
2025-05-20 18:28:55 +0800

    test: LCR 018 solution
    go

--- problems/problems_LCR_018/solution.go
+++ problems/problems_LCR_018/solution.go
@@ -1,13 +1,32 @@
 package problemLCR_018
 
 import (
+	"bytes"
 	"encoding/json"
 	"log"
 	"strings"
 )
 
 func isPalindrome(s string) bool {
+	isAlphaNumeric := func(c byte) bool {
+		return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')
+	}
 
+	byteArr := []byte(s)
+	byteArr = bytes.ToLower(byteArr)
+
+	for left, right := 0, len(s)-1; left < right; left, right = left+1, right-1 {
+		for left < right && !isAlphaNumeric(s[left]) {
+			left++
+		}
+		for left < right && !isAlphaNumeric(s[right]) {
+			right--
+		}
+		if byteArr[left] != byteArr[right] {
+			return false
+		}
+	}
+	return true
 }
 
 func Solve(inputJsonValues string) any {
