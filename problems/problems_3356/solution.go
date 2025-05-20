e1567a23c1ee4e0c6d2b15781aa08753e6967ff8
benhao <qubenhao2@gmail.com>
2025-05-21 07:43:14 +0800

    test: 3356, LCR 103 solution
    go

--- problems/problems_3356/solution.go
+++ problems/problems_3356/solution.go
@@ -7,7 +7,59 @@ import (
 )
 
 func minZeroArray(nums []int, queries [][]int) int {
-    
+	//// 二分+差分
+	//n, m := len(nums), len(queries)
+	//check := func(x int) bool {
+	//	diff := make([]int, n+1)
+	//	for i := range x {
+	//		l, r, v := queries[i][0], queries[i][1], queries[i][2]
+	//		diff[l] += v
+	//		diff[r+1] -= v
+	//	}
+	//	s := 0
+	//	for i, num := range nums {
+	//		s += diff[i]
+	//		if s < num {
+	//			return false
+	//		}
+	//	}
+	//	return true
+	//}
+	//left, right := 0, m+1
+	//for left < right {
+	//	mid := (left + right) / 2
+	//	if check(mid) {
+	//		right = mid
+	//	} else {
+	//		left = mid + 1
+	//	}
+	//}
+	//if left == m+1 {
+	//	return -1
+	//}
+	//return left
+
+	// 差分+双指针
+	n, m := len(nums), len(queries)
+	diff := make([]int, n+1)
+	k := 0
+	cur := 0
+	for i, num := range nums {
+		cur += diff[i]
+		for k < m && cur < num {
+			l, r, v := queries[k][0], queries[k][1], queries[k][2]
+			diff[l] += v
+			diff[r+1] -= v
+			if l <= i && i <= r {
+				cur += v
+			}
+			k++
+		}
+		if cur < num {
+			return -1
+		}
+	}
+	return k
 }
 
 func Solve(inputJsonValues string) any {
--- problems/problems_LCR_103/solution.go
+++ problems/problems_LCR_103/solution.go
@@ -7,7 +7,20 @@ import (
 )
 
 func coinChange(coins []int, amount int) int {
-
+	dp := make([]int, amount+1)
+	for i := range dp {
+		dp[i] = amount + 1
+	}
+	dp[0] = 0
+	for _, coin := range coins {
+		for i := coin; i <= amount; i++ {
+			dp[i] = min(dp[i], dp[i-coin]+1)
+		}
+	}
+	if dp[amount] == amount+1 {
+		return -1
+	}
+	return dp[amount]
 }
 
 func Solve(inputJsonValues string) any {
