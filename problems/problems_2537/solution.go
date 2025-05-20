f66904eb11566c8d04d16b59d401c19948d4fce0
qubh <qubh@xiaopeng.com>
2025-05-20 14:13:37 +0800

    test: 2537 solution
    go

--- problems/problems_2537/solution.go
+++ problems/problems_2537/solution.go
@@ -6,8 +6,21 @@ import (
 	"strings"
 )
 
-func countGood(nums []int, k int) int64 {
-
+func countGood(nums []int, k int) (ans int64) {
+	counter := map[int]int{}
+	left, pairs := 0, 0
+	for _, num := range nums {
+		pairs += counter[num]
+		counter[num]++
+		for pairs >= k {
+			counter[nums[left]]--
+			pairs -= counter[nums[left]]
+			left++
+		}
+		// 以当前num为右端点，0~left为左端点的子数组均满足条件
+		ans += int64(left)
+	}
+	return
 }
 
 func Solve(inputJsonValues string) any {
