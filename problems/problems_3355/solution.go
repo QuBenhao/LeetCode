fd9438575c92f0603c2c4c1208c25fccb93b2d1f
qubh <qubh@xiaopeng.com>
2025-05-20 10:50:04 +0800

    test: 3355, LCR 028 solution
    go

--- problems/problems_3355/solution.go
+++ problems/problems_3355/solution.go
@@ -7,7 +7,20 @@ import (
 )
 
 func isZeroArray(nums []int, queries [][]int) bool {
-    
+	n := len(nums)
+	diff := make([]int, n+1)
+	for _, query := range queries {
+		diff[query[0]]++
+		diff[query[1]+1]--
+	}
+	s := 0
+	for i := range n {
+		s += diff[i]
+		if s < nums[i] {
+			return false
+		}
+	}
+	return true
 }
 
 func Solve(inputJsonValues string) interface{} {
--- problems/problems_LCR_028/solution.go
+++ problems/problems_LCR_028/solution.go
@@ -1,6 +1,9 @@
 package problemLCR_028
 
 import (
+	"encoding/json"
+	. "leetCode/golang/double_linked_node_child"
+	"log"
 	"strings"
 )
 
@@ -15,13 +18,41 @@ import (
  */
 
 func flatten(root *Node) *Node {
-    
+	var dfs func(*Node) *Node
+	dfs = func(node *Node) *Node {
+		if node == nil {
+			return node
+		}
+		last := node
+		for node != nil {
+			if node.Child != nil {
+				childLast := dfs(node.Child)
+				childLast.Next = node.Next
+				if node.Next != nil {
+					node.Next.Prev = childLast
+				}
+				node.Next = node.Child
+				node.Child.Prev = node
+				node.Child = nil
+				node = childLast
+			}
+			last = node
+			node = node.Next
+		}
+		return last
+	}
+	dfs(root)
+	return root
 }
 
-func Solve(inputJsonValues string) interface{} {
+func Solve(inputJsonValues string) any {
 	inputValues := strings.Split(inputJsonValues, "\n")
 	var root *Node
+	var arr0 []any
+	if err := json.Unmarshal([]byte(inputValues[0]), &arr0); err != nil {
+		log.Fatal(err)
+	}
+	root = IntArrayToDoubleLinkedNode(arr0)
 
-
-	return FIXME(flatten(root))
+	return DoubleLinkedNodeToIntArray(flatten(root))
 }
