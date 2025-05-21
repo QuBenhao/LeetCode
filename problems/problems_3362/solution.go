6b2161189fdcdf6da1c65beec75c3d986ad2fcb5
benhao <qubenhao2@gmail.com>
2025-05-22 07:54:00 +0800

    test: 3362, LCR 087 solution
    go

--- problems/problems_3362/solution.go
+++ problems/problems_3362/solution.go
@@ -1,13 +1,46 @@
 package problem3362
 
 import (
+	"container/heap"
 	"encoding/json"
 	"log"
+	"slices"
+	"sort"
 	"strings"
 )
 
 func maxRemoval(nums []int, queries [][]int) int {
-    
+	slices.SortFunc(queries, func(a, b []int) int { return a[0] - b[0] })
+	h := &hp{}
+	cur, j := 0, 0
+	diff := make([]int, len(nums)+1)
+	for i, num := range nums {
+		cur += diff[i]
+		for ; j < len(queries) && queries[j][0] <= i; j++ {
+			heap.Push(h, queries[j][1])
+		}
+		for cur < num && h.Len() > 0 && h.IntSlice[0] >= i {
+			cur++
+			diff[heap.Pop(h).(int)+1]--
+		}
+		if cur < num {
+			return -1
+		}
+	}
+	return h.Len()
+}
+
+type hp struct{ sort.IntSlice }
+
+func (h hp) Less(i, j int) bool { return h.IntSlice[i] > h.IntSlice[j] }
+func (h *hp) Push(x any) {
+	h.IntSlice = append(h.IntSlice, x.(int))
+}
+func (h *hp) Pop() any {
+	n := h.IntSlice
+	x := n[len(n)-1]
+	h.IntSlice = n[:len(n)-1]
+	return x
 }
 
 func Solve(inputJsonValues string) any {
--- problems/problems_LCR_087/solution.go
+++ problems/problems_LCR_087/solution.go
@@ -6,8 +6,57 @@ import (
 	"strings"
 )
 
-func restoreIpAddresses(s string) []string {
+func restoreIpAddresses(s string) (ans []string) {
+	isValid := func(seg string) bool {
+		if seg[0] == '0' && len(seg) > 1 {
+			return false
+		}
+		num := 0
+		for i := 0; i < len(seg); i++ {
+			num = num*10 + int(seg[i]-'0')
+		}
+		return num <= 255
+	}
 
+	if len(s) < 4 || len(s) > 12 {
+		return
+	}
+	var cur []string
+	n := len(s)
+
+	var backtrack func(int)
+	backtrack = func(start int) {
+		if start == n {
+			return
+		}
+		if len(cur) == 3 {
+			if n-start > 3 || (s[start] == '0' && start < n-1) {
+				return
+			}
+			if isValid(s[start:]) {
+				cur = append(cur, s[start:])
+				ans = append(ans, strings.Join(cur, "."))
+				cur = cur[:len(cur)-1]
+			}
+			return
+		}
+		if s[start] == '0' {
+			cur = append(cur, "0")
+			backtrack(start + 1)
+			cur = cur[:len(cur)-1]
+			return
+		}
+		for i := 1; i <= min(n-start, 3); i++ {
+			seg := s[start : start+i]
+			if isValid(seg) {
+				cur = append(cur, seg)
+				backtrack(start + i)
+				cur = cur[:len(cur)-1]
+			}
+		}
+	}
+	backtrack(0)
+	return
 }
 
 func Solve(inputJsonValues string) any {
