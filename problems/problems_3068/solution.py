b6de6b72c5fec43af980dd527d70eb261d3e5522
benhao <qubenhao2@gmail.com>
2025-05-23 07:23:12 +0800

    test: 3068, LCR 119 solution
    py

--- problems/problems_3068/solution.py
+++ problems/problems_3068/solution.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+
+from math import inf
+
 import solution
 from typing import *
 
@@ -7,5 +11,20 @@ class Solution(solution.Solution):
         return self.maximumValueSum(*test_input)
 
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
-        pass
+        graph = defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
 
+        def dfs(_u, _pa) -> Tuple[int, int]:
+            # f0: 以_u为根的子树中, 除去_u, _u和k异或了偶数次的最大值
+            # f1: 以_u为根的子树中, 除去_u, _u和k异或了奇数次的最大值
+            f0, f1 = 0, -inf
+            for _v in graph[_u]:
+                if _v == _pa:
+                    continue
+                r0, r1 = dfs(_v, _u)
+                f0, f1 = max(f0 + r0, f1 + r1), max(f0 + r1, f1 + r0)
+            return max(f0 + nums[_u], f1 + (nums[_u] ^ k)), max(f0 + (nums[_u] ^ k), f1 + nums[_u])
+
+        return dfs(0, -1)[0]
--- problems/problems_LCR_119/solution.py
+++ problems/problems_LCR_119/solution.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import solution
 from typing import *
 
@@ -7,5 +9,52 @@ class Solution(solution.Solution):
         return self.longestConsecutive(test_input)
 
     def longestConsecutive(self, nums: List[int]) -> int:
-        pass
+        class UnionFind:
+            def __init__(self, size):
+                self.parent = list(range(size))
+                self.rank = [1] * size
 
+            def find(self, x):
+                while self.parent[x] != x:
+                    self.parent[x] = self.parent[self.parent[x]]  # 路径压缩
+                    x = self.parent[x]
+                return x
+
+            def union(self, x, y):
+                root_x = self.find(x)
+                root_y = self.find(y)
+
+                if root_x == root_y:
+                    return False  # 已经在同一集合
+
+                # 按秩合并
+                if self.rank[root_x] > self.rank[root_y]:
+                    self.parent[root_y] = root_x
+                else:
+                    self.parent[root_x] = root_y
+                    if self.rank[root_x] == self.rank[root_y]:
+                        self.rank[root_y] += 1
+                return True
+
+            def is_connected(self, x, y):
+                return self.find(x) == self.find(y)
+
+        n = len(nums)
+        uf = UnionFind(n)
+        idx_map = {}
+        for i, num in enumerate(nums):
+            if num in idx_map:
+                continue
+            idx_map[num] = i
+            if num - 1 in idx_map:
+                uf.union(i, idx_map[num - 1])
+            if num + 1 in idx_map:
+                uf.union(i, idx_map[num + 1])
+        ans = 0
+        # 统计每个集合的大小
+        size_map = defaultdict(int)
+        for i in idx_map.values():
+            root = uf.find(i)
+            size_map[root] += 1
+            ans = max(ans, size_map[root])
+        return ans
