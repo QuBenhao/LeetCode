cf432c20b11c321696fa09d368feba091eed912b
qubh <qubh@xiaopeng.com>
2025-05-22 12:45:36 +0800

    test: multithreading 1114
    py, go

---
+++ multi_threading/1114/solution.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+)
+
+type Foo struct {
+	muFirst  *sync.Mutex
+	muSecond *sync.Mutex
+}
+
+func Constructor() *Foo {
+	muFirst := sync.Mutex{}
+	muSecond := sync.Mutex{}
+	muFirst.Lock()
+	muSecond.Lock()
+	return &Foo{&muFirst, &muSecond}
+}
+
+func (f *Foo) first(printFirst func()) {
+	printFirst()
+	f.muFirst.Unlock()
+}
+
+func (f *Foo) second(printSecond func()) {
+	f.muFirst.Lock()
+	printSecond()
+	f.muFirst.Unlock()
+	f.muSecond.Unlock()
+}
+
+func (f *Foo) third(printThird func()) {
+	f.muSecond.Lock()
+	printThird()
+}
+
+func main() {
+	foo := Constructor()
+	threads := make([]func(), 3)
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+	threads[0] = func() { foo.first(func() { fmt.Println("first") }); wg.Done() }
+	threads[1] = func() { foo.second(func() { fmt.Println("second") }); wg.Done() }
+	threads[2] = func() { foo.third(func() { fmt.Println("third") }); wg.Done() }
+	rand.Shuffle(len(threads), func(i, j int) { threads[i], threads[j] = threads[j], threads[i] })
+	for _, thread := range threads {
+		go thread()
+	}
+	wg.Wait()
+}
---
+++ multi_threading/1114/solution.py
@@ -0,0 +1,39 @@
+import random
+import threading
+from typing import Callable
+
+
+class Foo:
+    def __init__(self):
+        self.first_lock = threading.Lock()
+        self.second_lock = threading.Lock()
+        self.first_lock.acquire()
+        self.second_lock.acquire()
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.first_lock.release()
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        with self.first_lock:
+            # printSecond() outputs "second". Do not change or remove this line.
+            printSecond()
+            self.second_lock.release()
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        with self.second_lock:
+            # printThird() outputs "third". Do not change or remove this line.
+            printThird()
+
+
+if __name__ == "__main__":
+    foo = Foo()
+    thread_a = threading.Thread(target=foo.first, args=(lambda: print("first"),))
+    thread_b = threading.Thread(target=foo.second, args=(lambda: print("second"),))
+    thread_c = threading.Thread(target=foo.third, args=(lambda: print("third"),))
+
+    threads = [thread_a, thread_b, thread_c]
+    random.shuffle(threads)
+    for thread in threads:
+        thread.start()
