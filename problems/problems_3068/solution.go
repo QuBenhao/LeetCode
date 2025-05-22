303242ad9ee30a856f175a0ae7cdbff14c0a1d70
benhao <qubenhao2@gmail.com>
2025-05-23 07:41:26 +0800

    test: 3068, LCR 119 solution
    go

--- problems/problems_3068/solution.go
+++ problems/problems_3068/solution.go
@@ -3,11 +3,39 @@ package problem3068
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"strings"
 )
 
+type pair struct {
+	x int64
+	y int64
+}
+
 func maximumValueSum(nums []int, k int, edges [][]int) int64 {
-    
+	graph := make(map[int][]int)
+	for _, edge := range edges {
+		graph[edge[0]] = append(graph[edge[0]], edge[1])
+		graph[edge[1]] = append(graph[edge[1]], edge[0])
+	}
+
+	var dfs func(int, int) pair
+	dfs = func(node, parent int) pair {
+		var f0, f1 int64
+		f1 = math.MinInt64
+		for _, neigh := range graph[node] {
+			if neigh == parent {
+				continue
+			}
+			child := dfs(neigh, node)
+			f0, f1 = max(f0+child.x, f1+child.y), max(f0+child.y, f1+child.x)
+		}
+		return pair{
+			max(f0+int64(nums[node]), f1+int64(nums[node]^k)),
+			max(f1+int64(nums[node]), f0+int64(nums[node]^k)),
+		}
+	}
+	return dfs(0, -1).x
 }
 
 func Solve(inputJsonValues string) any {
--- problems/problems_LCR_119/solution.go
+++ problems/problems_LCR_119/solution.go
@@ -6,8 +6,80 @@ import (
 	"strings"
 )
 
-func longestConsecutive(nums []int) int {
+type UnionFind struct {
+	parent []int
+	rank   []int
+}
 
+func NewUnionFind(size int) *UnionFind {
+	uf := &UnionFind{
+		parent: make([]int, size),
+		rank:   make([]int, size),
+	}
+	for i := range uf.parent {
+		uf.parent[i] = i
+		uf.rank[i] = 1
+	}
+	return uf
+}
+
+func (uf *UnionFind) Find(x int) int {
+	for uf.parent[x] != x {
+		uf.parent[x] = uf.parent[uf.parent[x]] // 路径压缩
+		x = uf.parent[x]
+	}
+	return x
+}
+
+func (uf *UnionFind) Union(x, y int) bool {
+	rootX := uf.Find(x)
+	rootY := uf.Find(y)
+
+	if rootX == rootY {
+		return false // 已经在同一集合
+	}
+
+	// 按秩合并
+	if uf.rank[rootX] > uf.rank[rootY] {
+		uf.parent[rootY] = rootX
+	} else {
+		uf.parent[rootX] = rootY
+		if uf.rank[rootX] == uf.rank[rootY] {
+			uf.rank[rootY]++
+		}
+	}
+	return true
+}
+
+func (uf *UnionFind) IsConnected(x, y int) bool {
+	return uf.Find(x) == uf.Find(y)
+}
+
+func longestConsecutive(nums []int) (ans int) {
+	n := len(nums)
+	s := make(map[int]int)
+	for i, num := range nums {
+		if _, ok := s[num]; ok {
+			continue
+		}
+		s[num] = i
+	}
+	uf := NewUnionFind(n)
+	for k, idx := range s {
+		if i, ok := s[k-1]; ok {
+			uf.Union(idx, i)
+		}
+		if i, ok := s[k+1]; ok {
+			uf.Union(idx, i)
+		}
+	}
+	sizeCounts := make(map[int]int)
+	for i := range n {
+		root := uf.Find(i)
+		sizeCounts[root]++
+		ans = max(ans, sizeCounts[root])
+	}
+	return
 }
 
 func Solve(inputJsonValues string) any {
