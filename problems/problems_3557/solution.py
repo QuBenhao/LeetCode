035cab0e711d358ff9a919c7604e2cbcad607cc8
benhao <qubenhao2@gmail.com>
2025-05-25 15:59:22 +0800

    test: biweekly contest 157 solution
    3556, 3557, 3558, 3559 rank#70

--- problems/problems_3557/solution.py
+++ problems/problems_3557/solution.py
@@ -10,17 +10,13 @@ class Solution(solution.Solution):
         return self.maxSubstrings(test_input)
 
     def maxSubstrings(self, word: str) -> int:
-        idx_map = defaultdict(list)
+        ans = 0
+        pos = {}
         for i, c in enumerate(word):
-            idx_map[c].append(i)
-        n = len(word)
-        dp = [0] * (n + 1)
-        for i, c in enumerate(word):
-            idx = bisect_left(idx_map[c], max(i-1,0))
-            while idx >= 0 and i - idx_map[c][idx] < 3:
-                idx -= 1
-            if idx >= 0 and i - idx_map[c][idx] >= 3:
-                dp[i + 1] = max(dp[i], dp[idx_map[c][idx]] + 1)
-            else:
-                dp[i + 1] = dp[i]
-        return dp[n]
+            if c not in pos:
+                pos[c] = i
+            elif i - pos[c] > 2:
+                # 右端点越小越好，找到就清除
+                ans += 1
+                pos.clear()
+        return ans
