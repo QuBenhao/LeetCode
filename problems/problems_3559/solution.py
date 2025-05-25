34e1b385083e8bbb5666d114fe1169c91556faf2
benhao <qubenhao2@gmail.com>
2025-05-25 15:40:35 +0800

    test: biweekly contest 157 solution
    3556, 3557, 3558, 3559 rank#70

--- problems/problems_3559/solution.py
+++ problems/problems_3559/solution.py
@@ -19,32 +19,64 @@ class Solution(solution.Solution):
                 exp //= 2
             return result
 
-        n = len(edges) + 1
-        graph = defaultdict(list)
-        for u, v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
-        depth = [0] * (n + 1)
-        parent = [0] * (n + 1)
-        def dfs(node: int, par: int):
-            for neighbor in graph[node]:
-                if neighbor != par:
-                    depth[neighbor] = depth[node] + 1
-                    parent[neighbor] = node
-                    dfs(neighbor, node)
-        dfs(1, 0)
+        class TreeAncestor:
+            def __init__(self, edges: List[List[int]]):
+                n = len(edges) + 1
+                m = n.bit_length()
+                graph = defaultdict(list)
+                for x, y in edges:  # 节点编号从 1 开始
+                    graph[x-1].append((y-1, 1))
+                    graph[y-1].append((x-1, 1))
+
+                depth = [0] * n
+                pa = [[-1] * m for _ in range(n)]
+                distance = [0] * n
+
+                def dfs(x: int, fa: int) -> None:
+                    pa[x][0] = fa
+                    for y, w in graph[x]:
+                        if y != fa:
+                            depth[y] = depth[x] + 1
+                            distance[y] = distance[x] + w
+                            dfs(y, x)
+
+                dfs(0, -1)
+
+                for i in range(m - 1):
+                    for x in range(n):
+                        if (p := pa[x][i]) != -1:
+                            pa[x][i + 1] = pa[p][i]
+                self.depth = depth
+                self.pa = pa
+                self.distance = distance
+
+            def get_kth_ancestor(self, node: int, k: int) -> int:
+                for i in range(k.bit_length()):
+                    if k >> i & 1:  # k 二进制从低到高第 i 位是 1
+                        node = self.pa[node][i]
+                return node
+
+            # 返回 x 和 y 的最近公共祖先（节点编号从 0 开始）
+            def get_lca(self, x: int, y: int) -> int:
+                if self.depth[x] > self.depth[y]:
+                    x, y = y, x
+                # 使 y 和 x 在同一深度
+                y = self.get_kth_ancestor(y, self.depth[y] - self.depth[x])
+                if y == x:
+                    return x
+                for i in range(len(self.pa[x]) - 1, -1, -1):
+                    px, py = self.pa[x][i], self.pa[y][i]
+                    if px != py:
+                        x, y = px, py  # 同时往上跳 2**i 步
+                return self.pa[x][0]
+
+            def get_distance(self, x: int, y: int) -> int:
+                return self.distance[x] + self.distance[y] - 2 * self.distance[self.get_lca(x, y)]
+
+        ta = TreeAncestor(edges)
         mod = 10**9 + 7
-        ans = [0] * len(queries)
-        def lca(a: int, b: int, parent: List[int]) -> int:
-            while a != b:
-                if depth[a] > depth[b]:
-                    a = parent[a]
-                else:
-                    b = parent[b]
-            return a
-
-        for i, (a, b) in enumerate(queries):
-            d = depth[a] + depth[b] - 2 * depth[lca(a, b, parent)] - 1
-            ans[i] = fast_pow(2, d, mod) if d >= 0 else 0
+        ans = []
+        for a, b in queries:
+            dis = ta.get_distance(a-1, b-1)
+            ans.append(fast_pow(2, dis-1, mod) if dis > 0 else 0)
         return ans
-
