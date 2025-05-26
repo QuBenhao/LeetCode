209dc5c2eeafa0f8906cfe9b446dbb3f9fd9ae2e
qubh <qubh@xiaopeng.com>
2025-05-26 16:16:49 +0800

    test: 1857, LCR 077 solution
    c++

--- problems/problems_1857/Solution.cpp
+++ problems/problems_1857/Solution.cpp
@@ -1,29 +1,65 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
+#include <deque>
+#include <vector>
 
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    int largestPathValue(string colors, vector<vector<int>>& edges) {
-        
+  int largestPathValue(string colors, vector<vector<int>> &edges) {
+    size_t n = colors.length();
+    vector<vector<int>> graph(n);
+    vector<int> indegree(n, 0);
+    for (auto edge : edges) {
+      graph[edge[0]].push_back(edge[1]);
+      indegree[edge[1]]++;
     }
+    deque<int> queue;
+    vector<vector<int>> count(n, vector<int>(26, 0));
+    for (size_t i = 0; i < n; i++) {
+      if (indegree[i] == 0) {
+        queue.push_back(i);
+      }
+    }
+    int ans = 0;
+    size_t c = 0;
+    while (!queue.empty()) {
+      auto node = queue.front();
+      queue.pop_front();
+      c++;
+      ans = max(ans, ++count[node][colors[node] - 'a']);
+      for (auto child : graph[node]) {
+        for (int i = 0; i < 26; i++) {
+          count[child][i] = max(count[child][i], count[node][i]);
+        }
+        indegree[child]--;
+        if (indegree[child] == 0) {
+          queue.push_back(child);
+        }
+      }
+    }
+    if (c < n) {
+      return -1;
+    }
+    return ans;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	string colors = json::parse(inputArray.at(0));
-	vector<vector<int>> edges = json::parse(inputArray.at(1));
-	return solution.largestPathValue(colors, edges);
+  Solution solution;
+  string colors = json::parse(inputArray.at(0));
+  vector<vector<int>> edges = json::parse(inputArray.at(1));
+  return solution.largestPathValue(colors, edges);
 }
--- problems/problems_LCR_077/Solution.cpp
+++ problems/problems_LCR_077/Solution.cpp
@@ -17,23 +17,58 @@ using json = nlohmann::json;
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-
+  ListNode *sortList(ListNode *head) {
+    if (head == nullptr || head->next == nullptr) {
+      return head;
     }
+    ListNode *fast = head, *slow = head, *prev;
+    while (fast != nullptr && fast->next != nullptr) {
+      fast = fast->next->next;
+      prev = slow;
+      slow = slow->next;
+    }
+    prev->next = nullptr;
+    ListNode *left = sortList(head);
+    ListNode *right = sortList(slow);
+    return mergeSort(left, right);
+  }
+
+private:
+  ListNode *mergeSort(ListNode *l1, ListNode *l2) {
+    if (l1 == nullptr) {
+      return l2;
+    }
+    if (l2 == nullptr) {
+      return l1;
+    }
+    ListNode *dummy = new ListNode();
+    ListNode *cur = dummy;
+    while (l1 != nullptr || l2 != nullptr) {
+      if (l1 == nullptr || (l2 != nullptr && l2->val < l1->val)) {
+        cur->next = l2;
+        l2 = l2->next;
+      } else {
+        cur->next = l1;
+        l1 = l1->next;
+      }
+      cur = cur->next;
+    }
+    return dummy->next;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	std::vector<int> head_array = json::parse(inputArray.at(0));
-	ListNode *head = IntArrayToListNode(head_array);
-	return ListNodeToIntArray(solution.sortList(head));
+  Solution solution;
+  std::vector<int> head_array = json::parse(inputArray.at(0));
+  ListNode *head = IntArrayToListNode(head_array);
+  return ListNodeToIntArray(solution.sortList(head));
 }
