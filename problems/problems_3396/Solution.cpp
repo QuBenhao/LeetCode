5da82736bcab528176fb91e0e9e3f5ec46d6d927
qubh <qubh@xiaopeng.com>
2025-05-27 17:46:00 +0800

    test: 3396 solution
    go, c++

--- problems/problems_3396/Solution.cpp
+++ problems/problems_3396/Solution.cpp
@@ -1,28 +1,38 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
+#include <set>
 
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    int minimumOperations(vector<int>& nums) {
-        
+  int minimumOperations(vector<int> &nums) {
+    auto s = set<int>();
+    int ans = 0;
+    for (int idx = nums.size() - 1; idx >= 0; idx--) {
+      if (s.contains(nums[idx])) {
+        ans = idx / 3 + 1;
+        break;
+      }
+      s.insert(nums[idx]);
     }
+    return ans;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<int> nums = json::parse(inputArray.at(0));
-	return solution.minimumOperations(nums);
+  Solution solution;
+  vector<int> nums = json::parse(inputArray.at(0));
+  return solution.minimumOperations(nums);
 }
--- problems/problems_3396/solution.go
+++ problems/problems_3396/solution.go
@@ -7,7 +7,14 @@ import (
 )
 
 func minimumOperations(nums []int) int {
-
+	s := make(map[int]any, len(nums))
+	for i := len(nums) - 1; i >= 0; i-- {
+		if _, ok := s[nums[i]]; ok {
+			return i/3 + 1
+		}
+		s[nums[i]] = nil
+	}
+	return 0
 }
 
 func Solve(inputJsonValues string) any {
