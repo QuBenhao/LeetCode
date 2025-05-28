b803828e161b006dad456ba9647acfc122e81dde
qubh <qubh@xiaopeng.com>
2025-05-28 09:58:51 +0800

    test: 3372, LCR 030 solution
    c++

--- problems/problems_3372/Solution.cpp
+++ problems/problems_3372/Solution.cpp
@@ -1,30 +1,67 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
-
+#include <vector>
 
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
-public:
-    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
-        
+  vector<vector<int>> buildTree(const vector<vector<int>> &edges) {
+    vector<vector<int>> g(edges.size() + 1);
+    for (auto &e : edges) {
+      int x = e[0], y = e[1];
+      g[x].push_back(y);
+      g[y].push_back(x);
     }
+    return g;
+  }
+  int dfs(int node, int pa, int k, const vector<vector<int>> &g) {
+    if (k < 0) {
+      return 0;
+    }
+    if (k == 0) {
+      return 1;
+    }
+    k--;
+    int res = 1;
+    for (int neigh : g[node]) {
+      if (neigh == pa) {
+        continue;
+      }
+      res += dfs(neigh, node, k, g);
+    }
+    return res;
+  }
+
+public:
+  vector<int> maxTargetNodes(vector<vector<int>> &edges1, vector<vector<int>> &edges2, int k) {
+    auto t2 = buildTree(edges2);
+    int max2 = 0;
+    for (int i = 0; i < edges2.size() + 1; i++) {
+      max2 = max(max2, dfs(i, -1, k - 1, t2));
+    }
+    auto t1 = buildTree(edges1);
+    vector<int> ans(edges1.size() + 1);
+    for (int i = 0; i < edges1.size() + 1; i++) {
+      ans[i] = max2 + dfs(i, -1, k, t1);
+    }
+    return ans;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<vector<int>> edges1 = json::parse(inputArray.at(0));
-	vector<vector<int>> edges2 = json::parse(inputArray.at(1));
-	int k = json::parse(inputArray.at(2));
-	return solution.maxTargetNodes(edges1, edges2, k);
+  Solution solution;
+  vector<vector<int>> edges1 = json::parse(inputArray.at(0));
+  vector<vector<int>> edges2 = json::parse(inputArray.at(1));
+  int k = json::parse(inputArray.at(2));
+  return solution.maxTargetNodes(edges1, edges2, k);
 }
--- problems/problems_LCR_030/Solution.cpp
+++ problems/problems_LCR_030/Solution.cpp
@@ -1,31 +1,52 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
-
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 using json = nlohmann::json;
 
 class RandomizedSet {
+  vector<int> values;
+  unordered_map<int, int> idxMap;
+
 public:
-    /** Initialize your data structure here. */
-    RandomizedSet() {
+  /** Initialize your data structure here. */
+  RandomizedSet() {}
 
+  /** Inserts a value to the set. Returns true if the set did not already
+   * contain the specified element. */
+  bool insert(int val) {
+    auto iter = idxMap.find(val);
+    if (iter != idxMap.end()) {
+      return false;
     }
-    
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
-    bool insert(int val) {
+    values.push_back(val);
+    idxMap[val] = values.size() - 1;
+    return true;
+  }
 
+  /** Removes a value from the set. Returns true if the set contained the
+   * specified element. */
+  bool remove(int val) {
+    auto iter = idxMap.find(val);
+    if (iter == idxMap.end()) {
+      return false;
     }
-    
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
-    bool remove(int val) {
+    auto idx = iter->second;
+    int n = values.size() - 1;
+    values[idx] = values[n];
+    idxMap[values[n]] = idx;
+    values.pop_back();
+    idxMap.erase(val);
+    return true;
+  }
 
-    }
-    
-    /** Get a random element from the set. */
-    int getRandom() {
-
-    }
+  /** Get a random element from the set. */
+  int getRandom() {
+    auto idx = rand() % values.size();
+    return values[idx];
+  }
 };
 
 /**
@@ -37,33 +58,33 @@ public:
  */
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	vector<string> operators = json::parse(inputArray[0]);
-	vector<vector<json>> op_values = json::parse(inputArray[1]);
-	auto obj0 = make_shared<RandomizedSet>();
-	vector<json> ans = {nullptr};
-	for (size_t i = 1; i < op_values.size(); i++) {
-		if (operators[i] == "insert") {
-			ans.push_back(obj0->insert(op_values[i][0]));
-			continue;
-		}
-		if (operators[i] == "remove") {
-			ans.push_back(obj0->remove(op_values[i][0]));
-			continue;
-		}
-		if (operators[i] == "getRandom") {
-			ans.push_back(obj0->getRandom());
-			continue;
-		}
-		ans.push_back(nullptr);
-	}
-	return ans;
+  vector<string> operators = json::parse(inputArray[0]);
+  vector<vector<json>> op_values = json::parse(inputArray[1]);
+  auto obj0 = make_shared<RandomizedSet>();
+  vector<json> ans = {nullptr};
+  for (size_t i = 1; i < op_values.size(); i++) {
+    if (operators[i] == "insert") {
+      ans.push_back(obj0->insert(op_values[i][0]));
+      continue;
+    }
+    if (operators[i] == "remove") {
+      ans.push_back(obj0->remove(op_values[i][0]));
+      continue;
+    }
+    if (operators[i] == "getRandom") {
+      ans.push_back(obj0->getRandom());
+      continue;
+    }
+    ans.push_back(nullptr);
+  }
+  return ans;
 }
