5974fc13e6d10dcda972041ff3217d2c3384e3b1
qubh <qubh@xiaopeng.com>
2025-05-30 14:40:29 +0800

    test: 2359, LCR 071 solution
    java

--- problems/problems_2359/Solution.java
+++ problems/problems_2359/Solution.java
@@ -1,13 +1,40 @@
 package problems.problems_2359;
 
+import java.util.Arrays;
+
 import com.alibaba.fastjson.JSON;
-import java.util.*;
+
 import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
+    private void dfs(int[] edges, int node, int[] dis) {
+        int d = 0;
+        for (int cur = node; cur != -1 && dis[cur] == -1; cur = edges[cur]) {
+            dis[cur] = d++;
+        }
+    }
+
     public int closestMeetingNode(int[] edges, int node1, int node2) {
-        
+        int n = edges.length;
+        int[] dis1 = new int[n];
+        int[] dis2 = new int[n];
+        Arrays.fill(dis1, -1);
+        Arrays.fill(dis2, -1);
+        dfs(edges, node1, dis1);
+        dfs(edges, node2, dis2);
+        int ans = -1, ansD = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (dis1[i] == -1 || dis2[i] == -1) {
+                continue;
+            }
+            int d = Math.max(dis1[i], dis2[i]);
+            if (d < ansD) {
+                ans = i;
+                ansD = d;
+            }
+        }
+        return ans;
     }
 
     @Override
--- problems/problems_LCR_071/Solution.java
+++ problems/problems_LCR_071/Solution.java
@@ -1,18 +1,39 @@
 package problems.problems_LCR_071;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import com.alibaba.fastjson.JSON;
-import java.util.*;
+
 import qubhjava.BaseSolution;
 
 
-class Solution {
-
-    public Solution(int[] w) {
-
+class S {
+	private final int[] prefix;
+	private final Random random;
+    public S(int[] w) {
+		int n = w.length;
+		prefix = new int[n+1];
+		for (int i = 0; i < n; i++) {
+			prefix[i+1] = prefix[i] + w[i];
+		}
+		random = new Random();
     }
     
     public int pickIndex() {
-
+		int n = prefix.length-1;
+		int v = random.nextInt(prefix[n]) + 1;
+		int left = 0, right = n;
+		while (left < right) {
+			int mid = ((right - left) >> 1) + left;
+			if (prefix[mid] < v) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		return left - 1;
     }
 }
 
@@ -30,7 +51,7 @@ public class Solution extends BaseSolution {
         String[] operators = jsonArrayToStringArray(inputJsonValues[0]);
 		String[][] opValues = jsonArrayToString2DArray(inputJsonValues[1]);
 		int[] w = jsonArrayToIntArray(opValues[0][0]);
-		Solution obj = new Solution(w);
+		S obj = new S(w);
 		List<Object> ans = new ArrayList<>(operators.length);
 		ans.add(null);
 		for (int i = 1; i < operators.length; i++) {
