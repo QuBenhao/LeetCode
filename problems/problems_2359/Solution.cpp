47285da3f293ce0cabe2d250932cc651b536e112
qubh <qubh@xiaopeng.com>
2025-05-30 11:47:09 +0800

    test: 2359, LCR 071 solution
    py, cpp

--- problems/problems_2359/Solution.cpp
+++ problems/problems_2359/Solution.cpp
@@ -8,7 +8,30 @@ using json = nlohmann::json;
 class Solution {
 public:
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
-        
+        auto calc_dis = [&](int x) -> vector<int> {
+            vector<int> dis(edges.size(), -1);
+            for (int c = x, d = 0; c != -1 && dis[c] == -1; c = edges[c]) {
+                dis[c] = d;
+                d++;
+            }
+            return dis;
+        };
+        auto dis1 = calc_dis(node1);
+        auto dis2 = calc_dis(node2);
+        int ans = INT_MAX;
+        int ans_idx = -1;
+        int n = static_cast<int>(edges.size());
+        for (int i = 0; i < n; i++) {
+            if (dis1[i] == -1 || dis2[i] == -1) {
+                continue;
+            }
+            int mx = max(dis1[i], dis2[i]);
+            if (mx < ans) {
+                ans = mx;
+                ans_idx = i;
+            }
+        }
+        return ans_idx;
     }
 };
 
--- problems/problems_2359/solution.py
+++ problems/problems_2359/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+from math import inf
 
 
 class Solution(solution.Solution):
@@ -7,5 +8,26 @@ class Solution(solution.Solution):
         return self.closestMeetingNode(*test_input)
 
     def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
-        pass
-
+        n = len(edges)
+        
+        dis1 = [-1] * n
+        dis2 = [-1] * n
+        ref = (dis1, dis2)
+        
+        def dfs(node, i):
+            d = 0
+            while node != -1 and ref[i][node] == -1:
+                ref[i][node] = d
+                node = edges[node]
+                d += 1
+        
+        dfs(node1, 0)
+        dfs(node2, 1)
+        ans, ans_dis = -1, inf
+        for i, (a, b) in enumerate(zip(dis1, dis2)):
+            if a == -1 or b == -1:
+                continue
+            if (d := max(a, b)) < ans_dis:
+                ans_dis = d
+                ans = i
+        return ans
