afe8a5d34b142e4b947c85a5ed8dfcd3226074fa
qubh <qubh@xiaopeng.com>
2025-05-30 13:50:17 +0800

    test: 2359, LCR 071 solution
    py, cpp

--- problems/problems_LCR_071/Solution.cpp
+++ problems/problems_LCR_071/Solution.cpp
@@ -1,19 +1,26 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
-
+#include <algorithm>
 
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
+  vector<int> prefix;
+
 public:
-    Solution(vector<int>& w) {
-
+  Solution(vector<int> &w) {
+    int n = w.size();
+    prefix = vector<int>(n + 1);
+    for (int i = 0; i < n; i++) {
+      prefix[i + 1] = prefix[i] + w[i];
     }
-    
-    int pickIndex() {
+  }
 
-    }
+  int pickIndex() {
+    int v = rand() % prefix.back() + 1;
+    return lower_bound(prefix.begin(), prefix.end(), v) - prefix.begin() - 1;
+  }
 };
 
 /**
@@ -23,26 +30,26 @@ public:
  */
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	vector<string> operators = json::parse(inputArray[0]);
-	vector<vector<json>> op_values = json::parse(inputArray[1]);
-	vector<int> w_array = op_values[0][0].get<vector<int>>();
-	auto obj0 = make_shared<Solution>(w_array);
-	vector<json> ans = {nullptr};
-	for (size_t i = 1; i < op_values.size(); i++) {
-		if (operators[i] == "pickIndex") {
-			ans.push_back(obj0->pickIndex());
-			continue;
-		}
-		ans.push_back(nullptr);
-	}
-	return ans;
+  vector<string> operators = json::parse(inputArray[0]);
+  vector<vector<json>> op_values = json::parse(inputArray[1]);
+  vector<int> w_array = op_values[0][0].get<vector<int>>();
+  auto obj0 = make_shared<Solution>(w_array);
+  vector<json> ans = {nullptr};
+  for (size_t i = 1; i < op_values.size(); i++) {
+    if (operators[i] == "pickIndex") {
+      ans.push_back(obj0->pickIndex());
+      continue;
+    }
+    ans.push_back(nullptr);
+  }
+  return ans;
 }
