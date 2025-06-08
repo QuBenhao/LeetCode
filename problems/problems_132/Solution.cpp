430d94ad2ec75ef83e9b0d6b8f31e22d4134f273
benhao <qubenhao2@gmail.com>
2025-06-08 19:35:07 +0800

    test: 132 solution
    c++

--- problems/problems_132/Solution.cpp
+++ problems/problems_132/Solution.cpp
@@ -8,7 +8,25 @@ using json = nlohmann::json;
 class Solution {
 public:
     int minCut(string s) {
-        
+        int n = s.length();
+        vector<vector<bool>> is_palindrome(n, vector<bool>(n, false));
+        for (int i = 0; i < n; ++i) {
+            is_palindrome[i][i] = true;
+            for (int j = i - 1; j >= 0; --j) {
+                is_palindrome[j][i] = (s[i] == s[j]) && (i - j < 2 || is_palindrome[j + 1][i - 1]);
+            }
+        }
+
+        vector<int> cuts(n + 1, n);
+        cuts[0] = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j <= i; ++j) {
+                if (is_palindrome[j][i]) {
+                    cuts[i + 1] = min(cuts[i + 1], cuts[j] + 1);
+                }
+            }
+        }
+        return cuts[n] - 1; // Subtract 1 because cuts[n] counts the number of partitions, not cuts
     }
 };
 
