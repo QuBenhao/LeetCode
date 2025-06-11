3a400f5b2b25e5a792d8dfb93b7c30e29bc5d15e
qubh <qubenhao2@gmail.com>
2025-06-11 13:46:58 +0800

    feat: java dynamic problem
    load from daily

--- qubhjava/test/TestMain.java
+++ qubhjava/test/TestMain.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.Timeout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import problems.problems_3442.Solution;
-// import premiums.premiums_1056.Solution;
 import org.testng.util.Strings;
 import qubhjava.Testcase;
 
@@ -17,14 +15,21 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
+import qubhjava.BaseSolution;
+import java.io.FileInputStream;
+import java.lang.reflect.InvocationTargetException;
+
 
 @Timeout(10)
 public class TestMain {
 
     private static final Logger log = LoggerFactory.getLogger(TestMain.class);
-    private static final String PROBLEM_ID = "3442";
+    private static final String PROBLEM_ID, PROBLEM_FOLDER;
 
-    private Testcase[] loadTestcases() throws IOException {
+    static {
+        String problemId = null;
         String problemFolder = null;
         try {
             Dotenv dotenv = Dotenv.load();
@@ -35,23 +40,41 @@ public class TestMain {
         if (Strings.isNullOrEmpty(problemFolder)) {
             problemFolder = "problems";
         }
-        return Common.loadTestcases(log, PROBLEM_ID, problemFolder);
+        PROBLEM_FOLDER = problemFolder;
+        try {
+            // read json daily-{problemFolder}.json
+            FileInputStream fis = new FileInputStream(String.format("daily-%s.json", problemFolder));
+            byte[] bytes = fis.readAllBytes();
+            String content = new String(bytes);
+            JSONObject jsonObject = JSONObject.parseObject(content);
+            problemId = jsonObject.getString("daily");
+        } catch (IOException e) {
+            log.error("Error reading daily-{problemFolder}.json", e);
+        } catch (JSONException e) {
+            log.error("Error parsing JSON from daily-{problemFolder}.json", e);
+        }
+        if (Strings.isNullOrEmpty(problemId)) {
+            throw new RuntimeException("Problem ID is not set in daily-{problemFolder}.json");
+        }
+        PROBLEM_ID = problemId;
+        log.info("Problem ID: {}, Problem Folder: {}", PROBLEM_ID, PROBLEM_FOLDER);
     }
 
-
     @TestFactory
-    Collection<DynamicTest> test() throws IOException {
+    Collection<DynamicTest> test() throws IOException, ClassNotFoundException, InstantiationException,
+            IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         log.info("Starting TestMain, test problem: {}", PROBLEM_ID);
         List<DynamicTest> tests = new ArrayList<>();
-        Testcase[] testcases = loadTestcases();
+        Testcase[] testcases = Common.loadTestcases(log, PROBLEM_ID, PROBLEM_FOLDER);
         if (testcases == null || testcases.length == 0) {
             log.error("Load Testcases error");
             return tests;
         }
-        Solution solution = new Solution();
+        Class<BaseSolution> dynamicClass = (Class<BaseSolution>)Class.forName(String.format("%s.%s_%s.Solution", PROBLEM_FOLDER, PROBLEM_FOLDER, PROBLEM_ID));
+        BaseSolution baseSolution = dynamicClass.getDeclaredConstructor().newInstance();
         int idx = 0;
         for (Testcase testcase : testcases) {
-            tests.add(Common.addTest(solution, testcase, PROBLEM_ID, idx++));
+            tests.add(Common.addTest(baseSolution, testcase, PROBLEM_ID, idx++));
         }
         return tests;
     }
