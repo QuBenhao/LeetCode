5a6f52f536a6d0f85166b6b81cedad7880b0b26b
benhao <qubenhao2@gmail.com>
2025-06-11 21:32:47 +0800

    test: 792 solution
    py

--- problems/problems_792/solution.py
+++ problems/problems_792/solution.py
@@ -1,7 +1,7 @@
 import solution
 from typing import *
 from collections import defaultdict
-from bisect import bisect_left
+from bisect import bisect_right
 
 
 class Solution(solution.Solution):
@@ -15,13 +15,13 @@ class Solution(solution.Solution):
         ans = 0
         for word in words:
             idx = 0
-            s_idx = 0
+            s_idx = -1
             n = len(word)
             while idx < n:
-                s_idx = bisect_left(idx_map[word[idx]], s_idx)
-                if s_idx == len(idx_map[word[idx]]):
+                i = bisect_right(idx_map[word[idx]], s_idx)
+                if i == len(idx_map[word[idx]]):
                     break
-                s_idx = idx_map[word[idx]][s_idx] + 1
+                s_idx = idx_map[word[idx]][i]
                 idx += 1
             ans += idx == n
         return ans
