ad2c53d4c4f0b14c763f264a91611f4645c8b07d
qubh <qubenhao2@gmail.com>
2025-06-11 09:25:32 +0800

    test: 3445 solution
    c++, go, java

--- problems/problems_3445/solution.go
+++ problems/problems_3445/solution.go
@@ -3,11 +3,36 @@ package problem3445
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"strings"
 )
 
 func maxDifference(s string, k int) int {
-    
+	ans := math.MinInt
+	for x := range 5 {
+		for y := range 5 {
+			if x == y {
+				continue
+			}
+			curSum := make([]int, 5)
+			prevSum := make([]int, 5)
+			minS := [][]int{{math.MaxInt32, math.MaxInt32}, {math.MaxInt32, math.MaxInt32}}
+			left := 0
+			for right, char := range s {
+				curSum[char-'0']++
+				for right-left+1 >= k && curSum[x] > prevSum[x] && curSum[y] > prevSum[y] {
+					p, q := prevSum[x]&1, prevSum[y]&1
+					minS[p][q] = min(minS[p][q], prevSum[x]-prevSum[y])
+					prevSum[s[left]-'0']++
+					left++
+				}
+				if right+1 >= k {
+					ans = max(ans, curSum[x]-curSum[y]-minS[curSum[x]&1^1][curSum[y]&1])
+				}
+			}
+		}
+	}
+	return ans
 }
 
 func Solve(inputJsonValues string) any {
