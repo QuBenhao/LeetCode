dffb5c8cf67d38afd67bcf098e102198a0f7fec7
qubh <qubenhao2@gmail.com>
2025-06-17 18:32:18 +0800

    test: 813 solution
    py

--- golang/solution_test.go
+++ golang/solution_test.go
@@ -1,10 +1,10 @@
 package golang
 
 import (
-	problem "leetCode/problems/problems_808"
+	problem "leetCode/problems/problems_813"
 	"testing"
 )
 
 func TestSolution(t *testing.T) {
-	TestEach(t, "808", "problems", problem.Solve)
+	TestEach(t, "813", "problems", problem.Solve)
 }
--- problems/problems_813/solution.py
+++ problems/problems_813/solution.py
@@ -1,3 +1,6 @@
+from functools import cache
+from itertools import accumulate
+from math import inf
 import solution
 from typing import *
 
@@ -7,5 +10,14 @@ class Solution(solution.Solution):
         return self.largestSumOfAverages(*test_input)
 
     def largestSumOfAverages(self, nums: List[int], k: int) -> float:
-        pass
+        prefix_sum = list(accumulate(nums, initial=0))
 
+        @cache
+        def dfs(l: int, r: int, remain: int) -> float:
+            if remain == 1:
+                return (prefix_sum[r] - prefix_sum[l - 1]) / (r - l + 1)
+            ans = -inf
+            for i in range(l, r + 2 - remain): # r - (i + 1) + 1 >= remain - 1  ==> i <= r + 1 - remain
+                ans = max(ans, (prefix_sum[i] - prefix_sum[l - 1]) / (i - l + 1) + dfs(i + 1, r, remain - 1))
+            return ans
+        return dfs(1, len(nums), k)
