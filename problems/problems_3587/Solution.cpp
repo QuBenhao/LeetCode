7125804b69029378a79539ff4a326c2896074158
benhao <qubenhao2@gmail.com>
2025-06-22 08:24:15 +0800

    test: 3587, 3589 solution
    c++

--- problems/problems_3587/Solution.cpp
+++ problems/problems_3587/Solution.cpp
@@ -1,28 +1,51 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    int minSwaps(vector<int>& nums) {
-        
+  int minSwaps(vector<int> &nums) {
+    array<vector<int>, 2> values;
+    int n = nums.size();
+    for (int i = 0; i < n; ++i) {
+      values[nums[i] & 1].push_back(i);
     }
+    auto swap = [&values, n](int start) {
+      int count = 0;
+      const auto &arr = values[start];
+      for (int i = 0, idx = 0; i < n; i += 2) {
+        count += abs(arr[idx++] - i);
+      }
+      return count;
+    };
+    int even_size = values[0].size(), odd_size = values[1].size();
+    if (abs(even_size - odd_size) > 1) {
+      return -1;
+    }
+    int ans = INT_MAX;
+    if (even_size >= odd_size) {
+      ans = min(ans, swap(0));
+    }
+    if (odd_size >= even_size) {
+      ans = min(ans, swap(1));
+    }
+    return ans;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<int> nums = json::parse(inputArray.at(0));
-	return solution.minSwaps(nums);
+  Solution solution;
+  vector<int> nums = json::parse(inputArray.at(0));
+  return solution.minSwaps(nums);
 }
--- problems/problems_3589/Solution.cpp
+++ problems/problems_3589/Solution.cpp
@@ -1,29 +1,80 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
+#include <deque>
 
 using namespace std;
 using json = nlohmann::json;
 
+#define MAXN ((int)5e4)
+bool inited = false, is_prime[MAXN + 1];
+void init() {
+  if (inited)
+    return;
+  inited = true;
+  fill(is_prime, is_prime + MAXN + 1, true);
+  is_prime[0] = is_prime[1] = false;
+  for (int i = 2; i * i <= MAXN; ++i) {
+    if (is_prime[i]) {
+      for (int j = i * i; j <= MAXN; j += i) {
+        is_prime[j] = false;
+      }
+    }
+  }
+}
+
 class Solution {
 public:
-    int primeSubarray(vector<int>& nums, int k) {
-        
+  int primeSubarray(vector<int> &nums, int k) {
+    init();
+    deque<int> q, max_q, min_q;
+    int n = nums.size(), ans = 0, left = 0;
+    for (int right = 0; right < n; ++right) {
+      int num = nums[right];
+      if (is_prime[num]) {
+        q.push_back(right);
+        while (!max_q.empty() && nums[max_q.back()] <= num) {
+          max_q.pop_back();
+        }
+        max_q.push_back(right);
+        while (!min_q.empty() && nums[min_q.back()] >= num) {
+          min_q.pop_back();
+        }
+        min_q.push_back(right);
+      }
+      while (!q.empty() && (nums[max_q.front()] - nums[min_q.front()] > k)) {
+        left = q.front() + 1;
+        q.pop_front();
+        while (!max_q.empty() && max_q.front() < left) {
+          max_q.pop_front();
+        }
+        while (!min_q.empty() && min_q.front() < left) {
+          min_q.pop_front();
+        }
+      }
+      if (q.size() > 1) {
+        int tmp = q.back();
+        q.pop_back();
+        ans += q.back() - left + 1;
+        q.push_back(tmp);
+      }
     }
+    return ans;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<int> nums = json::parse(inputArray.at(0));
-	int k = json::parse(inputArray.at(1));
-	return solution.primeSubarray(nums, k);
+  Solution solution;
+  vector<int> nums = json::parse(inputArray.at(0));
+  int k = json::parse(inputArray.at(1));
+  return solution.primeSubarray(nums, k);
 }
