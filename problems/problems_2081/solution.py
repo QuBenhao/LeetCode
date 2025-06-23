000884e0ed5491eb2724b3a2af32b99885b65a6e
benhao <qubenhao2@gmail.com>
2025-06-23 08:28:00 +0800

    test: 2081 solution
    py

--- problems/problems_2081/solution.py
+++ problems/problems_2081/solution.py
@@ -1,3 +1,5 @@
+from itertools import accumulate
+
 import solution
 from typing import *
 
@@ -7,5 +9,48 @@ class Solution(solution.Solution):
         return self.kMirror(*test_input)
 
     def kMirror(self, k: int, n: int) -> int:
-        pass
+        return ans[k][n - 1]
 
+MAX_N = 30
+ans = [[] for _ in range(10)]
+
+# 力扣 9. 回文数
+def is_k_palindrome(x: int, k: int) -> bool:
+    if x % k == 0:
+        return False
+    rev = 0
+    while rev < x // k:
+        rev = rev * k + x % k
+        x //= k
+    return rev == x or rev == x // k
+
+def do_palindrome(x: int) -> bool:
+    done = True
+    for k in range(2, 10):
+        if len(ans[k]) < MAX_N and is_k_palindrome(x, k):
+            ans[k].append(x)
+        if len(ans[k]) < MAX_N:
+            done = False
+    if not done:
+        return False
+    for k in range(2, 10):
+        ans[k] = list(accumulate(ans[k]))
+    return True
+
+def init() -> None:
+    base = 1
+    while True:
+        # 生成奇数长度回文数，例如 base = 10，生成的范围是 101 ~ 999
+        for i in range(base, base * 10):
+            s = str(i)
+            x = int(s + s[::-1][1:])
+            if do_palindrome(x):
+                return
+        # 生成偶数长度回文数，例如 base = 10，生成的范围是 1001 ~ 9999
+        for i in range(base, base * 10):
+            s = str(i)
+            x = int(s + s[::-1])
+            if do_palindrome(x):
+                return
+        base *= 10
+init()
