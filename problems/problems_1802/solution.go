60e63142684b87d2f271203f948555b962d369d9
benhao <qubenhao2@gmail.com>
2025-06-25 16:14:53 +0800

    test: 1802, 2040 solution
    go

--- problems/problems_1802/solution.go
+++ problems/problems_1802/solution.go
@@ -7,7 +7,30 @@ import (
 )
 
 func maxValue(n int, index int, maxSum int) int {
-    
+	helper := func(mid int) bool {
+		var left, right int
+		if mid > index {
+			left = (mid - 1 + mid - index) * index / 2
+		} else {
+			left = (mid-1+1)*(mid-1)/2 + (index - mid + 1)
+		}
+		if mid > n-index-1 {
+			right = (mid - 1 + mid - (n - index - 1)) * (n - index - 1) / 2
+		} else {
+			right = (mid-1+1)*(mid-1)/2 + (n - index - 1 - mid + 1)
+		}
+		return left+right <= maxSum-mid
+	}
+	left, right := 1, maxSum-n+1
+	for left < right {
+		mid := (left + right + 1) / 2
+		if helper(mid) {
+			left = mid
+		} else {
+			right = mid - 1
+		}
+	}
+	return left
 }
 
 func Solve(inputJsonValues string) any {
--- problems/problems_2040/solution.go
+++ problems/problems_2040/solution.go
@@ -3,11 +3,86 @@ package problem2040
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"strings"
 )
 
 func kthSmallestProduct(nums1 []int, nums2 []int, k int64) int64 {
-    
+	m, n := int64(len(nums1)), int64(len(nums2))
+	bisectLeft := func(arr []int, target int) int64 {
+		left, right := int64(0), int64(len(arr))
+		for left < right {
+			mid := left + (right-left)/2
+			if arr[mid] < target {
+				left = mid + 1
+			} else {
+				right = mid
+			}
+		}
+		return left
+	}
+	zero1, zero2 := bisectLeft(nums1, 0), bisectLeft(nums2, 0)
+	left, right := int64(math.MaxInt64), int64(math.MinInt64)
+	for _, v := range []int64{int64(nums1[0]) * int64(nums2[0]), int64(nums1[m-1]) * int64(nums2[n-1]),
+		int64(nums1[0]) * int64(nums2[n-1]), int64(nums1[m-1]) * int64(nums2[0])} {
+		left = min(left, v)
+		right = max(right, v)
+	}
+	count := func(x int64) bool {
+		var ans int64
+		if x < 0 {
+			ans = 0
+			i, j := int64(0), zero2
+			for i < zero1 && j < n {
+				if int64(nums1[i])*int64(nums2[j]) > x {
+					j++
+				} else {
+					ans += n - j
+					i++
+				}
+			}
+			i, j = zero1, int64(0)
+			for i < m && j < zero2 {
+				if int64(nums1[i])*int64(nums2[j]) > x {
+					i++
+				} else {
+					ans += m - i
+					j++
+				}
+			}
+		} else {
+			ans = zero1*(n-zero2) + zero2*(m-zero1)
+			i, j := int64(0), zero2-1
+			for i < zero1 && j >= 0 {
+				if int64(nums1[i])*int64(nums2[j]) > x {
+					i++
+				} else {
+					ans += zero1 - i
+					j--
+				}
+			}
+			i, j = zero1, n-1
+			for i < m && j >= zero2 {
+				if int64(nums1[i])*int64(nums2[j]) > x {
+					j--
+				} else {
+					ans += j - zero2 + 1
+					i++
+				}
+			}
+		}
+		return ans >= k
+	}
+
+	for left < right {
+		mid := left + (right-left)/2
+		if count(mid) {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
 }
 
 func Solve(inputJsonValues string) any {
