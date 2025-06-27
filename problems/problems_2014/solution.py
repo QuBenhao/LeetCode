eca2cb269085cd916d081a0464a81a9188f52add
benhao <qubenhao2@gmail.com>
2025-06-27 09:15:36 +0800

    test: 2014 solution
    py

--- problems/problems_2014/solution.py
+++ problems/problems_2014/solution.py
@@ -1,3 +1,6 @@
+from collections import Counter
+from itertools import permutations
+
 import solution
 from typing import *
 
@@ -7,5 +10,18 @@ class Solution(solution.Solution):
         return self.longestSubsequenceRepeatedK(*test_input)
 
     def longestSubsequenceRepeatedK(self, s: str, k: int) -> str:
-        pass
+        # x * k <= n < 8 * k
+        # x < 8
+        def is_sub_sequence(_s, _seq):
+            it = iter(_s)
+            return all(c in it for c in _seq)
 
+        counter = Counter(s)
+        a = [c for c, freq in counter.items() for _ in range(freq // k)]
+        a.sort(reverse=True)
+        for i in range(len(a), 0, -1):
+            for perm in permutations(a, i):
+                seq = "".join(perm)
+                if is_sub_sequence(s, seq * k):
+                    return seq
+        return ""
