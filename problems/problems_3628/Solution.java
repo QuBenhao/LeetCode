3af8eec6e9bc7ca11c5044ca8505e6e676127005
benhao <qubenhao2@gmail.com>
2025-07-28 08:26:34 +0800

    test: 3628, 3629 solution
    java

--- problems/problems_3628/Solution.java
+++ problems/problems_3628/Solution.java
@@ -7,7 +7,26 @@ import qubhjava.BaseSolution;
 
 public class Solution extends BaseSolution {
     public long numOfSubsequences(String s) {
-        
+        int n = s.length();
+        int[] sufT = new int[n + 1];
+        int[] sufC = new int[n + 1];
+        for (int i = n - 1; i >= 0; --i) {
+            sufT[i] = sufT[i + 1] + (s.charAt(i) == 'T' ? 1 : 0);
+            sufC[i] = sufC[i + 1] + (s.charAt(i) == 'C' ? sufT[i] : 0);
+        }
+        long ans = 0, maxAdd = 0;
+        int preL = 0, preC = 0;
+        for (int i = 0; i < n; ++i) {
+            char c = s.charAt(i);
+            if (c == 'L') {
+                ++preL;
+                ans += sufC[i];
+            } else if (c == 'C') {
+                preC += preL;
+            }
+            maxAdd = Math.max(maxAdd, Math.max(Math.max(preC, sufC[i]), (long) preL * sufT[i]));
+        }
+        return ans + maxAdd;
     }
 
     @Override
--- problems/problems_3629/Solution.java
+++ problems/problems_3629/Solution.java
@@ -6,8 +6,63 @@ import qubhjava.BaseSolution;
 
 
 public class Solution extends BaseSolution {
+    private static final int MAX_N = 1000001;
+    private static final List<Integer>[] PRIME_FACTORS = new List[MAX_N];
+    static {
+        for (int i = 0; i < MAX_N; ++i) {
+            PRIME_FACTORS[i] = new ArrayList<>();
+        }
+        for (int i = 2; i < MAX_N; ++i) {
+            if (PRIME_FACTORS[i].isEmpty()) {
+                for (int j = i; j < MAX_N; j += i) {
+                    PRIME_FACTORS[j].add(i);
+                }
+            }
+        }
+    }
     public int minJumps(int[] nums) {
-        
+        int n = nums.length;
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < n; ++i) {
+            int num = nums[i];
+            for (int prime : PRIME_FACTORS[num]) {
+                graph.putIfAbsent(prime, new ArrayList<>());
+                graph.get(prime).add(i);
+            }
+        }
+        Deque<Integer> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[n];
+        queue.offer(0);
+        visited[0] = true;
+        int steps = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; ++i) {
+                int index = queue.pollFirst();
+                if (index == n - 1) {
+                    return steps;
+                }
+                if (PRIME_FACTORS[nums[index]].size() == 1 && graph.containsKey(nums[index])) {
+                    for (int nxt: graph.get(nums[index])) {
+                        if (!visited[nxt]) {
+                            visited[nxt] = true;
+                            queue.offerLast(nxt);
+                        }
+                    }
+                    graph.remove(nums[index]);
+                }
+                if (!visited[index+1]) {
+                    visited[index + 1] = true;
+                    queue.offerLast(index + 1);
+                }
+                if (index > 0 && !visited[index - 1]) {
+                    visited[index - 1] = true;
+                    queue.offerLast(index - 1);
+                }
+            }
+            ++steps;
+        }
+        return -1; // If we cannot reach the last index
     }
 
     @Override
