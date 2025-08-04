47ca67cf586bef789abca195529994166641bb2c
GitHub Action <action@github.com>
2025-08-04 16:07:50 +0000

    test: [20250805] Add (3477)

--- golang/solution_test.go
+++ golang/solution_test.go
@@ -1,10 +1,10 @@
 package golang
 
 import (
-	problem "leetCode/problems/problems_2069"
+	problem "leetCode/problems/problems_3477"
 	"testing"
 )
 
 func TestSolution(t *testing.T) {
-	TestEach(t, "2069", "problems", problem.Solve)
+	TestEach(t, "3477", "problems", problem.Solve)
 }
---
+++ problems/problems_3477/solution.rs
@@ -0,0 +1,17 @@
+use serde_json::{json, Value};
+
+pub struct Solution;
+
+impl Solution {
+    pub fn num_of_unplaced_fruits(fruits: Vec<i32>, baskets: Vec<i32>) -> i32 {
+        
+    }
+}
+
+#[cfg(feature = "solution_3477")]
+pub fn solve(input_string: String) -> Value {
+	let input_values: Vec<String> = input_string.split('\n').map(|x| x.to_string()).collect();
+	let fruits: Vec<i32> = serde_json::from_str(&input_values[0]).expect("Failed to parse input");
+	let baskets: Vec<i32> = serde_json::from_str(&input_values[1]).expect("Failed to parse input");
+	json!(Solution::num_of_unplaced_fruits(fruits, baskets))
+}
