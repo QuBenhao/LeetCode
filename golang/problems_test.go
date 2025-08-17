ff81467fde5db66db569e9529b48fe834daa36b5
benhao <qubenhao2@gmail.com>
2025-08-17 16:41:02 +0800

    test: 3652, 3653, 3654 solution
    py, c++, go, java

--- golang/problems_test.go
+++ golang/problems_test.go
@@ -1,16 +1,14 @@
 package golang
 
 import (
-	problem3648 "leetCode/problems/problems_3648"
-	problem3649 "leetCode/problems/problems_3649"
-	problem3650 "leetCode/problems/problems_3650"
-	problem3651 "leetCode/problems/problems_3651"
+	problem3652 "leetCode/problems/problems_3652"
+	problem3653 "leetCode/problems/problems_3653"
+	problem3654 "leetCode/problems/problems_3654"
 	"testing"
 )
 
 func TestSolutions(t *testing.T) {
-	TestEach(t, "3648", "problems", problem3648.Solve)
-	TestEach(t, "3649", "problems", problem3649.Solve)
-	TestEach(t, "3650", "problems", problem3650.Solve)
-	TestEach(t, "3651", "problems", problem3651.Solve)
+	TestEach(t, "3652", "problems", problem3652.Solve)
+	TestEach(t, "3653", "problems", problem3653.Solve)
+	TestEach(t, "3654", "problems", problem3654.Solve)
 }
