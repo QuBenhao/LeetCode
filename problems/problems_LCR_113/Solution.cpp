44e8ace3d1f6decbb42e35f9aacf17c6947b507b
benhao <qubenhao2@gmail.com>
2025-09-17 16:56:29 +0800

    test: LCR 113 solution
    py, c++, go, java

--- problems/problems_LCR_113/Solution.cpp
+++ problems/problems_LCR_113/Solution.cpp
@@ -1,29 +1,54 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
-
+#include <queue>
 
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-
+  vector<int> findOrder(int numCourses,
+                        const vector<vector<int>> &prerequisites) {
+    vector<int> degree(numCourses);
+    vector<vector<int>> children(numCourses);
+    for (const auto &prerequisity : prerequisites) {
+      ++degree[prerequisity[0]];
+      children[prerequisity[1]].emplace_back(prerequisity[0]);
     }
+    queue<int> q;
+    for (int i = 0; i < numCourses; ++i) {
+      if (degree[i] == 0) {
+        q.push(i);
+      }
+    }
+    vector<int> ans(numCourses);
+    int idx = 0;
+    while (!q.empty()) {
+      int node = q.front();
+      ans[idx++] = node;
+      q.pop();
+      for (const auto &child : children[node]) {
+        if (--degree[child] == 0) {
+          q.push(child);
+        }
+      }
+    }
+    return ans;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	int numCourses = json::parse(inputArray.at(0));
-	vector<vector<int>> prerequisites = json::parse(inputArray.at(1));
-	return solution.findOrder(numCourses, prerequisites);
+  Solution solution;
+  int numCourses = json::parse(inputArray.at(0));
+  vector<vector<int>> prerequisites = json::parse(inputArray.at(1));
+  return solution.findOrder(numCourses, prerequisites);
 }
--- problems/problems_LCR_113/solution.go
+++ problems/problems_LCR_113/solution.go
@@ -6,8 +6,31 @@ import (
 	"strings"
 )
 
-func findOrder(numCourses int, prerequisites [][]int) []int {
-
+func findOrder(numCourses int, prerequisites [][]int) (ans []int) {
+	degree := make([]int, numCourses)
+	children := make(map[int][]int)
+	for _, prerequisite := range prerequisites {
+		degree[prerequisite[0]]++
+		children[prerequisite[1]] = append(children[prerequisite[1]], prerequisite[0])
+	}
+	var q []int
+	for i, c := range degree {
+		if c == 0 {
+			q = append(q, i)
+		}
+	}
+	for len(q) > 0 {
+		node := q[0]
+		ans = append(ans, node)
+		q = q[1:]
+		for _, child := range children[node] {
+			degree[child]--
+			if degree[child] == 0 {
+				q = append(q, child)
+			}
+		}
+	}
+	return
 }
 
 func Solve(inputJsonValues string) any {
