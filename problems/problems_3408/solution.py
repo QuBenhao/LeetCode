29c15e98564d6d880d51218357ea158ffefada4b
benhao <qubenhao2@gmail.com>
2025-09-18 07:13:10 +0800

    test: 3408 solution
    py

--- problems/problems_3408/solution.py
+++ problems/problems_3408/solution.py
@@ -1,3 +1,5 @@
+from heapq import heapify, heappush, heappop
+
 import solution
 from typing import *
 from python.object_libs import call_method
@@ -12,17 +14,27 @@ class Solution(solution.Solution):
 
 class TaskManager:
     def __init__(self, tasks: List[List[int]]):
-        pass
+        self.mp = {taskId: (priority, userId) for userId, taskId, priority in tasks}
+        self.h = [(-priority, -taskId, userId) for userId, taskId, priority in tasks]  # 取相反数，变成最大堆
+        heapify(self.h)
 
     def add(self, userId: int, taskId: int, priority: int) -> None:
-        pass
+        self.mp[taskId] = (priority, userId)
+        heappush(self.h, (-priority, -taskId, userId))
 
     def edit(self, taskId: int, newPriority: int) -> None:
-        pass
+        # 懒修改
+        self.add(self.mp[taskId][1], taskId, newPriority)
 
     def rmv(self, taskId: int) -> None:
-        pass
+        # 懒删除
+        self.mp[taskId] = (-1, -1)
 
     def execTop(self) -> int:
-        pass
-
+        while self.h:
+            priority, taskId, userId = heappop(self.h)
+            if self.mp[-taskId] == (-priority, userId):
+                self.rmv(-taskId)
+                return userId
+            # else 货不对板，堆顶和 mp 中记录的不一样，说明堆顶数据已被修改或删除，不做处理
+        return -1
