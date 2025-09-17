9a2aeabf0d82c1b263258ab94a47832dc78f7d0d
benhao <qubenhao2@gmail.com>
2025-09-17 16:36:23 +0800

    test: LCR 091
    golang

--- problems/problems_LCR_091/solution.go
+++ problems/problems_LCR_091/solution.go
@@ -3,11 +3,26 @@ package problemLCR_091
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"strings"
 )
 
-func minCost(costs [][]int) int {
-
+func minCost(costs [][]int) (ans int) {
+	dp := make([][]int, 2)
+	for i := range dp {
+		dp[i] = make([]int, 3)
+	}
+	for i, cost := range costs {
+		pre, cur := i%2, (i+1)%2
+		dp[cur][0] = cost[0] + min(dp[pre][1], dp[pre][2])
+		dp[cur][1] = cost[1] + min(dp[pre][0], dp[pre][2])
+		dp[cur][2] = cost[2] + min(dp[pre][0], dp[pre][1])
+	}
+	ans = math.MaxInt32
+	for _, j := range dp[len(costs)%2] {
+		ans = min(ans, j)
+	}
+	return
 }
 
 func Solve(inputJsonValues string) any {
