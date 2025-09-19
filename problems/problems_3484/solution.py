36b5deb2343345833d72fde126a99f09900bba0d
benhao <qubenhao2@gmail.com>
2025-09-19 16:13:12 +0800

    test: 3484 solution
    py

--- problems/problems_3484/solution.py
+++ problems/problems_3484/solution.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import solution
 from typing import *
 from python.object_libs import call_method
@@ -12,14 +14,26 @@ class Solution(solution.Solution):
 
 class Spreadsheet:
     def __init__(self, rows: int):
-        pass
+        self.map = [defaultdict(int) for _ in range(26)]
 
     def setCell(self, cell: str, value: int) -> None:
-        pass
+        self.map[ord(cell[0]) - ord('A')][cell] = value
 
     def resetCell(self, cell: str) -> None:
-        pass
+        self.map[ord(cell[0]) - ord('A')][cell] = 0
 
     def getValue(self, formula: str) -> int:
-        pass
+        def get_val(cell: str) -> int:
+            return self.map[ord(cell[0]) - ord('A')][cell]
 
+        formula = formula.split("=")[1]
+        x1, x2 = formula.split("+")
+        if x1[0].isalpha():
+            v1 = get_val(x1)
+        else:
+            v1 = int(x1)
+        if x2[0].isalpha():
+            v2 = get_val(x2)
+        else:
+            v2 = int(x2)
+        return v1 + v2
--- problems/problems_3484/testcase.py
+++ problems/problems_3484/testcase.py
@@ -8,6 +8,7 @@ class Testcase(testcase.Testcase):
 	def __init__(self):
 		self.testcases = []
 		self.testcases.append(case(Input=[['Spreadsheet', 'getValue', 'setCell', 'getValue', 'setCell', 'getValue', 'resetCell', 'getValue'], [[3], ['=5+7'], ['A1', 10], ['=A1+6'], ['B2', 15], ['=A1+B2'], ['A1'], ['=A1+B2']]], Output=[None, 12, None, 16, None, 25, None, 15]))
+		self.testcases.append(case(Input=[["Spreadsheet","getValue"],[[934],["=3138+15425"]]], Output=[None,18563]))
 
 	def get_testcases(self):
 		return self.testcases
