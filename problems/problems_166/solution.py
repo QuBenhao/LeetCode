f11e38eb3bafaa964e5682511357969eaecadfef
benhao <qubenhao2@gmail.com>
2025-09-24 08:03:53 +0800

    test: 166 solution
    py

--- problems/problems_166/solution.py
+++ problems/problems_166/solution.py
@@ -7,5 +7,15 @@ class Solution(solution.Solution):
         return self.fractionToDecimal(*test_input)
 
     def fractionToDecimal(self, numerator: int, denominator: int) -> str:
-        pass
-
+        if numerator % denominator == 0:
+            return str(numerator // denominator)
+        a, b = abs(numerator), abs(denominator)
+        ans = ("-" if numerator * denominator < 0 else "") + str(a // b) + "."
+        cache, i = {}, len(ans)
+        while (a := a % b * 10) > 0:
+            if a in cache:
+                return ans[:cache[a]] + "(" + ans[cache[a]:] + ")"
+            ans += str(a//b)
+            cache[a] = i
+            i += 1
+        return ans
--- problems/problems_166/testcase.py
+++ problems/problems_166/testcase.py
@@ -10,6 +10,7 @@ class Testcase(testcase.Testcase):
 		self.testcases.append(case(Input=[1, 2], Output="0.5"))
 		self.testcases.append(case(Input=[2, 1], Output="2"))
 		self.testcases.append(case(Input=[4, 333], Output="0.(012)"))
+		self.testcases.append(case(Input=[-50,8], Output="-6.25"))
 
 	def get_testcases(self):
 		return self.testcases
