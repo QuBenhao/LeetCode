ed12fe5ab1b64eaf0ba7fe4c17392b3b97c6d6ef
benhao <qubenhao2@gmail.com>
2025-10-07 08:13:41 +0800

    test: 1488 solution
    py

--- problems/problems_1488/solution.py
+++ problems/problems_1488/solution.py
@@ -1,3 +1,6 @@
+import heapq
+from collections import defaultdict, deque
+
 import solution
 from typing import *
 
@@ -7,5 +10,30 @@ class Solution(solution.Solution):
         return self.avoidFlood(test_input)
 
     def avoidFlood(self, rains: List[int]) -> List[int]:
-        pass
-
+        n = len(rains)
+        ans = [-1] * n
+        next_idxes = defaultdict(deque)
+        to_be_cleared = set()
+        for i, r in enumerate(rains):
+            if r != 0:
+                next_idxes[r].append(i)
+        pq = []
+        for i, r in enumerate(rains):
+            if r == 0:
+                if not pq:
+                    ans[i] = 1
+                else:
+                    idx = heapq.heappop(pq)
+                    ans[i] = rains[idx]
+                    to_be_cleared.remove(rains[idx])
+                continue
+            if r in to_be_cleared:
+                return []
+            to_be_cleared.add(r)
+            if next_idxes[r]:
+                next_idx = next_idxes[r].popleft()
+                if next_idx <= i and next_idxes[r]:
+                    next_idx = next_idxes[r].popleft()
+                if next_idx > i:
+                    heapq.heappush(pq, next_idx)
+        return ans
--- problems/problems_1488/testcase.py
+++ problems/problems_1488/testcase.py
@@ -10,6 +10,7 @@ class Testcase(testcase.Testcase):
 		self.testcases.append(case(Input=[1, 2, 3, 4], Output=[-1, -1, -1, -1]))
 		self.testcases.append(case(Input=[1, 2, 0, 0, 2, 1], Output=[-1, -1, 2, 1, -1, -1]))
 		self.testcases.append(case(Input=[1, 2, 0, 1, 2], Output=[]))
+		self.testcases.append(case(Input=[69,0,0,0,69], Output=[-1,69,1,1,-1]))
 
 	def get_testcases(self):
 		return self.testcases
