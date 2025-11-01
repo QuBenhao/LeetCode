7ced8fcf7b5827f11fb605a8b20cf86a462994f5
benhao <qubenhao2@gmail.com>
2025-11-02 06:47:41 +0800

    test: 2257 solution
    py, c++

--- problems/problems_2257/Solution.cpp
+++ problems/problems_2257/Solution.cpp
@@ -1,31 +1,58 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
+constexpr int DIRS[4][2]{{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+
 class Solution {
 public:
-    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
-        
+  int countUnguarded(int m, int n, const vector<vector<int>> &guards,
+                     const vector<vector<int>> &walls) {
+    vector<vector<int>> grid(m, vector<int>(n));
+    for (const auto &g : guards) {
+      grid[g[0]][g[1]] = -1;
     }
+    for (const auto &w : walls) {
+      grid[w[0]][w[1]] = -1;
+    }
+    for (const auto &g : guards) {
+      for (const auto &d : DIRS) {
+        int x = g[0] + d[0], y = g[1] + d[1];
+        while (0 <= x && x < m && 0 <= y && y < n && grid[x][y] != -1) {
+          grid[x][y] = 1;
+          x += d[0];
+          y += d[1];
+        }
+      }
+    }
+    int ans = 0;
+    for (const auto &row : grid) {
+      for (const auto &g : row) {
+        if (g == 0) {
+          ++ans;
+        }
+      }
+    }
+    return ans;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	int m = json::parse(inputArray.at(0));
-	int n = json::parse(inputArray.at(1));
-	vector<vector<int>> guards = json::parse(inputArray.at(2));
-	vector<vector<int>> walls = json::parse(inputArray.at(3));
-	return solution.countUnguarded(m, n, guards, walls);
+  Solution solution;
+  int m = json::parse(inputArray.at(0));
+  int n = json::parse(inputArray.at(1));
+  vector<vector<int>> guards = json::parse(inputArray.at(2));
+  vector<vector<int>> walls = json::parse(inputArray.at(3));
+  return solution.countUnguarded(m, n, guards, walls);
 }
--- problems/problems_2257/solution.py
+++ problems/problems_2257/solution.py
@@ -7,5 +7,17 @@ class Solution(solution.Solution):
         return self.countUnguarded(*test_input)
 
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
-        pass
+        grid = [[0] * n for _ in range(m)]
+        for x, y in guards:
+            grid[x][y] = -1
+        for x, y in walls:
+            grid[x][y] = -1
+        for x, y in guards:
+            for dx, dy in DIRECTIONS:
+                nx, ny = x + dx, y + dy
+                while 0 <= nx < m and 0 <= ny < n and grid[nx][ny] != -1:
+                    grid[nx][ny] = 1
+                    nx, ny = nx + dx, ny + dy
+        return sum(sum(g == 0 for g in row) for row in grid)
 
+DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
