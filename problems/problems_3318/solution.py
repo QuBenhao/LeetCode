f4e4925f21a256e22e4184c0d5f359965c0a34a0
benhao <qubenhao2@gmail.com>
2025-11-04 06:52:25 +0800

    test: 3318 solution
    py

--- problems/problems_3318/solution.py
+++ problems/problems_3318/solution.py
@@ -7,5 +7,19 @@ class Solution(solution.Solution):
         return self.findXSum(*test_input)
 
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
-        pass
-
+        ans = []
+        counter = Counter()
+        length = 0
+        for i, num in enumerate(nums):
+            counter[num] += 1
+            length += 1
+            if length == k:
+                most_common = sorted(counter.items(), key=lambda item: (-item[1], -item[0]))
+                top_x = [item[0] for item in most_common]
+                tmp = 0
+                for j in range(min(len(top_x), x)):
+                    tmp += top_x[j] * counter[top_x[j]]
+                ans.append(tmp)
+                counter[nums[i - k + 1]] -= 1
+                length -= 1
+        return ans
