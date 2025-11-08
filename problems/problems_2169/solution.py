776dbf0c8b579d53393cbf638fa5c4ce7ae7341e
benhao <qubenhao2@gmail.com>
2025-11-09 07:43:35 +0800

    test: 2169 solution
    py

--- problems/problems_2169/solution.py
+++ problems/problems_2169/solution.py
@@ -7,8 +7,10 @@ class Solution(solution.Solution):
         return self.countOperations(*test_input)
 
     def countOperations(self, num1: int, num2: int) -> int:
-        if num1 == 0 or num2 == 0:
-            return 0
-        if num1 < num2:
-            num1, num2 = num2, num1
-        return num1 // num2 + self.countOperations(num2, num1 % num2)
+        x, y = max(num1, num2), min(num1, num2)
+        ans = 0
+        while y > 0:
+            d, r = divmod(x, y)
+            ans += d
+            x, y = y, r
+        return ans
