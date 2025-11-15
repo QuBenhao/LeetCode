c98ed7780bf0675e584168060f636714b684030b
benhao <qubenhao2@gmail.com>
2025-11-16 06:50:54 +0800

    test: 1513 solution
    py

--- problems/problems_1513/solution.py
+++ problems/problems_1513/solution.py
@@ -7,5 +7,13 @@ class Solution(solution.Solution):
         return self.numSub(test_input)
 
     def numSub(self, s: str) -> int:
-        pass
+        ans = cur = 0
+        for c in s + '0':
+            if c == '1':
+                cur += 1
+            else:
+                ans = (ans + (cur + 1) * cur // 2) % MOD
+                cur = 0
+        return ans
 
+MOD = 10**9 + 7
