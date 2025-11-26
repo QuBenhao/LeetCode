d152e19012f7bf53fadf2426461594d9df4c95f8
benhao <qubenhao2@gmail.com>
2025-11-27 06:49:40 +0800

    test: 3381 solution
    py

--- problems/problems_3381/solution.py
+++ problems/problems_3381/solution.py
@@ -1,3 +1,5 @@
+from math import inf
+
 import solution
 from typing import *
 
@@ -7,5 +9,12 @@ class Solution(solution.Solution):
         return self.maxSubarraySum(*test_input)
 
     def maxSubarraySum(self, nums: List[int], k: int) -> int:
-        pass
-
+        min_s = [inf] * k
+        min_s[-1] = s = 0
+        ans = -inf
+        for j, num in enumerate(nums):
+            s += num
+            i = j % k
+            ans = max(ans, s - min_s[i])
+            min_s[i] = min(min_s[i], s)
+        return ans
