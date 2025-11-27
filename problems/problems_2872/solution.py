0347c3235f882c42304186a5765b4add02e06a21
benhao <qubenhao2@gmail.com>
2025-11-28 07:37:29 +0800

    test: 2872 solution
    py

--- problems/problems_2872/solution.py
+++ problems/problems_2872/solution.py
@@ -7,5 +7,22 @@ class Solution(solution.Solution):
         return self.maxKDivisibleComponents(*test_input)
 
     def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
-        pass
+        g = [[] for _ in range(n)]
+        for x, y in edges:
+            g[x].append(y)
+            g[y].append(x)
 
+        # 返回子树 x 的点权和
+        def dfs(x: int, fa: int) -> int:
+            s = values[x]
+            for y in g[x]:
+                if y != fa:  # 避免访问父节点
+                    # 加上子树 y 的点权和，得到子树 x 的点权和
+                    s += dfs(y, x)
+            nonlocal ans
+            ans += s % k == 0
+            return s
+
+        ans = 0
+        dfs(0, -1)
+        return ans
