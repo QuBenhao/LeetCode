0a8479d54adb27a020219edcea6506910f3bb1cc
benhao <qubenhao2@gmail.com>
2025-12-04 05:45:25 +0800

    test: 2211 solution
    py

--- problems/problems_2211/solution.py
+++ problems/problems_2211/solution.py
@@ -7,5 +7,18 @@ class Solution(solution.Solution):
         return self.countCollisions(test_input)
 
     def countCollisions(self, directions: str) -> int:
-        pass
-
+        ans = 0
+        r = 0
+        has_stop = False
+        for d in directions:
+            if d == 'R':
+                r += 1
+            else:
+                ans += r
+                if r and d == 'L':
+                    ans += 1
+                elif has_stop and d == 'L':
+                    ans += 1
+                has_stop = has_stop or r > 0 or d == 'S'
+                r = 0
+        return ans
--- problems/problems_2211/testcase.py
+++ problems/problems_2211/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input="RLRSLL", Output=5))
 		self.testcases.append(case(Input="LLRR", Output=0))
+		self.testcases.append(case(Input="LSSSLLSSSSLRRSLLLSLSLRRLLLLLRSSRSRRSLLLSSS", Output=24))
 
 	def get_testcases(self):
 		return self.testcases
