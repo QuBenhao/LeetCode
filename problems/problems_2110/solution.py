5ae741df222daa1a5f452cf2a2f5d1c877577eed
benhao <qubenhao2@gmail.com>
2025-12-15 07:43:02 +0800

    test: 2110 solution
    py

--- problems/problems_2110/solution.py
+++ problems/problems_2110/solution.py
@@ -1,3 +1,6 @@
+from itertools import pairwise
+from math import inf
+
 import solution
 from typing import *
 
@@ -7,5 +10,12 @@ class Solution(solution.Solution):
         return self.getDescentPeriods(test_input)
 
     def getDescentPeriods(self, prices: List[int]) -> int:
-        pass
-
+        ans = len(prices)
+        length = 0
+        for a, b in pairwise(prices + [inf]):
+            if a - b == 1:
+                length = 2 if length == 0 else length + 1
+            else:
+                ans += length * (length - 1) // 2
+                length = 0
+        return ans
--- problems/problems_2110/testcase.py
+++ problems/problems_2110/testcase.py
@@ -10,6 +10,7 @@ class Testcase(testcase.Testcase):
 		self.testcases.append(case(Input=[3, 2, 1, 4], Output=7))
 		self.testcases.append(case(Input=[8, 6, 7, 7], Output=4))
 		self.testcases.append(case(Input=[1], Output=1))
+		self.testcases.append(case(Input=[12,11,10,9,8,7,6,5,4,3,4,3,10,9,8,7], Output=68))
 
 	def get_testcases(self):
 		return self.testcases
