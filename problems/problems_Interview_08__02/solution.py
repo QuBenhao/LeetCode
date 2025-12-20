6506df4a31af5267548eb436141037f4a372c8c1
benhao <qubenhao2@gmail.com>
2025-12-20 10:09:55 +0800

    test: 944, 面试题 08.02 solution
    py

--- problems/problems_Interview_08__02/solution.py
+++ problems/problems_Interview_08__02/solution.py
@@ -7,5 +7,25 @@ class Solution(solution.Solution):
         return self.pathWithObstacles(test_input)
 
     def pathWithObstacles(self, obstacleGrid: List[List[int]]) -> List[List[int]]:
-        pass
+        ans = []
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        visited = [[False for _ in range(n)] for _ in range(m)]
 
+        def backtrack(i, j):
+            if obstacleGrid[i][j]:
+                return False
+            if visited[i][j]:
+                return False
+            visited[i][j] = True
+            ans.append([i, j])
+            if i == m - 1 and j == n - 1:
+                return True
+            if j < n - 1 and backtrack(i, j + 1):
+                return True
+            if i < m - 1 and backtrack(i + 1, j):
+                return True
+            ans.pop()
+            return False
+
+        backtrack(0, 0)
+        return ans
