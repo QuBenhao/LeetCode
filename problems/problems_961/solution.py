af9fb8201739d60d9ece8249c7fc7e6835222d12
benhao <qubenhao2@gmail.com>
2026-01-02 09:02:33 +0800

    test: 961 solution
    py

--- problems/problems_961/solution.py
+++ problems/problems_961/solution.py
@@ -7,5 +7,22 @@ class Solution(solution.Solution):
         return self.repeatedNTimes(test_input)
 
     def repeatedNTimes(self, nums: List[int]) -> int:
-        pass
+        # if nums[0] == nums[3]:
+        #     return nums[0]
+        # for i, num in enumerate(nums):
+        #     if nums[i + 1] == num or nums[i + 2] == num:
+        #         return num
+        # return -1
 
+        # 摩尔投票
+        ans, ans_count = 0, 0
+        for num in nums[1:]:
+            if num == nums[0]:
+                return num
+            if ans_count == 0:
+                ans, ans_count = num, 1
+            elif num == ans:
+                return num
+            else:
+                ans_count -= 1
+        return ans
