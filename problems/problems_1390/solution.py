427fefa3dca101765dc85b4fb48cc536c03c0b60
benhao <qubenhao2@gmail.com>
2026-01-04 07:29:35 +0800

    test: 1390 solution
    py

--- problems/problems_1390/solution.py
+++ problems/problems_1390/solution.py
@@ -7,5 +7,13 @@ class Solution(solution.Solution):
         return self.sumFourDivisors(test_input)
 
     def sumFourDivisors(self, nums: List[int]) -> int:
-        pass
+        return sum(FACTORS_SUM[num] for num in nums if FACTORS_COUNT[num] == 4)
 
+# 预处理每个数的质因子列表
+mx = 100001
+FACTORS_COUNT = [0] * mx
+FACTORS_SUM = [0] * mx
+for i in range(1, mx):
+    for j in range(i, mx, i):  # i是因子
+        FACTORS_COUNT[j] += 1
+        FACTORS_SUM[j] += i
--- problems/problems_1390/testcase.py
+++ problems/problems_1390/testcase.py
@@ -10,6 +10,8 @@ class Testcase(testcase.Testcase):
 		self.testcases.append(case(Input=[21, 4, 7], Output=32))
 		self.testcases.append(case(Input=[21, 21], Output=64))
 		self.testcases.append(case(Input=[1, 2, 3, 4, 5], Output=0))
+		self.testcases.append(case(Input=[1,2,3,4,5,6,7,8,9,10], Output=45))
+		self.testcases.append(case(Input=[7286,18704,70773,8224,91675], Output=10932))
 
 	def get_testcases(self):
 		return self.testcases
