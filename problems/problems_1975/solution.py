eaa093752baceacdb11af3f767453c9975a22ef7
benhao <qubenhao2@gmail.com>
2026-01-05 07:13:23 +0800

    test: 1975 solution
    py

--- problems/problems_1975/solution.py
+++ problems/problems_1975/solution.py
@@ -1,3 +1,5 @@
+from math import inf
+
 import solution
 from typing import *
 
@@ -7,5 +9,13 @@ class Solution(solution.Solution):
         return self.maxMatrixSum(test_input)
 
     def maxMatrixSum(self, matrix: List[List[int]]) -> int:
-        pass
-
+        ans = 0
+        neg_cnt = 0
+        min_val = inf
+        for row in matrix:
+            for v in row:
+                if v < 0:
+                    neg_cnt += 1
+                min_val = min (min_val, abs(v))
+                ans += abs(v)
+        return ans if neg_cnt % 2 == 0 else ans - 2 * min_val
