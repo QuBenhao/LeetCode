4385ac8c04a7bc932da1ef62b49ba6f1cccf7978
benhao <qubenhao2@gmail.com>
2026-01-12 07:37:37 +0800

    test: 1266 solution
    py

--- problems/problems_1266/solution.py
+++ problems/problems_1266/solution.py
@@ -1,3 +1,5 @@
+from itertools import pairwise
+
 import solution
 from typing import *
 
@@ -7,5 +9,9 @@ class Solution(solution.Solution):
         return self.minTimeToVisitAllPoints(test_input)
 
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
-        pass
+        def move(p1, p2):
+            x1, y1 = p1
+            x2, y2 = p2
+            return max(abs(x2 - x1), abs(y2 - y1))
 
+        return sum(move(a, b) for a, b in pairwise(points))
