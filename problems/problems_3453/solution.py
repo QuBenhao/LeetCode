581cea3fb82b6880b21260265a76a79114102ad7
benhao <qubenhao2@gmail.com>
2026-01-13 07:14:09 +0800

    test: 3453 solution
    py

--- problems/problems_3453/solution.py
+++ problems/problems_3453/solution.py
@@ -7,5 +7,22 @@ class Solution(solution.Solution):
         return self.separateSquares(test_input)
 
     def separateSquares(self, squares: List[List[int]]) -> float:
-        pass
+        M = 100_000
+        total_area = sum(l * l for _, _, l in squares)
 
+        def check(y: float) -> bool:
+            area = 0
+            for _, yi, l in squares:
+                if yi < y:
+                    area += l * min(y - yi, l)
+            return area >= total_area / 2
+
+        left = 0
+        right = max_y = max(y + l for _, y, l in squares)
+        for _ in range((max_y * M).bit_length()):
+            mid = (left + right) / 2
+            if check(mid):
+                right = mid
+            else:
+                left = mid
+        return (left + right) / 2  # 区间中点误差小
