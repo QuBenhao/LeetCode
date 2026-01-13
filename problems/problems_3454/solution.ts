22d9c413695563b208263cfd3d92a6b418900687
benhao <qubenhao2@gmail.com>
2026-01-14 07:53:54 +0800

    Revert "test: [20260114] Add (3454)"
    This reverts commit b5bc110ea61b953c8e0e42f2947f625c81191f9a.

--- problems/problems_3454/solution.ts
+++
@@ -1,9 +0,0 @@
-function separateSquares(squares: number[][]): number {
-    
-};
-
-export function Solve(inputJsonElement: string): any {
-	const inputValues: string[] = inputJsonElement.split("\n");
-	const squares: number[][] = JSON.parse(inputValues[0]);
-	return separateSquares(squares);
-}
