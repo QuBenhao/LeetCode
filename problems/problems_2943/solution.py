d2cad8b06eada2daf394b33d8ae04b8f4c21080c
benhao <qubenhao2@gmail.com>
2026-01-15 07:53:12 +0800

    test: 2943 solution
    py

--- problems/problems_2943/solution.py
+++ problems/problems_2943/solution.py
@@ -1,3 +1,5 @@
+from itertools import pairwise
+
 import solution
 from typing import *
 
@@ -7,5 +9,20 @@ class Solution(solution.Solution):
         return self.maximizeSquareHoleArea(*test_input)
 
     def maximizeSquareHoleArea(self, n: int, m: int, hBars: List[int], vBars: List[int]) -> int:
-        pass
-
+        hBars.sort()
+        vBars.sort()
+        max_h = cur_h = 1
+        max_v = cur_v = 1
+        for a, b in pairwise(hBars):
+            if b == a + 1:
+                cur_h += 1
+            else:
+                cur_h = 1
+            max_h = max(max_h, cur_h)
+        for a, b in pairwise(vBars):
+            if b == a + 1:
+                cur_v += 1
+            else:
+                cur_v = 1
+            max_v = max(max_v, cur_v)
+        return (min(max_h, max_v) + 1) ** 2
--- problems/problems_2943/testcase.py
+++ problems/problems_2943/testcase.py
@@ -10,6 +10,8 @@ class Testcase(testcase.Testcase):
 		self.testcases.append(case(Input=[2, 1, [2, 3], [2]], Output=4))
 		self.testcases.append(case(Input=[1, 1, [2], [2]], Output=4))
 		self.testcases.append(case(Input=[2, 3, [2, 3], [2, 4]], Output=4))
+		self.testcases.append(case(Input=[1,1000000000,[2],[1000000001]], Output=4))
+		self.testcases.append(case(Input=[4,40,[5,3,2,4],[36,41,6,34,33]], Output=9))
 
 	def get_testcases(self):
 		return self.testcases
