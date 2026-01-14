8d47c7b196821aab580337a76302c870d2cf92b1
benhao <qubenhao2@gmail.com>
2026-01-14 22:03:07 +0800

    test: algo 0x02  递归实现指数型枚举 solution
    cpp

--- acoier/algo/0x00/0x02/0001/solution.cpp
+++ acoier/algo/0x00/0x02/0001/solution.cpp
@@ -8,6 +8,25 @@ using namespace std;
 typedef long long ll;
 #define lowbit(x) ((x)&(-(x)))
 
+int n;
+
+void dfs(int cur, vector<int>& path) {
+    if (cur > n) {
+        for (const auto& v: path) cout << v << " ";
+        cout << endl;
+        return;
+    }
+    // 不选当前
+    dfs(cur + 1, path);
+    // 选当前
+    path.emplace_back(cur);
+    dfs(cur + 1, path);
+    path.pop_back();
+}
+
 int main() {
+    cin >> n;
+    vector<int> path;
+    dfs(1, path);
     return 0;
 }
