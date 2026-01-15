6127eb524d408478f7b3cc43037517f9c867cef4
benhao <qubenhao2@gmail.com>
2026-01-16 06:54:10 +0800

    test: 2975 solution
    py

--- problems/problems_2975/solution.py
+++ problems/problems_2975/solution.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 import solution
 from typing import *
 
@@ -7,5 +9,16 @@ class Solution(solution.Solution):
         return self.maximizeSquareArea(*test_input)
 
     def maximizeSquareArea(self, m: int, n: int, hFences: List[int], vFences: List[int]) -> int:
-        pass
+        hFences.extend([1, m])
+        vFences.extend([1, n])
+        ans_d = 0
+        s1 = set()
+        for a, b in combinations(hFences, 2):
+            s1.add(abs(a - b))
+        for a, b in combinations(vFences, 2):
+            if (d := abs(a - b)) > ans_d and d in s1:
+                ans_d = d
 
+        return ans_d * ans_d % MOD if ans_d > 0 else -1
+
+MOD = int(1e9) + 7
--- problems/problems_2975/testcase.py
+++ problems/problems_2975/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=[4, 3, [2, 3], [2]], Output=4))
 		self.testcases.append(case(Input=[6, 7, [2], [4]], Output=-1))
+		self.testcases.append(case(Input=[3,9,[2],[8,6,5,4]], Output=4))
 
 	def get_testcases(self):
 		return self.testcases
