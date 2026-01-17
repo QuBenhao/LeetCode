4695c34090935a54ddf423f27e7992f75e21e5c9
benhao <qubenhao2@gmail.com>
2026-01-17 21:45:31 +0800

    test: algo 0x02 非递归实现组合型枚举 solution
    cpp

---
+++ acoier/algo/0x00/0x03/0001/solution.cpp
@@ -0,0 +1,18 @@
+//
+// Created by benhao on 2026/1/17.
+// 例题: 激光炸弹 acwing99
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define lowbit(x) ((x)&(-(x)))
+
+int N, R;
+int pre_sum[5005][5005];
+
+int main() {
+    cin >> N >> R;
+
+    return 0;
+}
