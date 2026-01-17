758a520c134937daa842c7d8469849d216373cb2
benhao <qubenhao2@gmail.com>
2026-01-17 08:49:56 +0800

    test: 3047 solution
    py

--- problems/problems_3047/solution.py
+++ problems/problems_3047/solution.py
@@ -7,5 +7,12 @@ class Solution(solution.Solution):
         return self.largestSquareArea(*test_input)
 
     def largestSquareArea(self, bottomLeft: List[List[int]], topRight: List[List[int]]) -> int:
-        pass
-
+        n = len(bottomLeft)
+        ans = 0
+        for i, ((x0, y0), (x1, y1)) in enumerate(zip(bottomLeft, topRight)):
+            for j in range(i + 1, n):
+                (x2, y2), (x3, y3) = bottomLeft[j], topRight[j]
+                c, d, a, b = min(x1, x3), min(y1, y3), max(x0, x2), max(y0, y2)
+                if a < c and b < d:
+                    ans = max(ans, min(d - b, c - a) ** 2)
+        return ans
--- problems/problems_3047/testcase.py
+++ problems/problems_3047/testcase.py
@@ -11,6 +11,7 @@ class Testcase(testcase.Testcase):
 		self.testcases.append(case(Input=[[[1, 1], [1, 3], [1, 5]], [[5, 5], [5, 7], [5, 9]]], Output=4))
 		self.testcases.append(case(Input=[[[1, 1], [2, 2], [1, 2]], [[3, 3], [4, 4], [3, 4]]], Output=1))
 		self.testcases.append(case(Input=[[[1, 1], [3, 3], [3, 1]], [[2, 2], [4, 4], [4, 2]]], Output=0))
+		self.testcases.append(case(Input=[[[2,2],[1,3]],[[3,4],[5,5]]], Output=1))
 
 	def get_testcases(self):
 		return self.testcases
