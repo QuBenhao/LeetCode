cebbf60bc6dfe51c3acd66929ce1121e271faae8
benhao <qubenhao2@gmail.com>
2026-01-19 07:44:37 +0800

    test: 1292 solution
    py

--- problems/problems_1292/solution.py
+++ problems/problems_1292/solution.py
@@ -7,5 +7,19 @@ class Solution(solution.Solution):
         return self.maxSideLength(*test_input)
 
     def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
-        pass
-
+        m, n = len(mat), len(mat[0])
+        pre_sum = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m):
+            for j in range(n):
+                pre_sum[i + 1][j + 1] = pre_sum[i + 1][j] + pre_sum[i][j + 1] - pre_sum[i][j] + mat[i][j]
+        ans = 0
+        for i in range(m):
+            if i < ans:
+                continue
+            for j in range(ans, n):
+                if j < ans:
+                    continue
+                while ans <= i and ans <= j and pre_sum[i + 1][j + 1] - pre_sum[i + 1][j - ans] - pre_sum[i - ans][
+                    j + 1] + pre_sum[i - ans][j - ans] <= threshold:
+                    ans += 1
+        return ans
