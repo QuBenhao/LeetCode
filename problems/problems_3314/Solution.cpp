cff0d0247b4616678c439ed742eef87877efc080
benhao <qubenhao2@gmail.com>
2026-01-20 08:06:43 +0800

    test: 3314 solution
    py, c++

--- problems/problems_3314/Solution.cpp
+++ problems/problems_3314/Solution.cpp
@@ -1,28 +1,34 @@
 //go:build ignore
 #include "cpp/common/Solution.h"
 
-
 using namespace std;
 using json = nlohmann::json;
 
 class Solution {
 public:
-    vector<int> minBitwiseArray(vector<int>& nums) {
-        
+  vector<int> minBitwiseArray(const vector<int> &nums) {
+    vector<int> ans(nums.size(), -1);
+    for (size_t i = 0; i < nums.size(); ++i) {
+      if (!(nums[i] & 1))
+        continue;
+      const int v = ((nums[i] + 1) & -(nums[i] + 1)) - 1;
+      ans[i] = (nums[i] - v) | v >> 1;
     }
+    return ans;
+  }
 };
 
 json leetcode::qubh::Solve(string input_json_values) {
-	vector<string> inputArray;
-	size_t pos = input_json_values.find('\n');
-	while (pos != string::npos) {
-		inputArray.push_back(input_json_values.substr(0, pos));
-		input_json_values = input_json_values.substr(pos + 1);
-		pos = input_json_values.find('\n');
-	}
-	inputArray.push_back(input_json_values);
+  vector<string> inputArray;
+  size_t pos = input_json_values.find('\n');
+  while (pos != string::npos) {
+    inputArray.push_back(input_json_values.substr(0, pos));
+    input_json_values = input_json_values.substr(pos + 1);
+    pos = input_json_values.find('\n');
+  }
+  inputArray.push_back(input_json_values);
 
-	Solution solution;
-	vector<int> nums = json::parse(inputArray.at(0));
-	return solution.minBitwiseArray(nums);
+  Solution solution;
+  vector<int> nums = json::parse(inputArray.at(0));
+  return solution.minBitwiseArray(nums);
 }
--- problems/problems_3314/solution.py
+++ problems/problems_3314/solution.py
@@ -7,5 +7,15 @@ class Solution(solution.Solution):
         return self.minBitwiseArray(test_input)
 
     def minBitwiseArray(self, nums: List[int]) -> List[int]:
-        pass
-
+        ans = [-1] * len(nums)
+        for i, num in enumerate(nums):
+            if not (num & 1):
+                continue
+            j = 0
+            while j < num.bit_length():
+                if not (num >> j & 1):
+                    break
+                j += 1
+            v = (1 << j) - 1
+            ans[i] = (num - v) | v >> 1
+        return ans
