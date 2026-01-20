e1f969a6d7e194c106e847d2c3fcef2cf2f9505c
benhao <qubenhao2@gmail.com>
2026-01-20 22:36:23 +0800

    test: 485 contest solution
    py

--- problems/problems_3815/solution.py
+++ problems/problems_3815/solution.py
@@ -15,28 +15,24 @@ class Solution(solution.Solution):
 
 class AuctionSystem:
     def __init__(self):
-        self.amount = {}  # (userId, itemId) -> bidAmount
-        self.item_h = defaultdict(list)  # itemId -> [(bidAmount, userId)]
+        self.bid_map = {} # (itemId, userId) -> bidAmount
+        self.item_map = defaultdict(list) # itemId -> (bidAmount, userId)
 
     def addBid(self, userId: int, itemId: int, bidAmount: int) -> None:
-        self.amount[(userId, itemId)] = bidAmount
-        heappush_max(self.item_h[itemId], (bidAmount, userId)) # 最大堆
+        self.bid_map[(itemId, userId)] = bidAmount
+        heappush_max(self.item_map[itemId], (bidAmount, userId))
 
     def updateBid(self, userId: int, itemId: int, newAmount: int) -> None:
         self.addBid(userId, itemId, newAmount)
 
     def removeBid(self, userId: int, itemId: int) -> None:
-        self.amount.pop((userId, itemId), None)
-        # 堆中元素在 getHighestBidder 中删除（懒删除）
+        self.bid_map.pop((itemId, userId), None)
 
     def getHighestBidder(self, itemId: int) -> int:
-        h = self.item_h.get(itemId, None)
+        h = self.item_map[itemId]
         while h:
-            bidAmount, userId = h[0]
-            if bidAmount == self.amount.get((userId, itemId), None):
-                return userId
-            # 货不对板，堆顶出价与实际不符
+            bid_amount, user_id = h[0]
+            if self.bid_map.get((itemId, user_id), None) == bid_amount:
+                return user_id
             heappop_max(h)
         return -1
-
-
--- problems/problems_3816/solution.py
+++ problems/problems_3816/solution.py
@@ -8,12 +8,12 @@ class Solution(solution.Solution):
         return self.lexSmallestAfterDeletion(test_input)
 
     def lexSmallestAfterDeletion(self, s: str) -> str:
-        left = Counter(s)
+        counts = Counter(s)
         st = []
-        for ch in s:
-            while st and ch < st[-1] and left[st[-1]] > 1:
-                left[st.pop()] -= 1
-            st.append(ch)
-        while left[st[-1]] > 1:
-            left[st.pop()] -= 1
-        return "".join(st)
+        for c in s:
+            while st and st[-1] > c and counts[st[-1]] > 1:
+                counts[st.pop()] -= 1
+            st.append(c)
+        while st and counts[st[-1]] > 1:
+            counts[st.pop()] -= 1
+        return ''.join(st)
