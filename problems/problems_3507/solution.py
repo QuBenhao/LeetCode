9806d2130c6f8f0c2e11660ef446ad26f3e57f65
benhao <qubenhao2@gmail.com>
2026-01-22 08:13:43 +0800

    test: 3507 solution
    py

--- problems/problems_3507/solution.py
+++ problems/problems_3507/solution.py
@@ -1,3 +1,6 @@
+from itertools import pairwise
+from math import inf
+
 import solution
 from typing import *
 
@@ -7,5 +10,13 @@ class Solution(solution.Solution):
         return self.minimumPairRemoval(test_input)
 
     def minimumPairRemoval(self, nums: List[int]) -> int:
-        pass
-
+        ans = 0
+        while nums != sorted(nums):
+            cur, cur_v = -1, inf
+            for i, (a, b) in enumerate(pairwise(nums)):
+                if cur_v > a + b:
+                    cur, cur_v = i, a + b
+            nums.pop(cur)
+            nums[cur] = cur_v
+            ans += 1
+        return ans
