e3c15550cefcfe950c7fd6d7904ab898ecfa2d62
benhao <qubenhao2@gmail.com>
2026-02-04 05:49:29 +0800

    test: 3640 solution
    py

--- problems/problems_3640/solution.py
+++ problems/problems_3640/solution.py
@@ -1,3 +1,6 @@
+from itertools import accumulate, pairwise
+from math import inf
+
 import solution
 from typing import *
 
@@ -7,5 +10,73 @@ class Solution(solution.Solution):
         return self.maxSumTrionic(test_input)
 
     def maxSumTrionic(self, nums: List[int]) -> int:
-        pass
+        ans = dp1 = dp2 = dp3 = -inf
+        for a, b in pairwise(nums):
+            dp3 = max(dp3, dp2) + b if a < b else -inf
+            dp2 = max(dp2, dp1) + b if a > b else -inf
+            dp1 = max(dp1, a) + b if a < b else -inf
+            ans = max(ans, dp3)
+        return ans
 
+    #     n = len(nums)
+    #     # split arr by equals adjacent
+    #     last = idx = 0
+    #     splits = []
+    #     while idx < n - 1:
+    #         if nums[idx] == nums[idx + 1]:
+    #             splits.append(nums[last:idx+1])
+    #             last = idx + 1
+    #         idx += 1
+    #     if last == 0:
+    #         return self.process_each(nums)
+    #     splits.append(nums[last:])
+    #     return max(self.process_each(s) for s in splits)
+    #
+    # def process_each(self, nums: List[int]) -> int:
+    #         if not nums:
+    #             return -inf
+    #
+    #         pre_sum = [0] + list(accumulate(nums))
+    #         n = len(nums)
+    #         ans = -inf
+    #         idx = 0
+    #         lz = -1
+    #         while idx < n - 1:
+    #             if lz != -1:
+    #                 # 刚刚已经遍历过递增
+    #                 l = idx
+    #             else:
+    #                 l = idx
+    #                 while l < n - 1 and nums[l] < nums[l + 1]:
+    #                     if lz == -1 and nums[l] >= 0:
+    #                         lz = l
+    #                     l += 1
+    #                 # 未找到第一段递增
+    #                 if l == idx or l == n - 1:
+    #                     idx += 1
+    #                     lz = -1
+    #                     continue
+    #                 # 尽可能少取负数
+    #                 if lz == -1:
+    #                     lz = l - 1
+    #             q = l
+    #             while q < n - 1 and nums[q] > nums[q + 1]:
+    #                 q += 1
+    #             # 不存在了
+    #             if q == n - 1:
+    #                 break
+    #             # 找第三段递增尽头
+    #             r = q
+    #             tmp = lz
+    #             # 减少重复统计递增段
+    #             lz = -1
+    #             while r < n - 1 and nums[r] < nums[r + 1]:
+    #                 if lz == -1 and nums[r] >= 0:
+    #                     lz = r
+    #                 r += 1
+    #             if lz == -1:
+    #                 lz = r - 1
+    #             # q + 2: 最后一段有可能全为负数, 尽可能少取
+    #             ans = max(ans, pre_sum[r + 1] - pre_sum[tmp], pre_sum[q + 2] - pre_sum[tmp])
+    #             idx = r
+    #         return ans
--- problems/problems_3640/testcase.py
+++ problems/problems_3640/testcase.py
@@ -9,6 +9,9 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=[0, -2, -1, -3, 0, 2, -1], Output=-4))
 		self.testcases.append(case(Input=[1, 4, 2, 7], Output=14))
+		self.testcases.append(case(Input=[2,993,-791,-635,-569], Output=-431))
+		self.testcases.append(case(Input=[286,528,-900,327,536,625,547,997], Output=3032))
+		self.testcases.append(case(Input=[1,4,2,2,3,1,2], Output=8))
 
 	def get_testcases(self):
 		return self.testcases
