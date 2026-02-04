1201067883b1a6fff0527d278e44f93a07beb79a
benhao <qubenhao2@gmail.com>
2026-02-05 06:50:48 +0800

    test: 3379 solution
    py

--- problems/problems_3379/solution.py
+++ problems/problems_3379/solution.py
@@ -7,5 +7,11 @@ class Solution(solution.Solution):
         return self.constructTransformedArray(test_input)
 
     def constructTransformedArray(self, nums: List[int]) -> List[int]:
-        pass
-
+        n = len(nums)
+        result = [0] * n
+        for i in range(n):
+            if nums[i] != 0:
+                result[i] = nums[(i + nums[i] % n) % n]
+            else:
+                result[i] = 0
+        return result
