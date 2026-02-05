6caa2a567915fc6114ea21b95d13fdebbcd7d98f
benhao <qubenhao2@gmail.com>
2026-02-06 06:31:03 +0800

    test: 3634 solution
    py

--- problems/problems_3634/solution.py
+++ problems/problems_3634/solution.py
@@ -1,3 +1,6 @@
+from bisect import bisect_right
+from collections import deque
+
 import solution
 from typing import *
 
@@ -7,5 +10,21 @@ class Solution(solution.Solution):
         return self.minRemoval(*test_input)
 
     def minRemoval(self, nums: List[int], k: int) -> int:
-        pass
+        nums.sort()
+        ans = 0
+        window = deque()
+        for num in nums:
+            while window and window[0] * k < num:
+                window.popleft()
+            window.append(num)
+            ans = max(ans, len(window))
+        return len(nums) - ans
 
+        # nums.sort()
+        # n = len(nums)
+        # ans = n - 1
+        # r = 0
+        # for i, num in enumerate(nums):
+        #     r = bisect_right(nums, num * k, lo=r)
+        #     ans = min(ans, n - (r - i))
+        # return ans
