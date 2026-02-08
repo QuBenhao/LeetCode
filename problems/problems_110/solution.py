22ece6daa17153498e3300eec9548a5008a8190d
benhao <qubenhao2@gmail.com>
2026-02-08 08:22:10 +0800

    test: 110 solution
    py

--- problems/problems_110/solution.py
+++ problems/problems_110/solution.py
@@ -1,37 +1,32 @@
+from typing import *
+
 import solution
 from python.object_libs import list_to_tree
 
 
-class Solution(solution.Solution):
-    def solve(self, test_input=None):
-        root = list_to_tree(test_input)
-        return self.isBalanced(root)
-
-    def isBalanced(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-
-        def depth(node, d):
-            if not node:
-                return d, True
-            d += 1
-            l, l_t = depth(node.left, d)
-            if not l_t:
-                return None, False
-            r, r_t = depth(node.right, d)
-            if not r_t:
-                return None, False
-            return max(l, r), abs(l - r) <= 1
-
-        _, res = depth(root, 0)
-        return res
-
-
-# Definition for a binary tree node.
-class TreeNode(object):
+class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        nums0 = test_input
+        root0 = list_to_tree(nums0)
+        return self.isBalanced(root0)
+
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        def dfs(node) -> Tuple[int, bool]:
+            if not node:
+                return 0, True
+            left, left_b = dfs(node.left)
+            right, right_b = dfs(node.right)
+            return max(left, right) + 1, left_b and right_b and abs(left - right) <= 1
+
+        _, balanced = dfs(root)
+        return balanced
--- problems/problems_110/testcase.py
+++ problems/problems_110/testcase.py
@@ -10,6 +10,7 @@ class Testcase(testcase.Testcase):
         self.testcases.append(case(Input=[3, 9, 20, None, None, 15, 7], Output=True))
         self.testcases.append(case(Input=[1, 2, 2, 3, 3, None, None, 4, 4], Output=False))
         self.testcases.append(case(Input=[1, 2, 2, 3, None, None, 3, 4, None, None, 4], Output=False))
+        self.testcases.append(case(Input=[1,2,3,4,5,6,None,8], Output=True))
 
     def get_testcases(self):
         return self.testcases
