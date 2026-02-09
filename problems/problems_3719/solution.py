46312cbd15f8628905612f507a91e4257407afed
benhao <qubenhao2@gmail.com>
2026-02-10 07:17:45 +0800

    test: 3719 solution
    py

--- problems/problems_3719/solution.py
+++ problems/problems_3719/solution.py
@@ -7,5 +7,15 @@ class Solution(solution.Solution):
         return self.longestBalanced(test_input)
 
     def longestBalanced(self, nums: List[int]) -> int:
-        pass
-
+        ans = 0
+        n = len(nums)
+        for i in range(n):
+            odds, evens = set(), set()
+            for j in range(i, n):
+                if nums[j] & 1:
+                    odds.add(nums[j])
+                else:
+                    evens.add(nums[j])
+                if len(odds) == len(evens):
+                    ans = max(ans, j - i + 1)
+        return ans
