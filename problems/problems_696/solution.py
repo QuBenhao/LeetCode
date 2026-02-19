63f17314dd2e4a7cffc165921edba646cfa4526f
benhao <qubenhao2@gmail.com>
2026-02-19 10:13:00 +0800

    test: 696 solution
    py

--- problems/problems_696/solution.py
+++ problems/problems_696/solution.py
@@ -7,5 +7,14 @@ class Solution(solution.Solution):
         return self.countBinarySubstrings(test_input)
 
     def countBinarySubstrings(self, s: str) -> int:
-        pass
-
+        ans = 0
+        n = len(s)
+        cur = pre = 0
+        for i in range(n):
+            cur += 1
+            if i == n - 1 or s[i] != s[i + 1]:
+                # 拐点 / 当前组的结束
+                ans += min(cur, pre)
+                pre = cur
+                cur = 0
+        return ans
--- problems/problems_696/testcase.py
+++ problems/problems_696/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input="00110011", Output=6))
 		self.testcases.append(case(Input="10101", Output=4))
+		self.testcases.append(case(Input="00100", Output=2))
 
 	def get_testcases(self):
 		return self.testcases
