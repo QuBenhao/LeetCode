c3446ea83d67e454ad407e8575f0ca977050a29a
benhao <qubenhao2@gmail.com>
2026-02-22 07:14:41 +0800

    test: 868 solution
    py

--- problems/problems_868/solution.py
+++ problems/problems_868/solution.py
@@ -7,5 +7,13 @@ class Solution(solution.Solution):
         return self.binaryGap(test_input)
 
     def binaryGap(self, n: int) -> int:
-        pass
-
+        last = -1
+        ans = 0
+        while n:
+            low_bit = n & -n
+            cur = low_bit.bit_length()
+            if last != -1:
+                ans = max(ans, cur - last)
+            last = cur
+            n ^= low_bit
+        return ans
