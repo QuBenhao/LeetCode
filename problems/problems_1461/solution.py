5fc573909a2a74786b90b095ded6d1b21fd21f68
benhao <qubenhao2@gmail.com>
2026-02-23 09:10:57 +0800

    test: 1461 solution
    py

--- problems/problems_1461/solution.py
+++ problems/problems_1461/solution.py
@@ -7,5 +7,19 @@ class Solution(solution.Solution):
         return self.hasAllCodes(*test_input)
 
     def hasAllCodes(self, s: str, k: int) -> bool:
-        pass
-
+        n = len(s)
+        target = 1 << k
+        if n < target + k - 1:
+            return False
+        st = set()
+        cur = 0
+        for i in range(k):
+            cur = cur << 1 | ord(s[i]) - ord('0')
+        full = target - 1
+        st.add(cur)
+        for i in range(k, n):
+            cur = ((cur << 1) & full) | ord(s[i]) - ord('0')
+            st.add(cur)
+            if len(st) == target:
+                return True
+        return False
--- problems/problems_1461/testcase.py
+++ problems/problems_1461/testcase.py
@@ -10,6 +10,8 @@ class Testcase(testcase.Testcase):
 		self.testcases.append(case(Input=['00110110', 2], Output=True))
 		self.testcases.append(case(Input=['0110', 1], Output=True))
 		self.testcases.append(case(Input=['0110', 2], Output=False))
+		self.testcases.append(case(Input=["00110",2], Output=True))
+		self.testcases.append(case(Input=["01",1], Output=True))
 
 	def get_testcases(self):
 		return self.testcases
