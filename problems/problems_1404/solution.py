910df16be21371d32a83f049ca3c2a34c1cfe441
benhao <qubenhao2@gmail.com>
2026-02-26 08:02:13 +0800

    test: 1404 solution
    py

--- problems/problems_1404/solution.py
+++ problems/problems_1404/solution.py
@@ -7,5 +7,17 @@ class Solution(solution.Solution):
         return self.numSteps(test_input)
 
     def numSteps(self, s: str) -> int:
-        pass
+        # carry = 0
+        # ans = len(s) - 1 # 除第一位以外每位1或0都至少操作一次
+        # for i in range(len(s) - 1, 0, -1):
+        #     cur = carry + int(s[i])
+        #     ans += 1 if cur == 1 else 0 # 奇数需要额外+1
+        #     carry = 0 if cur == 0 else 1 # 产生进位
+        # return ans + carry
 
+        # 从上面的规律可以发现, 最右侧1左边的0才会有额外的奇数贡献
+        ans = len(s) - 1
+        if (idx := s.rfind('1')) > 0:
+            # 存在最右侧1左边的0, 每个0需要额外+1, 且idx处和0处各需要+1
+            ans += s.count('0', 1, idx) + 2
+        return ans
