caef05f33f75b2f5dfa788780bd69ca02e55ec8e
benhao <qubenhao2@gmail.com>
2026-02-27 07:48:11 +0800

    test: 3666 solution
    py

--- problems/problems_3666/solution.py
+++ problems/problems_3666/solution.py
@@ -1,5 +1,6 @@
+from math import inf
+
 import solution
-from typing import *
 
 
 class Solution(solution.Solution):
@@ -7,5 +8,22 @@ class Solution(solution.Solution):
         return self.minOperations(*test_input)
 
     def minOperations(self, s: str, k: int) -> int:
-        pass
+        n = len(s)
+        z = s.count('0')
+        if z == 0:
+            return 0
+        if k == n:
+            return 1 if z == n else -1
 
+        ans = inf
+        # 情况一：操作次数 m 是偶数
+        if z % 2 == 0:  # z 必须是偶数
+            m = max((z + k - 1) // k, (z + n - k - 1) // (n - k))  # 下界
+            ans = m + m % 2  # 把 m 往上调整为偶数
+
+        # 情况二：操作次数 m 是奇数
+        if z % 2 == k % 2:  # z 和 k 的奇偶性必须相同
+            m = max((z + k - 1) // k, (n - z + n - k - 1) // (n - k))  # 下界
+            ans = min(ans, m | 1)  # 把 m 往上调整为奇数
+
+        return ans if ans < inf else -1
