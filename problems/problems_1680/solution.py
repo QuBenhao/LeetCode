e2d41f599439720043d0f5904a30d99ba8b91d56
benhao <qubenhao2@gmail.com>
2026-02-28 07:09:20 +0800

    test: 1680 solution
    py

--- problems/problems_1680/solution.py
+++ problems/problems_1680/solution.py
@@ -10,23 +10,9 @@ class Solution(solution.Solution):
         :type n: int
         :rtype: int
         """
+        ans = 0
+        for i in range(1, n + 1):
+            ans = (ans << i.bit_length() | i) % MOD
+        return ans
 
-        def multiply(X, Y):
-            return [[sum(a * b for a, b in zip(X_row, Y_col)) % 1000000007 for Y_col in zip(*Y)] for X_row in X]
-
-        def x_level_k_product(x, k):
-            res = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-            mat = [[2 ** k, 1, 0], [0, 1, 1], [0, 0, 1]]
-            while x > 0:
-                if x & 1: res = multiply(res, mat)
-                mat, x = multiply(mat, mat), x >> 1
-            return res
-
-        ans, acc, level = [[1], [2], [1]], 1, 1
-        while acc < n:
-            next_level = 2 ** (level + 1)
-            take = min(n, next_level - 1) - acc
-            ans = multiply(x_level_k_product(take, level + 1), ans)
-            acc, level = acc + take, level + 1
-
-        return ans[0][0]
+MOD = 10 ** 9 + 7
