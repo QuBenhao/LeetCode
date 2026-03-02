f8d5b26523fed98b538cb3259daf1b7dcba73be1
benhao <qubenhao2@gmail.com>
2026-03-02 09:01:50 +0800

    test: 1536 solution
    py

--- problems/problems_1536/solution.py
+++ problems/problems_1536/solution.py
@@ -7,5 +7,24 @@ class Solution(solution.Solution):
         return self.minSwaps(test_input)
 
     def minSwaps(self, grid: List[List[int]]) -> int:
-        pass
+        # 预处理每一行的尾零个数
+        n = len(grid)
+        tail_zeros = [n] * n
+        for i in range(n):
+            for j in range(n - 1, -1, -1):
+                if grid[i][j]:
+                    tail_zeros[i] = n - 1 - j
+                    break
 
+        ans = 0
+        for i in range(n - 1):
+            need_zeros = n - 1 - i
+            for j in range(i, n):
+                if tail_zeros[j] >= need_zeros:
+                    ans += j - i
+                    # 从 j 换到 i，原来 [i, j-1] 中的数据全体右移一位
+                    tail_zeros[i + 1: j + 1] = tail_zeros[i: j]
+                    break
+            else:  # 没找到符合要求的 tail_zeros[j]
+                return -1
+        return ans
--- problems/problems_1536/testcase.py
+++ problems/problems_1536/testcase.py
@@ -10,6 +10,7 @@ class Testcase(testcase.Testcase):
 		self.testcases.append(case(Input=[[0, 0, 1], [1, 1, 0], [1, 0, 0]], Output=3))
 		self.testcases.append(case(Input=[[0, 1, 1, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 1, 1, 0]], Output=-1))
 		self.testcases.append(case(Input=[[1, 0, 0], [1, 1, 0], [1, 1, 1]], Output=0))
+		self.testcases.append(case(Input=[[1,0,0,0,0,0],[0,1,0,1,0,0],[1,0,0,0,0,0],[1,1,1,0,0,0],[1,1,0,1,0,0],[1,0,0,0,0,0]], Output=2))
 
 	def get_testcases(self):
 		return self.testcases
