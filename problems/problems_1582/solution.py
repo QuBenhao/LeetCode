5fb7c75cb23d2b400ec23f52ceecaea8f73c6b2e
benhao <qubenhao2@gmail.com>
2026-03-04 07:02:18 +0800

    test: 1582 solution
    py

--- problems/problems_1582/solution.py
+++ problems/problems_1582/solution.py
@@ -7,5 +7,14 @@ class Solution(solution.Solution):
         return self.numSpecial(test_input)
 
     def numSpecial(self, mat: List[List[int]]) -> int:
-        pass
-
+        m, n = len(mat), len(mat[0])
+        sum_rows, sum_cols = [0] * m, [0] * n
+        check = set()
+        for i, row in enumerate(mat):
+            for j, val in enumerate(row):
+                if val == 0:
+                    continue
+                sum_rows[i] += val
+                sum_cols[j] += val
+                check.add((i, j))
+        return sum(sum_rows[i] == sum_cols[j] == 1 for i, j in check)
