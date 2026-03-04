dc69406786380e01abc19d8c90c12928488dafd6
benhao <qubenhao2@gmail.com>
2026-03-05 06:53:34 +0800

    test: 1758 solution
    py

--- problems/problems_1758/solution.py
+++ problems/problems_1758/solution.py
@@ -13,14 +13,8 @@ class Solution(solution.Solution):
         n = len(s)
         a1 = a2 = 0
         for i in range(n):
-            if i % 2 == 0:
-                if s[i] == '0':
-                    a1 += 1
-                else:
-                    a2 += 1
+            if i & 1 == ord(s[i]) & 1:
+                a1 += 1
             else:
-                if s[i] == '1':
-                    a1 += 1
-                else:
-                    a2 += 1
+                a2 += 1
         return min(a1,a2)
