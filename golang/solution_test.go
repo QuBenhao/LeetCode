f5c661202aea754803e394671850061c0398ae89
GitHub Action <action@github.com>
2026-03-05 16:20:55 +0000

    test: [20260306] Add (1784)

--- golang/solution_test.go
+++ golang/solution_test.go
@@ -1,10 +1,10 @@
 package golang
 
 import (
-	problem "leetCode/problems/problems_1758"
+	problem "leetCode/problems/problems_1784"
 	"testing"
 )
 
 func TestSolution(t *testing.T) {
-	TestEach(t, "1758", "problems", problem.Solve)
+	TestEach(t, "1784", "problems", problem.Solve)
 }
--- rust/test_executor/tests/solutions_test.rs
+++ rust/test_executor/tests/solutions_test.rs
@@ -1,11 +1,11 @@
-const PROBLEMS: [[&str; 2]; 1] = [["problems", "1758"]];
+const PROBLEMS: [[&str; 2]; 1] = [["problems", "1784"]];
 
 #[cfg(test)]
 mod test {
 	use test_executor::run_test::run_test;
 	use crate::PROBLEMS;
 
-	use solution_1758 as solution;
+	use solution_1784 as solution;
 
 	#[test]
 	fn test_solutions() {
