abafa41c5510289a4ba840c9c724fd7b066e322c
benhao <qubenhao2@gmail.com>
2026-03-13 05:40:58 +0800

    test: 3296 solution
    py

--- problems/problems_3296/solution.py
+++ problems/problems_3296/solution.py
@@ -1,3 +1,5 @@
+from math import floor, sqrt
+
 import solution
 from typing import *
 
@@ -7,5 +9,27 @@ class Solution(solution.Solution):
         return self.minNumberOfSeconds(*test_input)
 
     def minNumberOfSeconds(self, mountainHeight: int, workerTimes: List[int]) -> int:
-        pass
+        def check(x: int) -> bool:
+            s = 0
+            for wt in workerTimes:
+                if x < wt:
+                    continue
+                # (t + 1) * t * wt / 2 <= x
+                # (t + 1) * t <= 2 * x / wt
+                # t * t < (t + 1) * t <= 2 * x / wt
+                t = floor(sqrt(2 * x / wt))
+                while (t + 1) * t * wt // 2 <= x:
+                    t += 1
+                s += t - 1
+                if s >= mountainHeight:
+                    return True
+            return False
 
+        left, right = 1, (mountainHeight + 1) * mountainHeight // 2 * max(workerTimes)
+        while left < right:
+            mid = left + (right - left) // 2
+            if check(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
