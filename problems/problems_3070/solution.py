ab801dd5ddfe7376277c3fcc4c714497ef70bcb8
benhao <qubenhao2@gmail.com>
2026-03-18 07:24:49 +0800

    test: 3070 solution
    py

--- problems/problems_3070/solution.py
+++ problems/problems_3070/solution.py
@@ -7,5 +7,13 @@ class Solution(solution.Solution):
         return self.countSubmatrices(*test_input)
 
     def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
-        pass
-
+        ans = 0
+        sums = [0] * len(grid[0])
+        for row in grid:
+            pre = 0
+            for j, val in enumerate(row):
+                pre += val
+                sums[j] += pre
+                if sums[j] <= k:
+                    ans += 1
+        return ans
