e2e928dafa12a295e9c3f9e76f4da79ca85bb19e
benhao <qubenhao2@gmail.com>
2026-03-19 06:13:33 +0800

    test: 3212 solution
    py

--- problems/problems_3212/solution.py
+++ problems/problems_3212/solution.py
@@ -7,5 +7,18 @@ class Solution(solution.Solution):
         return self.numberOfSubmatrices(test_input)
 
     def numberOfSubmatrices(self, grid: List[List[str]]) -> int:
-        pass
-
+        n = len(grid[0])
+        xs, ys = [0] * n, [0] * n
+        ans = 0
+        for row in grid:
+            pre_x, pre_y = 0, 0
+            for j, val in enumerate(row):
+                if val == "X":
+                    pre_x += 1
+                if val == "Y":
+                    pre_y += 1
+                xs[j] += pre_x
+                ys[j] += pre_y
+                if xs[j] > 0 and xs[j] == ys[j]:
+                    ans += 1
+        return ans
