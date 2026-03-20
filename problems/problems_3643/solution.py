d3f6354e5f9b2de1c243b436ae891b52f84075dc
benhao <qubenhao2@gmail.com>
2026-03-21 06:31:31 +0800

    test: 3643 solution
    py

--- problems/problems_3643/solution.py
+++ problems/problems_3643/solution.py
@@ -7,5 +7,6 @@ class Solution(solution.Solution):
         return self.reverseSubmatrix(*test_input)
 
     def reverseSubmatrix(self, grid: List[List[int]], x: int, y: int, k: int) -> List[List[int]]:
-        pass
-
+        for i in range(k // 2):
+            grid[x + i][y:y + k], grid[x + k - 1 - i][y: y + k] = grid[x + k - 1 - i][y: y + k], grid[x + i][y:y + k]
+        return grid
