7e3112e656dca7748b176f8dc6703ceb643e31a9
benhao <qubenhao2@gmail.com>
2026-03-26 07:05:36 +0800

    test: 3548 solution
    py

---
+++ problems/problems_3548/solution.py
@@ -0,0 +1,43 @@
+import solution
+from typing import *
+from collections import defaultdict
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.canPartitionGrid(test_input)
+
+    def canPartitionGrid(self, grid: List[List[int]]) -> bool:
+        total = sum(sum(row) for row in grid)
+
+        # 能否水平分割
+        def check(a: List[List[int]]) -> bool:
+            m, n = len(a), len(a[0])
+
+            # 删除上半部分中的一个数，能否满足要求
+            def f(a: List[List[int]]) -> bool:
+                st = {0}  # 0 对应不删除数字
+                s = 0
+                for i, row in enumerate(a[:-1]):
+                    for j, x in enumerate(row):
+                        s += x
+                        # 第一行，不能删除中间元素
+                        if i > 0 or j == 0 or j == n - 1:
+                            st.add(x)
+                    # 特殊处理只有一列的情况，此时只能删除第一个数或者分割线上那个数
+                    if n == 1:
+                        if s * 2 == total or s * 2 - total == a[0][0] or s * 2 - total == row[0]:
+                            return True
+                        continue
+                    if s * 2 - total in st:
+                        return True
+                    # 如果分割到更下面，那么可以删第一行的元素
+                    if i == 0:
+                        st.update(row)
+                return False
+
+            # 删除上半部分中的数 or 删除下半部分中的数
+            return f(a) or f(a[::-1])
+
+        # 水平分割 or 垂直分割
+        return check(grid) or check(list(zip(*grid)))
--- problems/problems_3548/testcase.py
+++ problems/problems_3548/testcase.py
@@ -11,6 +11,7 @@ class Testcase(testcase.Testcase):
 		self.testcases.append(case(Input=[[1, 2], [3, 4]], Output=True))
 		self.testcases.append(case(Input=[[1, 2, 4], [2, 3, 5]], Output=False))
 		self.testcases.append(case(Input=[[4, 1, 8], [3, 2, 6]], Output=False))
+		self.testcases.append(case(Input=[[25372],[100000],[100000]], Output=True))
 
 	def get_testcases(self):
 		return self.testcases
