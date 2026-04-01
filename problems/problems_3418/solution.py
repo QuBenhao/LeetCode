f274d3c24b2e66319b6198db70b94360892e8d19
benhao <qubenhao2@gmail.com>
2026-04-02 07:12:25 +0800

    test: 3418 solution
    py

--- problems/problems_3418/solution.py
+++ problems/problems_3418/solution.py
@@ -1,3 +1,5 @@
+from math import inf
+
 import solution
 from typing import *
 
@@ -7,5 +9,12 @@ class Solution(solution.Solution):
         return self.maximumAmount(test_input)
 
     def maximumAmount(self, coins: List[List[int]]) -> int:
-        pass
-
+        n = len(coins[0])
+        f = [[-inf] * 3 for _ in range(n + 1)]
+        f[1] = [0] * 3
+        for row in coins:
+            for j, x in enumerate(row):
+                f[j + 1][2] = max(f[j][2] + x, f[j + 1][2] + x, f[j][1], f[j + 1][1])
+                f[j + 1][1] = max(f[j][1] + x, f[j + 1][1] + x, f[j][0], f[j + 1][0])
+                f[j + 1][0] = max(f[j][0], f[j + 1][0]) + x
+        return f[n][2]
