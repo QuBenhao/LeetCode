eaecf900352f840102b1f9c409d82ec62345e5b1
benhao <qubenhao2@gmail.com>
2026-04-03 05:01:48 +0800

    test: 3661 solution
    py

--- problems/problems_3661/solution.py
+++ problems/problems_3661/solution.py
@@ -1,3 +1,5 @@
+from math import inf
+
 import solution
 from typing import *
 
@@ -7,5 +9,35 @@ class Solution(solution.Solution):
         return self.maxWalls(*test_input)
 
     def maxWalls(self, robots: List[int], distance: List[int], walls: List[int]) -> int:
-        pass
+        n, m = len(robots), len(walls)
+        a = [(0, 0)] + sorted(zip(robots, distance), key=lambda p: p[0]) + [(inf, 0)]
+        walls.sort()
 
+        f0 = f1 = left = cur = right0 = right1 = 0
+        for i in range(1, n + 1):
+            x, d = a[i]
+
+            # 往左射，墙的坐标范围为 [left_x, x]
+            left_x = max(x - d, a[i - 1][0] + 1)  # +1 表示不能射到左边那个机器人
+            while left < m and walls[left] < left_x:
+                left += 1
+            while cur < m and walls[cur] < x:
+                cur += 1
+            cur1 = cur
+            if cur < m and walls[cur] == x:
+                cur += 1
+            left_res = f0 + cur - left  # 下标在 [left, cur-1] 中的墙都能摧毁
+
+            # 往右射，右边那个机器人往左射，墙的坐标范围为 [x, right_x]
+            x2, d2 = a[i + 1]
+            right_x = min(x + d, x2 - d2 - 1)  # -1 表示不能射到右边那个机器人
+            while right0 < m and walls[right0] <= right_x:
+                right0 += 1
+            f0 = max(left_res, f1 + right0 - cur1)  # 下标在 [cur1, right0-1] 中的墙都能摧毁
+
+            # 往右射，右边那个机器人往右射，墙的坐标范围为 [x, right_x]
+            right_x = min(x + d, x2 - 1)  # -1 表示不能射到右边那个机器人
+            while right1 < m and walls[right1] <= right_x:
+                right1 += 1
+            f1 = max(left_res, f1 + right1 - cur1)  # 下标在 [cur1, right1-1] 中的墙都能摧毁
+        return f1
--- problems/problems_3661/testcase.py
+++ problems/problems_3661/testcase.py
@@ -10,6 +10,7 @@ class Testcase(testcase.Testcase):
 		self.testcases.append(case(Input=[[4], [3], [1, 10]], Output=1))
 		self.testcases.append(case(Input=[[10, 2], [5, 1], [5, 2, 7]], Output=3))
 		self.testcases.append(case(Input=[[1, 2], [100, 1], [10]], Output=0))
+		self.testcases.append(case(Input=[[17,59,32,11,72,18],[5,7,6,5,2,10],[17,25,33,29,54,53,18,35,39,37,20,14,34,13,16,58,22,51,56,27,10,15,12,23,45,43,21,2,42,7,32,40,8,9,1,5,55,30,38,4,3,31,36,41,57,28,11,49,26,19,50,52,6,47,46,44,24,48]], Output=37))
 
 	def get_testcases(self):
 		return self.testcases
