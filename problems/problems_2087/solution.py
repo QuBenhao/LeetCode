b83f76bcccbd53c2e7a3f29a4a26c3c1d6a5f4d3
benhao <qubenhao2@gmail.com>
2026-04-04 05:28:41 +0800

    test: 2087 solution
    py

--- problems/problems_2087/solution.py
+++ problems/problems_2087/solution.py
@@ -7,5 +7,10 @@ class Solution(solution.Solution):
         return self.minCost(*test_input)
 
     def minCost(self, startPos: List[int], homePos: List[int], rowCosts: List[int], colCosts: List[int]) -> int:
-        pass
-
+        x0, y0 = startPos
+        x1, y1 = homePos
+        if x0 > x1:
+            x0, x1 = x1 - 1, x0 - 1
+        if y0 > y1:
+            y0, y1 = y1 - 1, y0 - 1
+        return sum(rowCosts[x0+1:x1+1]) + sum(colCosts[y0+1:y1+1])
