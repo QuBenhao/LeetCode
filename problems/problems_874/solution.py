aed5492d9f8702d74e295a7f34c8230b30f567be
benhao <qubenhao2@gmail.com>
2026-04-06 08:04:04 +0800

    test: 874 solution
    py

--- problems/problems_874/solution.py
+++ problems/problems_874/solution.py
@@ -7,5 +7,18 @@ class Solution(solution.Solution):
         return self.robotSim(*test_input)
 
     def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
-        pass
+        obstacles_ = set(map(tuple, obstacles))
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        x = y = d = ans = 0
 
+        for c in commands:
+            if c < 0:
+                d = (d + (1 if c == -1 else 3)) % 4
+            else:
+                dx, dy = dirs[d]
+                for _ in range(c):
+                    if (x + dx, y + dy) in obstacles_:
+                        break
+                    x, y = x + dx, y + dy
+                ans = max(ans, x * x + y * y)
+        return ans
