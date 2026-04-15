ad6bd02f190b387d80d7b3098ba869c6c4261b75
benhao <qubenhao2@gmail.com>
2026-04-16 05:26:38 +0800

    test: 3488 solution
    py

--- problems/problems_3488/solution.py
+++ problems/problems_3488/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+from collections import defaultdict
 
 
 class Solution(solution.Solution):
@@ -7,5 +8,27 @@ class Solution(solution.Solution):
         return self.solveQueries(*test_input)
 
     def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
-        pass
+        n = len(nums)
 
+        # 预处理：每个值的所有位置
+        pos = defaultdict(list)
+        for i, num in enumerate(nums):
+            pos[num].append(i)
+
+        # 每个位置到最近同值的最小距离
+        dist = [n] * n
+        for indices in pos.values():
+            k = len(indices)
+            if k == 1:
+                continue
+            for i, idx in enumerate(indices):
+                prev_idx = indices[i - 1]
+                next_idx = indices[(i + 1) % k]
+                # 左边距离：i=0 时环形
+                d_left = idx - prev_idx if i > 0 else idx + n - prev_idx
+                # 右边距离：i=k-1 时环形
+                d_right = next_idx - idx if i < k - 1 else next_idx + n - idx
+                dist[idx] = min(d_left, d_right)
+
+        # 查询
+        return [dist[q] if dist[q] < n else -1 for q in queries]
--- problems/problems_3488/testcase.py
+++ problems/problems_3488/testcase.py
@@ -9,6 +9,8 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=[[1, 3, 1, 4, 1, 3, 2], [0, 3, 5]], Output=[2, -1, 3]))
 		self.testcases.append(case(Input=[[1, 2, 3, 4], [0, 1, 2, 3]], Output=[-1, -1, -1, -1]))
+		self.testcases.append(case(Input=[[6,12,17,9,16,7,6],[5,6,0,4]], Output=[-1,1,1,-1]))
+		self.testcases.append(case(Input=[[1,3,1,4,1,3,2],[0,3,5]], Output=[2,-1,3]))
 
 	def get_testcases(self):
 		return self.testcases
