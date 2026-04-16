ba5c98025a524bd504a8861da29eeb42fce426f6
benhao <qubenhao2@gmail.com>
2026-04-17 06:11:07 +0800

    test: 3761 solution
    py

--- problems/problems_3761/solution.py
+++ problems/problems_3761/solution.py
@@ -7,5 +7,19 @@ class Solution(solution.Solution):
         return self.minMirrorPairDistance(test_input)
 
     def minMirrorPairDistance(self, nums: List[int]) -> int:
-        pass
+        n = len(nums)
+        ans = n
+        def cache_key(x):
+            # c = 0
+            # while x:
+            #     x, r = divmod(x, 10)
+            #     c = c * 10 + r
+            # return c
+            return int(str(x)[::-1])
 
+        mp = {}
+        for i, num in enumerate(nums):
+            if num in mp:
+                ans = min(ans, i - mp[num])
+            mp[cache_key(num)] = i
+        return ans if ans < n else -1
