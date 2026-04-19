df14240f220b272c830b6889ce65ab3d71bf5a98
benhao <qubenhao2@gmail.com>
2026-04-20 05:55:40 +0800

    test: 2078 solution
    py

--- problems/problems_2078/solution.py
+++ problems/problems_2078/solution.py
@@ -7,5 +7,19 @@ class Solution(solution.Solution):
         return self.maxDistance(test_input)
 
     def maxDistance(self, colors: List[int]) -> int:
-        pass
+        n = len(colors)
+        # 首尾颜色不同，直接返回最大距离
+        if colors[0] != colors[-1]:
+            return n - 1
 
+        # 从右往左找第一个与首颜色不同的位置
+        right = n - 1
+        while right > 0 and colors[right] == colors[0]:
+            right -= 1
+
+        # 从左往右找第一个与尾颜色不同的位置
+        left = 0
+        while left < n - 1 and colors[left] == colors[-1]:
+            left += 1
+
+        return max(right, n - 1 - left)
