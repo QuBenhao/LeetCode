21ac0fe4c4536ac816163f398c56681e7188b1de
benhao <qubenhao2@gmail.com>
2026-04-23 06:49:33 +0800

    test: 2615 solution
    py

--- problems/problems_2615/solution.py
+++ problems/problems_2615/solution.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import solution
 from typing import *
 
@@ -7,5 +9,14 @@ class Solution(solution.Solution):
         return self.distance(test_input)
 
     def distance(self, nums: List[int]) -> List[int]:
-        pass
-
+        idx_map = defaultdict(list)
+        for i, num in enumerate(nums):
+            idx_map[num].append(i)
+        ans = [0] * len(nums)
+        for v in idx_map.values():
+            s, n = sum(v), len(v)
+            prefix = 0
+            for k, i in enumerate(v):
+                ans[i] = s - 2 * prefix + i * (2 * k - n)
+                prefix += i
+        return ans
--- problems/problems_2615/testcase.py
+++ problems/problems_2615/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=[1, 3, 1, 1, 2], Output=[5, 0, 3, 4, 0]))
 		self.testcases.append(case(Input=[0, 5, 3], Output=[0, 0, 0]))
+		self.testcases.append(case(Input=[2,0,2,2,6,5,2], Output=[11,0,7,7,0,0,13]))
 
 	def get_testcases(self):
 		return self.testcases
