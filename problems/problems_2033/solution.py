a464915699b2edf95cc87f3abb54d6eef6c64740
benhao <qubenhao2@gmail.com>
2026-04-28 08:34:08 +0800

    test: 2033 solution
    py

--- problems/problems_2033/solution.py
+++ problems/problems_2033/solution.py
@@ -7,5 +7,15 @@ class Solution(solution.Solution):
         return self.minOperations(*test_input)
 
     def minOperations(self, grid: List[List[int]], x: int) -> int:
-        pass
+        d = grid[0][0] % x
+        ks = []
+        for row in grid:
+            for v in row:
+                if v % x != d:
+                    return -1
+                ks.append((v - grid[0][0]) // x)
+
+        ks.sort()
+        mid = ks[len(ks) // 2]
+        return sum(abs(k - mid) for k in ks)
 
