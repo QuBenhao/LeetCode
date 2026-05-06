d43098c496fdbc14e87ed3b4f4607e9ad0a28ef2
benhao <qubenhao2@gmail.com>
2026-05-07 07:25:12 +0800

    test: 3660 solution
    py

--- problems/problems_3660/solution.py
+++ problems/problems_3660/solution.py
@@ -7,5 +7,27 @@ class Solution(solution.Solution):
         return self.maxValue(test_input)
 
     def maxValue(self, nums: List[int]) -> List[int]:
-        pass
+        n = len(nums)
+        if n == 1:
+            return nums[:]
 
+        # 前缀最大值
+        preMax = [0] * n
+        preMax[0] = nums[0]
+        for i in range(1, n):
+            preMax[i] = max(preMax[i - 1], nums[i])
+
+        # 从右往左遍历，维护后缀最小值
+        ans = [0] * n
+        ans[n - 1] = preMax[n - 1]  # 最后一个位置可以跳到全局最大值
+        sufMin = nums[n - 1]
+
+        for i in range(n - 2, -1, -1):
+            # 如果 preMax[i] > sufMin，可以桥接到 i+1
+            if preMax[i] > sufMin:
+                ans[i] = ans[i + 1]
+            else:
+                ans[i] = preMax[i]
+            sufMin = min(sufMin, nums[i])
+
+        return ans
