46cd9e38dce7d87ac7cb3a9a0444404ce6639594
benhao <qubenhao2@gmail.com>
2026-05-10 08:09:54 +0800

    test: 2770 solution
    py

--- problems/problems_2770/solution.py
+++ problems/problems_2770/solution.py
@@ -7,5 +7,11 @@ class Solution(solution.Solution):
         return self.maximumJumps(*test_input)
 
     def maximumJumps(self, nums: List[int], target: int) -> int:
-        pass
-
+        n = len(nums)
+        dp = [-1] * n
+        dp[0] = 0
+        for j in range(1, n):
+            for i in range(j):
+                if dp[i] != -1 and abs(nums[j] - nums[i]) <= target:
+                    dp[j] = max(dp[j], dp[i] + 1)
+        return dp[-1]
