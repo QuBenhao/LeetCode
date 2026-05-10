cd66950d73dd80d1b908acf04268dea3d2235ed5
GitHub Action <action@github.com>
2026-05-10 16:15:27 +0000

    test: [20260511] Add (2553)

--- rust/test_executor/tests/solutions_test.rs
+++ rust/test_executor/tests/solutions_test.rs
@@ -1,11 +1,11 @@
-const PROBLEMS: [[&str; 2]; 1] = [["problems", "2770"]];
+const PROBLEMS: [[&str; 2]; 1] = [["problems", "2553"]];
 
 #[cfg(test)]
 mod test {
 	use test_executor::run_test::run_test;
 	use crate::PROBLEMS;
 
-	use solution_2770 as solution;
+	use solution_2553 as solution;
 
 	#[test]
 	fn test_solutions() {
