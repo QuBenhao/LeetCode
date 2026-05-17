4731a6121c31d9809678f08f28e95040c74b51e3
benhao <qubenhao2@gmail.com>
2026-05-18 07:14:00 +0800

    test: 1345 solution
    go

--- golang/solution_test.go
+++ golang/solution_test.go
@@ -1,10 +1,10 @@
 package golang
 
 import (
-	problem "leetCode/problems/problems_153"
+	problem "leetCode/problems/problems_1345"
 	"testing"
 )
 
 func TestSolution(t *testing.T) {
-	TestEach(t, "153", "problems", problem.Solve)
+	TestEach(t, "1345", "problems", problem.Solve)
 }
--- problems/problems_1345/solution.go
+++ problems/problems_1345/solution.go
@@ -7,7 +7,38 @@ import (
 )
 
 func minJumps(arr []int) int {
-    
+	n := len(arr)
+	connected := make(map[int][]int)
+	for i, v := range arr {
+		connected[v] = append(connected[v], i)
+	}
+	dp := make([]int, n)
+	for i := range n {
+		dp[i] = n + 5
+	}
+	dp[0] = 0
+	q := []int{0}
+	for len(q) > 0 {
+		cur := q[0]
+		q = q[1:]
+		for _, nxt := range connected[arr[cur]] {
+			if dp[nxt] <= dp[cur]+1 {
+				continue
+			}
+			dp[nxt] = dp[cur] + 1
+			q = append(q, nxt)
+		}
+		delete(connected, arr[cur])
+		for _, d := range []int{-1, 1} {
+			nxt := cur + d
+			if nxt < 0 || nxt == n || dp[nxt] <= dp[cur]+1 {
+				continue
+			}
+			dp[nxt] = dp[cur] + 1
+			q = append(q, nxt)
+		}
+	}
+	return dp[n-1]
 }
 
 func Solve(inputJsonValues string) any {
