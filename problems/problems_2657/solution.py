5253ca96986d08be30a0a38e35eba5936e10b9be
benhao <qubenhao2@gmail.com>
2026-05-20 06:17:35 +0800

    test: 2657 solution
    py

--- problems/problems_2657/solution.py
+++ problems/problems_2657/solution.py
@@ -7,5 +7,10 @@ class Solution(solution.Solution):
         return self.findThePrefixCommonArray(*test_input)
 
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
-        pass
-
+        ans = []
+        p, q = 0, 0
+        for a, b in zip(A, B):
+            p |= 1 << a
+            q |= 1 << b
+            ans.append((p & q).bit_count())
+        return ans
