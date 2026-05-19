6644162b315a90d76ac850e5da7134b506ed56a6
benhao <qubenhao2@gmail.com>
2026-05-19 08:20:31 +0800

    test: 2540 solution
    py

--- problems/problems_2540/solution.py
+++ problems/problems_2540/solution.py
@@ -7,5 +7,12 @@ class Solution(solution.Solution):
         return self.getCommon(*test_input)
 
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        pass
-
+        l, r = 0, 0
+        while l < len(nums1) and r < len(nums2):
+            if nums1[l] < nums2[r]:
+                l += 1
+            elif nums1[l] > nums2[r]:
+                r += 1
+            else:
+                return nums1[l]
+        return -1
