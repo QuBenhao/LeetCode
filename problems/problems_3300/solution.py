4f20b6c187aa712621b62feb0083c25bb9166997
benhao <qubenhao2@gmail.com>
2026-05-29 05:36:08 +0800

    test: 3300 solution
    py

--- problems/problems_3300/solution.py
+++ problems/problems_3300/solution.py
@@ -7,5 +7,7 @@ class Solution(solution.Solution):
         return self.minElement(test_input)
 
     def minElement(self, nums: List[int]) -> int:
-        pass
-
+        ans = 100000
+        for num in nums:
+            ans = min(ans, sum(map(int, str(num))))
+        return ans
