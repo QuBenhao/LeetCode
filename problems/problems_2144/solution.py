a01f267285ef6635cfbe24d6c4973b348fdd0ae4
benhao <qubenhao2@gmail.com>
2026-06-01 07:05:27 +0800

    test: 2144 solution
    py

--- problems/problems_2144/solution.py
+++ problems/problems_2144/solution.py
@@ -7,5 +7,12 @@ class Solution(solution.Solution):
         return self.minimumCost(test_input)
 
     def minimumCost(self, cost: List[int]) -> int:
-        pass
-
+        ans = 0
+        cost.sort(reverse=True)
+        n = len(cost)
+        for i in range(0, n, 3):
+            if i + 2 < n:
+                ans += cost[i] + cost[i + 1]
+            else:
+                ans += sum(cost[i:])
+        return ans
