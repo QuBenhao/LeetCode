20306b5b9cfa4b8f1e39d058f4d1e37c3a9afd1a
benhao <qubenhao2@gmail.com>
2026-06-02 08:40:05 +0800

    test: 3633 solution
    py

--- problems/problems_3633/solution.py
+++ problems/problems_3633/solution.py
@@ -1,3 +1,5 @@
+from math import inf
+
 import solution
 from typing import *
 
@@ -7,5 +9,11 @@ class Solution(solution.Solution):
         return self.earliestFinishTime(*test_input)
 
     def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
-        pass
+        def solve(first, second):
+            min_finish_time = inf
+            for s, d in first:
+                min_finish_time = min(min_finish_time, s + d)
+            return min(max(min_finish_time, s) + d for s, d in second)
 
+        return min(solve(zip(landStartTime, landDuration), zip(waterStartTime, waterDuration)),
+                   solve(zip(waterStartTime, waterDuration), zip(landStartTime, landDuration)))
