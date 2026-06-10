d622a1e4feeeb31931e9405a13d76399562b2b55
benhao <qubenhao2@gmail.com>
2026-06-11 05:40:13 +0800

    test: 3558 solution
    go

--- problems/problems_3558/solution.go
+++ problems/problems_3558/solution.go
@@ -6,8 +6,44 @@ import (
 	"strings"
 )
 
+const MOD int64 = 1e9 + 7
+
+func fastpow(a, b int64) int {
+	res := int64(1)
+	a %= MOD
+	for b > 0 {
+		if (b & 1) == 1 {
+			res = res * a % MOD
+		}
+		a = a * a % MOD
+		b >>= 1
+	}
+	return int(res)
+}
+
 func assignEdgeWeights(edges [][]int) int {
-    
+	n := len(edges) + 1
+	graph := make([][]int, n)
+	for _, edge := range edges {
+		u, v := edge[0]-1, edge[1]-1
+		graph[u] = append(graph[u], v)
+		graph[v] = append(graph[v], u)
+	}
+
+	var dfs func(int, int) int
+	dfs = func(node, parent int) int {
+		maxDepth := 0
+		for _, child := range graph[node] {
+			if child == parent {
+				continue
+			}
+			maxDepth = max(maxDepth, dfs(child, node))
+		}
+		return maxDepth + 1
+	}
+
+	depth := dfs(0, -1) - 1
+	return int(fastpow(2, int64(depth-1)))
 }
 
 func Solve(inputJsonValues string) any {
--- problems/problems_3558/testcase.py
+++ problems/problems_3558/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=[[1, 2]], Output=1))
 		self.testcases.append(case(Input=[[1, 2], [1, 3], [3, 4], [3, 5]], Output=2))
+		self.testcases.append(case(Input=[[3,2],[2,1]], Output=2))
 
 	def get_testcases(self):
 		return self.testcases
