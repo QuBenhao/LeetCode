ae2e738362003a354c701664791de52ae526f3a0
benhao <qubenhao2@gmail.com>
2026-06-13 10:00:14 +0800

    test: 3838 solution
    py

--- problems/problems_3838/solution.py
+++ problems/problems_3838/solution.py
@@ -7,5 +7,10 @@ class Solution(solution.Solution):
         return self.mapWordWeights(*test_input)
 
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
-        pass
-
+        ans = ""
+        for word in words:
+            cur = 0
+            for c in word:
+                cur = (cur + weights[ord(c) - ord('a')]) % 26
+            ans += chr(ord('z') - cur)
+        return ans
