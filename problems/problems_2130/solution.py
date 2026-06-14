17a3e30357b97a7b135e5e5a288213e1b610d486
benhao <qubenhao2@gmail.com>
2026-06-14 09:35:57 +0800

    test: 2130 solution
    py

--- problems/problems_2130/solution.py
+++ problems/problems_2130/solution.py
@@ -16,5 +16,30 @@ class Solution(solution.Solution):
         return self.pairSum(head0)
 
     def pairSum(self, head: Optional[ListNode]) -> int:
-        pass
+        # 1. 快慢指针找中点
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # 2. 反转后半段链表
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+        # prev 是反转后的后半段头部
+
+        # 3. 同时遍历前半段和反转后的后半段，计算最大 twin sum
+        max_sum = 0
+        first, second = head, prev
+        while second:
+            cur_sum = first.val + second.val
+            if cur_sum > max_sum:
+                max_sum = cur_sum
+            first = first.next
+            second = second.next
+
+        return max_sum
 
