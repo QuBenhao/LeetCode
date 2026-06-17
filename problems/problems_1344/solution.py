76767b3b31f724134f3c4e98abed7e7a9797bfb7
benhao <qubenhao2@gmail.com>
2026-06-18 05:27:17 +0800

    test: 1344 solution
    py

--- problems/problems_1344/solution.py
+++ problems/problems_1344/solution.py
@@ -7,5 +7,5 @@ class Solution(solution.Solution):
         return self.angleClock(*test_input)
 
     def angleClock(self, hour: int, minutes: int) -> float:
-        pass
-
+        hour %= 12
+        return min(a := abs((hour * 60 + minutes) / 2 - minutes * 6), 360 - a)
--- problems/problems_1344/testcase.py
+++ problems/problems_1344/testcase.py
@@ -10,6 +10,7 @@ class Testcase(testcase.Testcase):
 		self.testcases.append(case(Input=[12, 30], Output=165))
 		self.testcases.append(case(Input=[3, 30], Output=75))
 		self.testcases.append(case(Input=[3, 15], Output=7.5))
+		self.testcases.append(case(Input=[1,57], Output=76.50000))
 
 	def get_testcases(self):
 		return self.testcases
