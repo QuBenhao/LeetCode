c627eddb3c5ebd3d2d1fda5768e06e9478c445a5
benhao <qubenhao2@gmail.com>
2026-06-25 05:33:05 +0800

    test: 3737 solution
    py

--- problems/problems_3737/solution.py
+++ problems/problems_3737/solution.py
@@ -7,5 +7,14 @@ class Solution(solution.Solution):
         return self.countMajoritySubarrays(*test_input)
 
     def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
-        pass
+        n = len(nums)
+        ans = 0
+        for i in range(n):
+            cnt =0
+            for j in range(i, n):
+                if nums[j] == target:
+                    cnt += 1
+                if cnt * 2 > j - i + 1:
+                    ans += 1
+        return ans
 
