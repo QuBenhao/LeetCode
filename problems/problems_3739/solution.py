0f31e84bf7a8bfc29e580c82b809bcbc8022899d
benhao <qubenhao2@gmail.com>
2026-06-26 04:57:05 +0800

    test: 3739 solution
    py

--- problems/problems_3739/solution.py
+++ problems/problems_3739/solution.py
@@ -1,3 +1,5 @@
+from sortedcontainers import SortedList
+
 import solution
 from typing import *
 
@@ -7,5 +9,10 @@ class Solution(solution.Solution):
         return self.countMajoritySubarrays(*test_input)
 
     def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
-        pass
-
+        sl = SortedList([0])  # 为什么加个 0？见 525 题我的题解
+        ans = s = 0
+        for x in nums:
+            s += 1 if x == target else -1
+            ans += sl.bisect_left(s)
+            sl.add(s)
+        return ans
