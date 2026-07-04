4327878337d1ccfe52c6b6d845c357e47fad6cd5
benhao <qubenhao2@gmail.com>
2026-07-04 16:52:33 +0800

    test: 2492 solution
    py

--- problems/problems_2492/solution.py
+++ problems/problems_2492/solution.py
@@ -1,3 +1,5 @@
+from math import inf
+
 import solution
 from typing import *
 
@@ -7,5 +9,22 @@ class Solution(solution.Solution):
         return self.minScore(*test_input)
 
     def minScore(self, n: int, roads: List[List[int]]) -> int:
-        pass
+        g = [[] for _ in range(n + 1)]
+        for x, y, dis in roads:
+            g[x].append((y, dis))
+            g[y].append((x, dis))
 
+        vis = [False] * (n + 1)
+        ans = inf
+
+        def dfs(x: int) -> None:
+            nonlocal ans
+            vis[x] = True  # 避免重复访问
+            for y, dis in g[x]:
+                ans = min(ans, dis)
+                if not vis[y]:
+                    dfs(y)
+
+        # 遍历节点 1 所在连通块
+        dfs(1)
+        return ans
