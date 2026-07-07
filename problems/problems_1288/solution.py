cd798feeaa5d7f6b021e61ca6550b4f2349118fe
benhao <qubenhao2@gmail.com>
2026-07-08 06:33:41 +0800

    test: 1288 solution
    py

--- problems/problems_1288/solution.py
+++ problems/problems_1288/solution.py
@@ -7,5 +7,11 @@ class Solution(solution.Solution):
         return self.removeCoveredIntervals(test_input)
 
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
-        pass
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+        right = ans = 0
+        for _, r in intervals:
+            if r > right:
+                ans += 1
+                right = r
+        return ans
 
