5c1ea56566d57afb280744528c603660d48244ff
GitHub Action <action@github.com>
2026-07-08 17:15:10 +0000

    test: [20260709] Add (3532)

--- golang/solution_test.go
+++ golang/solution_test.go
@@ -1,10 +1,10 @@
 package golang
 
 import (
-	problem "leetCode/problems/problems_3756"
+	problem "leetCode/problems/problems_3532"
 	"testing"
 )
 
 func TestSolution(t *testing.T) {
-	TestEach(t, "3756", "problems", problem.Solve)
+	TestEach(t, "3532", "problems", problem.Solve)
 }
---
+++ problems/problems_3532/solution.ts
@@ -0,0 +1,12 @@
+function pathExistenceQueries(n: number, nums: number[], maxDiff: number, queries: number[][]): boolean[] {
+    
+};
+
+export function Solve(inputJsonElement: string): any {
+	const inputValues: string[] = inputJsonElement.split("\n");
+	const n: number = JSON.parse(inputValues[0]);
+	const nums: number[] = JSON.parse(inputValues[1]);
+	const maxDiff: number = JSON.parse(inputValues[2]);
+	const queries: number[][] = JSON.parse(inputValues[3]);
+	return pathExistenceQueries(n, nums, maxDiff, queries);
+}
