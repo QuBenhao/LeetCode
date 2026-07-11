97c27c7593b90a15c5ccea98d2707efc65500c50
benhao <qubenhao2@gmail.com>
2026-07-11 19:01:21 +0800

    test: 2685 solution
    py

--- problems/problems_2685/solution.py
+++ problems/problems_2685/solution.py
@@ -7,5 +7,47 @@ class Solution(solution.Solution):
         return self.countCompleteComponents(*test_input)
 
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
-        pass
+        uf = UnionFind(n)
+        for a, b in edges:
+            uf.union(a, b)
+        return sum(uf.links[i] * 2 == uf.size[i] * (uf.size[i] - 1) for i in range(n) if uf.find(i) == i)
 
+
+class UnionFind:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.rank = [1] * n
+        self.size = [1] * n
+        self.links = [0] * n
+        self.cc = n
+
+    def find(self, x: int) -> int:
+        while self.parent[x] != x:
+            self.parent[x] = self.parent[self.parent[x]]  # 路径压缩
+            x = self.parent[x]
+        return x
+
+    def union(self, x: int, y: int) -> bool:
+        root_x = self.find(x)
+        root_y = self.find(y)
+
+        if root_x == root_y:
+            self.links[root_x] += 1
+            return False  # 已经在同一集合
+
+        # 按秩合并
+        if self.rank[root_x] > self.rank[root_y]:
+            self.parent[root_y] = root_x
+            self.size[root_x] += self.size[root_y]
+            self.links[root_x] += self.links[root_y] + 1
+        else:
+            self.parent[root_x] = root_y
+            if self.rank[root_x] == self.rank[root_y]:
+                self.rank[root_y] += 1
+            self.size[root_y] += self.size[root_x]
+            self.links[root_y] += self.links[root_x] + 1
+        self.cc -= 1
+        return True
+
+    def is_connected(self, x:int, y:int) -> bool:
+        return self.find(x) == self.find(y)
--- problems/problems_2685/testcase.py
+++ problems/problems_2685/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=[6, [[0, 1], [0, 2], [1, 2], [3, 4]]], Output=3))
 		self.testcases.append(case(Input=[6, [[0, 1], [0, 2], [1, 2], [3, 4], [3, 5]]], Output=1))
+		self.testcases.append(case(Input=[5,[[1,2],[3,4],[1,4],[2,3],[1,3],[2,4]]], Output=2))
 
 	def get_testcases(self):
 		return self.testcases
