5ba7d3a59581cc74a0d9d16e164000f0160349a5
benhao <qubenhao2@gmail.com>
2026-07-14 22:10:59 +0800

    feat: 每日一题 3336 最大公约数相等的子序列数量

--- problems/problems_3336/Solution.cpp
+++ problems/problems_3336/Solution.cpp
@@ -8,7 +8,27 @@ using json = nlohmann::json;
 class Solution {
 public:
     int subsequencePairCount(vector<int>& nums) {
-        
+        const int MOD = 1000000007;
+        const int N = 201;
+        vector<long long> dp(N * N, 0);
+        dp[0] = 1;
+        for (int x : nums) {
+            vector<long long> ndp = dp;
+            for (int ga = 0; ga < N; ++ga) {
+                for (int gb = 0; gb < N; ++gb) {
+                    long long c = dp[ga * N + gb];
+                    if (c == 0) continue;
+                    int nga = std::gcd(ga, x);
+                    ndp[nga * N + gb] = (ndp[nga * N + gb] + c) % MOD;
+                    int ngb = std::gcd(gb, x);
+                    ndp[ga * N + ngb] = (ndp[ga * N + ngb] + c) % MOD;
+                }
+            }
+            dp = std::move(ndp);
+        }
+        long long ans = 0;
+        for (int g = 1; g < N; ++g) ans = (ans + dp[g * N + g]) % MOD;
+        return (int)ans;
     }
 };
 
--- problems/problems_3336/solution.py
+++ problems/problems_3336/solution.py
@@ -1,5 +1,6 @@
 import solution
 from typing import *
+from math import gcd
 
 
 class Solution(solution.Solution):
@@ -7,5 +8,29 @@ class Solution(solution.Solution):
         return self.subsequencePairCount(test_input)
 
     def subsequencePairCount(self, nums: List[int]) -> int:
-        pass
+        # dp[ga * 201 + gb]: #ways with current gcd(seq1)=ga, gcd(seq2)=gb
+        # ga=0 / gb=0 means the sequence is still empty
+        dp = [0] * (201 * 201)
+        dp[0] = 1
+        for x in nums:
+            ndp = dp[:]
+            for ga in range(201):
+                row = ga * 201
+                for gb in range(201):
+                    c = dp[row + gb]
+                    if c == 0:
+                        continue
+                    # put x into seq1
+                    nga = gcd(ga, x)
+                    ndp[nga * 201 + gb] = (ndp[nga * 201 + gb] + c) % MOD
+                    # put x into seq2
+                    ngb = gcd(gb, x)
+                    ndp[ga * 201 + ngb] = (ndp[ga * 201 + ngb] + c) % MOD
+            dp = ndp
+        ans = 0
+        for g in range(1, 201):
+            ans = (ans + dp[g * 201 + g]) % MOD
+        return ans
 
+
+MOD = 10 ** 9 + 7
