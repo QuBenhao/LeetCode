23ed9f3ee51ab52898ee340039b6773768b1fd58
benhao <qubenhao2@gmail.com>
2026-07-26 10:06:03 +0800

    feat: 628 maximumProduct 一趟扫描取三大/两小一大

--- problems/problems_628/solution.py
+++ problems/problems_628/solution.py
@@ -7,5 +7,18 @@ class Solution(solution.Solution):
         return self.maximumProduct(test_input)
 
     def maximumProduct(self, nums: List[int]) -> int:
-        pass
+        max1 = max2 = max3 = -1001
+        min1 = min2 = 1001
+        for x in nums:
+            if x > max1:
+                max1, max2, max3 = x, max1, max2
+            elif x > max2:
+                max2, max3 = x, max2
+            elif x > max3:
+                max3 = x
+            if x < min1:
+                min1, min2 = x, min1
+            elif x < min2:
+                min2 = x
+        return max(max1 * max2 * max3, min1 * min2 * max1)
 
