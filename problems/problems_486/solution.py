43baab3c79d2fdae6d472156ed718e8916af7f23
benhao <qubenhao2@gmail.com>
2026-08-01 06:07:59 +0800

    test: 486 solution
    py

--- problems/problems_486/solution.py
+++ problems/problems_486/solution.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 import solution
 from typing import *
 
@@ -7,5 +9,17 @@ class Solution(solution.Solution):
         return self.predictTheWinner(test_input)
 
     def predictTheWinner(self, nums: List[int]) -> bool:
-        pass
+        n = len(nums)
+        @cache
+        def minmax(l, r):
+            sig = -1 if (r - l + 1) & 1 != n & 1 else 1
+            if l == r:
+                return nums[l] * sig
+            ans = minmax(l + 1, r) + sig * nums[l]
+            if sig < 0:
+                sig = min(ans, minmax(l, r - 1) + sig * nums[r])
+            else:
+                sig = max(ans, minmax(l, r - 1) + sig * nums[r])
+            return sig
 
+        return minmax(0, n - 1) >= 0
--- problems/problems_486/testcase.py
+++ problems/problems_486/testcase.py
@@ -9,6 +9,7 @@ class Testcase(testcase.Testcase):
 		self.testcases = []
 		self.testcases.append(case(Input=[1, 5, 2], Output=False))
 		self.testcases.append(case(Input=[1, 5, 233, 7], Output=True))
+		self.testcases.append(case(Input=[0], Output=True))
 
 	def get_testcases(self):
 		return self.testcases
