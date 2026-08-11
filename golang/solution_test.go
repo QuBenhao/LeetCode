269ba502ebef20d3d5a2ebb84e1bfc08cf1f977a
GitHub Action <action@github.com>
2026-08-11 16:42:34 +0000

    test: [20260812] Add (2958)

--- golang/solution_test.go
+++ golang/solution_test.go
@@ -1,10 +1,10 @@
 package golang
 
 import (
-	problem "leetCode/problems/problems_2996"
+	problem "leetCode/problems/problems_2958"
 	"testing"
 )
 
 func TestSolution(t *testing.T) {
-	TestEach(t, "2996", "problems", problem.Solve)
+	TestEach(t, "2958", "problems", problem.Solve)
 }
---
+++ problems/problems_2958/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.maxSubarrayLength(*test_input)
+
+    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        pass
+
