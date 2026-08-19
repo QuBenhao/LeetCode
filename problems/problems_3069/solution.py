5a8275d6acc469579b024809d7ff768d9019000c
benhao <qubenhao2@gmail.com>
2026-08-20 06:35:53 +0800

    test: 3069 solution
    py

--- problems/problems_3069/solution.py
+++ problems/problems_3069/solution.py
@@ -7,5 +7,10 @@ class Solution(solution.Solution):
         return self.resultArray(test_input)
 
     def resultArray(self, nums: List[int]) -> List[int]:
-        pass
-
+        arr1, arr2 = [nums[0]], [nums[1]]
+        for i in range(2, len(nums)):
+            if arr1[-1] > arr2[-1]:
+                arr1.append(nums[i])
+            else:
+                arr2.append(nums[i])
+        return arr1 + arr2
