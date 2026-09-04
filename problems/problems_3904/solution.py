0b34218af47d1d127e1690a128395aa7322b3538
benbotagent <benhao@agent.qq.com>
2026-09-05 07:03:14 +0800

    test: 3904 solution
    py

--- problems/problems_3904/solution.py
+++ problems/problems_3904/solution.py
@@ -9,15 +9,15 @@ class Solution(solution.Solution):
         return self.firstStableIndex(*test_input)
 
     def firstStableIndex(self, nums: list[int], k: int) -> int:
-        left_max, right_min = list(nums), list(nums)
+        right_min = list(nums)
         n = len(nums)
-        for i in range(n - 1):
-            if left_max[i] > left_max[i + 1]:
-                left_max[i + 1] = left_max[i]
         for i in range(n - 2, -1, -1):
             if right_min[i + 1] < right_min[i]:
                 right_min[i] = right_min[i + 1]
+        l = nums[0]
         for i in range(n):
-            if left_max[i] - right_min[i] <= k:
+            if nums[i] > l:
+                l = nums[i]
+            if l - right_min[i] <= k:
                 return i
         return -1
