228e7941898c5eca33dad40a0d72a876568a30c7
GitHub Action <action@github.com>
2026-09-07 19:26:46 +0000

    test: [20260908] Add (3870)

--- golang/solution_test.go
+++ golang/solution_test.go
@@ -1,10 +1,10 @@
 package golang
 
 import (
-	problem "leetCode/problems/problems_940"
+	problem "leetCode/problems/problems_3870"
 	"testing"
 )
 
 func TestSolution(t *testing.T) {
-	TestEach(t, "940", "problems", problem.Solve)
+	TestEach(t, "3870", "problems", problem.Solve)
 }
---
+++ problems/problems_3870/solution.go
@@ -0,0 +1,22 @@
+package problem3870
+
+import (
+	"encoding/json"
+	"log"
+	"strings"
+)
+
+func countCommas(n int) int {
+    
+}
+
+func Solve(inputJsonValues string) any {
+	inputValues := strings.Split(inputJsonValues, "\n")
+	var n int
+
+	if err := json.Unmarshal([]byte(inputValues[0]), &n); err != nil {
+		log.Fatal(err)
+	}
+
+	return countCommas(n)
+}
