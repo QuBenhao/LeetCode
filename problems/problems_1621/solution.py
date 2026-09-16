a72391e784412ca69ac33542b2dab4a29a79b728
benbotagent <benhao@agent.qq.com>
2026-09-16 08:00:47 +0800

    test: 1621 solution
    py

--- problems/problems_1621/solution.py
+++ problems/problems_1621/solution.py
@@ -1,3 +1,5 @@
+from math import comb
+
 import solution
 from typing import *
 
@@ -7,5 +9,19 @@ class Solution(solution.Solution):
         return self.numberOfSets(*test_input)
 
     def numberOfSets(self, n: int, k: int) -> int:
-        pass
+        mod = 10 ** 9 + 7
+        return comb(n + k - 1, 2 * k) % mod
 
+        # # dp[j]: 前 i 个点中选 j 条线段的方案数（滚动 i）
+        # # pre[j]: sum(dp_t[j] for t in 1..i)，即各层第 j 列的前缀和
+        # dp = [1] + [0] * k
+        # pre = [0] * (k + 1)
+        # for _ in range(n):
+        #     cur = [1] + [0] * k
+        #     for j in range(k, 0, -1):
+        #         # 第 i 个点不使用 -> dp[j]；作为最后一条线段右端点 -> pre[j-1]
+        #         cur[j] = (dp[j] + pre[j - 1]) % mod
+        #     for j in range(k + 1):
+        #         pre[j] = (pre[j] + cur[j]) % mod
+        #     dp = cur
+        # return dp[k]
