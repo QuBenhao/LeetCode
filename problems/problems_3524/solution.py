314b6f9a9d538295b20d35c8ac0c7e80681715af
anlly <anllybenhao@gmail.com>
2026-09-21 08:02:34 +0800

    test: 3524 solution
    py

--- problems/problems_3524/solution.py
+++ problems/problems_3524/solution.py
@@ -7,5 +7,16 @@ class Solution(solution.Solution):
         return self.resultArray(*test_input)
 
     def resultArray(self, nums: List[int], k: int) -> List[int]:
-        pass
-
+        res = [0] * k
+        cur = [0] * k
+        for num in nums:
+            m = num % k
+            nxt = [0] * k
+            for v in range(k):
+                if cur[v]:
+                    nxt[v * m % k] += cur[v]
+            nxt[m] += 1
+            cur = nxt
+            for v in range(k):
+                res[v] += cur[v]
+        return res
